cafac320063f695fb9fe1bd9f8af6651690397bb
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-16 23:02:09 +0700

    Create 8kyu\008-Remove-First-and-Last-Character

---
+++ 8kyu/008-Remove-First-and-Last-Character/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+    public static string Remove_char(string s)
+    {
+        // Your Code
+        string temp = "";
+        for(int i = 0; i < s.Length; i++){
+            if(i == 0 || i == (s.Length-1)){
+                continue;
+            }
+            temp += s[i];
+        }
+        return temp;
+
+        // Alternative 1
+        // return s.Substring(1,(s.Length - 2));
+        // Alternative 2
+        // return s.Remove(s.Length - 1, 1).Remove(0, 1);
+    }
+}
+
+public class Program
+{
+    public static void Main(){
+        Console.WriteLine(Kata.Remove_char("country"));
+    }
+}
