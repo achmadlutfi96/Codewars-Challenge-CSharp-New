7c1265fb4d05b4f0d5adfedac34a222e90ecbf26
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-17 16:30:17 +0700

    Create 8kyu\011-Grasshopper-Summation

---
+++ 8kyu/011-Grasshopper-Summation/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int summation(int num)
+    {
+        //Code here
+        int sum = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            sum+=i;
+        }
+        return sum;
+
+        // Alternative 1
+        // return Enumerable.Range(1, num).Sum();
+        // Alternative 2
+        // return num*(num+1)/2;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(summation(8));
+    }
+}
