e36563f830dc71e3853c7b040800e909553f6673
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-17 15:22:24 +0700

    Create 8kyu\009-Square-n-Sum

---
+++ 8kyu/009-Square-n-Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+public static class Kata
+{
+    public static int SquareSum(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum +=(int) MathF.Pow(number, 2);
+        }
+        return sum;
+
+        // alternative
+        // return numbers.Sum(n => n * n);
+    }
+}
+
+public static class Program
+{
+    public static void Main(){
+        Console.WriteLine(Kata.SquareSum(new int[] {1,2,2}));
+    }
+}
