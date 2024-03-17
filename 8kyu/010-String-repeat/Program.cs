767ac853c9ccec35cfc5bfa6157913f8adc54727
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-17 15:30:49 +0700

    Create 8kyu\010-String-repeat

---
+++ 8kyu/010-String-repeat/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            string res ="";
+            for(int i = 0; i < n; i++){
+                res += s;
+            }
+            return res;
+
+            // Alternative 1
+            // return String.Concat(Enumerable.Repeat(s, n));
+            // Alternative 2
+            // return new StringBuilder(s.Length * n).Insert(0, s, n).ToString();
+        }
+        public static void Main()
+        {
+            Console.WriteLine(RepeatStr(3, "*"));
+        }
+    }
+}
