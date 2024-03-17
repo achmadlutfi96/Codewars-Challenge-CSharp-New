e890563753f0509f72a7e7611589ac1d75ce053a
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-17 16:43:55 +0700

    Create 8kyu\012-Remove-String-Spaces

---
+++ 8kyu/012-Remove-String-Spaces/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace Solution 
+{
+  public static class SpacesRemover
+  {
+    public static string NoSpace(string input)
+    {
+        //Code it!
+        string res = "";
+        foreach (char s in input)
+        {
+            if(!String.Equals(s,' ')){
+                // res = String.Concat(res, s);
+                res+=s;
+            }
+        }
+        return res;
+
+        // Alternative
+        // return input.Replace(" ", string.Empty);
+    }
+
+    public static void Main(){
+        Console.WriteLine(NoSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+    }  
+  }
+}
