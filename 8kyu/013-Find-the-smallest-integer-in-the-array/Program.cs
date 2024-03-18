f940c573a9e18b16febb862462668a3ee27bdd48
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-18 08:16:30 +0700

    Create 8kyu\013-Find-the-smallest-integer-in-the-array

---
+++ 8kyu/013-Find-the-smallest-integer-in-the-array/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+public class Kata 
+{
+    public static int FindSmallestInt(int[] args) 
+    {
+        // return args.Min();
+        return args.Min<int>(); // Pakai LINQ
+    }
+    public static void Main(){
+        Console.WriteLine(FindSmallestInt(new int[]{78,56,232,12,11,43}));
+    }
+}
