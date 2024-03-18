bf4d0a789c30123aba69c4f678fee2c45843a3a4
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-18 08:53:51 +0700

    Create 8kyu\016-Convert-a-String-to-a-Number

---
+++ 8kyu/016-Convert-a-String-to-a-Number/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+public class Kata
+{
+    public static int StringToNumber(String str)
+    {
+        //TODO: Convert str into a number
+        return Convert.ToInt32(str);
+    }
+
+    public static void Main(){
+        Console.WriteLine(StringToNumber("1"));
+    }
+}
