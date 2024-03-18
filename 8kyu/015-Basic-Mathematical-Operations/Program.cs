b1bda996a82d074ae24eeeb2e5ee2ad637852578
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-18 08:36:06 +0700

    Create 8kyu\015-Basic-Mathematical-Operations

---
+++ 8kyu/015-Basic-Mathematical-Operations/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static double basicOp(char operation, double value1, double value2)
+    {
+        switch(operation){
+            case '+': return value1 + value2;
+            case '-': return value1 - value2;
+            case '*': return value1 * value2;
+            case '/': return value1 / value2;
+            default : return 0;   
+        }
+    }
+
+    public static void Main(){
+        Console.WriteLine(basicOp('+', 4, 7));
+    }
+  }
+}
