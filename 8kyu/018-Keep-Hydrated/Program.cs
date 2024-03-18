e3a6ef7a2e7a2f2056159ab525ca1240cc2d9125
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-18 12:39:38 +0700

    Create 8kyu\018-Keep-Hydrated

---
+++ 8kyu/018-Keep-Hydrated/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+  public static int Litres(double time)
+  {
+    //The fun begins here.
+    return Convert.ToInt32(Math.Floor(time*0.5));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Litres(12.3));
+  }
+}
