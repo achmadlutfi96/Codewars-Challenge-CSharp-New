3aa6f4f1e8cca0eca4c7b62753cff4a7e2c31f2f
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-18 09:16:27 +0700

    Create 8kyu\017-Century-From-Year

---
+++ 8kyu/017-Century-From-Year/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+public static class Kata
+{
+    public static int СenturyFromYear(int year)
+    {
+        return (int)Math.Ceiling((double)year/100); // up(pembulatan ke atas)
+
+        // Alternative
+        // return (year - 1) / 100 + 1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(СenturyFromYear(1705));
+    }
+}
