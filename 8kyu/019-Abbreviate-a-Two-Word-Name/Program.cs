9d3ad89da31940c2b5ebde53ccc8dea529551d4d
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-18 12:57:58 +0700

    Create kyu\019-Abbreviate-a-Two-Word-Name

---
+++ 8kyu/019-Abbreviate-a-Two-Word-Name/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string AbbrevName(string name)
+    {
+        string res="";
+        res+=name[0];
+        for(int i=1; i<name.Length;i++){
+            if(name[i].Equals(' ')){
+                res+='.';
+                res+=name[i+1];
+            }
+        }
+        return res.ToUpper();
+
+        // Alternative
+        // return string.Join(".", name.Split(' ').Select(w => w[0])).ToUpper();
+    }
+    public static void Main()
+    {
+        Console.WriteLine(AbbrevName("Sam Harris"));
+    }
+}
