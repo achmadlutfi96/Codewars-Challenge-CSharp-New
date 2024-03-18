4d4a77b81e9a970f3c523712bfaec2d5393d8a0d
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-18 08:24:57 +0700

    Create 8kyu\014-Counting-sheep

---
+++ 8kyu/014-Counting-sheep/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int CountSheeps(bool[] sheeps)
+    {
+        //TODO
+        return sheeps.Where(i => i == true).Count();
+
+        // Alternative 1
+        // return sheeps.Count(s => s);
+
+        // Alternative Traditional / Manual
+        // int count = 0;
+        // foreach (bool sheep in sheeps)
+        // {
+        //     if (sheep) count++;
+        // }
+        // return count;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(CountSheeps(new bool[] { true, false, true }));
+    }
+}
