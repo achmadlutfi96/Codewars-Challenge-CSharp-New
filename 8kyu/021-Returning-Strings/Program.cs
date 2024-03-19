7d03bb9eaaebf34c5670421fbefe8fbfe5f73228
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-19 14:35:33 +0700

    Create 8kyu\020-Convert-number-to-reversed-array-of-digits

---
+++ 8kyu/021-Returning-Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class Kata
+{
+    public static string Greet(string name)
+    {
+        // throw new NotImplementedException("Greet is not implemented.");
+        return $"Hello, {name} how are you doing today?";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Greet("Ryan"));
+    }
+}
