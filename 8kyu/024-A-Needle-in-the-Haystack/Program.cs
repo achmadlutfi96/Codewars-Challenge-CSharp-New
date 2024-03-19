224256d2984c1d25f7d7a63f8b77ea72f1b841a4
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-19 18:24:23 +0700

    Create 8kyu\024-A-Needle-in-the-Haystack

---
+++ 8kyu/024-A-Needle-in-the-Haystack/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+    public static string FindNeedle(object[] haystack)
+    {
+        //Code goes here!
+        return $"found the needle at position {Array.IndexOf(haystack, "needle")}";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(FindNeedle(new object[]{'3', "123124234", null, "needle", "world", "hay", 2, '3', true, false}));
+    }
+}
