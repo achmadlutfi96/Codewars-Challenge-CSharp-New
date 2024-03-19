6a4bb7f470114dac1da7d6710530bda0e245e69c
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-19 14:42:58 +0700

    Create 8kyu\022-Is-n-divisible-by-x-and-y

---
+++ 8kyu/022-Is-n-divisible-by-x-and-y/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+public class DivisibleNb
+{
+    public static bool IsDivisible(int n, int x, int y)
+    {
+        // your code
+        return n%x==0 && n%y==0;
+    }
+
+    public static void Main(){
+        Console.WriteLine(IsDivisible(12, 4, 3));
+    }
+}
+
