ff8c5384d897cb5e0bfdd74c088b25ae3e408b7a
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-19 13:33:01 +0700

    Create 8kyu\020-Convert-number-to-reversed-array-of-digits

---
+++ 8kyu/020-Convert-number-to-reversed-array-of-digits/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            // Code goes here
+            string str = n.ToString(); // Conververt to string 
+            char[] charArr = str.ToCharArray(); // convert to char Array
+            Array.Reverse(charArr); // reverse character
+            return Array.ConvertAll(charArr, a => (Int64) char.GetNumericValue(a)); // convert all to long
+            // char.GetNumericValue() digunakan untuk mencari unicode dari karakter 
+
+            // ALTERNATIVE 
+            // return n.ToString().Reverse().Select(t => Convert.ToInt64(t.ToString())).ToArray();
+        }
+        public static void Main()
+        {
+            Console.WriteLine(Digitize(35231));
+        }
+    }
+}
