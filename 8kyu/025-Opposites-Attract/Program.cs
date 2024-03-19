bfedffdd50d013b28fc34912f5befce548741b94
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-19 18:46:03 +0700

    Create 8kyu\025-Opposites-Attract

---
+++ 8kyu/025-Opposites-Attract/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+public class LoveDetector
+{
+    public static bool lovefunc(int flower1, int flower2)
+    { 
+        //Moment of truth...
+        return !(flower1%2 == flower2%2);
+        // ALTERNATIVE
+        // return flower1 % 2 != flower2 % 2 ;
+    }
+
+    public static void Main(){
+        Console.WriteLine(lovefunc(1,4));
+        Console.WriteLine(lovefunc(2,2));
+    }
+}
