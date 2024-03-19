fbcf4494cf309cedf03bb76ec43c1dee4462dd20
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-19 14:57:26 +0700

    Create 8kyu\023-Function-1-hello-world\

---
+++ 8kyu/023-Function-1-hello-world/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+public static class Kata
+{
+    // Write a public static method "greet" that returns "hello world!"
+    public static string greet()
+    {
+        return "hello world!";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(greet());
+    }
+}
