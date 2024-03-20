5d94402cc45ae26c891239ddbd238f36abcd24e6
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-20 14:36:34 +0700

    Create 8kyu\028-Convert-a-Boolean-to-a-String

---
+++ 8kyu/028-Convert-a-Boolean-to-a-String/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+    public static string BooleanToString(bool b)
+    {
+        //Please don't delete me!
+        return b.ToString();
+    }
+    public static void Main()
+    {
+        Console.WriteLine(BooleanToString(true));
+    }
+}
