95e24ce4c52aca9c10b27f6466f3ed566fad3fdb
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-20 10:50:13 +0700

    Create 8kyu\026-Beginner-Lost-Without-a-Map

---
+++ 8kyu/026-Beginner-Lost-Without-a-Map/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int[] Maps(int[] x)
+  {
+    return x.Select(i => i+i).ToArray(); // hasil dari select adalah kelas enumerable maka harus diconvert ke array
+  }
+  public static void Main(){
+    foreach (var item in Maps(new [] { 1, 2, 3 }))
+    {
+        Console.WriteLine(item);
+    }
+    
+  }
+}
