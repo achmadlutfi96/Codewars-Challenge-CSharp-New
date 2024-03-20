c3bef1945fd0623b2db6f88a219e84f6543589b5
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-20 15:25:50 +0700

    Create 8kyu\030-Beginner-Series-2-Clock

---
+++ 8kyu/030-Beginner-Series-2-Clock/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+public static class Clock
+{
+    public static int Past(int h, int m, int s)
+    {
+        //#Happy Coding! ^_^
+        return (h*3600*1000)+(m*60*1000)+(s*1000);
+        //Alternative
+        // return (int)(new TimeSpan(h, m, s)).TotalMilliseconds;
+        // return ((h * 60 + m) * 60 + s) * 1000;
+    }
+    public static void Main(){
+        Console.WriteLine(Past(0,1,1));
+    }
+}
