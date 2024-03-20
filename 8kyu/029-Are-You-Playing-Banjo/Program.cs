509a2328a01ee1707c80aa29d275eefbc6c3f0b9
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-20 15:13:33 +0700

    Create 8kyu\029-Are-You-Playing-Banjo

---
+++ 8kyu/029-Are-You-Playing-Banjo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+  public static string AreYouPlayingBanjo(string name)
+  {
+    //Implement me
+    return name.ToLower().StartsWith('r')? $"{name} plays banjo" : $"{name} does not play banjo";
+  }
+
+  public static void Main(){
+    Console.WriteLine(AreYouPlayingBanjo("Martin"));
+    Console.WriteLine(AreYouPlayingBanjo("Rikke"));
+  }
+}
