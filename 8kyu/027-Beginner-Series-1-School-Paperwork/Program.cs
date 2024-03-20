0b99503cf24e588e56a2a8f050d44ccb4d98e3fb
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-20 14:30:10 +0700

    Create kyu\027-Beginner-Series-1-School-Paperwork

---
+++ 8kyu/027-Beginner-Series-1-School-Paperwork/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+public static class Paper
+{
+    public static int Paperwork(int n, int m)
+    {
+        //#Happy Coding! ^_^
+        return n < 0 || m < 0 ? 0 : n * m;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Paperwork(5, -5));
+    }
+}
+
