7c3286b5bd824d904036a356b2b020aad689fa79
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-21 14:16:41 +0700

    Create 8kyu\032-Calculate-average

---
+++ 8kyu/032-Calculate-average/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+using System;
+class AverageSolution
+{
+  public static double FindAverage(double[] array)
+  {
+    // Your code here
+    return array.Length != 0 ? array.Average() : 0;
+    // Alternative
+    // return array.DefaultIfEmpty().Average();
+  }
+  public static void Main(){
+    Console.WriteLine(FindAverage(new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 }));
+    Console.WriteLine(FindAverage(new double[] {}));
+  }
+} 
