0516c184dffee5a311c4d78c34349ea1869c8a7e
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-21 15:52:16 +0700

    Create 8kyu\035-Simple-multiplication

---
+++ 8kyu/035-Simple-multiplication/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Multiplier
+{
+    public static int Multiply(int x)
+    {
+        // your code........
+        return x%2 == 1 ? x*9 : x*8;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Multiply(1));
+    }
+}
