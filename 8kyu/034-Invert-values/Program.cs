ef5de1a3341deb899626653e8b9ecd2334dd61dc
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-21 15:44:15 +0700

    Create 8kyu\034-Invert-values

---
+++ 8kyu/034-Invert-values/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Linq;
+namespace Solution
+{
+    public static class ArraysInversion
+    {
+        public static int[] InvertValues(int[] input)
+        {
+            //Code it!
+            return input.Select(i => -i).ToArray();
+        }
+        public static void Main()
+        {
+            foreach(var i in InvertValues(new int[] { -1, -2, -3, -4, -5 })){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
