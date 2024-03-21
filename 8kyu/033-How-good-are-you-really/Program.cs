437275c6b4b6f04ff115841eda5bd1e2d1fb14f8
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-21 14:25:03 +0700

    Create 8kyu\033-How-good-are-you-really

---
+++ 8kyu/033-How-good-are-you-really/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+public class Kata
+{
+  public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+  {
+    //Insert brain here
+    return ClassPoints.Average() < YourPoints;
+  }
+  public static void Main(){
+    Console.WriteLine(BetterThanAverage(new int[]{100, 40, 34, 57, 29, 72, 57, 88}, 75));
+  }
+}
