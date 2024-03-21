9bb32f6c7783a3ce6133124da456b4cb283f6ae0
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-21 14:03:53 +0700

    Create 8kyu\031-Sum-Arrays

---
+++ 8kyu/031-Sum-Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+public class Kata
+{
+    public static double SumArray(double[] array)
+    {
+        return array.Sum();
+    }
+    public static void Main()
+    {
+        Console.WriteLine(SumArray(new double[] {1, 5.2, 4, 0, -1}));
+    }
+}
