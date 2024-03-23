e97d9020d842604dddb05455d667e8002c2b8c19
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-23 22:53:07 +0700

    Create 8kyu\038-You-only-need-one-Beginner

---
+++ 8kyu/038-You-only-need-one-Beginner/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+public class Kata
+{
+    public static bool Check(object[] a, object x)
+    {
+        return a.Contains(x);
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Check(new object[] { 66, 101 }, 66));
+    }
+}
