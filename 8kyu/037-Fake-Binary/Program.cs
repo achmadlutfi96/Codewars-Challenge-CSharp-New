cb805ef07097e95922a28d452a7b8649bf0d2f5c
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-23 22:38:53 +0700

    Create 8kyu\037-Fake-Binary

---
+++ 8kyu/037-Fake-Binary/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+public class Kata
+{
+    public static string FakeBin(string x)
+    {
+        char[] str = x.ToCharArray();
+        string res = "";
+        foreach (var item in str)
+        {
+            int i =(int) char.GetNumericValue(item);
+            string s= i < 5 ? "0" : "1";
+            res += s;
+        }
+        return res;
+
+        // Alternative
+        // return string.Concat(x.Select(a => a < '5' ? "0" : "1"));
+    }
+    public static void Main()
+    {
+        Console.WriteLine(FakeBin("509321967506747"));
+    }
+}
