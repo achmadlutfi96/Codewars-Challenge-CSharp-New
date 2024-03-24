cf73691887656ed23e7072bb01817c94c11e9890
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-24 15:07:17 +0700

    Create 8kyu\041-MakeUpperCase

---
+++ 8kyu/041-MakeUpperCase/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+    public static string MakeUpperCase(string str)
+    {
+        return str.ToUpper();
+    }
+
+    public static void Main(){
+        Console.WriteLine(MakeUpperCase("hello"));
+    }
+}
+
