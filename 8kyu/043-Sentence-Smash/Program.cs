ea53e3e5b55ccc7901a06b490c7329404608b9e4
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-24 20:35:55 +0700

    Create 8kyu\043-Sentence-Smash

---
+++ 8kyu/043-Sentence-Smash/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static string Smash(string[] words)
+  {
+    // Smash words
+    return String.Join(' ', words);
+  }
+  public static void Main(){
+    Console.WriteLine(Smash(new string[] {"hello", "world"}));
+  }
+}
