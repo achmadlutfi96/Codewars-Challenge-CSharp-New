fe8db6a9a1b6ba1a5d2255cbc8c387e91c421266
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-24 14:21:11 +0700

    Create 8kyu\040-Beginner-Reduce-but-Gro

---
+++ 8kyu/040-Beginner-Reduce-but-Grow/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+public class Kata
+{
+    public static int Grow(int[] x)
+    {
+        int res = 1;
+        foreach (var i in x)
+        {
+            res*=i;
+        }
+        return res;
+
+        // Alternative
+        // return x.Aggregate((p, next) => p * next);
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Grow(new[] { 1, 2, 3 }));
+    }
+}
