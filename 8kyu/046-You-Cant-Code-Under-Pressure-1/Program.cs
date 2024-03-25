a32b5e546c53164ed7d298a511e6e87605984c53
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-25 12:17:47 +0700

    Create 8kyu\046-You-Cant-Code-Under-Pressure-1

---
+++ 8kyu/046-You-Cant-Code-Under-Pressure-1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class Kata
+{
+    public static int DoubleInteger(int n)
+    {
+        //Double the input!
+        return n * 2;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(DoubleInteger(2));
+    }
+}
+
