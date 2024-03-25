f08d40a766671758b59521760d426e7a796e0bea
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-25 12:30:06 +0700

    Create 8kyu\047-Count-by-X

---
+++ 8kyu/047-Count-by-X/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int[] CountBy(int x, int n)
+  {
+    return Enumerable.Range(x, n*x).Where(i=>i%x==0).ToArray();
+
+    // Alternative
+    // return Enumerable.Range(1, n).Select(i => i * x).ToArray(); 
+  }
+  public static void Main(){
+    foreach (var i in CountBy(1,5))
+    {
+        Console.WriteLine(i);
+    }
+  }
+}
