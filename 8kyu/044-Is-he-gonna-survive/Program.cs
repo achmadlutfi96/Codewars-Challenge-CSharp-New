fb5a826bb46a0712ac4b9f559a659fbf703568ae
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-25 11:49:51 +0700

    Create 8kyu\044-Is-he-gonna-survive

---
+++ 8kyu/044-Is-he-gonna-survive/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+class Kata
+{
+    public static bool Hero(int bullets, int dragons)
+    {
+        //Do Some Magic...
+        return bullets/2 >= dragons;
+    }
+    public static void Main(){
+        Console.WriteLine(Hero(10, 5));
+    }
+}
