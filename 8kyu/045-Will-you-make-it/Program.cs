3fc96cfc18bf69dc9a8223b05d6a513c6fb82cc8
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-25 11:55:58 +0700

    Create 8kyu\045-Will-you-make-it

---
+++ 8kyu/045-Will-you-make-it/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class Kata
+{
+    public static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft)
+    {
+        // throw new NotImplementedException();
+        return distanceToPump <= mpg*fuelLeft;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(ZeroFuel(50, 25, 2));
+        Console.WriteLine(ZeroFuel(100, 50, 1));
+    }
+}
