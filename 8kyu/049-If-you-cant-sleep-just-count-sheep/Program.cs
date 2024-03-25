8019bde2df99305437c71fb1754c5fba7f309944
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-25 13:14:28 +0700

    Create 8kyu\049-If-you-cant-sleep-just-count-sheep

---
+++ 8kyu/049-If-you-cant-sleep-just-count-sheep/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string CountSheep(int n)
+    {
+        // throw new NotImplementedException();
+        string res = "";
+        for (int i = 1; i <= n; i++)
+        {
+            res = $"{res}{i} sheep...";
+        }
+        return res;
+        // Alternative
+        // return string.Concat(Enumerable.Range(1, n).Select(i => $"{i} sheep..."));
+    }
+    public static void Main()
+    {
+        Console.WriteLine(CountSheep(1));
+        Console.WriteLine(CountSheep(5));
+    }
+}
