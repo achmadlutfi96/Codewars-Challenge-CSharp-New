4e2f02aff719e7ce7da2f281dc65559d16c18395
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-26 10:52:32 +0700

    Create 8kyu\055-Array-plus-array

---
+++ 8kyu/055-Array-plus-array/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+public static class Kata
+{
+  public static int ArrayPlusArray(int[] arr1, int[] arr2)
+  {
+    return arr1.Sum()+arr2.Sum();
+  }
+  public static void Main(){
+    Console.WriteLine(ArrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+  }
+}
