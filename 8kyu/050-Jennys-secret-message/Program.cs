2cd2fb4ec2abb3c9bc5fd064d3d109cc1c2296ab
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-26 09:05:19 +0700

    Create 8kyu\050-Jennys-secret-message

---
+++ 8kyu/050-Jennys-secret-message/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class Kata
+{
+    public static string greet(string name)
+    {
+        
+        if (name == "Johnny") return "Hello, my love!";
+        return "Hello, " + name + "!";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(greet("Jim"));
+        Console.WriteLine(greet("Johnny"));
+    }
+}
