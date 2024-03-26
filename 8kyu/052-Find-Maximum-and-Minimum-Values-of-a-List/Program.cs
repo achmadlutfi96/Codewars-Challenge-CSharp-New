77624faed0b32cd01290a7e57a4e23293580bade
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-26 09:32:02 +0700

    Create 8kyu\052-Find-Maximum-and-Minimum-Values-of-a-List

---
+++ 8kyu/052-Find-Maximum-and-Minimum-Values-of-a-List/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+public class Kata
+{
+  public static int Min(int[] list)
+  {
+    return list.Min();
+  }
+  
+  public static int Max(int[] list)
+  {
+    return list.Max();
+  }
+
+  public static void Main(){
+    Console.WriteLine(Min(new []{-52, 56, 30, 29, -54, 0, -110}));
+    Console.WriteLine(Max(new []{4,6,2,1,9,63,-134,566}));
+  }
+}
