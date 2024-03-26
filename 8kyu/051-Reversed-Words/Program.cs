959d5d59bd959b1ea572cfc1511d3e3699e9f28a
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-26 09:22:46 +0700

    Create 8kyu\051-Reversed-Words

---
+++ 8kyu/051-Reversed-Words/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string ReverseWords(string str)
+    {
+        return string.Join(' ',str.Split(' ').Reverse());
+    }
+    public static void Main()
+    {
+        Console.WriteLine(ReverseWords("hello world!"));
+    }
+}
