c5fbc80b3d91257ca3f2fea973e0cb242b286115
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-26 10:28:10 +0700

    Create 8kyu\053-Convert-a-string-to-an-array

---
+++ 8kyu/053-Convert-a-string-to-an-array/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace Solution {
+  using System;
+
+  public class Solution
+  {
+    public static string[] StringToArray(string str)
+    {
+      // code code code
+      return str.Split(' ');
+    }
+    public static void Main(){
+        foreach (var item in StringToArray("Robin Singh"))
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+  }
+  
+}
