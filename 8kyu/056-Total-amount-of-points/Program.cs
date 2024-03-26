45175af8265749894bed521b419e83b247330b7b
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-26 11:14:43 +0700

    Create 8kyu\056-Total-amount-of-points

---
+++ 8kyu/056-Total-amount-of-points/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Linq;
+using System;
+
+public static class Kata {
+    public static int TotalPoints(string[] games) {
+        // insert magic here
+        int res = 0;
+        foreach (var s in games)
+        {
+            string[] i = s.Split(':').ToArray();
+            int a = Convert.ToInt32(i[0]);
+            int b = Convert.ToInt32(i[1]);
+            if(a > b){
+                res += 3;
+            }else if(a < b){
+                res += 0;
+            }else{
+                res += 1;
+            }
+        }
+        return res;
+
+        // Alternative
+        // return games.Sum(s => s[0] < s[2] ? 0 : s[0] == s[2] ? 1 : 3);
+    }
+    public static void Main(){
+        Console.WriteLine(TotalPoints(new[] { "1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3" }));
+    }
+}
