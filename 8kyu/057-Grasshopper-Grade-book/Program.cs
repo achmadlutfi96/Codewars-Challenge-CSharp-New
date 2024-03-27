26af145c0012e546677484f28816e1e4881fc8b4
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-27 17:21:42 +0700

    Create 8kyu\057-Grasshopper-Grade-book

---
+++ 8kyu/057-Grasshopper-Grade-book/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+  public static char GetGrade(int s1, int s2, int s3)
+  {
+    //Your code goes here...
+    int score = (s1+s2+s3)/3;
+    if(90 <= score) return 'A';
+    else if (80 <= score) return 'B';
+    else if (70 <= score) return 'C';
+    else if (60 <= score) return 'D';
+    else return 'F';
+  }
+  public static void Main(){
+    Console.WriteLine(GetGrade(95,90,93));
+  }
+}
