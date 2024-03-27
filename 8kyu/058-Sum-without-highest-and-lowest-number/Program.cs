8d6e7dd6f5c075977d2177c4290737fac00a9f41
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-27 22:07:21 +0700

    Create 8kyu\058-Sum-without-highest-and-lowest-number

---
+++ 8kyu/058-Sum-without-highest-and-lowest-number/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int Sum(int[] numbers)
+  {
+    if(numbers == null) return 0;
+    if(numbers.Length == 0) return 0;
+    Array.Sort(numbers);
+    return numbers.Skip(1).SkipLast(1).Sum();
+    // Alternative
+    // return numbers == null || numbers.Length < 2 ? 0 : numbers.Sum() - numbers.Max() - numbers.Min();
+  }
+  public static void Main(){
+    Console.WriteLine(Sum(new[] { 6, 2, 1, 8, 10}));
+    Console.WriteLine(Sum(new[] { 6, 2, 1, 1, 8, 10}));
+  }
+}
