2b6bf645a59c022614c6b5b670972b989ed978be
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-28 15:53:53 +0700

    Create 8kyu\061-Grasshopper-Personalized-Message

---
+++ 8kyu/061-Grasshopper-Personalized-Message/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+    public static string Greet(string name, string owner)
+    {
+        return name == owner ? "Hello boss":"Hello guest";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Greet("Daniel", "Daniel"));
+    }
+}
