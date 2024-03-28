b67043dc7a1ff07c8b9e1a7e0a5a1dab37c9ccd0
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-28 16:30:26 +0700

    Create 8kyu\063-Remove-exclamation-mark

---
+++ 8kyu/063-Remove-exclamation-marks/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+  public static string RemoveExclamationMarks(string s)
+  {
+    // Your code goes here
+    return s.Replace("!", String.Empty);
+  }
+  public static void Main(){
+    Console.WriteLine(RemoveExclamationMarks("!!"));
+    Console.WriteLine(RemoveExclamationMarks("!?!"));
+  }
+}
