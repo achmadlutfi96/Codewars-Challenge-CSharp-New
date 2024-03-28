45298e31d4f7d864aea12656d67f85ac1f5ef0ef
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-28 16:16:36 +0700

    Create 8kyu\062-Sum-Mixed-Array

---
+++ 8kyu/062-Sum-Mixed-Array/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+using System;
+public class Kata
+{
+  public static int SumMix(object[] x)
+  {
+    return x.Select(i=> Convert.ToInt32(i)).Sum();
+    // Alternative
+    // return x.Sum(ToInt32);
+  }
+  public static void Main(){
+    Console.WriteLine(SumMix(new object[] {9, 3, "7", "3"}));
+  }
+}
