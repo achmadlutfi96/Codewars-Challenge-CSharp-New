2b1603eb8da9df1413ef7e9d490e0d03829dad6f
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-28 15:39:53 +0700

    Create 8kyu\060-Area-or-Perimeter

---
+++ 8kyu/060-Area-or-Perimeter/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class MathCheck {
+    public static int AreaOrPerimeter(int l, int w) {
+        // code goes here
+        return l == w ? l*w : 2*(l+w);
+    }
+    public static void Main(){
+        Console.WriteLine(AreaOrPerimeter(4, 4));
+        Console.WriteLine(AreaOrPerimeter(6, 10));
+    }
+}
