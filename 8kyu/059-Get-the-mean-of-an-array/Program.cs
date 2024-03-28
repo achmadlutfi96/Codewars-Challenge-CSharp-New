a1b04d259e0b848ef00d6cab86aa8cdea4544332
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-28 15:30:11 +0700

    Create 8kyu\059-Get-the-mean-of-an-array

---
+++ 8kyu/059-Get-the-mean-of-an-array/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+public class Kata
+{
+  public static int GetAverage(int[] marks)
+  {
+    // ...
+    return (int) marks.Average();
+  }
+  public static void Main(){
+    Console.WriteLine(GetAverage(new int[]{5,10}));
+  }
+}
