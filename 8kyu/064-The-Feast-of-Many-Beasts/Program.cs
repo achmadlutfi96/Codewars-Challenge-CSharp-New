17292ca4f260ff93957d5197b0d3099b50225b3c
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-28 16:42:08 +0700

    Create 8kyu\064-The-Feast-of-Many-Beasts

---
+++ 8kyu/064-The-Feast-of-Many-Beasts/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+using System;
+public class Kata
+{
+    public static bool Feast(string beast, string dish)
+    {
+        return beast.First() == dish.First() && beast.Last() == dish.Last();
+    }
+    public static void Main(){
+        Console.WriteLine(Feast("great blue heron", "garlic naan"));
+        Console.WriteLine(Feast("brown bear", "bear claw"));
+    }
+}
