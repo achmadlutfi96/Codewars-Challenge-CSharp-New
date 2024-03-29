aa6bb87abc3e8561af1d78776f3d32539157e68a
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-29 13:14:45 +0700

    Create 8kyu\070-Do-I-get-a-bonus

---
+++ 8kyu/070-Do-I-get-a-bonus/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public static class Kata
+{
+    public static string bonus_time(int salary, bool bonus)
+    {
+        // your code
+        return bonus ? $"${salary*10}" : $"${salary}";
+    }
+    public static void Main(){
+        Console.WriteLine(bonus_time(10000, true));
+    }
+}
