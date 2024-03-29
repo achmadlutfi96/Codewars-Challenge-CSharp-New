413a1955e5cd248fe500b02de54ce2987258f350
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-29 12:30:17 +0700

    Create 8kyu\067-Double-Char

---
+++ 8kyu/067-Double-Char/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+public class Kata
+{
+  public static string DoubleChar(string s)
+  {
+    // your code here
+    string res = "";
+    foreach (var i in s)
+    {
+        res = res+i+i;
+    }
+    return res;
+    // Alternative
+    // return string.Concat(s.Select(x => $"{x}{x}"));
+    // return string.Join("", s.Select(x => "" + x + x));
+  }
+  public static void Main(){
+    Console.WriteLine(DoubleChar("abcd"));
+  }
+}
