a161a4170ffcf1d5c4db1b069aeb985940e91565
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-29 13:08:44 +0700

    Create 8kyu\069-L1-Set-Alarm

---
+++ 8kyu/069-L1-Set-Alarm/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata {
+  public static bool SetAlarm(bool employed, bool vacation) {
+    // your code here
+    return employed && !vacation;
+  }
+  public static void Main(){
+    Console.WriteLine(SetAlarm(true, true));
+    Console.WriteLine(SetAlarm(false, true));
+    Console.WriteLine(SetAlarm(true, false));
+    Console.WriteLine(SetAlarm(false, false));
+  }
+}
