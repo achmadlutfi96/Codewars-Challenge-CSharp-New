d3c2a701f5a4f7efe6c5d69dbb03d77a9a7e9a8d
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-29 08:47:42 +0700

    Create 8kyu\065-Transportation-on-vacation

---
+++ 8kyu/065-Transportation-on-vacation/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class RentalCar {
+    
+    public static int RentalCarCost(int d) {
+        // your code
+        return d >= 7? d*40-50 : d >= 3 ? d*40-20 : d*40;
+    }
+    public static void Main(){
+        Console.WriteLine(RentalCarCost(3));
+    }
+}
