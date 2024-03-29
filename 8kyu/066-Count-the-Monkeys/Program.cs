56dc551304924ebf2a5b63417daa33d05fc17af3
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-29 11:53:40 +0700

    Create 8kyu\066-Count-the-Monkeys

---
+++ 8kyu/066-Count-the-Monkeys/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+
+
+public static class MonkeyCounter
+{
+  public static int[] MonkeyCount(int n)
+  {
+    // throw new System.NotImplementedException();
+    return Enumerable.Range(1, n).ToArray();
+  }
+  public static void Main(){
+    foreach (var item in MonkeyCount(5))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
