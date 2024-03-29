622d14415247f10ce9bf917a8cffb679a9b278c5
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-29 13:00:48 +0700

    Create 8kyu\068-Thinkful-Logic-Drills-Traffic-light

---
+++ 8kyu/068-Thinkful-Logic-Drills-Traffic-light/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static string UpdateLight(string current)
+    {
+        //Do Some Magic
+        return new Dictionary<string, string>(){
+            {"green","yellow"},
+            {"yellow", "red"},
+            {"red","green"}} [current];
+    }
+    public static void Main(){
+        Console.WriteLine(UpdateLight("green"));
+    }
+}
