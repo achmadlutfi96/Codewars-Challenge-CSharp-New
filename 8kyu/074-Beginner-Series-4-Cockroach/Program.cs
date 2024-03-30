83717e846209add3f0778aab85580565910e1232
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-30 09:55:41 +0700

    Create 8kyu\074-Beginner-Series-4-Cockroach

---
+++ 8kyu/074-Beginner-Series-4-Cockroach/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+public class Cockroach
+{
+    public static int CockroachSpeed(double x)
+    {
+        //#Happy Coding! ^_^
+        return (int) Math.Floor((x*100000)/3600);
+    }
+    public static void Main()
+    {
+        Console.WriteLine(CockroachSpeed(1.08));
+    }
+}
