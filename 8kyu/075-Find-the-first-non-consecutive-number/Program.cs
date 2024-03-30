0d72ebccf255d12628aac6fa535abcad9d91a27e
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-30 10:35:32 +0700

    Create 8kyu\075-Find-the-first-non-consecutive-number

---
+++ 8kyu/075-Find-the-first-non-consecutive-number/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+  public static object FirstNonConsecutive(int[] arr)
+  {
+    for(int i = 0; i < arr.Length-1; i++){
+        if(arr[i]+1 != arr[i+1]){
+            return arr[i+1];
+        }
+    }
+    return null;
+    // Alternative
+    // return arr.Cast<int?>().Skip(1).FirstOrDefault(i => i != ++arr[0]);
+  }
+  public static void Main(){
+    Console.WriteLine(FirstNonConsecutive(new int[] {1, 2, 3, 4, 6, 7, 8}));
+  }
+}
