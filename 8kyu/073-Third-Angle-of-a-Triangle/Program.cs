2f2787092e5cebf072f954b6537c06b950cd0cce
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-30 09:46:02 +0700

    Create 8kyu\073-Third-Angle-of-a-Triangle

---
+++ 8kyu/073-Third-Angle-of-a-Triangle/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class Kata
+{
+  public static int OtherAngle(int a, int b)
+  {
+    // throw new NotImplementedException();
+    return 180-a-b;
+  }
+  public static void Main(){
+    Console.WriteLine(OtherAngle(30, 60));
+  }
+}
