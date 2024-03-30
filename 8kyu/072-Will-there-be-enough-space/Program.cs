d47375cdbf652a3b90e3b0094f42a3cbfba8f515
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-30 09:39:21 +0700

    Create 8kyu\072-Will-there-be-enough-space

---
+++ 8kyu/072-Will-there-be-enough-space/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class Kata
+{
+    public static int Enough(int cap, int on, int wait)
+    {
+        // throw new NotImplementedException();
+        return cap < (on + wait) ? (on + wait) - cap : 0;
+        //Alternative
+        // return Math.Max(on + wait - cap, 0);
+    }
+    public static void Main(){
+        Console.WriteLine(Enough(100, 60, 50));
+        Console.WriteLine(Enough(20, 5, 5));
+    }
+}
