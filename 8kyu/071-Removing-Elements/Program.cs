a4bb4b6bf2c89e23161ac734ab8e1ff6554e5bb8
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-30 09:27:58 +0700

    Create 8kyu\071-Removing-Elements

---
+++ 8kyu/071-Removing-Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+
+public static class Kata
+{
+    public static object[] RemoveEveryOther(object[] arr)
+    {
+        // happy coding
+        return arr.Where((obj, index)=> index%2 == 0).ToArray();
+    }
+    public static void Main(){
+        foreach (var item in RemoveEveryOther(new object[]{"Hello", "Goodbye", "Hello Again"}))
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
