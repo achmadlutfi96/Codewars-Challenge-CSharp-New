8e9d8150a63ffde07f6f4452aed75f08062aed1f
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-31 21:59:25 +0700

    Create 8kyu\081-Volume-of-a-Cuboid

---
+++ 8kyu/081-Volume-of-a-Cuboid/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata {
+  public static double GetVolumeOfCuboid(double length, double width, double height) {
+    // Your code here...
+    return length*width*height;
+  }
+  public static void Main(){
+    Console.WriteLine(GetVolumeOfCuboid(2,5,6));
+  }
+}
