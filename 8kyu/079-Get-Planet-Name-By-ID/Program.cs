423ca6303fc39a0cf3ddb5da7f41cef38f2bc045
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-31 21:28:02 +0700

    Create 8kyu\079-Get-Planet-Name-By-ID

---
+++ 8kyu/079-Get-Planet-Name-By-ID/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+    public static string GetPlanetName(int id)
+    {
+        string? name = null;
+        switch (id)
+        {
+            case 1:
+                name = "Mercury"; break;
+            case 2:
+                name = "Venus"; break;
+            case 3:
+                name = "Earth"; break;
+            case 4:
+                name = "Mars"; break;
+            case 5:
+                name = "Jupiter"; break;
+            case 6:
+                name = "Saturn"; break;
+            case 7:
+                name = "Uranus"; break;
+            case 8:
+                name = "Neptune"; break;
+        }
+        return name;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(GetPlanetName(2));
+    }
+}
