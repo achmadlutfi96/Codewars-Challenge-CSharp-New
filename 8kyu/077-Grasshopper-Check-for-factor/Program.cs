bedf48a2fe41e7a727e5cc55b7f88aab2f805d65
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-31 14:02:36 +0700

    Create 8kyu\077-Grasshopper-Check-for-factor

---
+++ 8kyu/077-Grasshopper-Check-for-factor/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+  public static bool CheckForFactor(int num, int factor)
+  {
+    // code here
+    return num%factor == 0;
+  }
+  public static void Main(){
+    Console.WriteLine(CheckForFactor(10,2));
+  }
+}
