e0011b528ef9ed297e0658927fe86e340f1eed7c
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-31 21:18:25 +0700

    Create 8kyu\078-Keep-up-the-hoop

---
+++ 8kyu/078-Keep-up-the-hoop/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+    public static string HoopCount(int n)
+    {
+        //Your code goes here
+        return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(HoopCount(6));
+        Console.WriteLine(HoopCount(22));
+    }
+}
