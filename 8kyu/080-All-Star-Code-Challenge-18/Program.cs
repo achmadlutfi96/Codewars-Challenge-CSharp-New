f53e75c49cb61a2d83b44efbce06c6b4e00d5c3f
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-31 21:53:35 +0700

    Create 8kyu\080-All-Star-Code-Challenge-18

---
+++ 8kyu/080-All-Star-Code-Challenge-18/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+class Kata
+{
+    public static int StrCount(string str, char letter)
+    {
+        return String.Join("",str.ToCharArray().Where(s => Char.Equals(s, letter))).Length; //Do Some Magic
+        // Alternative
+        // return str.Count(a => a.Equals(letter));
+    }
+    public static void Main(){
+        Console.WriteLine(StrCount("Hello", 'o'));
+        Console.WriteLine(StrCount("Hello", 'l'));
+        Console.WriteLine(StrCount("Hello", 'p'));
+        Console.WriteLine(StrCount("", 'z'));
+        
+    }
+}
