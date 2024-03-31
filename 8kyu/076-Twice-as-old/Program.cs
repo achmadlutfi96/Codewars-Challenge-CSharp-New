63fd11bba3a18ff1cba72357282991e58e6b6957
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-31 13:57:41 +0700

    Create 8kyu\076-Twice-as-old

---
+++ 8kyu/076-Twice-as-old/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace Solution
+{
+  public class TwiceAsOldSolution
+  {
+    public static int TwiceAsOld(int dadYears, int sonYears)
+    {
+      // Add you code here.
+      return Math.Abs(dadYears-(sonYears*2));
+    }
+
+    public static void Main(){
+        Console.WriteLine(TwiceAsOld(30, 0));
+        Console.WriteLine(TwiceAsOld(30, 7));
+    }
+  }
+}
