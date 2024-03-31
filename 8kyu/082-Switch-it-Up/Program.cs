b906766827834b3ee7472ab971bdf56a44cde6a9
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-31 22:08:19 +0700

    Create 8kyu\082-Switch-it-Up

---
+++ 8kyu/082-Switch-it-Up/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+  public static string SwitchItUp(int number)
+  {
+    return number switch{
+        1 => "One",
+        2 => "Two",
+        3 => "Three",
+        4 => "Four",
+        5 => "Five",
+        6 => "Six",
+        7 => "Seven",
+        8 => "Eight",
+        9 => "Nine",
+        _ => "Zero"
+    };
+  }
+  public static void Main(){
+    Console.WriteLine(SwitchItUp(1));
+  }
+}
