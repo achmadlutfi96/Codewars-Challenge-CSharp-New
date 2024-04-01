d7ff01a69051cce921f7182fa22a337d83e17535
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-01 21:16:53 +0700

    Create 8kyu\091-Powers-of-2

---
+++ 8kyu/091-Powers-of-2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Numerics;
+using System.Linq;
+using System;
+
+public class Kata
+{
+  public static BigInteger[] PowersOfTwo(int n)
+  {
+
+    // return new BigInteger[0];
+    return Enumerable.Range(0, n+1).Select(i => (BigInteger) Math.Pow(2, i)).ToArray();
+  }
+  public static void Main(){
+    foreach (var item in PowersOfTwo(4))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
