76c051384250cfcda23d89785dd399dc28f29005
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-01 15:41:36 +0700

    Create 8kyu\083-Correct-the-mistakes-of-the-chara

---
+++ 8kyu/083-Correct-the-mistakes-of-the-character-recognition-software/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+  public static string Correct(string text)
+  {
+    return text.Replace('1','I').Replace('5','S').Replace('0', 'O');
+  }
+  public static void Main(){
+    Console.WriteLine(Correct("51NGAP0RE"));
+  }
+}
