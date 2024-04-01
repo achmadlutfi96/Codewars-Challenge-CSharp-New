411e3297d60072da449053a5866637633f5848c1
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-01 16:32:01 +0700

    Create 8kyu\087-altERnaTIng-cAsE-ALTerNAtiNG-CaSe

---
+++ 8kyu/087-altERnaTIng-cAsE-ALTerNAtiNG-CaSe/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+namespace Extensions
+{
+  public static class StringExt
+  {
+    public static string ToAlternatingCase (this string s)
+    {
+    //   throw new NotImplementedException();
+        return String.Join("", s.Select(v => v == char.ToUpper(v) ? char.ToLower(v): char.ToUpper(v)));
+        // Alternative
+        // return string.Concat(s.Select(c => char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)));
+    }
+    public static void Main(){
+        Console.WriteLine("hello world".ToAlternatingCase());
+    }
+  }
+}
