7ea308f9531ae4f3bb48b72113d2f2bdaab18497
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-01 16:01:07 +0700

    Create 8kyu\084-Parse-nice-int-from-char-problem

---
+++ 8kyu/084-Parse-nice-int-from-char-problem/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+  public static int GetAge(string inputString)
+  {
+    // return correct age (int). Happy coding :)
+    return Int32.Parse(inputString[0].ToString());
+    // Alternative
+    // return (int)char.GetNumericValue(inputString[0]);
+  }
+  public static void Main(){
+    Console.WriteLine(GetAge("3 years old"));
+  }
+}
