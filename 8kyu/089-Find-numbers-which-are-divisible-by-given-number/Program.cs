1dba315530aecfdb2e54966d732c7c8625035bfe
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-01 17:02:59 +0700

    Create 8kyu\089-Find-numbers-which-are-divisible-by-giv

---
+++ 8kyu/089-Find-numbers-which-are-divisible-by-given-number/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+
+public class Kata
+{
+  public static int[] DivisibleBy(int[] numbers, int divisor)
+  {
+    return numbers.Where(n => n%divisor == 0).ToArray();
+  }
+  public static void Main(){
+    foreach (var item in DivisibleBy(new int[]{1,2,3,4,5,6}, 2))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
