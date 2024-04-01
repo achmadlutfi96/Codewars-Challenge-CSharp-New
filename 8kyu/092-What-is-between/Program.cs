d04a0858ffa31e11ab25571751f2016089f7e774
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-01 21:36:47 +0700

    Create 8kyu\092-What-is-between

---
+++ 8kyu/092-What-is-between/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Linq;
+using System.Collections.Generic;
+
+public class Kata {
+  public static int[] Between(int a, int b) {
+    // your code here
+    // Alternative
+    // return Enumerable.Range(a, b - a + 1).ToArray();
+
+    List<int> results = new List<int>();
+    for (int i = a; i <= b; i++)
+    {
+        results.Add(i);
+    }
+    return results.ToArray<int>();
+  }
+
+  public static void Main(){
+    foreach (var item in Between(-2, 2))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
