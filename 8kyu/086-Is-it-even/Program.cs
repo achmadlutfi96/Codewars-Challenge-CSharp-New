8a2985085d641ae02d9e066eaeb19dbe3d489109
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-01 16:22:30 +0700

    Create 8kyu\086-Is-it-even

---
+++ 8kyu/086-Is-it-even/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Number
+{
+  public static bool IsEven(double n)
+  {
+    // Your awesome code here!
+    return n%2 == 0;
+  }
+  public static void Main(){
+    Console.WriteLine(IsEven(1));
+  }
+}
