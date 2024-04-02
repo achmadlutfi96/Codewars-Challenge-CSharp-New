d8e928973d13fa9a42708bc61b8525c8228af36f
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-02 09:36:11 +0700

    Create 8kyu\093-Count-Odd-Numbers-below-n

---
+++ 8kyu/093-Count-Odd-Numbers-below-n/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class Kata
+{
+  public static ulong OddCount(ulong n)
+  {
+    // throw new NotImplementedException();
+    return n/2;
+  }
+  public static void Main(){
+    Console.WriteLine(OddCount(15023));
+    Console.WriteLine(OddCount(15));
+  }
+}
