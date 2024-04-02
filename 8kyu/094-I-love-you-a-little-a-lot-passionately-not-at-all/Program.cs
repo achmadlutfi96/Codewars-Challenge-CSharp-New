9bb97359a826c8bb8c7b964276ca7826975c4903
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-02 13:56:52 +0700

    Create 8kyu\094-I-love-you-a-little-a-lot-passionately-not-at-all

---
+++ 8kyu/094-I-love-you-a-little-a-lot-passionately-not-at-all/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Kata
+{
+  public static string HowMuchILoveYou(int nb_petals)
+  {
+    // your code
+    return (nb_petals%6) switch{
+        0 => "not at all",
+        1 => "I love you",
+        2 => "a little",
+        3 => "a lot",
+        4 => "passionately",
+        _ => "madly"
+    };
+  }
+  public static void Main(){
+    Console.WriteLine(HowMuchILoveYou(7));
+  }
+}
