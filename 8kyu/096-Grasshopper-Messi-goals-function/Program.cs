26387c8d27e0e3fd9b596387c71d8d05ca0cbafc
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-02 21:23:18 +0700

    Create 8kyu\096-Grasshopper-Messi-goals-function

---
+++ 8kyu/096-Grasshopper-Messi-goals-function/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+  public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+  {
+    // code goes here
+    return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+  }
+  public static void Main(){
+    Console.WriteLine(GetGoals(43,10,5));
+  }
+}
