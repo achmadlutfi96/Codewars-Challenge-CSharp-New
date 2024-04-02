481094eeb74fef760e4947c53c8c94933298acd7
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-02 14:08:20 +0700

    Create 8kyu\095-Difference-of-Volumes-of-Cuboids

---
+++ 8kyu/095-Difference-of-Volumes-of-Cuboids/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+public class Kata
+{
+  public static int FindDifference(int[] a, int[] b)
+  {
+    //loading...
+    return Math.Abs((a[0] * a[1] * a[2])-(b[0]*b[1]*b[2]));
+    // Alternative
+    // return Math.Abs(a.Aggregate(1, (x,y) => x*y) - b.Aggregate(1, (x,y) => x*y));
+  }
+  public static void Main(){
+    Console.WriteLine(FindDifference(new int[] {3, 2, 5}, new int[] {1, 4, 4}));
+  }
+}
