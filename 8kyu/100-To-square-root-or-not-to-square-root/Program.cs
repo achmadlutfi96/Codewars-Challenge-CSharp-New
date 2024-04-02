ed525aa5f67d4f0f7083b315d29ffc24381b5fff
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-02 22:40:14 +0700

    Create 8kyu\100-To-square-root-or-not-to-square-root

---
+++ 8kyu/100-To-square-root-or-not-to-square-root/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+public class Kata
+{
+    public static int[] SquareOrSquareRoot(int[] array)
+    {
+        return Array.ConvertAll(array.Select(i => (int) Math.Pow(Math.Floor(Math.Sqrt(i)),2) == i ?  Math.Sqrt(i) : i*i).ToArray(), new Converter<double, int>(a => (int)a));
+
+        // Alternative
+        // return array.Select(x => (int)(Math.Sqrt(x) % 1 == 0 ? Math.Sqrt(x) : x * x)).ToArray();
+    }
+
+    public static void Main()
+    {
+        foreach (var item in SquareOrSquareRoot(new int[] { 4, 3, 9, 7, 2, 1 }))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
