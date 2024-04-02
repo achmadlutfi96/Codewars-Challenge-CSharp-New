a0616eb8f042f9710a91a27a91b856e8f5967301
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-02 21:34:52 +0700

    Create 8kyu\097-Students-Final-Grade

---
+++ 8kyu/097-Students-Final-Grade/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+namespace Solution
+{
+    public class Kata
+    {
+        public static int FinalGrade(int exam, int projects)
+        {
+            if (exam > 90 || projects > 10)
+            {
+                return 100;
+            }
+            else if (exam > 75 && projects >= 5)
+            {
+                return 90;
+            }
+            else if (exam > 50 && projects >= 2)
+            {
+                return 75;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static void Main()
+        {
+            Console.WriteLine(FinalGrade(100, 12));
+        }
+    }
+}
+
