00a7ac7cbc28c5a2613279d95f2091c95a170b75
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-02 21:38:52 +0700

    Create 8kyu\098-Grasshopper-Debug-sayHello

---
+++ 8kyu/098-Grasshopper-Debug-sayHello/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+  public static string SayHello(string name)
+  {
+    return $"Hello, {name}"; 
+  }
+  public static void Main(){
+    Console.WriteLine(SayHello("Mr. Spock"));
+  }
+}
