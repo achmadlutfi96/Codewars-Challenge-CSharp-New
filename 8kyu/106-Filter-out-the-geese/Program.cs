9b70d369d2cae230b4410b5297776857f52f73aa
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 11:02:59 +0700

    Create 8kyu\106-Filter-out-the-geese

---
+++ 8kyu/106-Filter-out-the-geese/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Filter
+{
+  public static IEnumerable<string> GooseFilter(IEnumerable<string> birds)
+  {
+    // return IEnumerable of string containing all of the strings in the input collection, except those that match strings in geese
+    string[] geese = new string[] { "African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher" };
+
+    return birds.Except(geese);
+  }
+  public static void Main(){
+    foreach (var item in GooseFilter(new string[] {"Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"}))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
