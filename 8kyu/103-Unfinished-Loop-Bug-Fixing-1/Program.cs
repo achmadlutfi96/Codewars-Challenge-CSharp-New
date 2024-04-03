826453bcc10e6de9f1954610fa8c55913a83d0cc
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 10:40:42 +0700

    Create 8kyu\103-Unfinished-Loop-Bug-Fixing-1

---
+++ 8kyu/103-Unfinished-Loop-Bug-Fixing-1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+  public static List<int> CreateList(int number)
+  {
+    List<int> list = new List<int>();
+    
+    for(int counter = 1; counter <= number; counter++)
+    {
+      list.Add(counter);
+    }
+    
+    return list;
+  }
+
+  public static void Main(){
+    foreach (var item in CreateList(6))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
