670bd87d4cccf932ba14651a365ddd497734b9a7
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 11:14:13 +0700

    Create 8kyu\107-Stringy-Strings

---
+++ 8kyu/107-Stringy-Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string Stringy(int size)
+  {
+    // your code here
+     return string.Join("", Enumerable.Range(1, size).Select(i => i%2==1 ? '1' : '0'));
+     // Alternative
+     // return string.Join("", Enumerable.Range(0, size).Select(x => (x + 1) % 2));
+     // return string.Concat(Enumerable.Range(0, size).Select( x => x % 2 == 0 ? "1" : "0"));
+  }
+  public static void Main(){
+    Console.WriteLine(Stringy(10));
+  }
+}
