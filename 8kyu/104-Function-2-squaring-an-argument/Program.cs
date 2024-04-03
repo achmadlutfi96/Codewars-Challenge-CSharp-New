673d3812c5deddb899ca13a2e0914e8763f6af81
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 10:44:39 +0700

    Create 8kyu\104-Function-2-squaring-an-argument

---
+++ 8kyu/104-Function-2-squaring-an-argument/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+  // Write a static function "Square" here which takes a double
+  // and returns the square of that double
+  public static double Square(double n){
+    return n*n;
+  }
+  public static void Main(){
+    Console.WriteLine(Square(6));
+  }
+}
