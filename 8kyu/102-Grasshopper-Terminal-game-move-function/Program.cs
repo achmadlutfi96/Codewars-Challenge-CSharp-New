9267e8f7105b4717500baf9f5ed1f48a6b773254
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 10:34:32 +0700

    Create 8kyu\102-Grasshopper-Terminal-game-move-function

---
+++ 8kyu/102-Grasshopper-Terminal-game-move-function/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Drawing;
+
+public class Game
+{
+  public static int Move(int position, int roll) 
+  {
+  	// throw new NotImplementedException();
+    return position+(roll*2);
+  }
+  public static void Main(){
+    Console.WriteLine(Move(0,4));
+  }
+}
