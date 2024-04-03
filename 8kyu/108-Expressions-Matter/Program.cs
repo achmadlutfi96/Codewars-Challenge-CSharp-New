301b731f980aa2c825568c88b45d13eee149122e
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 11:30:05 +0700

    Create 8kyu\108-Expressions-Matter

---
+++ 8kyu/108-Expressions-Matter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Kata
+{
+    public static int ExpressionsMatter(int a, int b, int c)
+    {
+        // Your Code here... Happy Coding!
+        List<int> val = new List<int>();
+        val.Add(a * (b + c));
+        val.Add(a * b * c);
+        val.Add(a + b * c);
+        val.Add((a + b) * c);
+        val.Add(a + b + c);
+
+        return val.Max();// highest achievable result
+
+        // Alternative
+        // int[] res =
+    // {
+    //  a*b*c,
+    //  a*b+c,
+    //  a*(b+c),
+    //  (a+b)*c,
+    //  a+b*c,
+    //  a+b+c
+    //  };
+    //     return res.Max();
+    }
+    public static void Main()
+    {
+        Console.WriteLine(ExpressionsMatter(1, 2, 3));
+    }
+}
