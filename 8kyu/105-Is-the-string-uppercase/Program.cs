c269a9eaf1f8b414431450970499f3c51aef94c2
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 10:51:18 +0700

    Create 8kyu\105-Is-the-string-uppercase

---
+++ 8kyu/105-Is-the-string-uppercase/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+public static class StringExtensions
+{
+  public static bool IsUpperCase(this string text)
+  {
+    // your code here
+    return text.ToUpper() == text;
+  }
+  public static void Main(){
+    Console.WriteLine(IsUpperCase("hello I AM DONALD"));
+    Console.WriteLine(IsUpperCase("HELLO I AM DONALD"));
+  }
+}
