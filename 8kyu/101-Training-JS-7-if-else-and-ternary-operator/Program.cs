e453dacf70a624c1e22e8ebc583b2abb27496373
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-03 10:23:33 +0700

    Create 8kyu\101-Training-JS-7-if-else-and-ternary-operator

---
+++ 8kyu/101-Training-JS-7-if-else-and-ternary-operator/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+    public static int SaleHotDogs(int n)
+    {
+        return n<5 ? n*100 : n>=5 && n<10 ? n*95 : n*90;
+        // Alternative
+        // n * (n < 5 ? 100 : n < 10 ? 95 : 90);
+    }
+    public static void Main()
+    {
+        Console.WriteLine(SaleHotDogs(4));
+    }
+}
