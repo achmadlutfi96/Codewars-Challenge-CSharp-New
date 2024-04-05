9052e90b1f023f6ea51fec1d7016bf578a3c3e86
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 19:57:53 +0700

    Create 8kyu\114-Super-Duper-Easy

---
+++ 8kyu/114-Super-Duper-Easy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Kata
+{
+  public static string Problem(String a)
+  {
+    //magic code
+    try {
+        decimal i = Convert.ToDecimal(a);
+        return ((int)(i*50+6)).ToString();
+    }catch(FormatException){
+        return "Error";
+    }
+
+    // Alternative
+    // return double.TryParse(a, out var n) ? $"{n * 50 + 6}" : "Error";
+  }
+  public static void Main(){
+    Console.WriteLine(Problem("hello"));
+    Console.WriteLine(Problem("1"));
+  }
+}
