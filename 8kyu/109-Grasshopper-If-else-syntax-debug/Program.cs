1c08f9ee57889fe8c2b4c117fe20ac4aa5a86fb8
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 18:50:14 +0700

    Create 8kyu\109-Grasshopper-If-else-syntax-debug

---
+++ 8kyu/109-Grasshopper-If-else-syntax-debug/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Player
+{
+  private int health = 100;
+  public int Health
+  {
+    get
+    {
+      return this.health;
+    }
+    set
+    {
+      health = value;
+    }
+  }
+  
+  public Player()
+  {
+    
+  }
+  
+  //FIXME: This method won't compile for some reason! :(
+  public bool CheckAlive(){
+    bool status = false;
+    if (this.Health < 0)
+    {
+      status=false;
+    }
+    else if (this.Health > 0)
+    {
+      status=true;
+    }
+    return status;
+
+    // Alternatif
+    // return this.Health > 0 ;
+
+  }
+}
+
+public class Program{
+    public static void Main(){
+        Player greg = new Player();
+        greg.Health = 5;
+        Console.WriteLine(greg.CheckAlive());
+    }
+}
