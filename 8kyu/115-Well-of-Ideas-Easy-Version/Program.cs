a5419a00f991377bbd77e7ce8bb56a3dbca9c557
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 20:20:07 +0700

    Create 8kyu\115-Well-of-Ideas-Easy-Version

---
+++ 8kyu/115-Well-of-Ideas-Easy-Version/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string Well(string[] x)
+    {
+        int result = x.Where(i=>i.Equals("good")).Count();
+        if (  result == 1 || result == 2)
+        {
+            return "Publish!";
+        }else if(result > 2){
+            return "I smell a series!";
+        }else{
+            return "Fail!";
+        }
+
+        // Alternative
+        // int count = x.Count(v => v == "good");
+        // return (count > 2) ? "I smell a series!" : (count >= 1) ? "Publish!" : "Fail!";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Well(new string[] { "bad", "bad", "bad" }));
+        Console.WriteLine(Well(new string[] { "good", "bad", "bad", "bad", "bad" }));
+        Console.WriteLine(Well(new string[] { "good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good" }));
+    }
+}
