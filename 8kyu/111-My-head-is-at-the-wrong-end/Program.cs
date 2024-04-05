12b2d7cc9c3ab29fb7aed461b5102c58f4ee1ef2
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 19:31:12 +0700

    Create 8kyu\111-My-head-is-at-the-wrong-end

---
+++ 8kyu/111-My-head-is-at-the-wrong-end/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string[] FixTheMeerkat(string[] arr)
+  {
+    //your code here
+    return arr.Reverse().ToArray();
+  }
+  public static void Main(){
+    foreach (var item in FixTheMeerkat(new string[] {"tail", "body", "head"}))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
---
+++ 8kyu/112-Reverse-List-Order/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+public class Kata
+{
+    public static List<int> ReverseList(List<int> list)
+    {
+        // Return a new list with its elements in reverse order compared to the input list
+        // Do not mutate the original list!
+        return list.Reverse<int>().ToList();
+    }
+    public static void Main()
+    {
+        foreach (var item in ReverseList(new List<int> { 4, 3, 2, 1 }))
+        {
+            Console.WriteLine();
+        }
+    }
+}
