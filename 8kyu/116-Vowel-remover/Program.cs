2a78000f2af4d27abe3356d5e409a6015d1c5de1
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 21:36:52 +0700

    Create 8kyu\116-Vowel-remover

---
+++ 8kyu/116-Vowel-remover/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string Shortcut(string input)
+    {
+        // TODO: Remove vowels
+        char[] v = {'a', 'e', 'i', 'o', 'u' };
+        return String.Concat(input.Where(c => !v.Contains(c)));
+
+        // ALTERNATIVE
+        // return String.Concat(input.Where(c => !"aeiou".Contains(c)));
+    }
+    public static void Main(){
+        Console.WriteLine(Shortcut("hello"));
+        Console.WriteLine(Shortcut("codewars"));
+        Console.WriteLine(Shortcut("HELLO"));
+    }
+}
