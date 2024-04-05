726eefb1e02c6f26201b214d610c09871c805e32
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 19:16:10 +0700

    Create 8kyu\110-Find-Multiples-of-a-Number

---
+++ 8kyu/110-Find-Multiples-of-a-Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+    public static List<int> FindMultiples(int integer, int limit)
+    {
+        // Your code here!
+        
+        List<int> res = new List<int>();
+        foreach (var item in Enumerable.Range(1, limit))
+        {
+            if(item%integer == 0){
+                res.Add(item);
+            }
+        }
+        return res;
+
+        // ALTERNATIVE
+        // Enumerable.Range(1, limit / num).Select(x => x * num).ToList();
+    }
+    public static void Main()
+    {
+        foreach (var item in FindMultiples(5, 25))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
