58f7b2a8704552e4951e9f4481065496c5226237
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 21:54:54 +0700

    Create 8kyu\118-Short-Long-Short

---
+++ 8kyu/118-Short-Long-Short/Program.cs
@@ -0,0 +1,10 @@
+﻿public class ShortLongShort
+{
+  public static string Solution(string a, string b)
+  {
+    return a.Length > b.Length ? b+a+b : a+b+a;
+  }
+  public static void Main(){
+    Console.WriteLine(Solution("1", "22"));
+  }
+}
