7c6c3194186bf0d02fc25d902912cfb19c1ba9e3
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-05 21:48:06 +0700

    Create 8kyu\117-Lario-and-Muigi-Pipe-Problem

---
+++ 8kyu/117-Lario-and-Muigi-Pipe-Problem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Fixer
+{
+  public static List<int> PipeFix(List<int> numbers)
+  {
+    //Good luck!
+    return Enumerable.Range(numbers.First(), numbers.Last()-numbers.First()+1).ToList();
+  }
+  public static void Main(){
+    foreach (var item in PipeFix(new List<int>{1,2,3,5,6,8,9}))
+    {
+        Console.WriteLine(item);
+    }
+    foreach (var item in PipeFix(new List<int>{6,9}))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
