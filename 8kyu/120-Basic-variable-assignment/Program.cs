b0f89ce776210f864c1672f4a8144af8614213b2
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-06 20:18:52 +0700

    Create 8kyu\120-Basic-variable-assignment

---
+++ 8kyu/120-Basic-variable-assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Kata
+{
+  public static string A = "code";
+  public static string B = "wa.rs";
+  public static string Name = A + B;
+
+  public static void Main(){
+    Console.WriteLine(Name);
+  }
+}
