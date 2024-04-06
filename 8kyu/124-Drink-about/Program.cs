de9085f702d3067b25ac489265a7ad80044b139e
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-06 21:33:05 +0700

    Create 8kyu\124-Drink-about

---
+++ 8kyu/124-Drink-about/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+  public static string PeopleWithAgeDrink(int old)
+  {
+    return String.Concat("drink ", old < 14 ? "toddy" : old<18 ? "coke" : old<21 ? "beer" : "whisky");
+    // Alternative
+    // return "drink "+(old < 14 ? "toddy" : old<18 ? "coke" : old<21 ? "beer" : "whisky");
+  }
+  public static void Main(string[] args){
+    Console.WriteLine(PeopleWithAgeDrink(22));
+  }
+}
