3b5152054dacd8930d3fe1ff98ad4ae924ee3876
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-06 21:20:37 +0700

    Create 8kyu\123-Exclamation-marks-series-1

---
+++ 8kyu/123-Exclamation-marks-series-1-Remove-an-exclamation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Kata
+{
+  public static string Remove(string s)
+  {
+    //coding and coding....
+    return s.EndsWith('!') ? s.Substring(0, s.Length - 1) : s;
+    // Alternative
+    // return s.EndsWith("!") ? s.Remove(s.Length-1) : s;
+  }
+  public static void Main(){
+    Console.WriteLine(Remove("Hi!"));
+    Console.WriteLine(Remove("Hi!!!"));
+    Console.WriteLine(Remove("Hi"));
+  }
+}
