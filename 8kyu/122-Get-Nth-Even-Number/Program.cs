f1923e3343dd0c0b2c0c2d6d51b70869b63d9a1d
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-06 20:58:15 +0700

    Create 8kyu\122-Get-Nth-Even-Number

---
+++ 8kyu/122-Get-Nth-Even-Number/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Kata
+{
+  public static int NthEven(int n)
+  {
+    //Your code
+    return n*2-2;
+    // Altenative
+    // return 2 * (n - 1);
+  }
+  public static void Main(){
+    Console.WriteLine(NthEven(3));
+  }
+}
