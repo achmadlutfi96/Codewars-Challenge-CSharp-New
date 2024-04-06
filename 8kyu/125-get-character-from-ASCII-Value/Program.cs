5bc29fe82ec6c7c55a69329d96cb5af97d409e4a
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-06 21:39:36 +0700

    Create 8kyu\125-get-character-from-ASCII-Value

---
+++ 8kyu/125-get-character-from-ASCII-Value/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Kata
+{
+  public static char GetChar(int charcode)
+  {
+    // ...
+    return (char)(charcode >> 32);
+    // Alternative
+    // return (char)charcode;
+  }
+  public static void Main(){
+    Console.WriteLine(GetChar(55));
+    Console.WriteLine(GetChar(64));
+  }
+}
