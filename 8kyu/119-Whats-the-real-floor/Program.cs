6f1b81c94ebee3fd1f89dd3f6cbfcf357ba4ace4
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-06 20:15:05 +0700

    Create 8kyu\119-Whats-the-real-floor

---
+++ 8kyu/119-Whats-the-real-floor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int GetRealFloor(int n)
+  {
+    // throw new NotImplementedException();
+    if (n > 13)
+    {
+        return n-2;
+    }else if(n > 1){
+        return n-1;
+    }else if(n==1){
+        return 0;
+    }else{
+        return n;
+    }
+    // Alternative
+    // return n < 1 ? n : n > 13 ? n -2 : n- 1;
+  }
+  public static void Main(){
+    Console.WriteLine(GetRealFloor(1));
+    Console.WriteLine(GetRealFloor(5));
+    Console.WriteLine(GetRealFloor(15));
+  }
+}
