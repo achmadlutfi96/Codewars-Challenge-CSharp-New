ef2a370d24101fac4552ffd8e38ea83e74e203d4
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-06 20:25:04 +0700

    Create 8kyu\121-Name-Shuffler

---
+++ 8kyu/121-Name-Shuffler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{   
+  public static string NameShuffler(string str)
+  {
+    //your code here...
+    return String.Join(" ", str.Split(' ').Reverse());
+  }
+  public static void Main(string[] args){
+    Console.WriteLine(NameShuffler("john McClane"));
+  }
+}
