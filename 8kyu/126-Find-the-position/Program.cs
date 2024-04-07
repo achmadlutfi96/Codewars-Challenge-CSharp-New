a6875ee9c8d6733c6694aaa89ff2625f51f7f6fb
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-07 15:40:52 +0700

    Create 8kyu\126-Find-the-position

---
+++ 8kyu/126-Find-the-position/Program.cs
@@ -0,0 +1,14 @@
+﻿
+public class Kata
+{
+  public static string Position(char alphabet)
+  {
+    return $"Position of alphabet: {((int)alphabet)-96}";
+    // Alternative
+    // return $"Position of alphabet: {(1 + alphabet - 'a')}";
+  }
+  public static void Main(){
+    Console.WriteLine(Position('a'));
+    Console.WriteLine(Position('z'));
+  }
+}
