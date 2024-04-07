45c2acf37b3c7bf9bb090e7135caca19d07f5d3d
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-07 15:53:41 +0700

    Create 8kyu\128-Exclusive-or-xor-Logical-Operator

---
+++ 8kyu/128-Exclusive-or-xor-Logical-Operator/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+  public static bool Xor(bool a, bool b)
+  {
+    return a != b;
+    // Alternative
+    // return a ^ b // bitwise operator XOR
+  }
+  public static void Main(string[] args){
+    Console.WriteLine(Xor(false, false));
+  }
+}
