b27d2bc5db97b93ca32dcb8cf998791e85d069ba
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-07 16:29:38 +0700

    Create 8kyu\130-Capitalization-and-Mutability

---
+++ 8kyu/130-Capitalization-and-Mutability/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Kata
+{
+  public static string CapitalizeWord(string word)
+  {
+    string res = "";
+    res += word[0].ToString().ToUpper()[0];
+    res += word.Substring(1);
+    return res;
+
+    // Alternative
+    // return char.ToUpper(word[0]) + word.Substring(1);
+    // return char.ToUpper(word[0]) + word[1..];
+  }
+  public static void Main(){
+    Console.WriteLine(CapitalizeWord("word"));
+  }
+}
