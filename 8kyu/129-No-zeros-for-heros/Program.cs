1160203635cbe75b202465da68b1a892dde0f3ad
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-07 16:14:47 +0700

    Create 8kyu\129-No-zeros-for-heros

---
+++ 8kyu/129-No-zeros-for-heros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class NoBoring 
+{
+    public static int NoBoringZeros(int n) 
+    {
+        // your code
+        // return (n == 0) ? 0 : Convert.ToInt32(n.ToString().TrimEnd('0'));
+        while(n%10 == 0 && n != 0){
+            n/=10;
+        }
+        return n;
+    }
+    public static void Main(){
+        Console.WriteLine(NoBoringZeros(960000));
+    }
+}
