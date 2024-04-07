e2a239c367af2b29f115b8181adb78ccd444ecd9
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-07 15:46:35 +0700

    Create 8kyu\127-Grasshopper-Terminal-game-combat-function

---
+++ 8kyu/127-Grasshopper-Terminal-game-combat-function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Game
+{
+  public static float Combat(float health, float damage)
+  {
+    // throw new NotImplementedException();
+
+    return health > damage ? health-damage : 0;
+    // Alternative
+    // return Math.Max(0, health - damage);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Combat(20, 30));
+  }
+}
