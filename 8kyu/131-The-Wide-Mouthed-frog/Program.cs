4ba3a296e1e572f77238cf683dd965c5ce8944ab
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-08 11:59:26 +0700

    Create 8kyu\131-The-Wide-Mouthed-frog

---
+++ 8kyu/131-The-Wide-Mouthed-frog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+public class Kata
+{
+  public static string MouthSize(string animal)
+  {
+    // code here
+    return animal.ToLower().Equals("alligator") ? "small" : "wide";
+
+    // Alternative
+    // return animal.Equals("alligator", StringComparison.OrdinalIgnoreCase) ? "small" : "wide";
+  }
+  public static void Main(){
+    Console.WriteLine(MouthSize("toucan"));
+    Console.WriteLine(MouthSize("alligator"));
+  }
+}
