090ea8f91707d066ffaead3dd485ad0cb4cb5b13
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-08 12:55:58 +0700

    Create 8kyu\133-Reversing-Words-in-a-String

---
+++ 8kyu/133-Reversing-Words-in-a-String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static string Reverse(string text)
+  {
+    return String.Join(" ",text.Split(" ").Reverse());
+  }
+  public static void Main(){
+    Console.WriteLine(Reverse("Hello World"));
+  }
+}
