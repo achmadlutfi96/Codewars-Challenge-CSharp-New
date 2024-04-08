5ff607a78e6ecdcc7033219fcc29c5eddddf12c3
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-08 12:59:12 +0700

    Create 8kyu\134-Grasshopper-Basic-Function-Fixer

---
+++ 8kyu/134-Grasshopper-Basic-Function-Fixer/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+  public static int AddFive(int num)
+  {
+    int total = num + 5;
+    return total;
+  }
+
+  public static void Main(){
+    Console.WriteLine(AddFive(10));
+  }
+}
