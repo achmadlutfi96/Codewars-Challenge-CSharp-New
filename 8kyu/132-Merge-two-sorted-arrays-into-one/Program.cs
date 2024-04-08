0f1ba10f7569a4fcf7e8855b6b34ea9b11066434
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-08 12:50:54 +0700

    Create 8kyu\132-Merge-two-sorted-arrays-into-on

---
+++ 8kyu/132-Merge-two-sorted-arrays-into-one/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Kata
+{
+  public static int[] MergeArrays(int[] arr1, int[] arr2)
+  {
+    int[] arr3 = arr1.Union(arr2).ToArray();
+    Array.Sort(arr3);
+    return arr3;
+    // Alternative
+    // return arr1.Union(arr2).OrderBy(i => i).ToArray();
+    // return arr1.Concat(arr2).Distinct().OrderBy(x => x).ToArray();
+  }
+  public static void Main(){
+    foreach(var item in MergeArrays(new[] {1, 2, 3, 4}, new[] {5, 6, 7, 8})){
+        Console.WriteLine(item);
+    }
+  }
+}
