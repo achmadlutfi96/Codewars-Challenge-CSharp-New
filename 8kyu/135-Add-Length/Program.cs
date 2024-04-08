4e1aabb508fd24f95d78a360fededad1a87fc751
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-08 13:05:25 +0700

    Create 8kyu\135-Add-Length

---
+++ 8kyu/135-Add-Length/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string[] AddLength(string str)
+  {
+    // TODO
+    return str.Split(' ').Select(s=>$"{s} {s.Length}").ToArray();
+  }
+  public static void Main(){
+    foreach (var item in AddLength("apple ban"))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
