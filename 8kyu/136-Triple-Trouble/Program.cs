24b410b9aad697efe03c233c1e797faef084e4bc
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-09 14:00:37 +0700

    Create 8kyu\136-Triple-Trouble

---
+++ 8kyu/136-Triple-Trouble/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string TripleTrouble(string one, string two, string three)
+  {
+    //Solution
+    string res = "";
+    for (int i = 0; i < one.Length; i++)
+    {
+        res = res + one[i] + two[i] + three[i];
+        // res += $"{one[i]}{two[i]}{three[i]}";
+    }
+    return res;
+
+    // Alternative
+    // return string.Concat(one.Select((x,i) => x.ToString() + two[i] + three[i]));
+  }
+  public static void Main(){
+    Console.WriteLine(TripleTrouble("aa","bb","cc"));
+  }
+}
