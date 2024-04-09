c2cea72b1e5c6ee336e188cfaa420fda11363456
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-09 19:39:44 +0700

    Create 8kyu\140-Is-this-my-tail

---
+++ 8kyu/140-Is-this-my-tail/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Kata
+{
+  public static bool CorrectTail(string body, string tail)
+  {
+    string sub = body.Substring(body.Length - (tail.Length));
+    
+    if (sub == tail) return true;
+    else return false;
+
+    // Alternative
+    // return body.EndsWith(tail);
+  }
+  public static void Main(){
+    Console.WriteLine(CorrectTail("Fox", "x"));
+    Console.WriteLine(CorrectTail("Emu", "t"));
+  }
+}
