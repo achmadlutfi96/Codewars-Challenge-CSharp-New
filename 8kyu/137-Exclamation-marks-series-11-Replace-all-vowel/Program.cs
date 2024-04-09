4fc3fde9e6c55422fafe84179743a04866debe57
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-09 14:15:05 +0700

    Create 8kyu\137-Exclamation-marks-series-11-Replace-all-vowel

---
+++ 8kyu/137-Exclamation-marks-series-11-Replace-all-vowel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+  public static string Replace(string s)
+  {
+    // throw new NotImplementedException();   
+    return String.Concat(s.Select(x=>"aiueo".Contains(char.ToLower(x))?'!':x));
+
+    // Alternative
+    // return Regex.Replace(s, "[aeiouAEIOU]", "!");
+  }
+  public static void Main(){
+    Console.WriteLine(Replace("ABCDE"));
+  }
+}
