237f79adfef480ea6404eb7052d238096d6c8363
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-10 13:03:51 +0700

    Create 8kyu\145-FIXME-Replace-all-dots

---
+++ 8kyu/145-FIXME-Replace-all-dots/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+public class Kata
+{
+  public static string ReplaceDots(string str)
+  {
+    // return new Regex(".").Replace(str, "-");
+    return str.Replace('.', '-');
+    // return new Regex(@"\.").Replace(str, "-");
+  }
+
+  public static void Main(){
+    Console.WriteLine(ReplaceDots("one.two.three"));
+  }
+}
