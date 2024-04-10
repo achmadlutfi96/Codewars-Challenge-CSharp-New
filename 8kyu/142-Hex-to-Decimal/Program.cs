8cad753054bdbd83aecd84ca8ddbc4cb4cea43fb
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-10 12:25:28 +0700

    Create 8kyu\142-Hex-to-Decimal

---
+++ 8kyu/142-Hex-to-Decimal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Kata
+{
+  public static int HexToDec(string hexString)
+  {
+    return hexString.Contains('-') ? -Convert.ToInt32(hexString.Replace("-",""), 16) : Convert.ToInt32(hexString, 16);
+
+    // Alternative
+    // return Convert.ToInt32(hexString.TrimStart('-'), 16) * (hexString[0] == '-' ? -1 : 1);
+  }
+
+  public static void Main(){
+    Console.WriteLine(HexToDec("-C"));
+  }
+}
