72fc76f5b3aa1b236641e165119dc9a0ffa5eba4
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-10 12:14:54 +0700

    Create 8kyu\141-Generate-range-of-integers

---
+++ 8kyu/141-Generate-range-of-integers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+  public static int[] GenerateRange(int min, int max, int step)
+  {
+    // your code goes here
+    // return Enumerable.Range(0, (max-min)/step + 1).Select(x => min + x*step).ToArray();
+    List<int> numbers = new List<int>();
+    for (int i = min; i <= max; i+=step)
+    {
+        numbers.Add(i);
+    }
+    return numbers.ToArray();
+  }
+  public static void Main(){
+    foreach(var item in GenerateRange(-10, 1, 1)){
+        Console.WriteLine(item);
+    }
+  }
+}
