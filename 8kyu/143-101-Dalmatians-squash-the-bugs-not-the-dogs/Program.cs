be355d2054e63c76adf8bd6af2d55a6bc856b895
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-10 12:37:58 +0700

    Create 8kyu\143-101-Dalmatians-squash-the-bugs-not-the-dogs

---
+++ 8kyu/143-101-Dalmatians-squash-the-bugs-not-the-dogs/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+    public static string HowManyDalmatians(int n)
+    {
+        List<string> dogs = new List<string>()
+            {
+                "Hardly any",
+                "More than a handful!",
+                "Woah that's a lot of dogs!",
+                "101 DALMATIONS!!!"
+            };
+        string respond =  (n <= 10) ? dogs[0] : (n <= 50) ? dogs[1] : (n == 101) ? dogs[3] : dogs[2];
+        return respond;
+    }
+    public static void Main(){
+        Console.WriteLine(HowManyDalmatians(26));
+    }
+}
