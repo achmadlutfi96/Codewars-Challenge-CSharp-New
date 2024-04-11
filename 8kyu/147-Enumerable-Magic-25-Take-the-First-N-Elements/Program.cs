4d9a88a68e777492c1412bed5247d4aaf370717d
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-12 00:14:44 +0700

    Create 8kyu\147-Enumerable-Magic-25-Take-the-First-N

---
+++ 8kyu/147-Enumerable-Magic-25-Take-the-First-N-Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int[] Take(int[] arr, int n)
+    {
+        return arr.Take(n).ToArray();
+    }
+
+    public static void Main(){
+        foreach (var item in Take(new int[] { 0, 1, 2, 3, 5, 8, 13 }, 3))
+        {
+            Console.WriteLine(item);
+        }
+
+        foreach (var item in Take(new int[] { 0, 1, 2, 3, 5, 8, 13 }, 0))
+        {
+            Console.WriteLine(item);
+        }
+
+        foreach (var item in Take(new int[] { }, 3))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
