cc02a3f4ad35e11ef847393ea261f0bde84617ce
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-11 23:09:16 +0700

    Create 8kyu\146-Remove-duplicates-from-list

---
+++ 8kyu/146-Remove-duplicates-from-list/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static int[] distinct(int[] a)
+    {
+      return a.Distinct().ToArray();
+    }
+
+    public static void Main(string[] args){
+        foreach (var item in distinct(new int[]{1,1,2}))
+        {
+            Console.WriteLine(item);
+        }
+    }
+  }
+}
