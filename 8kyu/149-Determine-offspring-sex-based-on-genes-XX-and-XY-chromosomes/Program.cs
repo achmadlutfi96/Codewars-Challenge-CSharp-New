e95b5b8345739ffedb89d04e3915d9b594bf0001
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-15 11:01:53 +0700

    Create 8kyu\149-Determine-offspring-se

---
+++ 8kyu/149-Determine-offspring-sex-based-on-genes-XX-and-XY-chromosomes/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static string ChromosomeCheck(string sperm)
+  {
+    return sperm.Equals("XY") ? "Congratulations! You're going to have a son.": "Congratulations! You're going to have a daughter.";
+  }
+
+  public static void Main(){
+    Console.WriteLine(ChromosomeCheck("XY"));
+  }
+}
