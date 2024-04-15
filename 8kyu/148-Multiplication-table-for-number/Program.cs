cf45f03c572899825d1b4b99f2fb9828899c9be0
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-15 10:48:04 +0700

    Create 8kyu\148-Multiplication-table-for-number

---
+++ 8kyu/148-Multiplication-table-for-number/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string MultiTable(int number)
+    {
+        return String.Join("\n",Enumerable.Range(1, 10).Select(i => $"{i} * {number} = {i*number}"));
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(MultiTable(5));
+    }
+}
