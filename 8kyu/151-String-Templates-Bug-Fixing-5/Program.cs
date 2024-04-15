f10b971e632643cf87e202b12c88d97e4cd2d54b
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-15 11:28:51 +0700

    Create 8kyu\151-String-Templates-Bug-Fixing-5

---
+++ 8kyu/151-String-Templates-Bug-Fixing-5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Kata 
+{
+    public static string buildString(string[] args)
+    {
+      return String.Format("I like {0}!", String.Join(", ", args));
+    }
+
+    public static void Main(){
+        Console.WriteLine(buildString(new string[] {"Cheese","Milk","Chocolate"}));
+    }
+}
