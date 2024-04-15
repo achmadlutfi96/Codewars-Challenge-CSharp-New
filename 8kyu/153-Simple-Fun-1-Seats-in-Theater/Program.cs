51151989860a05d6599ad16c6c75b71e37789030
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-15 16:13:37 +0700

    Create 8kyu\153-Simple-Fun-1-Seats-in-Theater

---
+++ 8kyu/153-Simple-Fun-1-Seats-in-Theater/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Kata
+{
+  public static int SeatsInTheater(int nCols, int nRows, int col, int row)
+  {
+    //coding and coding...
+    return (nCols-(col-1)) * (nRows-row);
+  }
+  public static void Main(){
+    Console.WriteLine(SeatsInTheater(16, 11, 5, 3));
+    Console.WriteLine(SeatsInTheater(1, 1, 1, 1));
+    Console.WriteLine(SeatsInTheater(13, 6, 8, 3));
+  }
+}
