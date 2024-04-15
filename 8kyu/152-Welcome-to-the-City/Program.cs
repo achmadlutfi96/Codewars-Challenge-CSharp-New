bfefe679ddc600ca910a8805441d42cd50f9a3f0
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-15 11:37:35 +0700

    Create 8kyu\152-Welcome-to-the-City

---
+++ 8kyu/152-Welcome-to-the-City/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Kata
+{
+  public static string SayHello(string[] name, string city, string state)
+  {
+    // return $"Hello, {string.Join(' ', name)}! Welcome to {city}, {state}!";
+    return string.Format("Hello, {0}! Welcome to {1}, {2}!", string.Join(' ', name), city, state);
+  }
+  public static void Main(){
+    Console.WriteLine(SayHello(new string[] {"John", "Smith"}, "Phoenix", "Arizona"));
+  }
+}
