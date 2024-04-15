986bd3ac36b0f5967611f7475433b8b42274a11e
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-15 11:18:54 +0700

    Create 8kyu\150-Gravity-Flip

---
+++ 8kyu/150-Gravity-Flip/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static int[] Flip(char dir, int[] arr)
+    {
+        Array.Sort(arr);
+        if(dir == 'L') Array.Reverse(arr);        
+        return arr;
+
+        // Alternative
+        // return dir == 'R' ? arr.OrderBy(x=>x).ToArray() : arr.OrderByDescending(x=>x).ToArray();
+    }
+    public static void Main(){
+        foreach (var item in Flip('R', new int[]{ 3,2,1,2 }))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
