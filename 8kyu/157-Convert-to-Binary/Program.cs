1213b28341f8fd036457c52aad78b1898cfdfda2
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-16 23:08:52 +0700

    Create 8kyu\157-Convert-to-Binary

---
+++ 8kyu/157-Convert-to-Binary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int ToBinary(int n)
+  {
+    // throw new NotImplementedException();
+    return Convert.ToInt32(Convert.ToString(n, 2));
+
+    // Alternative
+    // int q=n,tmp=0,m=1;
+    // while(q!=0)
+    // {
+    //   tmp+=q%2*m;
+    //   q=q/2;
+    //   m*=10;
+    // }
+    // return tmp;
+
+  }
+
+  public static void Main(){
+    Console.WriteLine(ToBinary(5));
+  }
+}
