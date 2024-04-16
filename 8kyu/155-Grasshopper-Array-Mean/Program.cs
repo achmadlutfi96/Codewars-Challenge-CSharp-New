72e4865e76cee284e4949f06896d21abab466f81
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-16 22:17:29 +0700

    Create 8kyu\155-Grasshopper-Array-Mean

---
+++ 8kyu/155-Grasshopper-Array-Mean/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class GrassHopper
+{
+    public static int FindAverage(int[] nums)
+    {
+        // your code
+        return (int) nums.Average(x => x);
+    }
+    public static void Main(string[] args){
+        Console.WriteLine(FindAverage(new[] { 1, 3, 5, 7 }));
+    }
+}
