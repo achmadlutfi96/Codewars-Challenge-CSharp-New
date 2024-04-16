de1033968e8ce40627d4808f330d5a42ae7e5feb
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-16 22:45:20 +0700

    Create 8kyu\156-Holiday-VIII-Duty-Free

---
+++ 8kyu/156-Holiday-VIII-Duty-Free/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Kata
+{
+    public static int DutyFree(int normPrice, int Discount, int hol)
+    {
+        double d =(double) Discount/100;
+        double p = normPrice * d;
+        return (int) Math.Floor(hol/p);
+
+        // Alternative
+        // return (int)(hol / (normPrice * (Discount / 100f)));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(DutyFree(12, 50, 1000));
+        Console.WriteLine(DutyFree(17, 10, 500));
+        Console.WriteLine(DutyFree(24, 35, 3000));
+    }
+}
