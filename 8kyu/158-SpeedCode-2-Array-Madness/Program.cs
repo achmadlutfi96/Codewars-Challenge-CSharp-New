318fb3bd0923c1fa58d7a4f451d2b210a4ee8ee4
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-17 18:59:53 +0700

    Create 8kyu\158-SpeedCode-2-Array-Madness

---
+++ 8kyu/158-SpeedCode-2-Array-Madness/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System;
+public class Kata
+{
+  public static bool ArrayMadness(int[] a, int[] b)
+  {
+    // Ready, get, set, GO!!!
+    return a.Select(i=>Math.Pow(i, 2)).Sum() > b.Select(i=>Math.Pow(i, 3)).Sum();
+
+    // Alternative
+    // return a.Sum(n => n * n) > b.Sum(n => n * n * n);
+  }
+  public static void Main(){
+    Console.WriteLine(ArrayMadness(new int[] {4, 5, 6}, new int[] {1, 2, 3}));
+  }
+}
