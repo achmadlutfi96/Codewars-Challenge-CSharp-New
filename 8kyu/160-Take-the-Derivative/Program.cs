1c458c62bd8f352246ac473521e7a16c1ddad804
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-17 19:17:31 +0700

    Create 8kyu\160-Take-the-Derivative

---
+++ 8kyu/160-Take-the-Derivative/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static string Derive(double coefficient, double exponent)
+  {
+    return $"{coefficient*exponent}x^{exponent-1}";
+  }
+
+  public static void Main(){
+    Console.WriteLine(Derive(7, 8));
+  }
+}
