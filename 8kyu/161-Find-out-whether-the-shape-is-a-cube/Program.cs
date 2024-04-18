0463f58eb09c7f32048068ad1c7ddf3351be5555
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-18 08:51:26 +0700

    Create 8kyu\161-Find-out-whether-the-shape-is-a-cube

---
+++ 8kyu/161-Find-out-whether-the-shape-is-a-cube/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CubeCheck
+{
+    using System;
+    public class CubeChecker
+    {
+        public static bool IsCube(double volume, double side)
+        {
+            return volume == Math.Pow(side, 3) && (volume > 0 || side > 0);
+        }
+        public static void Main()
+        {
+            Console.WriteLine(IsCube(8, 2));
+            Console.WriteLine(IsCube(1, 1));
+            Console.WriteLine(IsCube(200, 4));
+            Console.WriteLine(IsCube(-8, -2));
+      }
+    }
+}
