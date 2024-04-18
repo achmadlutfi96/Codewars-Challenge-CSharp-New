97121fbc064996cd573b3747d864c9540947c674
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-18 09:48:07 +0700

    Create 8kyu\162-Dollars-and-Cents

---
+++ 8kyu/162-Dollars-and-Cents/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+public class Kata
+{
+  public static string FormatMoney(double amount)
+  {
+    // your formatting code 
+    // return String.Format(new CultureInfo("en-US", false), "{0:c}", amount);
+    return String.Format("${0:f2}", amount);
+    
+    // Alternative
+    // return $"${amount:F2}";
+    // return amount.ToString("$0.00");
+
+    // Another
+    // NumberFormatInfo nfi = new CultureInfo("en-US", false).NumberFormat;
+    // nfi.CurrencyGroupSeparator = "";
+    // return string.Format(nfi, "{0:C}", amount);
+  }
+
+  public static void Main(){
+    Console.WriteLine(FormatMoney(39.99));
+    Console.WriteLine(FormatMoney(3.1));
+    Console.WriteLine(FormatMoney(3));
+  }
+}
