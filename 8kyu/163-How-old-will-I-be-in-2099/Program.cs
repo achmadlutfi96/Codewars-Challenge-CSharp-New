bc27801d70a8bd1976156cd7b7b9b35d355f4f52
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-18 10:15:19 +0700

    Create 8kyu\163-How-old-will-I-be-in-2099

---
+++ 8kyu/163-How-old-will-I-be-in-2099/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public static class AgeDiff
+{
+    public static string CalculateAge(int birth, int yearTo)
+    {
+        int age = yearTo-birth;
+        if(age == 0){
+            return "You were born this very year!";
+        }else if(age > 0){
+            string plural = age==1? "year": "years";
+            return $"You are {age} {plural} old.";
+        }else{
+            age = Math.Abs(age);
+            string plural = age==1? "year": "years";
+            return $"You will be born in {age} {plural}.";
+        }
+        
+        // Alternative
+        // return (yearTo - birth) switch
+        // {
+        //     0 => "You were born this very year!",
+        //     1 => "You are 1 year old.",
+        //     >1 => $"You are {yearTo - birth} years old.",
+        //     -1 => "You will be born in 1 year.",
+        //     _ => $"You will be born in {birth - yearTo} years."
+        // };
+    }
+    public static void Main()
+    {
+        Console.WriteLine(CalculateAge(2019, 2020));
+    }
+}
