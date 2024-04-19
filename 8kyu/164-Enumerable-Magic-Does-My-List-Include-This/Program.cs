7bd5755e29678c2629afe57b00005cd1f2cc38bb
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-19 12:01:39 +0700

    Create 8kyu\164-Enumerable-Magic-Does-My-List-Include-This

---
+++ 8kyu/164-Enumerable-Magic-Does-My-List-Include-This/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static bool Include(int[] arr, int item)
+  {
+    return arr.Contains(item);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Include(new int[] { 1,2,3,4 }, 3));
+  }
+}
