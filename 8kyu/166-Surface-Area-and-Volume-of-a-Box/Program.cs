8306ee4921b77af4071b7c6115b9282a44a67dd3
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-19 12:25:45 +0700

    Create 8kyu\166-Surface-Area-and-Volume-of-a-Bo

---
+++ 8kyu/166-Surface-Area-and-Volume-of-a-Box/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Kata
+{
+    public static int[] Get_size(int w, int h, int d)
+    {
+        //Your code
+        return new int[] { 2*(w*h+w*d+h*d), w*h*d};
+    }
+
+    public static void Main(){
+        foreach (var item in Get_size(4, 2, 6))
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
