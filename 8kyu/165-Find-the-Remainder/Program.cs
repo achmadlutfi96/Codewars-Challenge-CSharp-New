979f5ea9ec99a912da0f49e7d39b5396d2aec922
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-19 12:12:55 +0700

    Create 8kyu\165-Find-the-Remainder

---
+++ 8kyu/165-Find-the-Remainder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Kata
+{
+  public static int Remainder(int a, int b)
+  {
+    // throw new NotImplementedException();
+    return a >= b ? a%b : b%a;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Remainder(17, 5));
+  }
+}
