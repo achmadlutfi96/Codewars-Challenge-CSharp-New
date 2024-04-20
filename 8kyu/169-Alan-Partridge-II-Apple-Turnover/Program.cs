9f3725d97a9171c0870849f87a98820484c511e3
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-20 14:40:18 +0700

    Create 8kyu\169-Alan-Partridge-II-Apple-Turnover

---
+++ 8kyu/169-Alan-Partridge-II-Apple-Turnover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Kata
+{
+  public static string Apple(object n)
+  {
+    return Math.Pow(Convert.ToInt32(n), 2)  > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox.";
+  }
+
+  public static void Main(){
+    Console.WriteLine(Apple("50"));
+  }
+}
