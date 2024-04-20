0ddf21913562cd0870b47fbd9a8a277cc1963914
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-20 14:30:17 +0700

    Create 8kyu\168-L1-Bartender-drinks

---
+++ 8kyu/168-L1-Bartender-drinks/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Kata
+{
+    public static string GetDrinkByProfession(string p)
+    {
+        return p.ToLower() switch
+        {
+            "jabroni"           => "Patron Tequila",
+            "school counselor"  => "Anything with Alcohol",
+            "programmer"        => "Hipster Craft Beer",
+            "bike gang member"  => "Moonshine",
+            "politician"        => "Your tax dollars",
+            "rapper"            => "Cristal",
+            _                   => "Beer"
+        };
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GetDrinkByProfession("bike ganG member"));
+    }
+}
