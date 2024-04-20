e0b95230e573778832817a77bf04b7060b4b337a
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-20 14:17:47 +0700

    Create 8kyu\167-How-many-lightsabers-do-you-own

---
+++ 8kyu/167-How-many-lightsabers-do-you-own/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Kata
+{
+  public static int HowManyLightsabersDoYouOwn(string name)
+  {
+    //Code goes here
+    return name.Equals("Zach") ? 18 : 0;
+  }
+
+  public static void Main(){
+    Console.WriteLine(HowManyLightsabersDoYouOwn("Zach"));
+  }
+}
