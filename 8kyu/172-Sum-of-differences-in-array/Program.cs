f40f96dd493d96a7d37328bb6fc791508b2a5453
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-21 10:40:10 +0700

    Create 8kyu\172-Sum-of-differences-in-array

---
+++ 8kyu/172-Sum-of-differences-in-array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int SumOfDifferences(int[] arr)
+  {
+    return arr.Length == 0 ? 0 : arr.Max()-arr.Min();
+
+    // Alternative 1
+    // return arr.Any() ? arr.Max() - arr.Min() : 0;
+  }
+
+  public static void Main(){
+    Console.WriteLine(SumOfDifferences(new int[] { 25, 24, -25, 2 }));
+  }
+}
