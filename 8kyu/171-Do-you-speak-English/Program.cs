7cb79122031f7821af45084803728806d449d121
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-21 10:24:25 +0700

    Create 8kyu\171-Do-you-speak-English

---
+++ 8kyu/171-Do-you-speak-English/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Solution
+{
+    static class Kata
+    {
+        public static bool SpeakEnglish(string sentence)
+        {
+            // Good luck!;
+            return sentence.ToLower().Contains("english");
+
+            // Alternative 1
+            // sentence.Contains("english", System.StringComparison.CurrentCultureIgnoreCase);
+
+            // Alternative 2
+            // return Regex.IsMatch(sentence, "(?i)english");
+        }
+
+        public static void Main(){
+            Console.WriteLine(SpeakEnglish("I speak English"));
+            Console.WriteLine(SpeakEnglish("I speak English"));
+        }
+    }
+}
