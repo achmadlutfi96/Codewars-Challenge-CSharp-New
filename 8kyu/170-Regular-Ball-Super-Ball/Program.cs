9ce8cc105674ffc6c76d1eb507f120498163ba40
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-21 10:07:58 +0700

    Create 8kyu\170-Regular-Ball-Super-Ball

---
+++ 8kyu/170-Regular-Ball-Super-Ball/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Ball {
+  public string ballType { get; set; }
+  
+  public Ball(string ballType = "regular") {
+    // your code goes here
+    this.ballType = ballType;
+  }
+
+}
+
+public class Program{
+    public static void Main(string[] args) {
+        Console.WriteLine(new Ball().ballType);
+        Console.WriteLine(new Ball("super").ballType);
+    }
+}
