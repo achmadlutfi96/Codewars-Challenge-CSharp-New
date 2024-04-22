8850bab43576a7e017636dc2982b5dcd8678de4e
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-22 10:20:20 +0700

    Create 8kyu\173-Return-the-day

---
+++ 8kyu/173-Return-the-day/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Kata
+{
+  public static string WhatDay(int n)
+  {
+    // your code
+    return n switch{
+        1 => "Sunday",
+        2 => "Monday",
+        3 => "Tuesday",
+        4 => "Wednesday",
+        5 => "Thursday",
+        6 => "Friday",
+        7 => "Saturday",
+        _ => "Wrong, please enter a number between 1 and 7"
+    };
+  }
+
+  public static void Main(){
+    Console.WriteLine(WhatDay(6));
+  }
+}
