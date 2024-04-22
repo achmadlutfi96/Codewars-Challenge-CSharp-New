76f9a95f8164c8ae33e8a80724b58ca39e22c3a9
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-22 11:25:02 +0700

    Create 8kyu\174-Sum-of-Multiples

---
+++ 8kyu/174-Sum-of-Multiples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System;
+public class Kata
+{
+  public static int SumMul(int n, int m)
+  {
+    // your idea here
+    if(n>=m || n < 1) throw new ArgumentException();
+    return Enumerable.Range(0, m-n).Select(x=>n+x).Where(i=>i%n==0).Sum();
+
+    // ALternative
+    // return Enumerable.Range(n, m - n).Where(x => x % n == 0).Sum();
+  }
+
+  public static void Main(){
+    Console.WriteLine(SumMul(5, 20));
+    Console.WriteLine(SumMul(2, 9));
+   Console.WriteLine(SumMul(9, 2));
+  }
+}
