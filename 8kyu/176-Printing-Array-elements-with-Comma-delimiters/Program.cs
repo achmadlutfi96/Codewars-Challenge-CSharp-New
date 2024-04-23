c05ad35de75e0b7df837a17ea255db71aa41ff22
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-23 14:39:30 +0700

    Create 8kyu\176-Printing-Array-elements-with-Comma-delimit

---
+++ 8kyu/176-Printing-Array-elements-with-Comma-delimiters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static string PrintArray(object[] array)
+  {
+    return string.Join(",",array.Select(x=>x is Object[]?PrintArray(x as object[]):x));
+    //Atau
+    // return string.Join(",", array.Select(a => a.GetType().IsArray ? string.Join(",", (object[])a) : a));
+  }
+
+  public static void Main(){
+    Console.WriteLine(PrintArray(new object[] { 2, 4, 5, 2 }));
+    Console.WriteLine(PrintArray(new object[]{new object[]{"hello","this","is","an","array!"}, new object[]{"a","b","c","d","e!"}}));
+  }
+}
