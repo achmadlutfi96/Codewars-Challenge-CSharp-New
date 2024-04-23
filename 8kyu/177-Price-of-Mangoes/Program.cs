d117aa15b61c678f2915c10c6ce6ae80594f6cec
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-23 15:05:16 +0700

    Create 8kyu\177-Price-of-Mangoes

---
+++ 8kyu/177-Price-of-Mangoes/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static int Mango(int quantity, int price)
+  {
+    return (quantity-(quantity/3))*price;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Mango(9, 5));
+  }
+}
