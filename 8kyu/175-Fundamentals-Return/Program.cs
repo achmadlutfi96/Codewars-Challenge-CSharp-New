75acadec5a3a5be2b7bf13caff5dda472b961eb0
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-23 11:47:53 +0700

    Create 8kyu\175-Fundamentals-Return

---
+++ 8kyu/175-Fundamentals-Return/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public static class Kata
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Multiply(int a, int b){
+        return a * b;
+    }
+
+    public static int Divide(int a, int b){
+        return a / b;
+    }
+
+    public static int Mod(int a, int b){
+        return a % b;
+    }
+
+    public static int Exponent(int a, int b){
+        return (int) Math.Pow(a, b);
+    }
+
+    public static int Sub(int a, int b){
+        return a - b;
+    }
+    // Add more functions Here. Refer to the description for function names.
+    // The code will NOT WORK IF YOU USE names other than the ones
+    // from the description
+
+}
