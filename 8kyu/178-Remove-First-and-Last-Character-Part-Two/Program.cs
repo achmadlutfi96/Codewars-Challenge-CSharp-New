a05c9cf4f8fd3cda8980e2dc42ca7df6390dcd01
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-24 11:23:03 +0700

    Create 8kyu\178-Remove-First-and-Last-Character-Part-Two

---
+++ 8kyu/178-Remove-First-and-Last-Character-Part-Two/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string? Array(string s)
+  {
+    string[] str= s.Split(',');
+    return str.Length < 3 ? null : string.Join(" ",str.Skip(1).SkipLast(1).ToArray());
+
+    // Alternative
+    // return s.Split(',').Length < 3 ? null : string.Join(" ", s.Split(',')[1..^1]);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Array("1,2,3"));
+    Console.WriteLine(Array("1, 3"));
+  }
+}
