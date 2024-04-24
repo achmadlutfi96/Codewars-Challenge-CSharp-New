3c9d93017cc6caad8dccca2cdf955c5e8a071706
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-24 11:32:35 +0700

    Create 8kyu\179-Find-Nearest-square-numbe

---
+++ 8kyu/179-Find-Nearest-square-number/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int NearestSq(int n)
+  {
+    // throw new NotImplementedException("NearestSq is not implemented.");
+    return (int) Math.Pow(Math.Round(Math.Sqrt(n)), 2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(NearestSq(111));
+  }
+}
