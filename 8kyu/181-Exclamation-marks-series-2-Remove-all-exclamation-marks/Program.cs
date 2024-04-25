509216353647a6e2d0e96f66f7dc4ac1528e0b1e
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-25 15:20:40 +0700

    Create 8kyu\181-Exclamation-marks-series-2-Remo

---
+++ 8kyu/181-Exclamation-marks-series-2-Remove-all-exclamation-marks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string Remove(string s)
+  {
+    //coding and coding...
+    int del = 0;
+    foreach (var item in s.Reverse())
+    {
+        if(item.Equals('!')){
+            del++;
+        }else{
+            break;
+        }
+    }
+    return s.Remove(s.Length - del, del);
+
+    // Alternative
+    // return s.TrimEnd('!'); 
+  }
+
+  public static void Main(){
+    Console.WriteLine(Remove("Hi!!!"));
+  }
+}
