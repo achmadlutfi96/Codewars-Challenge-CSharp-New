b91c4c06070e10961376b51ee4405450f7ddc8e1
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-26 22:19:08 +0700

    Create 8kyu\184-Area-of-a-Square

---
+++ 8kyu/184-Area-of-a-Square/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+  public static double SquareArea(double A)
+  {
+    return Math.Round(Math.Pow(4*A/(2*Math.PI),2),2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(SquareArea(2));
+    Console.WriteLine(SquareArea(14.05));
+  }
+}
