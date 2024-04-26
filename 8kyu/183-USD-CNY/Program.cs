a5cc35a730fe3c0f9e3f76b545d2f9043050e35f
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-26 20:44:44 +0700

    Create 8kyu\183-USD-CNY

---
+++ 8kyu/183-USD-CNY/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+public static class Kata
+{
+    public static string Usdcny(int usd)
+    {
+        return string.Format(new CultureInfo("en-US"), "{0:0.00} Chinese Yuan", usd * 6.75);
+
+        // Alternative 1
+        // return $"{(usd * 6.75f):0.00} Chinese Yuan";
+        // Alternative 2
+        // return $"{usd * 6.75:F} Chinese Yuan";
+    }
+
+    public static void Main(){
+        Console.WriteLine(Usdcny(15));
+        Console.WriteLine(Usdcny(465));
+    }
+}
