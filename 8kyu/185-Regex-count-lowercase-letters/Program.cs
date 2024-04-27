d64966574c44570387c1ebb9f060900135f74fc6
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-27 18:23:56 +0700

    Create 8kyu\185-Regex-count-lowercase-letters\185-Regex-count-lowercase-letters.sln

---
+++ 8kyu/185-Regex-count-lowercase-letters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+public class Kata
+{
+  public static int LowercaseCountCheck(string s)
+  {
+    // your code goes here ...
+    return s.Count(i=>Char.IsLower(i));
+
+    // Alternative 
+    // return Regex.Matches(s, "[a-z]").Count;
+  }
+
+  public static void Main(){
+    Console.WriteLine(LowercaseCountCheck("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"));
+  }
+}
