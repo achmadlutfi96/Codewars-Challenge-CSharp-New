373293824d05c31d322b8cb7b3cd42ef3ac331be
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-27 19:40:31 +0700

    Create 8kyu\186-Simple-validation-of-a-username-with-regex

---
+++ 8kyu/186-Simple-validation-of-a-username-with-regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+  public static bool ValidateUsr(string username) 
+  {
+    // use regex here
+    return Regex.IsMatch(username, @"\A[a-z0-9_]{4,16}\z");
+
+    // Alternative
+    // return Regex.IsMatch(username, "^[a-z0-9_]{4,16}$");
+  }
+
+  public static void Main(){
+    Console.WriteLine(ValidateUsr("asd43_34"));
+    Console.WriteLine(ValidateUsr("asd43 34"));
+  }
+}
