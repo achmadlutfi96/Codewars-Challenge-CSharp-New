686897ab2a4f3ccd664a74b0a7ce467b187f6846
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-27 19:46:25 +0700

    Create 8kyu\187-Grasshopper-Combine-strings

---
+++ 8kyu/187-Grasshopper-Combine-strings/Program.cs
@@ -0,0 +1,8 @@
+﻿class Kata {
+    public static string CombineNames(string a, string b){
+        return $"{a} {b}";
+    }
+    public static void Main(String[] args){
+        Console.WriteLine(CombineNames("James", "Stevens"));
+    }
+}
