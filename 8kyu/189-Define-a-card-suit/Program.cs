5b09701d226b04af59bc029491f9461179b3aea5
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-28 12:24:31 +0700

    Create 8kyu\189-Define-a-card-suit

---
+++ 8kyu/189-Define-a-card-suit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    public partial class Kata
+    {
+        public static string DefineSuit(string card)
+        {
+            return card.Last() switch
+            {
+                '♣' => "clubs",
+                '♦' => "diamonds",
+                '♥' => "hearts",
+                '♠' => "spades",
+                _ => ""
+            };
+
+            // Alternative
+            // return card[^1] switch
+            // {
+            //     '♣' => "clubs",
+            //     '♦' => "diamonds",
+            //     '♥' => "hearts",
+            //     '♠' => "spades"
+            // };
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(DefineSuit("3♣"));
+        }
+    }
+}
