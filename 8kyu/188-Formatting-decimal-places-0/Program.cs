87661dedf04ea88d17ed0f8c7d5dbd1723d0bb66
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-28 12:05:25 +0700

    Create 8kyu\188-Formatting-decimal-places-0

---
+++ 8kyu/188-Formatting-decimal-places-0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Numbers
+{
+  public static double TwoDecimalPlaces(double number)
+  {
+    //Write your code here
+    return Math.Round(number,2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(TwoDecimalPlaces(4.659725356));
+  }
+}
