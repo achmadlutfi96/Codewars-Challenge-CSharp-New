bf04c55490cb8e652a3dd6c2f605efca332c3084
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-29 12:46:26 +0700

    Create 8kyu\193-Exclamation-marks-series-6-Remov

---
+++ 8kyu/193-Exclamation-marks-series-6-Remove-n-exclamation-marks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+public class Kata
+{
+  public static string Remove(string s, int n)
+  {
+    //coding and coding....
+    List<char> lists = new List<char>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if(s[i] == '!' && n > 0){
+            n--;
+            continue;
+        }
+        lists.Add(s[i]);
+    }
+    return string.Concat(lists);
+
+    // Alternative 1
+    // return String.Concat(s.Where(x => !(x == '!' && 0 < n--)))
+    
+    // Alternative 2
+    // new Regex("!").Replace(s, "", n);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Remove("!!!Hi !!hi!!! !hi", 5));
+  }
+}
