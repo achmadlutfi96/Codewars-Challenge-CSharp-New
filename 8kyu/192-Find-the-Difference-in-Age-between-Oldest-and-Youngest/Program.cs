66803116f677f351c39ef8cee829b5d467649402
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-29 11:47:28 +0700

    Create 8kyu\192-Find-the-Differen

---
+++ 8kyu/192-Find-the-Difference-in-Age-between-Oldest-and-Youngest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int[] DifferenceInAges(int[] ages)
+  {
+    return new int[] { ages.Min(), ages.Max(), ages.Max()-ages.Min()};
+  }
+
+  public static void Main(){
+    foreach (var item in DifferenceInAges(new int[] {82, 15, 6, 38, 35}))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
