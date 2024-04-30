61f972b4bb7fd9d105abe91b0ffb4e22f3520030
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-30 14:06:00 +0700

    Create 8kyu\194-BASIC-Making-Six-Toast

---
+++ 8kyu/194-BASIC-Making-Six-Toast/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Kata
+{
+  public static int SixToast(int num)
+  {
+    // TODO: Write this function to the provided spec
+    return Math.Abs(num-6);
+  }
+
+  public static void Main(){
+    Console.WriteLine(SixToast(17));
+  }
+}
