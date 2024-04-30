e91bddead8fe67891547e7bb6d0d9cd8e9f0883d
Achmad Lutfi <alhacker1996@gmail.com>
2024-04-30 14:18:37 +0700

    Create 8kyu\195-simple-calculator

---
+++ 8kyu/195-simple-calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Kata
+{
+  public static double Calculator(double a, double b, char op)
+  {
+    return op switch {
+        '+' => a+b,
+        '*' => a*b,
+        '/' => a/b,
+        '-' => a-b,
+        _ => throw new ArgumentException()
+    };
+  }
+
+  public static void Main(){
+    Console.WriteLine(Calculator(6, 2, '+'));
+    Console.WriteLine(Calculator(6, 2, '$'));
+  }
+}
