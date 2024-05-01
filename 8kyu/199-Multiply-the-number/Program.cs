bbcbdbc7cdcee4fa1abd4b5ba9bced9f7d773576
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-01 21:01:53 +0700

    Create 8kyu\199-Multiply-the-number

---
+++ 8kyu/199-Multiply-the-number/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Kata
+{
+  public static int Multiply(int number)
+  {
+    //your code here
+    return (int)(number * Math.Pow(5, Math.Abs(number).ToString().Length));
+  }
+  public static void Main(){
+    Console.WriteLine(Multiply(200));
+    Console.WriteLine(Multiply(-2));
+  }
+}
