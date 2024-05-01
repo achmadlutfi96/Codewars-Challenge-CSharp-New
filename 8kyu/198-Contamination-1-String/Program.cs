64876793aca3e29d8161645f8a646c804df1636e
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-01 20:33:45 +0700

    Create 8kyu\198-Contamination-1-String

---
+++ 8kyu/198-Contamination-1-String/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static string Contamination(string text, string character)
+  {
+    // Code here ;)
+    return String.Concat(text.Select(s => character));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Contamination("abc", "z"));
+  }
+}
