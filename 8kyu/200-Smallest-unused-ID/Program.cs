21cb32ee1bdc0ae429bf443ff25c23f770697f13
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-02 13:12:31 +0700

    Create 8kyu\200-Smallest-unused-ID

---
+++ 8kyu/200-Smallest-unused-ID/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int NextId(int[] ids)
+    {
+        //this will be awesome!
+        var x = 0;
+        while (ids.Contains(x)) x++;
+        return x;
+
+        // ALternative
+        // return Enumerable.Range(0,ids.Length+1).Except(ids).Min();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(NextId(new int[] { 9, 9, 8 }));
+    }
+}
