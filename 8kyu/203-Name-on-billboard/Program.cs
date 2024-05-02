a42522a6cef07d303c295f74819a59800f59f585
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-02 13:48:50 +0700

    Create 8kyu\203-Name-on-billboard

---
+++ 8kyu/203-Name-on-billboard/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+public class Kata {
+  public static double Billboard(string name, double price = 30) {
+    double result = 0;
+    for (int i = 0; i < name.Length; i++)
+    {
+         result+=price; 
+    }
+    return result;
+
+    // Alternative
+    // return name.Sum(_ => price);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Billboard("Jeong-Ho Aristotelis"));
+  }
+}
