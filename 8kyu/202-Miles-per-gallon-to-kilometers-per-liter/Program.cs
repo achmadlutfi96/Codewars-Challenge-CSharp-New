4a8cdeca76abba75506f31ee929e722a6d208d6b
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-02 13:23:40 +0700

    Create 8kyu\202-Miles-per-gallon-to-kilometers-per-liter

---
+++ 8kyu/202-Miles-per-gallon-to-kilometers-per-liter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public static class Kata
+{
+	public static double Converter(int mpg)
+	{
+		// do your magic
+        return Math.Round(mpg * (1.609344/4.54609188), 2);
+	}
+
+    public static void Main(){
+        Console.WriteLine(Converter(12));
+    }
+}
