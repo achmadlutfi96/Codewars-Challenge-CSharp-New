815e12ed37678abbb7010ae79c4677c1fb401269
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-03 15:05:26 +0700

    Create 8kyu\207-Are-arrow-functions-odd

---
+++ 8kyu/207-Are-arrow-functions-odd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static List<int> Odds(List<int> values) =>
+      // arrow it
+      values.Where(v => v%2 == 1)
+            .ToList();
+
+    public static void Main(){
+        foreach (var item in Odds(new List<int> {1, 2, 3, 4, 5, 6}))
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
