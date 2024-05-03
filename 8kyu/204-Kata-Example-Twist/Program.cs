6ecf6f4dfa8ee7723902ff8c65f11d249819b437
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-03 09:17:12 +0700

    Create 8kyu\204-Kata-Example-Twist

---
+++ 8kyu/204-Kata-Example-Twist/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata 
+{
+  public static string[] Websites = Enumerable.Repeat("codewars", 1000).ToArray();
+
+
+  public static void Main(){
+    foreach (var item in Websites)
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
