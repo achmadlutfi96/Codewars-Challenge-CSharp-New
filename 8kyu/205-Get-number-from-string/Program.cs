343d2ba364d489cb6a15e1a4f1ec96ea332a3c67
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-03 09:35:19 +0700

    Create 8kyu\205-Get-number-from-string

---
+++ 8kyu/205-Get-number-from-string/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static int getNumberFromString(string s)
+        {
+            return Convert.ToInt32(string.Concat(s.Where(v => Regex.IsMatch(v.ToString(), @"[0-9]")).ToArray()));
+
+            // Alternative 1
+            // return Int32.Parse(Regex.Replace(s, @"[^\d]", ""));
+
+            // Alternative 2
+            // return int.Parse(string.Concat(s.Where(char.IsDigit)));
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(getNumberFromString("this is number: 7"));
+        }
+    }
+}
