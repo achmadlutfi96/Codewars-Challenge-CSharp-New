2b1389fd60fc1a6fbaf74c77b04fe292a38a30da
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-04 11:52:08 +0700

    Create 8kyu\208-Sleigh-Authentication

---
+++ 8kyu/208-Sleigh-Authentication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Sleigh
+{
+  public static bool Authenticate(string name, string password)
+  {
+    // TODO
+    return name.Equals("Santa Claus") && password.Equals("Ho Ho Ho!");
+  }
+
+  public static void Main(){
+    Console.WriteLine(Authenticate("Santa Claus", "Ho Ho Ho!"));
+  }
+}
---
+++ 8kyu/209-Is-there-a-vowel-in-there/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static object[] IsVow(object[] a)
+    {
+        string[] vow = new string[]{"a","i","u","e","o"};
+        return a.Select(v => vow.Contains(Convert.ToChar(v).ToString())? Convert.ToChar(v).ToString() : v).ToArray();
+
+        // Alternative
+        // return a.Select(x => "aeiou".Contains(Convert.ToChar(x)) ? Convert.ToChar(x).ToString() : x).ToArray();
+    }
+
+    public static void Main()
+    {
+        foreach (var item in IsVow(new object[] { 118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106 }))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
