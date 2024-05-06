e65a654911625e4095abe323db22221797c95102
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-06 22:52:01 +0700

    Create 8kyu\213-validate-code-with-simple-regex

---
+++ 8kyu/213-validate-code-with-simple-regex/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static bool ValidateCode(string code)
+    {
+        //use regex here
+        return Regex.IsMatch(code, @"^[1-3]");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(ValidateCode("248"));
+    }
+}
