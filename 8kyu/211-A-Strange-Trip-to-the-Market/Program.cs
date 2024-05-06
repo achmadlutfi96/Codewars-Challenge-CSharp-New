0325b2748841a849483ca90a60370627b790935f
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-06 22:42:45 +0700

    Create 8kyu\211-A-Strange-Trip-to-the-Market

---
+++ 8kyu/211-A-Strange-Trip-to-the-Market/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Kata 
+{
+  public static bool IsLockNessMonster(string sentence) 
+  {
+    // FIND THE LOCH NESS MONSTER. SAVE YOUR TREE FIDDY
+    string str = sentence.ToLower();
+    return str.Contains("tree fiddy") || str.Contains("3.50") || str.Contains("three fifty");
+
+    // Alternative
+    // return sentence.ToUpper().Contains("TREE FIDDY") || sentence.Contains("3.50");
+  }
+
+  public static void Main(){
+    Console.WriteLine("");
+  }
+}
