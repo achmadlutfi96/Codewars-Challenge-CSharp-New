df60c827de0236b1b20f7fda6a10f296543faeb7
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-06 22:46:58 +0700

    Create 8kyu\212-get-ascii-value-of-character

---
+++ 8kyu/212-get-ascii-value-of-character/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int GetASCII(char c)
+  {
+    // throw new NotImplementedException();
+    return Convert.ToByte(c);
+
+    // Alternative
+    // return c;
+    // return (int) c;
+  }
+
+  public static void Main(){
+    Console.WriteLine(GetASCII('A'));
+  }
+}
