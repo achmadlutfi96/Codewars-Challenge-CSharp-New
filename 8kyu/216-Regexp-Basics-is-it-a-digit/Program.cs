0879fbf9bb54bb4164214e50eb511d77e123b2ec
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-07 11:34:38 +0700

    Create 8kyu\216-Regexp-Basics-is-it-a-digit

---
+++ 8kyu/216-Regexp-Basics-is-it-a-digit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static bool Digit(this string s)
+    {
+        return s.Length > 1 ? false : Regex.IsMatch(s, @"\d");
+
+        // Alternative
+        // return Regex.IsMatch(s, @"^\d\z");
+    }
+
+    public static void Main(){
+        Console.WriteLine("a5".Digit());
+        Console.WriteLine("14".Digit());
+        Console.WriteLine("7".Digit());
+    }
+}
