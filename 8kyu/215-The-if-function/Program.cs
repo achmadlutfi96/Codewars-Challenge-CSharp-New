2d670ed7ca0be560fb047c07b7525084e4839351
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-07 11:06:58 +0700

    Create 8kyu\215-The-if-function

---
+++ 8kyu/215-The-if-function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Kata
+{
+  public static void If(bool condition, Action func1, Action func2)
+  {
+    // ...
+    if(condition){
+        func1();
+    }else{
+        func2();
+    }
+
+    // Alternative
+    // (condition ? func1 : func2)();
+  }
+
+  public static void Main(){
+    bool a = true;
+    If(false, () => a = true, () => a = false);
+
+    Console.WriteLine($"{a}");
+  }
+}
