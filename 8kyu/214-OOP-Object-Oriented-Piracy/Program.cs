0481c4da77cc41ae2688c1e17746bf7d303dcb67
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-07 10:49:35 +0700

    Create 8kyu\214-OOP-Object-Oriented-Piracy

---
+++ 8kyu/214-OOP-Object-Oriented-Piracy/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Ship
+{
+  public int Draft;
+  public int Crew;
+  
+  public Ship(int draft, int crew)
+  {
+    Draft = draft;
+    Crew = crew;
+  }
+  
+  // YOUR CODE HERE...
+  public bool IsWorthIt(){
+    return Draft-(Crew*1.5) > 20;
+  }
+}
+
+public class Program{
+    public static void Main(string[] args){
+        Console.WriteLine(new Ship(100, 20).IsWorthIt());
+    }
+}
