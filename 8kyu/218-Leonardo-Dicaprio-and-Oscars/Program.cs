bcd5214d1f60c7835b1a5208e6249c48935b5d30
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-08 11:51:07 +0700

    Create 8kyu\218-Leonardo-Dicaprio-and-Oscar

---
+++ 8kyu/218-Leonardo-Dicaprio-and-Oscars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class Kata
+{
+  public static string Leo(int oscar)
+  {
+    if(oscar == 88){
+        return "Leo finally won the oscar! Leo is happy";
+    }else if(oscar == 86){
+        return "Not even for Wolf of wallstreet?!";
+    }else if(oscar > 88){
+        return "Leo got one already!";
+    }else{
+        return "When will you give Leo an Oscar?";
+    }
+  }
+
+  public static void Main(){
+    Console.WriteLine(Leo(88));
+  }
+}
