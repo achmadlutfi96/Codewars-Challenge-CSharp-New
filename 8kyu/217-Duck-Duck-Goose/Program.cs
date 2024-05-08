2ab38b0753ed3ced3a48337df5ca86dbca05ad44
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-08 11:11:38 +0700

    Create 8kyu\217-Duck-Duck-Goose

---
+++ 8kyu/217-Duck-Duck-Goose/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Kata
+{
+  public static string DuckDuckGoose(Player[] players, int goose)
+  {
+    //Enter code here
+    return players[(goose-1)%players.Length].Name;
+  }
+
+  public static void Main(){
+    var exampleNames = new string[] {"a", "b", "c", "d", "c", "e", "f", "g", "h", "z"};
+    var players = exampleNames.Select(x=>new Player(x)).ToArray();
+    Console.WriteLine(DuckDuckGoose(players,10));
+  }
+}
+public class Player 
+{
+  public string Name {get;set;}
+  
+  public Player (string name) 
+  {
+	  this.Name = name;
+  }
+}
