dd81a753154373a31b0542be09a0dd57bee6ed1c
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-09 10:15:15 +0700

    Create 8kyu\221-UEFA-EURO-2016

---
+++ 8kyu/221-UEFA-EURO-2016/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static string UefaEuro2016(string[] teams, int[] scores)
+  {
+    return scores[0] == scores[1] ? $"At match {teams[0]} - {teams[1]}, teams played draw." : scores[0] > scores[1] ? $"At match {teams[0]} - {teams[1]}, {teams[0]} won!" : $"At match {teams[0]} - {teams[1]}, {teams[1]} won!";
+  }
+
+  public static void Main(){
+    Console.WriteLine(UefaEuro2016(new [] { "Belgium", "Italy"}, new [] { 0, 2 }));
+  }
+}
