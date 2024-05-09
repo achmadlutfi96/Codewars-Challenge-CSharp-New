8a5dafbb85d0b4584f3a42cefe238265aad4102e
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-09 09:59:38 +0700

    Create 8kyu\219-Template-Strings

---
+++ 8kyu/219-Template-Strings/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Templates{
+  public static string TempleStrings(string obj, string feature)
+  {
+      // show me the code!
+      return $"{obj} are {feature}";
+  }
+
+  public static void Main(){
+    Console.WriteLine(TempleStrings("Animals","Good"));
+  }
+}
