d86b814b1289477eca05170bdc9a578e366663d2
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-09 10:06:45 +0700

    Create 8kyu\220-ASCII-Total

---
+++ 8kyu/220-ASCII-Total/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static int UniTotal(string str)
+  {
+    // total up dem unicodes!
+    return str.Sum(x =>(int) x);
+  }
+
+  public static void Main(){
+    Console.WriteLine(UniTotal("aaa"));
+  }
+}
