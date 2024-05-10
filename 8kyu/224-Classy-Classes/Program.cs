08fca5ea789b990a4bfae6f1b63f51a9321423c0
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-10 12:23:34 +0700

    Create 8kyu\224-Classy-Classes

---
+++ 8kyu/224-Classy-Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Person
+{  
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Info{ get{return $"{Name}s age is {Age}"; }}
+
+  public Person(string name, int age)
+  {
+    this.Name = name;
+    this.Age = age;
+  }
+
+}
+
+public class Program{
+    public static void Main(string[] args){
+        Person person = new Person("john",34);
+        Console.WriteLine(person.Info);
+    }
+}
