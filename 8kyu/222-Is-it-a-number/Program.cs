3005005fe251554eb97425c44e5597e4fc5e6f86
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-10 11:48:49 +0700

    Create 8kyu\222-Is-it-a-number

---
+++ 8kyu/222-Is-it-a-number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+public class CodeWars
+{
+  public static bool IsDigit(string s) 
+  {
+    //your code
+    if (s == null) { return false; }
+    try{
+        Convert.ToDouble(s);
+        return true;
+    }catch(Exception e){
+        return false;
+    }
+
+    // Alternative
+    // return double.TryParse(s, out var _);
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsDigit("s2324"));
+    Console.WriteLine(IsDigit(null));
+  }
+}
