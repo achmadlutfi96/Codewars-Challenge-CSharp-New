b8514070828e3c12aba9920477d118674d85f532
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-10 12:13:13 +0700

    Create 8kyu\223-Enumerable-Magic-1-True-for-All

---
+++ 8kyu/223-Enumerable-Magic-1-True-for-All/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static bool All(int[] arr, Func<int, bool> fun)
+  {
+    return arr.Select(fun).All(v=>v == true);
+
+    // Alternative
+    // return arr.All(fun);
+  }
+
+  public static void Main(){
+    Console.WriteLine(All(new int[] { 1,2,3,4,5 }, v => v < 9));
+  }
+}
