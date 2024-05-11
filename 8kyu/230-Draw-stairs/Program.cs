89bc4ace3b1a4d2b81f8e14dd46f11ef58df3234
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-11 23:27:44 +0700

    Create 8kyu\230-Draw-stairs

---
+++ 8kyu/230-Draw-stairs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string DrawStairs(int n)
+  {
+    // Happy coding ;)
+    return string.Join("\n", Enumerable.Repeat("I", n).Select((v,i)=>string.Concat(Enumerable.Repeat(" ",i))+v).ToArray());
+
+    // Alternative 1
+    // return string.Join('\n', Enumerable.Range(1, n).Select("I".PadLeft));
+
+    // Alternative 2
+    // return string.Join("\n ", Enumerable.Repeat("I", n).Select((s, i) => s.PadLeft(i)));
+  }
+
+  public static void Main(){
+    Console.WriteLine(DrawStairs(3));
+  }
+}
