9931d488f866d2c45dfe7551983c6cb9f5cf48ca
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-11 13:38:38 +0700

    Create 8kyu\226-For-UFC-Fans-Total-Beginners

---
+++ 8kyu/226-For-UFC-Fans-Total-Beginners/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+  public static string Quote(string fighter)
+  {
+    // throw new NotImplementedException();
+    return fighter.ToLower() switch 
+    {
+        "george saint pierre"=>"I am not impressed by your performance.",
+        "conor mcgregor" => "I'd like to take this chance to apologize.. To absolutely NOBODY!",
+        _ => ""
+    };
+  }
+
+  public static void Main(){
+    Console.WriteLine(Quote("george saint pierre"));
+  }
+}
