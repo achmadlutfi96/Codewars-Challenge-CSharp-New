c31f14bf8a0227fd5003b098e90eb3fc10b136ba
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-11 17:44:27 +0700

    Create 8kyu\228-Collatz-Conjecture

---
+++ 8kyu/228-Collatz-Conjecture/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Kata
+{
+    public static uint count = 0;
+    public static uint Hotpo(uint n)
+    {
+        if (n == 1)
+        {
+            uint temp = count;
+            count = 0;
+            return temp;
+        }
+        else
+        {
+            count++;
+            return n % 2 == 0 ? Hotpo(n / 2) : Hotpo((3 * n) + 1);
+        }
+
+        // ALternative
+        // return n == 1 ? 0 : n % 2 == 0 ? Hotpo(n / 2) + 1 : Hotpo(3 * n + 1) + 1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Hotpo(23u));
+    }
+}
