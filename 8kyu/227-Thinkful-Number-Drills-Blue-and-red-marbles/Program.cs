09b1735f92f5769a919ad721c9d0b75e5c2fe487
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-11 14:18:59 +0700

    Create 8kyu\227-Thinkful-Number-Drills-Blue-and-red-marbles

---
+++ 8kyu/227-Thinkful-Number-Drills-Blue-and-red-marbles/Program.cs
@@ -0,0 +1,15 @@
+﻿public static class BlueAndRedMarbles
+{
+    public static double GuessBlue(uint blueStart, uint redStart, uint bluePulled, uint redPulled)
+    {
+        // Code goes here..
+        return(double) (blueStart - bluePulled) / ((blueStart - bluePulled) + (redStart - redPulled));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GuessBlue(5, 5, 2, 3));
+        Console.WriteLine(GuessBlue(5, 7, 4, 3));
+        Console.WriteLine(GuessBlue(12, 18, 4, 6));
+    }
+}
