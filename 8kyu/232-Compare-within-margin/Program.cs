428483d351e67d395ae9542b4cc38999ba1e0c56
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-12 10:46:27 +0700

    Create 8kyu\232-Compare-within-margin

---
+++ 8kyu/232-Compare-within-margin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Kata
+{
+  public static int CloseCompare(double a, double b, double margin = 0)
+  {
+    return Math.Abs(a-b) <= margin ? 0 : a < b ? -1 : 1;
+
+    // Alternative
+    // return Math.Abs(a - b) <= margin ? 0 : a.CompareTo(b);
+  }
+
+  public static void Main(){
+    Console.WriteLine(CloseCompare(8.1, 5, 3));
+  }
+}
