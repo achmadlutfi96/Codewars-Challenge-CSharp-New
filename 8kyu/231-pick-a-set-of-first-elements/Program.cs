f5f4f63b7c7c17c37516a01fcb5a69ff86046026
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-12 10:35:06 +0700

    Create 8kyu\231-pick-a-set-of-first-elements

---
+++ 8kyu/231-pick-a-set-of-first-elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static object[] TakeFirstElements(object[] array, int n = 1)
+  {
+    return array.Take(n).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",",TakeFirstElements(new object[] { 'a', 'b', 'c', 'd', 'e' }, 2)));
+  }
+}
