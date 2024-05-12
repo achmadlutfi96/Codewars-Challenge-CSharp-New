cd0c0a6d99f5e02eccf00fbbf3b4ac0fa76e70a4
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-12 11:02:17 +0700

    Create 8kyu\233-The-falling-speed-of-petals

---
+++ 8kyu/233-The-falling-speed-of-petals/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+  public static double SakuraFall(double v)
+  {
+    // throw new NotImplementedException();
+    return v > 0 ? 400/v : 0;;
+  }
+
+  public static void Main(){
+    Console.WriteLine(SakuraFall(5));
+  }
+}
