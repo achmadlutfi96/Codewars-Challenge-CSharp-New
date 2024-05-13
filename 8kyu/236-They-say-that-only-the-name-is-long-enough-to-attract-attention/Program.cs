322e9b670dd601298adeae3beaa6e729cd93d5a6
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-13 11:34:46 +0700

    Create 8kyu\236-They-say-that-only

---
+++ 8kyu/236-They-say-that-only-the-name-is-long-enough-to-attract-attention/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static bool IsOpposite(string s1, string s2)
+  {
+    //coding here...
+    return string.IsNullOrEmpty(s1) ? false : string.Concat(s1.Select(s=>char.IsUpper(s)? char.ToLower(s): char.ToUpper(s)).ToArray()).Equals(s2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsOpposite("aB","Ab"));
+  }
+}
