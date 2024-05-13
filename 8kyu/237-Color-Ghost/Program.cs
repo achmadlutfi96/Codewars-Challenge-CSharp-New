051528b9755383351de07a8661a3b1446ace21e3
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-13 11:52:42 +0700

    Create 8kyu\237-Color-Ghost

---
+++ 8kyu/237-Color-Ghost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Ghost
+{
+  public string Color { get; set; } = "white";
+  public string GetColor(){
+    string[] str = new string[]{"white", "purple", "red", "yellow"};
+    return str[new Random().Next(str.Length)];
+  }
+}
+
+public class Program{
+    public static void Main(string[] args){
+        Console.WriteLine(new Ghost().Color);
+    }
+}
