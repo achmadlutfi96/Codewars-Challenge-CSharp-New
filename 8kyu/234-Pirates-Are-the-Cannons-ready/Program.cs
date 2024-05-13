400d5764f19f84c4f014a307d0fd44aa6edf1876
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-13 10:53:11 +0700

    Create 8kyu\234-Pirates-Are-the-Cannons-ready

---
+++ 8kyu/234-Pirates-Are-the-Cannons-ready/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static string CannonsReady(Dictionary<string, string> gunners)
+  {
+    // throw new NotImplementedException();
+    return gunners.ContainsValue("nay") ?  "Shiver me timbers!" : "Fire!";
+  }
+
+  public static void Main(){
+    Dictionary<string, string> gunners1 = new Dictionary<string, string>
+      {
+        {"Mike", "aye"},
+        {"Joe", "aye"},
+        {"Johnson", "aye"},
+        {"Peter", "aye"}
+      };
+    Dictionary<string, string> gunners2 = new Dictionary<string, string>
+      {
+        {"Mike", "aye"},
+        {"Joe", "nay"},
+        {"Johnson", "aye"},
+        {"Peter", "aye"}
+      };
+    Console.WriteLine(CannonsReady(gunners1));
+    Console.WriteLine(CannonsReady(gunners2));
+  }
+}
