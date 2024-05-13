c548cefe1c63bae3beaed5c1aad9ab7fb50f93f5
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-13 11:14:48 +0700

    Create 8kyu\235-Geometry-Basics-Distance-between-points

---
+++ 8kyu/235-Geometry-Basics-Distance-between-points-in-2D/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Point{
+    public double X {get; set;}
+    public double Y {get; set;}
+    public Point(double x, double y) {
+        X = x;
+        Y = y;
+    }
+}
+public class Kata
+{
+  public static double DistanceBetweenPoints(Point a, Point b)
+  {
+    return Math.Sqrt(Math.Pow(b.X-a.X,2) + Math.Pow(b.Y-a.Y,2));
+  }
+
+  public static void Main(){
+    Console.WriteLine(DistanceBetweenPoints(new Point(    1,    6), new Point(  4,    2)));
+  }
+}
