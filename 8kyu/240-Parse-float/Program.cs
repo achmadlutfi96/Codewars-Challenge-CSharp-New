1654762476af676a721af6d31ca9ff4a4600e878
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-14 10:01:18 +0700

    Create 8kyu\240-Parse-float

---
+++ 8kyu/240-Parse-float/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Kata
+{
+  public static double? ParseF(object s = null)
+  {
+     try{
+        if(!Regex.IsMatch((string)s, @"\d")){
+            throw new Exception("");
+        }
+        return Convert.ToDouble(s);
+    }catch{
+        return null;
+    }
+
+    // Alternative
+    // return double.TryParse(s?.ToString(), out var result) ? result : (double?) null;
+  }
+
+  public static void Main(){
+    Console.WriteLine(ParseF("1"));
+    Console.WriteLine(ParseF("ssds"));
+  }
+}
