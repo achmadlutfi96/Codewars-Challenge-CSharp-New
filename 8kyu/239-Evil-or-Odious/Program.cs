b1a6e5ebd2b60ce45c3ffb6c6a7f5e6176d7f0b0
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-14 09:32:34 +0700

    Create 8kyu\239-Evil-or-Odious

---
+++ 8kyu/239-Evil-or-Odious/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static string Evil(int n)
+  {
+    return (Convert.ToString(n, 2)).Count(b=>b.Equals('1'))%2 == 1 ? "It's Odious!" : "It's Evil!";
+    // return Convert.ToString(n, 2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Evil(1));
+    Console.WriteLine(Evil(2));
+    Console.WriteLine(Evil(3));
+  }
+}
