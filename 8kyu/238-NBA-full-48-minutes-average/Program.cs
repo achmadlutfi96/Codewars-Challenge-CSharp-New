c6806a395da25ddee98ba31470084f1b51d414cf
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-14 08:59:46 +0700

    Create 8kyu\238-NBA-full-48-minutes-average

---
+++ 8kyu/238-NBA-full-48-minutes-average/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Kata
+{
+  public static double NbaExtrap(double ppg, double mpg)
+  {
+    // return double.PositiveInfinity;
+    return ppg == 0 || mpg == 0 ? 0 : Math.Round((48/mpg)*ppg, 1);
+  }
+
+  public static void Main(){
+    Console.WriteLine(NbaExtrap(12, 20));
+  }
+}
