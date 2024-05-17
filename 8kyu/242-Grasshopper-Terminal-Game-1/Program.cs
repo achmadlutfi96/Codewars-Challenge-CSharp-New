d11b3fa7d12febbd37c6c6718254bd461ef73494
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-17 21:37:18 +0700

    Create 8kyu\242-Grasshopper-Terminal-Game-1

---
+++ 8kyu/242-Grasshopper-Terminal-Game-1/Program.cs
@@ -0,0 +1,10 @@
+﻿class Hero{
+    public string Name{ get; set; } = "Hero";
+    public string Position { get; set; } = "00";
+    public float Health { get; set; } = 100;
+    public float Damage { get; set; } = 5;
+    public int Experience { get; set; } = 0;
+    public Hero(string name = "Hero"){
+      Name = name;
+    }
+}
