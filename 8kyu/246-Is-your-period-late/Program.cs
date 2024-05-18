1364c709d4856dd24052cb700aef860315df2624
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-18 10:34:21 +0700

    Create 8kyu\246-Is-your-period-late

---
+++ 8kyu/246-Is-your-period-late/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Kata
+{
+  public static bool PeriodIsLate(DateTime last, DateTime today, int cycleLength)
+  {
+    return today.Subtract(last).Days > cycleLength ? true : false ;
+
+    // Alternative 
+    // return (today - last).TotalDays > cycleLength;
+  }
+
+  public static void Main(){
+    Console.WriteLine(PeriodIsLate(new DateTime(2016, 06, 13), new DateTime(2016, 07, 16), 35));
+    Console.WriteLine(PeriodIsLate(new DateTime(2016, 06, 13), new DateTime(2016, 07, 16), 28));
+  }
+}
