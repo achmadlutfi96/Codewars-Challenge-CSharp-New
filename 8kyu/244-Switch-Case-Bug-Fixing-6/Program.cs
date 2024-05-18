aa415e5022e7690fc48e9e9fdb2ae2cd898dd3ca
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-18 09:47:10 +0700

    Create 8kyu\244-Switch-Case-Bug-Fixing-6

---
+++ 8kyu/244-Switch-Case-Bug-Fixing-6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Kata
+{
+  public static double EvalObject(double value1, double value2, char operation)
+  {
+    double result = 0;
+    switch(operation)
+    {
+      case '+': result = value1 + value2; break;
+      case '-': result = value1 - value2; break;
+      case '/': result = value1 / value2; break;
+      case '*': result = value1 * value2; break;
+      case '%': result = value1 % value2; break;
+      case '^': result = Math.Pow(value1, value2); break;
+    }
+    return result;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Kata.EvalObject(1, 1, '+'));
+  }
+}
