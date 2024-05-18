1d1bf075d809d9075a28d732b8afd273ce36ba4b
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-18 10:13:19 +0700

    Create 8kyu\245-Did-she-say-hallo

---
+++ 8kyu/245-Did-she-say-hallo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static bool Validate_hello(string greetings)
+    {
+        // your code
+        string [] lang = new string[]{"hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc", "hallowen"};
+
+        return Array.Exists(lang, (s)=> greetings.ToLower().Contains(s));
+
+        // Alternative
+        // return Regex.IsMatch(Greetings, "hello|ciao|salut|hallo|hola|ahoj|czesc", RegexOptions.IgnoreCase);
+    }
+
+    public static void Main(){
+        Console.WriteLine(Validate_hello("hello"));
+    }
+}
