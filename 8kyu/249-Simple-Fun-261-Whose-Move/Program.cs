abe89ed75243b5740e7ec8b52c24f013a8572311
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-19 12:39:01 +0700

    Create 8kyu\249-Simple-Fun-261-Whose-Move

---
+++ 8kyu/249-Simple-Fun-261-Whose-Move/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Kata
+{
+  public static string WhoseMove(string lastPlayer, bool win)
+  {
+    //coding and coding..
+    if(lastPlayer == "black"){
+        if(win){
+            return "black";
+        }else{
+            return "white";
+        }
+    }else{
+        if(win){
+            return "white";
+        }else{
+            return "black";
+        }
+    }
+    // Alternative
+    // return win ? lastPlayer : lastPlayer == "black" ? "white" : "black";
+  }
+
+  public static void Main(){
+    Console.WriteLine(WhoseMove("black", false));
+  }
+}
