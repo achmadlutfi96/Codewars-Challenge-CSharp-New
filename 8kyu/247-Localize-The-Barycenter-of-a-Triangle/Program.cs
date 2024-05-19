eadc672ee2713c6b166b3598db47891350335213
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-19 12:06:26 +0700

    Create 8kyu\247-Localize-The-Barycenter-of-a-Triangle

---
+++ 8kyu/247-Localize-The-Barycenter-of-a-Triangle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Barycenter
+{
+    public static double[] BarTriang(double[] x, double[] y, double[] z)
+    {
+        // your code
+        return new double[] { Math.Round((x[0]+y[0]+z[0])/3, 4), Math.Round((x[1]+y[1]+z[1])/3,4)};
+    }
+
+    public static void Main(){
+        foreach (var item in BarTriang( new double[]{4,6}, new double[]{12,4}, new double[]{10,10}))
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
