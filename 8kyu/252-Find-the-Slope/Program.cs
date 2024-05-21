0a2c20860994eba501f14b30bf4d5aa89eec0f71
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-21 21:29:23 +0700

    Create 8kyu\252-Find-the-Slope

---
+++ 8kyu/252-Find-the-Slope/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Slope
+{
+
+    public String slope(int[] points)
+    {
+        return (points[2]-points[0]) == 0 ? "undefined" : $"{(points[3]-points[1])/(points[2]-points[0])}";
+    }
+
+}
+
+public class Program{
+    public static void Main(){
+        Slope s = new Slope();
+        Console.WriteLine(s.slope(new int[]{19,3,20,3}));
+    }
+}
