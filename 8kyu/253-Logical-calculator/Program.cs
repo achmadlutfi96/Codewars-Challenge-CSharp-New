cf9def08c153812c50dec379776a79dcaf49780f
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-22 19:25:06 +0700

    Create 8kyu\253-Logical-calculator

---
+++ 8kyu/253-Logical-calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static bool LogicalCalc(bool[] array, string op)
+  {
+    return op switch{
+        "AND" => array.Aggregate((v,n)=> v && n),
+        "OR" => array.Aggregate((v,n)=> v || n),
+        "XOR" => array.Aggregate((v,n) => v != n),
+        _ => false
+    };
+  }
+
+  public static void Main(){
+    Console.WriteLine(LogicalCalc(new bool[] { true, true, true, false }, "AND"));
+  }
+}
