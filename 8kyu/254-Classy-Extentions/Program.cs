6a92be8042e9b4e645492f1ad6ac173490ea46dc
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-22 19:41:43 +0700

    Create 8kyu\254-Classy-Extentions

---
+++ 8kyu/254-Classy-Extentions/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Cat : Animal
+{
+    public Cat(string name) : base(name)
+    {
+
+    }
+
+    // TODO: Override Animal's Speak method
+    public override string Speak()
+    {
+        return $"{Name} meows.";
+    }
+}
+
+public class Animal
+{
+    protected string Name { get; set; }
+    public Animal(string name)
+    {
+        this.Name = name;
+    }
+    public virtual string Speak()
+    {
+        return "";
+    }
+}
