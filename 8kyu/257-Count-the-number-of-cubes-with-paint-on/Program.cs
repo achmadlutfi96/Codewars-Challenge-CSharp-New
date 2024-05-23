47a441a2cec76895f4b946e58cda9afa0b70faaa
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-23 14:23:18 +0700

    Create 8kyu\257-Count-the-number-of-cubes-with-paint-on

---
+++ 8kyu/257-Count-the-number-of-cubes-with-paint-on/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int CountSquares(int cuts)
+    {
+        //Happy Coding :)
+        return (int) (cuts > 1 ?Math.Pow(cuts+1, 3) - Math.Pow(cuts-1, 3) : Math.Pow(cuts+1, 3));
+
+        // Alternative
+        // return n == 0 ? 1 : 6*n*n + 2;
+    }
+
+    public static void Main(){
+        Console.WriteLine(CountSquares(5));
+    }
+}
