3efa77d9beb18da3be7bced473cd73e929891b60
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-23 13:09:55 +0700

    Create 8kyu\255-Job-Matching-1

---
+++ 8kyu/255-Job-Matching-1/Program.cs
@@ -0,0 +1,23 @@
+﻿// using StriveObjects;
+using System;
+
+public class Strive
+{
+  public static bool Match(Candidate c, Job j)
+  {
+    if(c.MinSalary == null || j.MaxSalary == null){
+         throw new NotImplementedException();
+       }
+        return c.MinSalary-(c.MinSalary*0.1) <= j.MaxSalary;
+  }
+}
+
+public class Candidate{
+    public int? MinSalary { get; set; }
+    public Candidate(int minSalary) { MinSalary = minSalary;}
+}
+
+public class Job{
+    public int? MaxSalary { get; set; }
+    public Job(int maxSalary) { MaxSalary = maxSalary;}
+}
