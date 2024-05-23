1d53b582e42f8b88cd05288d5965bbaea2aea220
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-23 13:50:47 +0700

    Create 8kyu\256-Quadrants\

---
+++ 8kyu/256-Quadrants/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int Quadrant(int x, int y)
+  {
+    if(x > 0 && y > 0){
+        return 1;
+    } 
+    if(x < 0 && y > 0){
+        return 2;
+    }
+    if(x < 0 && y < 0){
+        return 3;
+    }
+    if(x > 0 && y < 0){
+        return 4;
+    }
+    return 0;
+
+    // Alternative
+    // return x > 0 ? y < 0 ? 4 : 1 : y > 0 ? 2 : 3;
+  }
+  public static void Main(){
+    Console.WriteLine(Quadrant(3, 5));
+  }
+}
