9e52c83aeda17ffa4be3a322d2eab5597b02ae4b
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-24 15:45:38 +0700

    Create 8kyu\259-Age-Range-Compatibility-Equation

---
+++ 8kyu/259-Age-Range-Compatibility-Equation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Kata
+{
+  public static string DatingRange(int age)
+  {
+    // return min-max
+    int min, max;
+    if(age <= 14){
+        min = (int) Math.Floor(age - (0.10 * age));
+        max = (int) Math.Floor(age + (0.10 * age));
+        return $"{min}-{max}";
+    }else{
+        min = (age/2) + 7;
+        max = (age-7) * 2;
+        return $"{min}-{max}";
+    }
+  }
+
+  public static void Main(){
+    Console.WriteLine(DatingRange(17));
+  }
+}
