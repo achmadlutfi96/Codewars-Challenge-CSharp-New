17e9a81bece9b25c54dff60e05caf76caa4a7c0e
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-24 15:32:02 +0700

    Create 8kyu\258-Take-an-Arrow-to-the-knee-Functionally

---
+++ 8kyu/258-Take-an-Arrow-to-the-knee-Functionally/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+// Preloaded method Tools.FromCharCode(int i) available!
+
+public class Kata
+{
+  public static string ArrowFunc(int[] arr)
+  {
+    return string.Join("", arr.Select( s => Convert.ToChar(s) ).ToArray());
+
+    // Alternative
+    // return string.Join("", arr.Select( num => Tools.FromCharCode(num)   ).ToArray());
+    // return string.Concat(arr.Select(n => (char)n));
+  }
+
+  public static void Main(){
+    Console.WriteLine(ArrowFunc(new int[]{84,101,115,116}));
+  }
+}
