c47def4376412cc8f01dcfe73fc560954c27ca78
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-25 14:09:12 +0700

    Create 8kyu\262--and-Legs

---
+++ 8kyu/262--and-Legs/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Kata
+{
+  public static object Animals(int heads, int legs)
+  {
+    
+    if ((heads > 1) && (legs > 1) &&  (legs % 2 == 0) && (legs <= 1000) && (heads <= 1000)){
+      int cow =  (legs/2) - heads;
+      int chicken = heads - cow;
+      if(cow < 0 || chicken < 0){
+        return "No solutions";
+      }
+      return new int[2]{ chicken , cow};
+    }else if(heads == 0 && legs == 0){
+      return new int[]{0,0};
+    }else{
+      return "No solutions";
+    }
+
+    // Alternative
+    // if (legs % 2 != 0)
+    //     return "No solutions";
+  
+    //   int cows = legs/2 - heads;
+    //   int chickens = heads - cows;
+      
+    //   return (cows < 0 || chickens < 0) ? (object)"No solutions" : new int[2] { chickens, cows };
+  }
+
+  public static void Main(){
+    foreach (var item in Animals(72, 200).ToString())
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
