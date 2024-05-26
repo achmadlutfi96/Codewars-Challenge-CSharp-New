824d42be93e1c6847c7ecf8434b662b84909111f
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-26 14:11:03 +0700

    Create 8kyu\265-Points-of-Reflection

---
+++ 8kyu/265-Points-of-Reflection/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+  public static int[] SymmetricPoint(int[] p, int[] q)
+  {
+    int[] d = new int[]{p[0]-q[0], p[1]-q[1]};
+    return new int[] { q[0]-d[0], q[1]-d[1] };
+
+    // Alternative
+    // return new [] { 2 * q[0] - p[0], 2 * q[1] - p[1] };
+  }
+
+  public static void Main(){
+    foreach (var item in SymmetricPoint(new int[] {10, -10},  new int[] {-10, 10}))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
