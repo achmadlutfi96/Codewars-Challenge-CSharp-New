6194c589f62ef713576b838384de619263c3633f
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-26 14:18:25 +0700

    Create 8kyu\266-Finish-Guess-the-Number-Game

---
+++ 8kyu/266-Finish-Guess-the-Number-Game/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Guesser
+{
+    private int number;
+    private int lives;
+
+    public Guesser(int number, int lives)
+    {
+        this.number = number;
+        this.lives = lives;
+    }
+
+    public bool Guess(int n)
+    {
+        if(this.lives == 0){
+            throw new Exception("Expected error thrown");
+        }
+        if(n == this.number){
+            return true;
+        }else{
+            this.lives--;
+            return false;
+        }
+    }
+}
