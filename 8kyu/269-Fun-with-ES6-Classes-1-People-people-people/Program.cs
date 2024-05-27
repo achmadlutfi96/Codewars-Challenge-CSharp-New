c9aad5f1845fde6b70f2933d9e8addf5dc3a2b5e
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-27 08:58:52 +0700

    Create 8kyu\269-Fun-with-ES6-Classes-1-People-people-people

---
+++ 8kyu/269-Fun-with-ES6-Classes-1-People-people-people/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Person
+{
+    // TODO
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+
+    public Person(string firstName = "John", string lastName = "Doe", int age = 0, string gender = "Male"){
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.Gender = gender;
+    }
+
+    public string SayFullName(){
+        return $"{this.FirstName} {this.LastName}";
+    }
+
+    public static string GreetExtraTerrestrials(string raceName){
+        return $"Welcome to Planet Earth {raceName}";
+    }
+}
