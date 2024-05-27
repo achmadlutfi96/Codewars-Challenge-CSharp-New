b58a48f4fff7212d41c995aab7db2d91c3e8555d
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-27 08:49:27 +0700

    Create 8kyu\268-Playing-with-cubes-II

---
+++ 8kyu/268-Playing-with-cubes-II/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Cube
+{
+  private int Side;
+  
+  //This cube needs your help. 
+  //Define a constructor which takes one integer and assignes its value to 'Side'
+  public Cube(int side){
+    this.Side = Math.Abs(side);
+  }
+
+  public Cube(){
+    this.Side = 0;
+  }
+  
+  public int GetSide()
+  {
+    return this.Side;
+  }
+  
+  public void SetSide(int s)
+  {
+    this.Side = Math.Abs(s);
+  }
+}
