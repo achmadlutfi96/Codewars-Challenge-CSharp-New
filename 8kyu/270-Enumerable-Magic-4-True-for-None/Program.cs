3fda9bb06e9ee458d501006db6cc84a0d25c37bf
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-28 08:15:46 +0700

    Create 8kyu\270-Enumerable-Magic-4-True-for-None

---
+++ 8kyu/270-Enumerable-Magic-4-True-for-None/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static bool None(int[] arr, Func<int, bool> fun)
+  {
+    foreach (var item in arr)
+    {
+        if(fun(item) == true){
+            return false;
+        }
+    }
+    return true;
+
+    // Alternative
+    // return !arr.Any(fun);
+  }
+
+  public static void Main(){
+    Console.WriteLine(None(new int[] { 1,2,3,4,5 }, v => v > 5));
+    Console.WriteLine(None(new int[] { 1,2,3,4,5 }, v => v > 4));
+  }
+}
