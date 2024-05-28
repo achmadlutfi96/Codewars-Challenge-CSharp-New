913fcdf0166be33f80aafa5faef2a0b23371b1bc
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-28 08:50:27 +0700

    Create 8kyu\272-Never-visit-a

---
+++ 8kyu/272-Never-visit-a/Program.cs
@@ -0,0 +1,8 @@
+﻿// this is a joke kata :))
+    public class Kata
+    {
+        public static string SubtractSum(int number)
+        {
+            return "apple";//fruit name like "apple"
+        }
+    }
