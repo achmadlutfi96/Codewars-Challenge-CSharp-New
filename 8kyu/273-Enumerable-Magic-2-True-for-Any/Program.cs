c3480cf1467bf368e4f971df67a95ebfad42736c
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-28 08:58:53 +0700

    Create 8kyu\273-Enumerable-Magic-2-True-for-Any

---
+++ 8kyu/273-Enumerable-Magic-2-True-for-Any/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Kata
+{
+  public static bool Any(int[] arr, Func<int,bool> fun)
+  {
+    foreach (var item in arr)
+    {
+        if(fun(item) == true){
+            return true;
+        }        
+    }
+    return false;
+
+    // Alternative
+    // return arr.Any(fun);
+  }
+  public static void Main(){
+    Console.WriteLine(Any(new int[] { 1,2,3,4 }, v => v > 3));
+    Console.WriteLine(Any(new int[] { 1,2,3,4 }, v => v > 4));
+  }
+}
