9bab3a4f437075a8e748162cab74ef540fecb9eb
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-29 10:30:51 +0700

    Create 8kyu\275-Filtering-even-numbers-Bug-Fixes

---
+++ 8kyu/275-Filtering-even-numbers-Bug-Fixes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+    {
+        public static List<int> FilterOddNumber(List<int> listOfNumbers)
+        {
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (listOfNumbers[i]%2 == 0)
+                {
+                    listOfNumbers.RemoveAt(i);
+                    i--;
+                }
+            }
+            return listOfNumbers;
+
+            // Alternative
+            // return numbers.Where(n => n % 2 == 1).ToList();
+        }
+
+        public static void Main(){
+            foreach (var item in FilterOddNumber(new List<int>() {1, 2, 2, 2, 4, 3, 4, 5, 6, 7}))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
