f2ba0a259f361da07b0bb58463caaceb714c1cf0
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-30 14:04:05 +0700

    Create 8kyu\278-Add-new-item-collections-are-passed-by

---
+++ 8kyu/278-Add-new-item-collections-are-passed-by-reference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class AddMore
+{
+    public static List<int> AddExtra(List<int> listOfNumbers)
+    {
+        // your code here
+        // add an int to listOfNumbers and return it
+        List<int> nlist = new List<int>();
+          foreach(int i in listOfNumbers){
+            nlist.Add(i);
+          }
+        nlist.Add(9);
+        return nlist;
+
+        // Alternative
+        // return new List<int>(listOfNumbers){9};
+    }
+
+    public static void Main(){
+        foreach (var item in AddExtra(new List<int>() { 1, 2, 2, 2, 4, 3, 4, 5, 6, 7 }))
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
