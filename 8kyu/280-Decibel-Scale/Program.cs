4d2a6aa1251c522f3472a21a7f3b8c725b2fa70a
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-30 14:30:54 +0700

    Create 8kyu\280-Decibel-Scale

---
+++ 8kyu/280-Decibel-Scale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Kata
+{
+  public static double DbScale(double intensity)
+  {
+    // throw new NotImplementedException();
+    return Math.Round(10 * Math.Log10((double) intensity/Math.Pow(10, -12)));
+  }
+  public static void Main(){
+    Console.WriteLine(DbScale(100));
+  }
+}
