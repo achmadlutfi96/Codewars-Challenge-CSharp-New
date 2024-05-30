61fd38eb88eb7b9c9db5fe92a69b63a4d839fcd5
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-30 14:18:42 +0700

    Create 8kyu\279-Grasshopper-Terminal-Game-Turn-Function

---
+++ 8kyu/279-Grasshopper-Terminal-Game-Turn-Function/Program.cs
@@ -0,0 +1,15 @@
+﻿// using static Game.Logic;
+
+public class Kata
+{
+  public static void DoTurn()
+  {
+    // Call functions here
+    RollDice();
+    Move();
+    Combat();
+    GetCoins();
+    BuyHealth();
+    PrintStatus();
+  }
+}
