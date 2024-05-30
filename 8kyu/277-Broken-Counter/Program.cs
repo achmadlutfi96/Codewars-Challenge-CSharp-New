62a59b09ab883b50bd0842c9b6df94b86e073f12
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-30 13:00:13 +0700

    Create 8kyu\277-Broken-Counter

---
+++ 8kyu/277-Broken-Counter/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Counter
+{
+    private int _Value;
+  public int Value
+  {
+    get
+    {
+      return _Value;
+    }
+    private set
+    {
+      _Value = value;
+    }
+  }
+  
+  public Counter()
+  {
+    Value = 0;
+  }
+  
+  public void Increase()
+  {
+    ++Value;
+  }
+  
+  public void Reset()
+  {
+    Value = 0;
+  }
+}
+
+class Program {
+    public static void Main(string[] args){
+        Counter counter = new Counter();
+        Console.WriteLine(counter.Value);
+        counter.Increase();
+        Console.WriteLine(counter.Value);
+        counter.Reset();
+        Console.WriteLine(counter.Value);
+    }
+}
