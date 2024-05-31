2d36601d915175c76f723b9e2c54a462ca497c25
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-31 14:52:37 +0700

    Create 8kyu\281-Collinearity

---
+++ 8kyu/281-Collinearity/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Kata
+{
+    public static bool Collinearity(int x1, int y1, int x2, int y2)
+    {
+       return (x1*y2) - (x2*y1) == 0;
+
+       // Alternative
+    //    return x1 * y2 == y1 * x2;
+    }
+    
+    public static void Main(){
+        Console.WriteLine(Collinearity(1, -2, -2, 4));
+    }
+}
