11524a4d571a2ca175d764f2ef6b22b90bf5b38e
Achmad Lutfi <alhacker1996@gmail.com>
2024-05-31 15:21:52 +0700

    Create 8kyu\282-Wilson-primes

---
+++ 8kyu/282-Wilson-primes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static bool AmIWilson(int p)
+  {
+    return (Enumerable.Range(1, p-1).Aggregate(1, (n, item) => n * item) + 1 ) % ( p * p ) == 0; 
+
+    // Alternative
+    // return p == 5 || p == 13 || p == 563;
+  }
+
+  public static void Main(){
+    Console.WriteLine(AmIWilson(5));
+    Console.WriteLine(AmIWilson(9));
+  }
+}
