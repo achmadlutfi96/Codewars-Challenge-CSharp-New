b7bc43618da472813969cbedbb5aaee49af5ca25
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-01 21:55:00 +0700

    Create 7kyu\001-Vowel-Count

---
+++ 7kyu/001-Vowel-Count/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        // Your code here
+        return str.Where(s => new char[]{'a','e', 'i', 'o', 'u'}.Contains(s)).Count();
+
+        // Alternative
+        // return str.Count(i => "aeiou".Contains(i));
+    }
+
+    public static void Main(){
+        Console.WriteLine(GetVowelCount("abracadabra"));
+    }
+}
