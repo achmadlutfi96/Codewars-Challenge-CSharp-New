413240c7b5c5801051a80fcbeb93cbdd3fe9465a
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-02 21:56:26 +0700

    Create 7kyu\005-Descending-Order

---
+++ 7kyu/005-Descending-Order/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int DescendingOrder(int num)
+  {
+    // Bust a move right here
+    // return Convert.ToInt32(string.Concat(num.ToString().OrderDescending(x=>x).ToArray()));
+
+    char[] n = num.ToString().ToArray();
+    Array.Sort(n);
+    Array.Reverse(n);
+    return int.Parse(string.Concat(n));
+  }
+
+  public static void Main(){
+    Console.WriteLine(DescendingOrder(145263));
+  }
+}
