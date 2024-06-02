eee9cb0ef4b352ebdce9e17a29de3632ae209ac8
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-02 18:11:46 +0700

    Create 7kyu\002-Disemvowel-Trolls

---
+++ 7kyu/002-Disemvowel-Trolls/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        return string.Concat(str.Where(s=>!new char[]{'a','i','u','e','o'}.Contains(Char.ToLower(s))));
+
+        // Alternative
+        // return string.Concat(str.Where(ch => !"aeiouAEIOU".Contains(ch)));
+    }
+
+    public static void Main(){
+        Console.WriteLine(Disemvowel("This website is for losers LOL!"));
+    }
+}
