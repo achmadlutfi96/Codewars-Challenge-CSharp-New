fdd278c8b33891732da9e47f622cfb4c7a074116
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-02 20:59:26 +0700

    Create 7kyu\003-Square-Every-Digit

---
+++ 7kyu/003-Square-Every-Digit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int SquareDigits(int n)
+  {
+    return Convert.ToInt32(string.Concat(n.ToString().Select(i=>Math.Pow(Char.GetNumericValue(i),2).ToString())));
+  }
+
+  public static void Main(){
+    Console.WriteLine(SquareDigits(9119));
+  }
+}
