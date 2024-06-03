917a13d67db77cc23434c195e92c0950fd003a7a
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-03 11:31:55 +0700

    Create 7kyu\009-You-re-a-square

---
+++ 7kyu/009-You-re-a-square/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Kata
+{
+  public static bool IsSquare(int n)
+  {
+    //Your code goes here!
+    return n == 0 ? true : Math.Pow(Math.Ceiling(Math.Sqrt(n)), 2) == n ? true : false;
+
+    // Alternative
+    // return Math.Sqrt(n) % 1 == 0;
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsSquare(634129));
+  }
+}
