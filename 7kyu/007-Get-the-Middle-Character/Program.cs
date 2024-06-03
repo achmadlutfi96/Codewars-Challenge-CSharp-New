2737136c1e433f0ec797056ac8b0cf581d527d3e
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-03 10:56:08 +0700

    Create 7kyu\007-Get-the-Middle-Character

---
+++ 7kyu/007-Get-the-Middle-Character/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static string GetMiddle(string s)
+  {
+  //Code goes here!
+    return s.Length % 2 == 0 ? string.Concat(s[(int)MathF.Ceiling(s.Length/2)-1], s[(int)MathF.Floor(s.Length/2)]) : string.Format("{0}",s[(int)MathF.Floor(s.Length/2)]);
+
+    // Alternative
+    //  return string.IsNullOrEmpty(s)? s : s.Substring((s.Length - 1) / 2, 2 - s.Length % 2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(GetMiddle("test"));
+  }
+}
