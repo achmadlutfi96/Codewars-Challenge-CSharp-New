696ec3a9e09bad73921de122527f594a821d6169
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-03 11:51:26 +0700

    Create 7kyu\010-Isograms

---
+++ 7kyu/010-Isograms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static bool IsIsogram(string str) 
+  {
+    // Code on you crazy diamond!
+    foreach (var item in str)
+    {
+        if(str.Where(x => char.ToLower(x) == char.ToLower(item)).Count() > 1){
+            return false;
+        }
+    }
+    return true;
+
+    // Alternative
+    // return str.ToLower().Distinct().Count()==str.Length;
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsIsogram("Dermatoglyphics"));
+    Console.WriteLine(IsIsogram("aba"));
+  }
+}
