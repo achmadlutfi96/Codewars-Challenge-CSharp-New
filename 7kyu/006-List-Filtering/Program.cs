022f6eb17d13a287ffa39824a9a85785319ea60a
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-03 10:36:37 +0700

    Create 7kyu\006-List-Filtering

---
+++ 7kyu/006-List-Filtering/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ListFilterer
+{
+   public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+   {
+      return listOfItems.OfType<int>();
+   }
+
+   public static void Main(){
+    foreach (var item in GetIntegersFromList(new List<object>(){1,"a","b",0,15}))
+    {
+        Console.WriteLine(item);
+    }
+   }
+}
