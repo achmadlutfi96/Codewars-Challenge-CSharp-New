3b311e0fde48d95b05a6133530f00e0a520700ce
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-03 11:15:16 +0700

    Create 7kyu\008-Mumbling

---
+++ 7kyu/008-Mumbling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Accumul
+{
+    public static String Accum(string s)
+    {
+        // your code
+        return string.Join("-",s.Select((x,i) => string.Concat(char.ToUpper(x),string.Concat(Enumerable.Repeat(x,i).ToArray()).ToLower())));
+
+        // Alternative
+        // return string.Join("-",s.Select((x,i)=>char.ToUpper(x)+new string(char.ToLower(x),i)));
+    }
+
+    public static void Main(){
+        Console.WriteLine(Accum("ZpglnRxqenU"));
+    }
+}
