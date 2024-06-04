726879a5941a35da2e5dc47b037e5bddca55c1f4
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-04 09:38:18 +0700

    Create 7kyu\013-Shortest-Word

---
+++ 7kyu/013-Shortest-Word/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static int FindShort(string s)
+  {
+    return s.Split(" ").Select(v => v.Length).Min();
+
+    // Alternative
+    // return s.Split(' ').Min(x => x.Length);
+  }
+
+  public static void Main(){
+
+    Console.WriteLine(FindShort("bitcoin take over the world maybe who knows perhaps"));
+  }
+}
