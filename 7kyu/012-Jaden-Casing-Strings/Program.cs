dbae9ddfe49441a96dc2bad03097e18fa5564c19
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-04 09:21:00 +0700

    Create 7kyu\012-Jaden-Casing-Strings

---
+++ 7kyu/012-Jaden-Casing-Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+public static class JadenCase
+{
+  public static string ToJadenCase(this string phrase)
+  {
+    return string.Join(" ",phrase.Split(" ").Select(s => $"{char.ToUpper(s[0])}{s.Substring(1, s.Length-1)}"));
+
+    // Alternative
+    // return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
+  }
+
+  public static void Main(){
+    Console.WriteLine("How can mirrors be real if our eyes aren't real".ToJadenCase());
+  }
+}
