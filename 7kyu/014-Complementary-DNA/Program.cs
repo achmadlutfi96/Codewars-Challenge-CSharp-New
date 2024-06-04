411cbda1669a1d7ace10ded8ec79d3ee81664cb2
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-04 09:54:38 +0700

    Create 7kyu\014-Complementary-DNA

---
+++ 7kyu/014-Complementary-DNA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        //Your code
+        Dictionary<char,char> dnaDict = new Dictionary<char,char>(){
+            { 'A', 'T' },
+            { 'G', 'C' },
+            { 'T', 'A' },
+            { 'C', 'G' },
+        };
+        return string.Concat(dna.Select(x=>dnaDict[x]));
+    }
+
+    public static void Main(){
+        Console.WriteLine(MakeComplement("ATTGC"));
+    }
+}
