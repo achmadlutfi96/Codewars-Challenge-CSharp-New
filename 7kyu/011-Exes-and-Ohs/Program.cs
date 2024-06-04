39897c6b23fbc0f56c8d1eb9c9602fe30aeca4bb
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-04 09:04:56 +0700

    Create 7kyu\011-Exes-and-Ohs

---
+++ 7kyu/011-Exes-and-Ohs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System;
+public static class Kata 
+{
+  public static bool XO (string input)
+  {
+    return input.Where(s => char.ToLower(s) == 'x').Count() == input.Where(s => char.ToLower(s) == 'o').Count();//Code it!
+
+    // Alternative
+    // return input.ToLower().Count(i => i == 'x') == input.ToLower().Count(i => i == 'o');
+  }
+
+  public static void Main(){
+    Console.WriteLine(XO("xxOo"));
+  }
+}
