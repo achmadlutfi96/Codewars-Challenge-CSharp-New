c00ca24783c78c30981264dae254aac10a2ef6cd
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-05 12:32:03 +0700

    Create 7kyu\017-Beginner-Series-3-Sum-of-Numbers

---
+++ 7kyu/017-Beginner-Series-3-Sum-of-Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+public class Sum
+{
+    public static int GetSum(int a, int b)
+    {
+        //Good Luck!
+        int res = 0;
+        if (a < b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                res += i;
+            }
+        }
+        else if (a == b)
+        {
+            res = a;
+        }
+        else
+        {
+            for (int i = b; i <= a; i++)
+            {
+                res += i;
+            }
+        }
+        return res;
+
+        // Alternative 1
+        // return (a + b) * (Math.Abs(a - b) + 1) / 2;
+        // Alternative 2
+        // return Enumerable.Range(Math.Min(a,b), Math.Max(b,a)-Math.Min(b,a)+1).Sum();
+
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GetSum(1, 2));
+    }
+}
