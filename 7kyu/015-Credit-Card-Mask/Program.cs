c2a76aa943742efb79e9806b2060e678a9b9608b
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-05 12:10:43 +0700

    Create 7kyu\016-Sum-of-two-lowest-positive-integers

---
+++ 7kyu/015-Credit-Card-Mask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public static class Kata
+{
+  // return masked string
+  public static string Maskify(string cc)
+  {
+    return cc.Length > 4 ?  string.Concat(Enumerable.Repeat("#", cc.Length-4)) + cc.Substring(cc.Length-4) : cc ;
+
+    // Alternative
+    // return cc.Substring(cc.Length < 4 ? 0 : cc.Length - 4).PadLeft(cc.Length, '#');
+  }
+
+  public static void Main(){
+    Console.WriteLine(Maskify("4556364607935616"));
+  }
+}
---
+++ 7kyu/016-Sum-of-two-lowest-positive-integers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public static class Kata
+{
+	public static int sumTwoSmallestNumbers(int[] numbers)
+	{
+		//Code here...
+        Array.Sort(numbers);
+        return numbers[0]+numbers[1];
+
+        // Alternative
+        // return numbers.OrderBy(i => i).Take(2).Sum();
+	}
+
+    public static void Main(){
+        Console.WriteLine(sumTwoSmallestNumbers(new int[]{5, 8, 12, 19, 22}));
+    }
+}
