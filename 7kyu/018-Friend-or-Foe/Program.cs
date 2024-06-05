ffdbf503c40caa0276e326b4740140fd2544a9cd
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-05 12:44:52 +0700

    Create 7kyu\018-Friend-or-Foe

---
+++ 7kyu/018-Friend-or-Foe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static IEnumerable<string> FriendOrFoe(string[] names)
+    {
+        // Good luck!
+        return names.Where(x => x.Length == 4);
+    }
+
+    public static void Main(){
+        foreach (var item in FriendOrFoe(new string[]{ "Ryan", "Kieran", "Mark", "Jimmy" }))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
