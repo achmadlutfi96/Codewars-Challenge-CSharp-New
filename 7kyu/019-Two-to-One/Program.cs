4f655c7555e5ea8be94a17be06325c8d8896b143
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-06 18:21:22 +0700

    Create 7kyu\019-Two-to-One

---
+++ 7kyu/019-Two-to-One/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public class TwoToOne
+{
+
+    public static string Longest(string s1, string s2)
+    {
+        // your code
+        return string.Concat(s1.Concat(s2).Distinct().OrderBy(a=>a));
+
+        // Alternative
+        // return string.Concat((s1 + s2).Distinct().OrderBy(c => c));
+    }
+
+    public static void Main(){
+        Console.WriteLine(Longest("aretheyhere", "yestheyarehere"));
+    }
+}
