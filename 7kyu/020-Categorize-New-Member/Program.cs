ab6eaf822d68238bd5dbaf9c9b756be8e1809921
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-06 18:29:44 +0700

    Create 7kyu\020-Categorize-New-Member

---
+++ 7kyu/020-Categorize-New-Member/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static IEnumerable<string> OpenOrSenior(int[][] data)
+    {
+        //your code here
+        return data.Select(d => d[0] >= 55 && d[1]> 7 ? "Senior" : "Open");
+    }
+
+    public static void Main(){
+        foreach (var item in OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
