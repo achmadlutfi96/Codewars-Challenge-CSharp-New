d82cac363602b1cdc8b5e5d1108c5e4f5ca4235e
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-06 19:01:47 +0700

    Create 7kyu\021-Find-the-next-perfect-square

---
+++ 7kyu/021-Find-the-next-perfect-square/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Kata
+{
+  public static long FindNextSquare(long num)
+  {
+    long n = (long) Math.Ceiling(Math.Sqrt(num));
+    return  n*n == num ? (n+1)*(n+1) : -1;
+  }
+
+  public static void Main(){
+    Console.WriteLine(FindNextSquare(431800368986044969));
+  }
+}
