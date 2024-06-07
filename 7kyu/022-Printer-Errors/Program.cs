3960d0529b4b76bd94170c7724d60c9f307ab021
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-07 08:32:08 +0700

    Create 7kyu\022-Printer-Errors

---
+++ 7kyu/022-Printer-Errors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Printer 
+{
+    public static string PrinterError(String s) 
+    {
+        // your code
+        int n = s.Length;
+        int v = s.Where(x => !Regex.IsMatch(x.ToString(),@"[a-m]")).Count();
+        return $"{v}/{n}";
+
+        // Alternative
+        // return s.Where(c => c > 'm').Count() + "/" + s.Length;
+    }
+
+    public static void Main(){
+        Console.WriteLine(PrinterError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
+    }
+}
