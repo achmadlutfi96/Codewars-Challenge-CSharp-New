3040ae567b1dff7fdd1a07615f2d7f66684b10d5
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-07 09:54:55 +0700

    Create 7kyu\024-String-ends-with

---
+++ 7kyu/024-String-ends-with/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Kata
+{
+    public static bool Solution(string str, string ending)
+    {
+        // TODO: complete
+        return str.EndsWith(ending);
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Solution("!@#$%^&*() :-)", ":-)"));
+        Console.WriteLine(Solution("samurai", "ai"));
+    }
+}
