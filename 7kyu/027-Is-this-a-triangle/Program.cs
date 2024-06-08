1d59e380ce1002edab0dc34c13bc1cb065b784e7
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-08 09:17:17 +0700

    Create 7kyu\027-Is-this-a-triangle

---
+++ 7kyu/027-Is-this-a-triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Triangle
+{
+    public static bool IsTriangle(int a, int b, int c)
+    {
+        if(a > 0 && b > 0 && c > 0){
+            int[] n = [a,b,c];
+            Array.Sort(n);
+            return n[0]+n[1] > n[2];
+        }else{
+            return false;
+        }
+
+        // Alternative
+        // return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
+    }
+
+    public static void Main(){
+        Console.WriteLine(IsTriangle(5, 7, 10));
+    }
+}
