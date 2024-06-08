db643b8e70e8f2be21a8f3d9d8a7fe1b175f9786
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-08 08:36:28 +0700

    Create 7kyu\025-Binary-Addition

---
+++ 7kyu/025-Binary-Addition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Kata
+{
+  public static string AddBinary(int a, int b)
+  {
+   // your code ...
+//    return (a + b).ToString("B"); // "B" for Binary
+    return Convert.ToString(a+b, 2);
+     
+  }
+
+  public static void Main(){
+    Console.WriteLine(AddBinary(5, 9));
+  }
+}
