1767ea76ae271b426c200f236d00d6ac7c613b22
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-09 10:34:18 +0700

    Create 7kyu\029-Ones-and-Zeros

---
+++ 7kyu/029-Ones-and-Zeros/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace Solution
+{
+  class Kata
+    {
+      public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+          //Code here
+          return Convert.ToInt32(string.Concat(BinaryArray), 2);
+
+          // Alternative
+          // return BinaryArray.Aggregate((value, bit) => (value << 1) | bit);
+        }
+
+        public static void Main(string[] args){
+            Console.WriteLine(binaryArrayToNumber(new int[] {0,1,1,0}));
+        }
+    }
+}
