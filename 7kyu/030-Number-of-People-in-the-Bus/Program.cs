dfa9a8892ae292e17a2d83f637a4897e10e4efd6
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-09 10:44:59 +0700

    Create 7kyu\030-Number-of-People-in-the-Bus

---
+++ 7kyu/030-Number-of-People-in-the-Bus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int Number(List<int[]> peopleListInOut)
+    {
+        // Happy Coding
+        int p = 0;
+        foreach (var i in peopleListInOut)
+        {
+            p+=i[0];
+            p-=i[1];
+        }
+        return p;
+
+        // Alternative
+        // return peopleListInOut.Sum(Item => Item[0] - Item[1]);
+    }
+    public static void Main(){
+        Console.WriteLine(Number(new List<int[]>(){new []{10,0},new []{3,5}, new []{5,8}}));
+    }
+}
