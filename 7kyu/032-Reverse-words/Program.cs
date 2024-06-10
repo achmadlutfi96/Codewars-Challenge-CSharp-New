a1e6ffe8293825f2fd5288ce20324c9e15626988
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-10 07:35:16 +0700

    Create 7kyu\032-Reverse-words

---
+++ 7kyu/032-Reverse-words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string ReverseWords(string str)
+  {
+		//Write your code here
+        return string.Join(" ", str.Split(" ").Select(x=>string.Concat(x.Reverse())));
+
+        // Alternative
+        // return string.Join(" ", str.Split(' ').Select(i => new string(i.Reverse().ToArray())));
+  }
+
+  public static void Main(){
+    Console.WriteLine(ReverseWords("This is an example!"));
+  }
+}
