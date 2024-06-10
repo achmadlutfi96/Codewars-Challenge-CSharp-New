ead122e54775de02b3f779a0101ad95643951c6d
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-10 07:11:35 +0700

    Create 7kyu\031-Odd-or-Even

---
+++ 7kyu/031-Odd-or-Even/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string OddOrEven(int[] array)
+  {
+    //enter code here
+    return array.Length > 0 ? array.Sum()%2 == 0 ? "even" : "odd" : "even";
+
+    // Alternative
+    // return array.Sum() % 2 == 0 ? "even" : "odd";
+  }
+
+  public static void Main(){
+    Console.WriteLine(OddOrEven(new int[] {}));
+  }
+}
