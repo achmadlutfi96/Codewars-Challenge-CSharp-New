47c600a2be946b80031e90c5d1c0824a5c74d062
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-11 10:30:54 +0700

    Create 7kyu\036-Testing-1-2-3

---
+++ 7kyu/036-Testing-1-2-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+public class LineNumbering 
+{
+    public static List<string> Number(List<string> lines) 
+    {
+        //your code goes here
+        return lines.Select((v,i)=>$"{i+1}: {v}").ToList();
+
+    }
+
+    public static void Main(){
+        foreach (var item in Number(new List<string>{"a", "b", "c"}))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
