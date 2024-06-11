6ebc2fff2f7b6f36217e86482cf91a8333b9f7b6
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-11 10:24:16 +0700

    Create 7kyu\035-Remove-the-minimum

---
+++ 7kyu/035-Remove-the-minimum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Remover
+{
+  public static List<int> RemoveSmallest(List<int> numbers)
+  {
+    // Good Luck!
+    if (numbers.Count == 0){
+        return new List<int>();
+    }
+    List<int> res = new List<int>();
+    foreach (var item in numbers)
+    {
+        res.Add(item);
+    }
+    res.Remove(numbers.Min());
+    return res;
+
+    // Alternative
+    // numbers.Remove(numbers.DefaultIfEmpty().Min());
+    // return numbers;
+  }
+
+  public static void Main(){
+    foreach (var item in RemoveSmallest(new List<int>{5, 3, 2, 1, 4}))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
