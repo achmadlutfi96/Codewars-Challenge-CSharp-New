3e78f516ded8f7e7125497d14a0071f339f37dbf
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-12 15:59:51 +0700

    Create 7kyu\038-Don-t-give-me-five

---
+++ 7kyu/038-Don-t-give-me-five/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static int DontGiveMeFive(int start, int end)
+  {
+    
+    return Enumerable.Range(start, end-start+1).Where(x=>!x.ToString().Any<char>(n=>n.Equals('5'))).Count();
+
+    // Alternative
+    // return Enumerable.Range(start, end-start+1).Count(x => !x.ToString().Contains("5"));
+    
+  }
+
+  public static void Main(){
+    Console.WriteLine(DontGiveMeFive(1,9));
+    Console.WriteLine(DontGiveMeFive(4,17));
+  }
+}
