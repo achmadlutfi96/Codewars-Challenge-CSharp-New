ac1870f28caf906e5f2212218582e23608839fd9
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-12 16:28:47 +0700

    Create 7kyu\039-Find-the-stray-number

---
+++ 7kyu/039-Find-the-stray-number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+class Solution
+{
+    public static int Stray(int[] numbers)
+    {
+        
+        foreach (var item in numbers.Distinct())
+        {
+            if (numbers.Where(i => i==item).Count() == 1){
+                return item;
+            }
+        }
+        return 0;
+
+        // Alternative
+        // return numbers.First(x => numbers.Count(i => i == x) == 1);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Stray(new int[] { 1, 1, 2 }));
+    }
+}
