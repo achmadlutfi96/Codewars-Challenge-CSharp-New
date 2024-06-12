1a88d133089626cc621c98890a8149d6e5e1dbe9
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-12 12:15:51 +0700

    Create 7kyu\037-The-highest-profit-wins

---
+++ 7kyu/037-The-highest-profit-wins/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class MinMax 
+{
+    public static int[] minMax(int[] lst) 
+    {
+        // your code
+        return [lst.Min(), lst.Max()];
+    }
+
+    public static void Main(){
+        foreach(var n in minMax(new int[] {1, 2, 5, -1, 12, 20})){
+            Console.WriteLine(n);
+        }
+    }
+}
