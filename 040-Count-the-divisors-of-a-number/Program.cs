ca5ccb5b207519be950f1fe1d37258d1c800d558
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-13 10:31:25 +0700

    Create 040-Count-the-divisors-of-a-number

---
+++ 040-Count-the-divisors-of-a-number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int Divisors(int n)
+  {
+    // todo
+    return Enumerable.Range(1, n).Where(x=> n%x==0).Count(); 
+
+    // Alternative
+    // return Enumerable.Range(1, n).Count(x=> n%x==0);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Divisors(10));
+  }
+}
