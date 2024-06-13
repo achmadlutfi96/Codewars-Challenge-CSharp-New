41227ca73c1ecb89b0e6258e901d11b08da231b5
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-13 12:32:38 +0700

    Create 041-Sort-array-by-string-length

---
+++ 041-Sort-array-by-string-length/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+  public static string[] SortByLength (string[] array)
+  {
+    return array.OrderBy(x => x.Length).ToArray();
+
+    // Alternative
+    // Array.Sort(array, (x, y) => x.Length.CompareTo(y.Length));
+    // return array;
+  }
+
+  public static void Main(){
+    foreach (var item in SortByLength(new string[] { "Beg", "Life", "I", "To" }))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
