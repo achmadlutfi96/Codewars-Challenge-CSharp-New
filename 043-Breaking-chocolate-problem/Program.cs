8fb9515edffc366f7579aa36d28dabcfa1684ec3
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-14 15:29:39 +0700

    Create 043-Breaking-chocolate-problem

---
+++ 043-Breaking-chocolate-problem/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata 
+{
+  public static int BreakChocolate(int n, int m) 
+  {
+    //a miracle occurs...
+    return n>0 && m>0 ? n*m-1 : 0;
+  }
+
+  public static void Main(){
+    Console.WriteLine(BreakChocolate(5, 5));
+  }
+}
