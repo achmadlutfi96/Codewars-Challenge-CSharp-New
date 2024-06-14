e9c693a4966b12c3d9ff6cad50a5b7c7568b859d
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-14 22:12:20 +0700

    Create 045-Make-a-function-that-does-arithmetic

---
+++ 045-Make-a-function-that-does-arithmetic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Kata 
+{
+  public static double Arithmetic(double a, double b, string op) 
+  {
+    // throw new NotImplementedException();
+
+    return op switch{
+        "add" => a+b,
+        "subtract"=>a-b,
+        "multiply" => a*b,
+        "divide" => a/b,
+        _ => 0
+    };
+  }
+
+  public static void Main(){
+    Console.WriteLine(Arithmetic(8, 2, "subtract"));
+  }
+}
