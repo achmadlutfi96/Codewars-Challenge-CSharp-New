9fefcc986386ef1e7a22746eba880c5564af364a
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-15 11:44:31 +0700

    Create 048-Round-up-to-the-next-multiple-of-5

---
+++ 048-Round-up-to-the-next-multiple-of-5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Kata
+{
+  public static int RoundToNext5(int n)
+  {
+    //Your code goes here...
+    return (int) Math.Ceiling((double)n/5)*5;
+  }
+
+  public static void Main(){
+    Console.WriteLine(RoundToNext5(12));
+  }
+}
