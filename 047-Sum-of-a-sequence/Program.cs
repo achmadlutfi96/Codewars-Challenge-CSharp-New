0378d4f6579d240a7ac6f0ca45c7130e02cce3be
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-15 11:37:31 +0700

    Create 047-Sum-of-a-sequence

---
+++ 047-Sum-of-a-sequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int SequenceSum(int start, int end, int step)
+  {
+    int sum = 0;
+    for (int i = start; i <= end; i+=step){
+        sum += i;
+    }
+    return sum;
+
+    // Alternative
+    // return start > end ? 0 : Enumerable.Repeat(start, (end - start) / step + 1).Select((x, index) => x + step * index).Sum();
+  }
+
+  public static void Main(){
+    Console.WriteLine(SequenceSum(2, 6, 2));
+  }
+}
