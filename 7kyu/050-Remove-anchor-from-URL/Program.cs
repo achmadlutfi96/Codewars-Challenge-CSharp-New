b170b2477f2b365fa6073b135cd11dbddda34a75
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-16 12:43:20 +0700

    Create 7kyu\050-Remove-anchor-from-URL

---
+++ 7kyu/050-Remove-anchor-from-URL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+public static class Kata
+{
+  public static string RemoveUrlAnchor(string url)
+  {
+    // TODO: complete
+    return url.Any(x=>x.Equals('#')) ? url[..url.IndexOf('#')] : url;
+
+    // Alternative
+    // return url.Split('#')[0];
+
+  }
+
+  public static void Main(){
+    Console.WriteLine(RemoveUrlAnchor("www.codewars.com#about"));
+  }
+}
