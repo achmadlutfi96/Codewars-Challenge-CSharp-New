4fa45769bf3c7238753fc7205a000522671ac763
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-16 13:08:54 +0700

    Create 7kyu\051-Anagram-Detection

---
+++ 7kyu/051-Anagram-Detection/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static bool IsAnagram(string test, string original)
+  {
+    // your code goes here
+    // if(original.Length == test.Length){
+    //     // char[] a = test.Order().ToArray();
+    //     // char[] b = original.Order().ToArray();
+    //     char[] a = test.ToLower().ToCharArray();
+    //     Array.Sort(a);
+    //     char[] b = original.ToLower().ToCharArray();
+    //     Array.Sort(b);
+    //     for (int i = 0; i < a.Length; i++)
+    //     {
+    //         if(a[i] != b[i]){
+    //             return false;
+    //         }   
+    //     }
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+
+    // Alternative 1
+    return string.Concat(test.ToLower().OrderBy(x=>x)) == string.Concat(original.ToLower().OrderBy(x=>x));
+
+    // Alternative 2
+    // return test.ToLower().OrderBy(x => x).SequenceEqual(original.ToLower().OrderBy(x => x));
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsAnagram("Buckethead", "DeathCubeK"));
+  }
+}
