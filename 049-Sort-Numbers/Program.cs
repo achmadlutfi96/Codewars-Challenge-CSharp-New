727ee2018ea3d2ba397c2c6b5f2634666ad9dcb1
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-16 12:33:46 +0700

    Create 049-Sort-Numbers

---
+++ 049-Sort-Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Kata
+{
+  public static int[] SortNumbers(int[] nums)
+  {
+    if(nums == null || nums.Length == 0){
+      return new int[]{};
+    }
+    Array.Sort(nums);
+    return nums;
+
+    // Alternative
+    // return nums?.OrderBy(i=>i).ToArray() ?? new int[0];
+  }
+
+  public static void Main(){
+    foreach (var item in SortNumbers(new int[] { 20, 2, 10 }))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
