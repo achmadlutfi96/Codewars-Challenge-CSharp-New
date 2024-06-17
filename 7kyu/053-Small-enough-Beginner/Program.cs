10863936bfc7a81def76c52e53046e54fbf1b7f9
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-17 10:20:43 +0700

    Create 7kyu\053-Small-enough-Beginner

---
+++ 7kyu/053-Small-enough-Beginner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static bool SmallEnough(int[] a, int limit)
+  {
+    return !a.Any(x=>x > limit);
+
+    // Alternative
+    // return Array.TrueForAll(a, x => x <= limit);
+  }
+
+  public static void Main(){
+    Console.WriteLine(SmallEnough(new int[] { 78, 117, 110, 99, 104, 117, 107, 115 }, 100));
+  }
+}
