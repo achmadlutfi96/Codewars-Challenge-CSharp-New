36e3b5a7dfcf5bf3178d8dbaec6ac93fae01ca81
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-17 10:27:02 +0700

    Create 7kyu\054-Two-Oldest-Ages

---
+++ 7kyu/054-Two-Oldest-Ages/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+public class LargestTwo
+{
+  public static int[] TwoOldestAges(int[] ages)
+  {
+    return ages.OrderBy(x=>x).TakeLast(2).ToArray();
+  }
+
+  public static void Main(){
+    foreach (var item in TwoOldestAges(new[] {1,5,87,45,8,8}))
+    {
+        Console.WriteLine(item);     
+    }
+    
+  }
+}
