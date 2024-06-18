44604b0a2690432cb98b6333f9ad7fb1945e006d
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-18 19:32:16 +0700

    Create 7kyu\057-Simple-Fun-176-Reverse-Letter

---
+++ 7kyu/057-Simple-Fun-176-Reverse-Letter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Kata
+    {
+        public static string ReverseLetter(string str){
+          //coding and coding..
+          return string.Concat(str.Where(x=>Regex.IsMatch(x.ToString(), @"[a-zA-Z]")).Reverse());
+
+          // Alternative
+        //   return new String(str.Where(Char.IsLetter).Reverse().ToArray());
+        }
+
+        public static void Main(string[] args){
+            Console.WriteLine(ReverseLetter("ultr53o?n"));
+        }
+    }
+}
