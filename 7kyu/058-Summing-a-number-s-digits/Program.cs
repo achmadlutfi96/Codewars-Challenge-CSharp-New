295e04bfb4a4a74a10357717f8fc03b4b821b6a6
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-19 22:42:33 +0700

    Create 7kyu\058-Summing-a-number-s-digits

---
+++ 7kyu/058-Summing-a-number-s-digits/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static int SumDigits(int number)
+  {
+    return Math.Abs(number).ToString().Sum(x=>(int)char.GetNumericValue(x));
+  }
+
+  public static void Main(){
+    Console.WriteLine(SumDigits(99));
+  }
+}
