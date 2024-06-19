1634764f33c2fc40a745892aeae242b11c12a49f
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-19 22:51:13 +0700

    Create 7kyu\059-Fix-string-case

---
+++ 7kyu/059-Fix-string-case/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+class Kata
+{
+    public static string Solve(string s)
+    {
+        //Do Some Magic
+        return s.Where(x=>char.IsLower(x)).Count() >= s.Where(x=>char.IsUpper(x)).Count() ? s.ToLower() : s.ToUpper();
+
+        // Alternative
+        // return s.Count(char.IsUpper) > s.Count(char.IsLower) ? s.ToUpper() : s.ToLower();
+    }
+
+    public static void Main(){
+        Console.WriteLine(Solve("CODe"));
+    }
+}
