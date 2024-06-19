0ec4a1f717f4a46c09ef66a8b30982128d96a1a9
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-19 22:57:54 +0700

    Create 7kyu\060-Maximum-Multiple

---
+++ 7kyu/060-Maximum-Multiple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Kata
+{
+    public static int MaxMultiply(int divisor, int bound)
+    {
+        //Do some magic
+        return (int) Math.Ceiling((double) (bound/divisor)) * divisor;
+
+        // Alternative
+        // return bound / divisor * divisor;
+    }
+
+    public static void Main(){
+        Console.WriteLine(MaxMultiply(7, 100));
+    }
+}
