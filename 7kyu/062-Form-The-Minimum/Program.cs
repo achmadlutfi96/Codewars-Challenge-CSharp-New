fd5db43c4b9049fa402a3fe0f0fc304d44da5613
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-20 14:20:41 +0700

    Create 7kyu\062-Form-The-Minimum

---
+++ 7kyu/062-Form-The-Minimum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+class Kata
+{
+    public static long MinValue(int[] a)
+    {
+        //Magic
+        return Convert.ToInt64(string.Concat(a.OrderBy(x=>x).DistinctBy(x=>x).ToArray()));
+
+        // Alternative
+        //  return long.Parse(string.Concat(a.Distinct().OrderBy(x => x)));
+    }
+
+    public static void Main(){
+        Console.WriteLine(MinValue(new int[]{ 3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9}));
+    }
+}
