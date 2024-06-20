c0693c0b02c07d28c081af9beec162df9df93a36
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-20 12:11:51 +0700

    Create 7kyu\061-Factorial

---
+++ 7kyu/061-Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int Factorial(int n)
+  {
+    if (n < 0 || n > 12) {
+        throw new ArgumentOutOfRangeException();
+    }
+    int result = 1;
+    for (int i = 1; i <= n ; i++){
+        result *= i;
+    }
+    return result;
+
+    // Alternative
+    // return Enumerable.Range(1, n).Aggregate(1, (x,y) => x * y);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Factorial(3));
+  }
+}
