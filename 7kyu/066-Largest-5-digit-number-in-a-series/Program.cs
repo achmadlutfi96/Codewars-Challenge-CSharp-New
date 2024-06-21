30d3463171982f0a9d2a862a3a17ed731fcaa5d8
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-22 00:09:50 +0700

    Create 7kyu\066-Largest-5-digit-number-in-a-series

---
+++ 7kyu/066-Largest-5-digit-number-in-a-series/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Kata 
+{
+  public static int GetNumber(string str) 
+  {
+    List<int> temp = new List<int>();
+    for (int i = 0; i < str.Length-4; i++){
+        temp.Add(Convert.ToInt32(str.Substring(i, 5)));
+    }
+    return temp.Max();
+
+    // Alternative
+    // return int.Parse(Enumerable.Range(0, s.Length - 4).Max(i => s.Substring(i, 5)));
+  }
+
+  public static void Main(){
+    Console.WriteLine(GetNumber("731674765"));
+  }
+}
