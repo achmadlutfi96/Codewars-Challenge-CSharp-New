e12573cedbc8ca859a0ee9a02f34667583619a44
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-22 09:29:42 +0700

    Create 7kyu\070-Leap-Years

---
+++ 7kyu/070-Leap-Years/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Kata
+{
+  public static bool IsLeapYear(int year)
+  {
+    return DateTime.IsLeapYear(year);
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsLeapYear(2020));
+  }
+}
