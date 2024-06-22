77bc9e983ab82330785db615f896fbaaca8b8d89
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-22 09:24:56 +0700

    Create 7kyu\069-Factorial

---
+++ 7kyu/069-Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static ulong Factorial(int n)
+    {
+        if (n <= 1){
+            return 1;
+        }else{
+            return (ulong) n * Factorial(n-1);
+        }
+
+        // Alternative
+        // return (n == 0) ? 1 : n * factorial(n-1);
+    }
+
+    public static void Main(string[] args){
+        Console.WriteLine(Factorial(5));
+    }
+  }
+}
