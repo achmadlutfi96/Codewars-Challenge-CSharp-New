f147d1d6002133cdfad678f0db3f6bc7d8850e73
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-22 08:58:01 +0700

    Create 7kyu\067-Row-Weights

---
+++ 7kyu/067-Row-Weights/Program.cs
@@ -0,0 +1,21 @@
+﻿class Kata
+{
+    public static int[] RowWeights(int[] a)
+    {
+        //Do Some Magic
+        return new int[] {a.Where((v, i)=> (i+1)%2 == 1).Sum(), a.Where((v, i)=> (i+1)%2 == 0).Sum()}; 
+
+        // Alternative
+        // return new int[]
+            // {
+            //     numbers.Where((n, index) => index % 2 == 0).Sum(),
+            //     numbers.Where((n, index) => index % 2 == 1).Sum()
+            // };
+    }
+
+    public static void Main(){
+        foreach(var item in RowWeights(new int[]{13, 27, 49})){
+            Console.WriteLine(item);
+        }
+    }
+}
