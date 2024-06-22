c1c9fe312934e90393cc1bbfbc4b54b710028576
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-22 09:12:43 +0700

    Create 7kyu\068-The-Coupon-Code

---
+++ 7kyu/068-The-Coupon-Code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Kata
+{
+  public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
+  {
+    if(DateTime.Parse(currentDate) > DateTime.Parse(expirationDate)){
+        return false;
+    }
+    return enteredCode == correctCode;
+  }
+
+  public static void Main(){
+    Console.WriteLine(CheckCoupon("123a","123","September 5, 2014","October 1, 2014"));
+  }
+}
