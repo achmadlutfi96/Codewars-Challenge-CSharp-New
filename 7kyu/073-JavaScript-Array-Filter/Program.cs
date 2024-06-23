cad47afe47d80c6e668710f1007e503c89c92fbc
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-23 11:30:48 +0700

    Create 7kyu\073-JavaScript-Array-Filter

---
+++ 7kyu/073-JavaScript-Array-Filter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+public class Kata
+{
+  public static int[] GetEvenNumbers(int[] numbers)
+  {
+    // filter out the odd numbers
+    return numbers.Where(x=>x%2 == 0).ToArray();
+  }
+
+  public static void Main(){
+    foreach (var item in GetEvenNumbers(new int[] {12, 14, 15}))
+    {
+        Console.WriteLine(item.ToString());
+    }
+  }
+}
