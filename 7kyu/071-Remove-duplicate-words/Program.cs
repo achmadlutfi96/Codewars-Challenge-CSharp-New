963e5693ac8bdd44d7ff27ee89c7bed990541238
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-23 10:37:58 +0700

    Create 7kyu\071-Remove-duplicate-words

---
+++ 7kyu/071-Remove-duplicate-words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string RemoveDuplicateWords(string s)
+  {
+    // throw new NotImplementedException();
+    return string.Join(" ",s.Split(" ").Distinct());
+  }
+
+  public static void Main(){
+    Console.WriteLine(RemoveDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
+  }
+}
