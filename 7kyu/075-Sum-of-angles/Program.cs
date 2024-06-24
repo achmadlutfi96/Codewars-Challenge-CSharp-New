76c189cf81e201d68745789350bca7f390b60692
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-24 08:32:36 +0700

    Create 7kyu\075-Sum-of-angles

---
+++ 7kyu/075-Sum-of-angles/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+    public static int Angle(int n)
+    {
+        //your code here
+        return (n-2)*180;
+    }
+
+    public static void Main(){
+        Console.WriteLine(Angle(3));
+    }
+}
