7c32949adbaa4da1454457b2867186785ed3e111
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-24 08:17:16 +0700

    Create 7kyu\074-Love-vs-friendship

---
+++ 7kyu/074-Love-vs-friendship/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int WordsToMarks(string str)
+  {
+    // throw new NotImplementedException();
+    return str.Select(x=>x-96).Sum();
+
+    // Alternative
+    // return str.Sum(c => c - 96);
+  }
+
+  public static void Main(){
+    Console.WriteLine(WordsToMarks("friends"));
+  }
+}
