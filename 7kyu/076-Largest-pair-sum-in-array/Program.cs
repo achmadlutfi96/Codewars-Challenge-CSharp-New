3ead46b617de60e5b819fce1adef0c5a22a799a4
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-24 08:37:28 +0700

    Create 7kyu\076-Largest-pair-sum-in-array

---
+++ 7kyu/076-Largest-pair-sum-in-array/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+public class Kata
+{
+  public static int LargestPairSum(int[] numbers)
+  {
+    // TODO: Write your code here
+    return numbers.OrderBy(x=>x).TakeLast(2).Sum();
+  }
+
+  public static void Main(){
+    Console.WriteLine(LargestPairSum(new int[] {-8, -8, -16, -18, -19}));
+  }
+}
