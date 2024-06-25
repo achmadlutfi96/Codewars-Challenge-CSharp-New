6c18795aa9e5938153954dc24b2822a8dd78668c
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-25 14:12:07 +0700

    Create 7kyu\079-No-oddities-here

--- 7kyu/079-No-oddities-here/Program.cs
+++ 7kyu/079-No-oddities-here/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+public class NoOddities
+{
+    public static int[] NoOdds(int[] values) 
+    {
+        // Code??
+        return values.Where(x=>x%2 == 0).ToArray();
+    }
+
+    public static void Main(){
+        foreach(var i in NoOdds(new int[]{1,2,3,4,5})){
+            Console.WriteLine(i.ToString());
+        }
+    }
+}
