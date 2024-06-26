24cf360f1f14a4d60c117819f33000297aaa7ee9
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-26 14:31:29 +0700

    Create 7kyu\081-Bumps-in-the-Road

---
+++ 7kyu/081-Bumps-in-the-Road/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+class Kata
+{
+    public static string Bump(string input)
+    {
+        //Do Some Magic
+        return input.Count(x=>x=='n') <= 15 ? "Woohoo!" : "Car Dead";
+    }
+
+    public static void Main(){
+        Console.WriteLine(Bump("nnn_n__n_n___nnnnn___n__nnn__"));
+    }
+}
