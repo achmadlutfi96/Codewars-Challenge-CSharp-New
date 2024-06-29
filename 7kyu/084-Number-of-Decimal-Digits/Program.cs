20893311a1afed561cca8011a645a19753ec0c6a
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-29 13:45:41 +0700

    Create 7kyu\084-Number-of-Decimal-Digits

---
+++ 7kyu/084-Number-of-Decimal-Digits/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class DecTools {
+  public static int Digits(ulong n) {
+    return n.ToString().Length;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Digits(9876543210ul));
+  }
+}
