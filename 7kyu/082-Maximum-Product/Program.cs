d50bfffee92f8d5f3971b50ef7405951d5344d6e
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-29 12:49:39 +0700

    Create 7kyu\082-Maximum-Product

---
+++ 7kyu/082-Maximum-Product/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int AdjacentElementsProduct(int[] array)
+    {
+        //Do Some Magic
+        int[] products = new int[array.Length-1];
+        for (int i = 0; i < products.Length ; i++){
+            products[i] = array[i] * array[i+1];
+        }
+        return products.Max();
+
+        // Alternative
+        // return array.Skip(1).Zip(array, (x, y) => x * y).Max();
+    }
+
+    public static void Main(){
+        Console.WriteLine(AdjacentElementsProduct(new int[]{1, 2, 3}));
+    }
+}
