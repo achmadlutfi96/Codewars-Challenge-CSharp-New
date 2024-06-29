5e72e7a0f8617ccd4b09ac098d6e32e26cba9ab8
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-29 13:25:32 +0700

    Create 7kyu\083-Fizz-Buzz

---
+++ 7kyu/083-Fizz-Buzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FizzBuzz
+{
+    public static string[] GetFizzBuzzArray(int n)
+    {
+        // return Array.Empty<string>();
+        return Enumerable.Range(1, n).Select(s => s%3==0 && s%5==0 ? "FizzBuzz" : s%3==0 ? "Fizz" : s%5==0 ? "Buzz" : s.ToString()).ToArray();
+    }
+
+    public static void Main()
+    {
+        foreach (var item in GetFizzBuzzArray(15))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
