6c5acde1cc8133f8913de4275e85408c42fbce90
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-29 13:55:30 +0700

    Create 7kyu\085-Greet-Me

---
+++ 7kyu/085-Greet-Me/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+  public static string Greet(string name)
+  {
+    // throw new NotImplementedException();
+    return $"Hello {name.ToUpper()[0]}{name.Substring(1).ToLower()}!";
+  }
+
+  public static void Main(){
+    Console.WriteLine(Greet("JACK"));
+  }
+}
