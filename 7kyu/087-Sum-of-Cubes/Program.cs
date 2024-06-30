ae5779a40df107db0b43f83b318751993fa03be4
Achmad Lutfi <alhacker1996@gmail.com>
2024-06-30 10:27:14 +0700

    Create 7kyu\087-Sum-of-Cubes

---
+++ 7kyu/087-Sum-of-Cubes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static long SumCubes(int n)
+  {
+    return (long) Enumerable.Range(1, n).Sum(x=>Math.Pow(x, 3));
+
+    // Alternative
+    // return (long) Math.Pow(n*(n+1)/2,2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(SumCubes(3));
+  }
+}
