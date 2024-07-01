9f5c5276c5798071e2086a2c8855aea02280ab07
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-01 10:36:34 +0700

    Create 7kyu\090-Power-of-two

---
+++ 7kyu/090-Power-of-two/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+public static class Kata
+{
+  public static bool PowerOfTwo(int n)
+  {
+    //Math --> Magic
+    return n > 0 ? Math.Pow(2, Math.Ceiling(Math.Log2(n))) == n : false;
+
+    // Alternative 1
+    // return new BigInteger(n).IsPowerOfTwo;
+    // Alternative 2
+    // return BitOperations.IsPow2(n);
+  }
+
+  public static void Main(){
+    Console.WriteLine(PowerOfTwo(333));
+  }
+}
