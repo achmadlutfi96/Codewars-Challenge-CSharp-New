04a3d0df9f85921b7f7ffb28d2a28cd4b479ec63
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-01 10:23:56 +0700

    Create 7kyu\089-Triangular-Treasure

---
+++ 7kyu/089-Triangular-Treasure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int Triangular(int n)
+  {
+    return n > 0 ? Enumerable.Range(1,n).Sum(i => i) : 0;
+
+    // Alternative
+    // return n < 0 ? 0 : n*(n+1)/2;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Triangular(2));
+  }
+}
