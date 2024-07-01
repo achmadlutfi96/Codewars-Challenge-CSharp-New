003723a580b99505c3f54e4b3052fb153db313a6
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-01 10:58:03 +0700

    Create 7kyu\091-Most-digits

---
+++ 7kyu/091-Most-digits/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+  public static int FindLongest(int[] number)
+  {
+    // code here
+    List<int> nlist = number.Select(x => x.ToString().Length).ToList();
+    return number[nlist.IndexOf(nlist.Max())];
+
+    // Alternative 1
+    // return N.OrderByDescending(X => X.ToString().Length).First();
+    // Alternative 2
+    // return number.First(i => $"{i}".Length == $"{number.Max()}".Length);
+  }
+
+  public static void Main(){
+    Console.WriteLine(FindLongest(new int[] {8, 900, 500}));
+  }
+}
