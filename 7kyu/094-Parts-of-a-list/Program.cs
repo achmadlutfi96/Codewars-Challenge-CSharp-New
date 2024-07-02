03d1479cc2d6f07c17ebe0e9bf1c87e5a3ce686f
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-02 13:57:52 +0700

    Create 7kyu\094-Parts-of-a-list

---
+++ 7kyu/094-Parts-of-a-list/Program.cs
@@ -0,0 +1,30 @@
+﻿public class PartList
+{
+    public static string[][] Partlist(string[] arr)
+    {
+        // your code
+        string[][] res = new string[arr.Length - 1][];
+        for (int i = 0; i < arr.Length-1; i++)
+        {   
+            string[] temp = new string[2];
+            temp[0] = string.Join(" ", arr, 0, i+1);
+            temp[1] = string.Join(" ", arr, i+1 , arr.Length-i-1);
+            res[i] = temp;
+        }
+        return res;
+
+        // Alternative
+        // return arr.Select((s, i) => new[] {string.Join(" ", arr.Take(i)), string.Join(" ", arr.Skip(i))}).Skip(1).ToArray();
+    }
+
+    public static void Main()
+    {
+        foreach (var i in Partlist(new String[] { "vJQ", "anj", "mQDq", "sOZ" }))
+        {
+            foreach (var j in i)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
