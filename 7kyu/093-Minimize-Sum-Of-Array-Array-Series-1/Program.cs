8d89b07c1fb983312a9efba4030468863fdd54d9
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-02 13:06:06 +0700

    Create 7kyu\093-Minimize-Sum-Of-Array-Array-Series-1

---
+++ 7kyu/093-Minimize-Sum-Of-Array-Array-Series-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Kata
+{
+    public static int MinSum(int[] a)
+    {
+        //Do Some Magic
+        Array.Sort(a);
+        int[] a1 = a.Take(a.Length/2).ToArray();
+        int[] a2 = a.Reverse().Take(a.Length/2).ToArray();
+        return a1.Select((v,i) => v*a2[i]).Sum();
+
+        // Alternative
+        // return a.OrderBy(n => n).Skip(a.Length / 2).Zip(a.OrderByDescending(n => n).Skip(a.Length / 2), (x, y) => x * y).Sum();
+    }
+
+    public static void Main(){
+        Console.WriteLine(MinSum([5, 4, 2, 3]));
+    }
+}
