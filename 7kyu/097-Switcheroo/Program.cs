f8555ad3ba5dc03fef1da55e6b4dd90e90b1ebe7
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-03 12:20:34 +0700

    Create 7kyu\097-Switcheroo

---
+++ 7kyu/097-Switcheroo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string Switcheroo(string x)
+  {
+    return string.Concat(x.Select(c=>c.Equals('b') ? 'a' : c.Equals('a') ? 'b' : c).ToArray());
+
+    // Alternative
+    // return x.Replace("a","d").Replace("b","a").Replace("d","b");
+  }
+
+  public static void Main(){
+    Console.WriteLine(Switcheroo("bbbacccabbb"));
+  }
+}
