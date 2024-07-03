62b3d24da29df7846895fad23143013b5b54232b
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-03 12:06:40 +0700

    Create 7kyu\096-Convert-an-array-of-strings-to-array-of-num

---
+++ 7kyu/096-Convert-an-array-of-strings-to-array-of-numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿public class stringArrayMethods
+{
+  public static double[] ToDoubleArray(string[] stringArray)
+  {
+    return stringArray.Select(d=>double.Parse(d)).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(ToDoubleArray(["1", "2", "3"]));
+  }
+}
