2251960addb5f9036bdf86a88b93fe343f462538
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-03 12:02:21 +0700

    Create 7kyu\095-Build-a-square

---
+++ 7kyu/095-Build-a-square/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string GenerateShape(int n)
+  {
+    // throw new NotImplementedException();
+    return string.Join('\n', Enumerable.Range(1, n).Select(i=>string.Concat(Enumerable.Repeat("+", n).ToArray())).ToArray());  
+
+    // Alternative 1
+    // return string.Join("\n", Enumerable.Repeat(new string('+', n), n));
+
+    // Alternative 2
+    // return string.Join("\n", Enumerable.Repeat("".PadLeft(n, '+'), n));
+  }
+
+  public static void Main(){
+    Console.WriteLine(GenerateShape(3));
+  }
+}
