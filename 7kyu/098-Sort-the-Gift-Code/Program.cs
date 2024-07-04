ed158950d711bdb3bb64a03e67ea611bb4ff1187
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-04 11:05:42 +0700

    Create 7kyu\098-Sort-the-Gift-Code

---
+++ 7kyu/098-Sort-the-Gift-Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static string SortGiftCode(string code)
+  {
+    //TODO
+    return string.Concat(code.OrderBy(x=>x).ToArray());
+  }
+
+  public static void Main(){
+    Console.WriteLine(SortGiftCode("zyxwvutsrqponmlkjihgfedcba"));
+  }
+}
