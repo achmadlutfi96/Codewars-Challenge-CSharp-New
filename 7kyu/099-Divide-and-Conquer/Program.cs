41c85a2a089bc6a2192140a80b262039ad6bc98c
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-04 11:20:52 +0700

    Create 7kyu\099-Divide-and-Conquer

---
+++ 7kyu/099-Divide-and-Conquer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int DivCon(Object[] objArray)
+  {
+    return objArray.Where(i=>i.GetType()==typeof(int)).Sum(x=>(int)x) - objArray.Where(s=>s.GetType()==typeof(string)).Sum(x=>Convert.ToInt32(x));
+
+    // Alternative
+    // objArray.Sum(t=>(t is int) ?(int) t :-int.Parse((string)t))
+  }
+
+  public static void Main(){
+    Console.WriteLine(DivCon(new object[] {9, 3, "7", "3"}));
+  }
+}
