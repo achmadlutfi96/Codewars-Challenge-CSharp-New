fca59539255aaae4eeb62bf4e89984ae9ecb5b77
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-05 12:31:07 +0700

    Create 7kyu\101-Moves-in-squared-strings-I

---
+++ 7kyu/101-Moves-in-squared-strings-I/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Opstrings
+{   
+    public static string VertMirror(string strng)
+    {
+        // your 
+        return string.Join('\n',strng.Split("\n").Select(x=>string.Concat(x.Reverse())));
+    }
+    public static string HorMirror(string strng)
+    {
+       return string.Join("\n",strng.Split("\n").Reverse().ToArray());
+    }
+    public static string Oper(Func<string, string> fct, string s)
+    {
+        // your code and complete ... before operator
+        return fct(s);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine(Opstrings.Oper(Opstrings.HorMirror, "lVHt\nJVhv\nCSbg\nyeCt"));
+        Console.WriteLine(Opstrings.Oper(Opstrings.VertMirror,"hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"));
+    }
+}
