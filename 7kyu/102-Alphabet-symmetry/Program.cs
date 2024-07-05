6b883c09ec03177eb7c321e726f129fbc7399abe
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-05 15:12:30 +0700

    Create 7kyu\102-Alphabet-symmetry

---
+++ 7kyu/102-Alphabet-symmetry/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static List<int> Solve(List<string> arr)
+  {
+    return arr.Select(x=>x.ToLower().Where((v, i)=>(int)v-97 == i).Count()).ToList();
+
+    // Alternative
+    // return arr.Select(s => s.Where((c, i) => c % 32 == i + 1).Count()).ToList();
+  }
+
+  public static void Main(){
+    // int i = (int) 'a'- 96;
+    // Console.WriteLine(i);
+    foreach (var item in Solve(new List<string> {"abode", "ABc", "xyzD"}))
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
