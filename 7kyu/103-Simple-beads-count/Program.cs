d6f0290e786a06a6e6bf2f079b59e1c5937cfc71
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-05 16:11:39 +0700

    Create 7kyu\103-Simple-beads-count

---
+++ 7kyu/103-Simple-beads-count/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int CountRedBeads(int n)
+  {
+    // throw new NotImplementedException();
+    return n > 0 ? (n-1)*2 : 0;
+
+    // Alternative
+    // return Math.Max(0, 2 * n - 2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(CountRedBeads(3));
+  }
+}
