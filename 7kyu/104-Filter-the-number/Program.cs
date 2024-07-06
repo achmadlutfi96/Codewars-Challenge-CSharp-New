eb69db7d358246f22e2e1be0aa687db5547a9c36
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-06 10:11:54 +0700

    Create 7kyu\104-Filter-the-number

---
+++ 7kyu/104-Filter-the-number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int FilterString(string s)
+  {
+    //Your code goes here
+    return int.Parse(string.Concat(s.Where(x=>char.IsDigit(x))));
+
+    // Alternative
+    // return int.Parse(string.Concat(s.Where(char.IsNumber)));
+  }
+
+  public static void Main(){
+    Console.WriteLine(FilterString("aa1bb2cc3dd"));
+  }
+}
