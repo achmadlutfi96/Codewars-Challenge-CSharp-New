22e7ea520d4a9b6a98d18d4b8c0b847a657a849e
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-06 11:25:49 +0700

    Create 7kyu\106-Sum-of-numbers-from-0-to-N

---
+++ 7kyu/106-Sum-of-numbers-from-0-to-N/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+public class SequenceSum
+{
+    public static string ShowSequence(int n)
+    {
+        if (n == 0)
+        {
+            return "0=0";
+        }
+        else if (n < 0)
+        {
+            return $"{n}<0";
+        }
+        else
+        {
+            int[] numbers = Enumerable.Range(0, n + 1).ToArray();
+            return n == 0 ? "0=0" : $"{string.Join('+', numbers)} = {numbers.Sum()}";
+        }
+
+        // Alternative
+        // if(n < 0) return $"{n}<0";
+        // if(n == 0) return "0=0";
+
+        // IEnumerable<int> sequence = Enumerable.Range(0, n+1);
+
+        // return $"{String.Join("+", sequence)} = {sequence.Sum()}";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(ShowSequence(6));
+    }
+}
