89ce8cc93020fb5350f6314b1032da94de5d8d8e
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-06 10:20:32 +0700

    Create 7kyu\105-All-unique

---
+++ 7kyu/105-All-unique/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Code
+{
+  public class Unique
+  {
+    public static bool HasUniqueChars(string str)
+    {
+        //TODO
+        return str.Distinct().Count() == str.Length;
+    }
+
+    public static void Main(){
+        Console.WriteLine(HasUniqueChars("abcdef"));
+        Console.WriteLine(HasUniqueChars("aba"));
+    }
+  }
+}
