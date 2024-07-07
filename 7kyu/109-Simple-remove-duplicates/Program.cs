2a3b2b285343f459250b8f70d454b642e5f8d493
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-07 13:00:33 +0700

    Create 7kyu\109-Simple-remove-duplicates

---
+++ 7kyu/109-Simple-remove-duplicates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public static int[] solve(int[] arr)
+    {
+        //..      
+
+        return arr.Reverse().Distinct().Reverse().ToArray();
+    }
+
+    public static void Main()
+    {
+        foreach (var item in solve(new int[] { 3, 4, 4, 3, 6, 3 }))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
