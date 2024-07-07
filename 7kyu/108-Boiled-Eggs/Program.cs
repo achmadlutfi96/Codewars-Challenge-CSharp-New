2c318214d445bddc20e7f1ae59dfe92c3f36a550
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-07 12:46:33 +0700

    Create 7kyu\108-Boiled-Eggs

---
+++ 7kyu/108-Boiled-Eggs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Kata
+{
+  public static int CookingTime(int eggs)
+  {
+    // TODO
+    return (int) Math.Ceiling((double)eggs/8) * 5;
+  }
+
+  public static void Main(){
+    Console.WriteLine(CookingTime(10));
+  }
+}
