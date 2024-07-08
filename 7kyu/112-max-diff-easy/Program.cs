7231a82635b117dd095236cd2d1db009cbd736f4
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-08 13:12:48 +0700

    Create 7kyu\112-max-diff-easy

---
+++ 7kyu/112-max-diff-easy/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static int MaxDiff(int[] lst)
+  {
+    return lst.Length > 1 ? lst.Max()-lst.Min() : 0;
+  }
+
+  public static void Main(){
+    Console.WriteLine(MaxDiff(new[]{ 0, 1, 2, 3, 4, 5, 6 }));
+  }
+}
