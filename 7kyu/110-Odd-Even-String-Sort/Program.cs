184a949a65ab97d9f95aaa42a5eb381f80616ae4
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-08 12:25:49 +0700

    Create 7kyu\110-Odd-Even-String-Sort

---
+++ 7kyu/110-Odd-Even-String-Sort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string SortMyString(string s)
+    {
+        // your code here
+        return $"{string.Concat(s.Where((v,i)=>i%2==0))} {string.Concat(s.Where((v,i)=>i%2==1))}";
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(SortMyString("CodeWars"));
+    }
+}
