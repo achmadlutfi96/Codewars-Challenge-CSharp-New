189c892c9671ec7a4ce61a8ada9adca511910864
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-09 14:54:33 +0700

    Create 7kyu\115-Ordered-Count-of-Characters

---
+++ 7kyu/115-Ordered-Count-of-Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Solution {
+    public class Kata {
+        public static List<Tuple<char, int>> OrderedCount(string input) {
+            // Implement me!
+            return input.Distinct().Select(x=>new Tuple<char, int>(x, input.Where(i=>i==x).Count())).ToList();
+
+            // Alternative 1
+            // return input
+            //    .GroupBy(x => x)
+            //    .Select(x => Tuple.Create(x.Key, x.Count()))
+            //    .ToList();
+
+            // Alternative 2
+            // return input.GroupBy(x => x, (c, g) => (c, g.Count()).ToTuple());
+        }
+
+        public static void Main(){
+            foreach (var items in OrderedCount("abracadabra"))
+            {
+                Console.WriteLine($"{items.Item1}-{items.Item2}");
+            }
+        }
+    }
+}
