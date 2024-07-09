01c206dcf3cf76e7afa15323b9bf0bf53b507d49
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-09 14:02:17 +0700

    Create 7kyu\113-Palindrome-chain-length

---
+++ 7kyu/113-Palindrome-chain-length/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int palindromeChainLength(int n)
+    {
+        int step = 0;
+        long sum = n;
+        while (sum != long.Parse(string.Concat(sum.ToString().Reverse())))
+        {
+            sum += long.Parse(string.Concat(sum.ToString().Reverse()));
+            step++;
+        }
+
+        return step;
+
+        // Alternative 1
+        // int r = int.Parse(n.ToString().Reverse().Aggregate("", (s,c) => s+c));
+        // return (n == r) ? 0 : (1 + palindromeChainLength(r+n));
+
+        // Alternative 2
+        // var reversed = ulong.Parse(string.Concat($"{n}".Reverse()));
+        // return reversed == n ? 0 : 1 + palindromeChainLength(n + reversed);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(palindromeChainLength(87));
+    }
+}
