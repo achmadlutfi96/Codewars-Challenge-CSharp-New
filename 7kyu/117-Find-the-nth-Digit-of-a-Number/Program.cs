0cc13a650f9cc908588e9611fa9e700896109a0e
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-10 11:50:24 +0700

    Create 7kyu\117-Find-the-nth-Digit-of-a-Number

---
+++ 7kyu/117-Find-the-nth-Digit-of-a-Number/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+  public static int FindDigit(int num, int nth)
+  {
+    string digit = Math.Abs(num).ToString();
+    return digit.Length < nth ? 0 : nth < 1 ? -1 : (int) char.GetNumericValue(digit.Reverse().ToArray()[nth-1]);
+
+    // Alternative
+    // return nth <= 0 ? -1 : (int) Math.Abs(num / Math.Pow(10, nth - 1) % 10);
+  }
+
+  public static void Main(){
+    Console.WriteLine(FindDigit(-2825, 3));
+  }
+}
