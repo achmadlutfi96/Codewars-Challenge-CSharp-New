4eda3c308aa84da5ae811f8454f6d1e0bfee0441
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-10 11:35:51 +0700

    Create 7kyu\116-Sum-of-Odd-Cubed-Numbers

---
+++ 7kyu/116-Sum-of-Odd-Cubed-Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Kata
+{
+  public static int CubeOdd(int[] arr)
+  {
+    
+    // insert code here >.<
+    return arr.Select(x=>(int)Math.Pow(x,3)).Where(x=>x%2!=0).Sum();
+
+    // Alternative
+    // return arr.Sum(i => i % 2 == 0 ? 0 : i * i * i);
+  }
+
+  static void Main(){
+    Console.WriteLine(CubeOdd(new int[] {1, 2, 3, 4}));
+    Console.WriteLine(CubeOdd(new int[] {-3, -2, 2, 3}));
+  }
+}
