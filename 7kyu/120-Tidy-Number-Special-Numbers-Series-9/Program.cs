d050fc9da01a07f89293141a5832c6fcc1589d09
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-11 13:10:32 +0700

    Create 7kyu\120-Tidy-Number-Special-Numbers-Series-9

---
+++ 7kyu/120-Tidy-Number-Special-Numbers-Series-9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+class Kata
+{
+    public static bool TidyNumber(int n)
+    {
+        //Do Some Magic
+        return n.ToString() == string.Concat(n.ToString().OrderBy(x=>x));
+
+        // Alternative
+        // return $"{n}" == string.Concat($"{n}".OrderBy(c => c));
+    }
+
+    public static void Main(){
+        Console.WriteLine(TidyNumber(2789));
+    }
+}
