83d17ff98e04bf046b20ceb1ff1216a32d0998a7
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-11 13:01:07 +0700

    Create 7kyu\119-Nth-Smallest-Element-Array-Series-4

---
+++ 7kyu/119-Nth-Smallest-Element-Array-Series-4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+class Kata
+{
+    public static int NthSmallest(int[] arr, int pos)
+    {
+        //Do Some Magic
+        return arr.OrderBy(x => x).ElementAt(pos-1);
+    }
+
+    public static void Main(){
+        Console.WriteLine(NthSmallest([3,1,2], 2));
+    }
+}
