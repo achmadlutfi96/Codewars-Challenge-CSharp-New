3174a41f589cadc3928ca217d798decd1b226230
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-11 13:17:12 +0700

    Create 7kyu\121-Automorphic-Number-Special-Numbers-Seri

---
+++ 7kyu/121-Automorphic-Number-Special-Numbers-Series-6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Kata
+{
+    public static string Automorphic(int n)
+    {
+        //Do Some Magic
+        return Math.Pow(n,2).ToString().EndsWith(n.ToString()) ? "Automorphic" : "Not!!" ;
+    }
+
+    static void Main(){
+        Console.WriteLine(Automorphic(25));
+    }
+}
