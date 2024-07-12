45adae574dc864544f18c4502cad72d4995168a1
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-12 12:19:34 +0700

    Create 7kyu\122-Simple-Fun-152-Invite-More-Women

---
+++ 7kyu/122-Simple-Fun-152-Invite-More-Women/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public static bool InviteMoreWomen(int[] L){
+          //coding and coding..
+          return L.Where(x=>x == -1).Count() < L.Where(x=>x == 1).Count();
+
+          // Alternative
+        //   return L.Sum() > 0;
+          
+        }
+
+        public static void Main(){
+            Console.WriteLine(InviteMoreWomen(new int[]{1, -1, 1}));
+        }
+    }
+}
