ecf8686b2d8e984910e7e49888e5672671d7cbe9
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-13 10:37:17 +0700

    Create 7kyu\127-Maximum-Triplet-Sum-Array-Series-7

---
+++ 7kyu/127-Maximum-Triplet-Sum-Array-Series-7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+class Kata
+{
+    public static int MaxTriSum(int[] a)
+    {
+        //Do Some Magic
+        return a.Distinct().OrderBy(x => x).TakeLast(3).Sum();
+    }
+
+   public static void Main(){
+        Console.WriteLine(MaxTriSum(new int[]{3, 2, 6, 8, 2, 3}));
+    }
+}
