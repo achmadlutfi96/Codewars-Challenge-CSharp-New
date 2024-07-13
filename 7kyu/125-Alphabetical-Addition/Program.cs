504fc16615cb3a69550aa8367685c4b691861c4b
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-13 10:17:05 +0700

    Create 7kyu\125-Alphabetical-Addition

---
+++ 7kyu/125-Alphabetical-Addition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public class Kata {
+  public static char AddLetters(char[] letters) {
+    // your code here
+    if (letters.Length == 0) return 'z';
+    string alphabet = " abcdefghijklmnopqrstuvwxyz";
+    int res =letters.Select(x=>alphabet.IndexOf(x)).Sum();
+    return res > 26 ? alphabet[res%26] : alphabet[res];
+
+    // Alternative
+    // var val = letters.Sum(c => c-96) % 26;
+    // return val == 0 ? 'z' : (char)(val + 96);
+  }
+
+  static void Main(){
+    Console.WriteLine(AddLetters(new char[] {'a', 'b', 'c'}));
+    Console.WriteLine(AddLetters(new char[] {'z', 'a'}));
+    Console.WriteLine(AddLetters(new char[] {'z'}));
+    Console.WriteLine(AddLetters(new char[] {}));
+  }
+}
+
