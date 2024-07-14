9841d622a501e437578442adcfb88045c4ae01f4
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-14 12:26:47 +0700

    Create 7kyu\129-Currying-functions-multiply-all-element

---
+++ 7kyu/129-Currying-functions-multiply-all-elements-in-an-array/Program.cs
@@ -0,0 +1,13 @@
+﻿/* left blank for unlimited creativity :) */
+class Kata {
+    public static Func<int, int[]> MultiplyAll(int[] n){
+        return (int m) => n.Select(x=>x*m).ToArray();
+    }
+
+    public static void Main(){
+        foreach (var item in MultiplyAll(new int[] {1, 2, 3})(2))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
