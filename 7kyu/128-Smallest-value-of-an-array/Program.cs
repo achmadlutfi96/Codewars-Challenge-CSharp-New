2442be12b325b9af7511354398226386e1dc3091
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-14 11:37:10 +0700

    Create 7kyu\128-Smallest-value-of-an-array

---
+++ 7kyu/128-Smallest-value-of-an-array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int FindSmallest(int[] numbers, string toReturn)
+  {
+    //Code Me
+    return toReturn == "index" ? Array.IndexOf(numbers, numbers.Min()) : numbers.Min();
+  }
+
+  public static void Main(){
+    Console.WriteLine(FindSmallest(new int[]{ 5, 4, 3, 2, 1}, "index"));
+  }
+}
