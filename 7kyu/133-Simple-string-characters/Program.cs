84edebbdfda8242b46549226df05851a221ca6d5
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-15 13:18:21 +0700

    Create 7kyu\133-Simple-string-characters

---
+++ 7kyu/133-Simple-string-characters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Solution
+{
+    public static int[] solve(String s)
+    {
+        //..
+        int first = s.Where(char.IsUpper).Count();
+        int second = s.Where(char.IsLower).Count();
+        int third = s.Where(char.IsDigit).Count();
+        int fourth = s.Where(x => !char.IsLetterOrDigit(x)).Count();
+        return new int[] { first, second, third, fourth };
+
+        // Alternative
+        // return new[]
+        // {
+        //     s.Count(char.IsUpper),
+        // s.Count(char.IsLower),
+        // s.Count(char.IsDigit),
+        // s.Count(x => !char.IsLetterOrDigit(x))
+        // };
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", solve("Codewars@codewars123.com")));
+    }
+}
