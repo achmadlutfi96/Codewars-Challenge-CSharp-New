56245499e2dddf31bba2486dd4d649557059d02a
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-16 14:18:54 +0700

    Create 7kyu\136-Sort-by-Last-Char

---
+++ 7kyu/136-Sort-by-Last-Char/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string[] Last(string x)
+  {
+    return x.Split(' ').OrderBy(x=>x.Last()).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(", ", Last("abc cba cab")));
+  }
+}
