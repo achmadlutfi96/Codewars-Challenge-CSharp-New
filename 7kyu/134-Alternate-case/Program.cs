40d282bf15666ed54bbd6f5f91430e9d2e05378a
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-16 13:56:28 +0700

    Create 7kyu\134-Alternate-case

---
+++ 7kyu/134-Alternate-case/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static string alternateCase(string s)
+    {
+      return string.Concat(s.Select(x=>char.IsUpper(x) ? char.ToLower(x) : char.ToUpper(x)));
+    }
+
+    public static void Main(){
+        Console.WriteLine(alternateCase("Hello World!"));
+    }
+  }
+}
