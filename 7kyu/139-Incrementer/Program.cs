9140436ed32f495ffa2020f5bc0e07e62cf4ed56
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-17 14:07:40 +0700

    Create 7kyu\139-Incrementer

---
+++ 7kyu/139-Incrementer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+public static class Kata
+{
+    public static int[] Incrementer(int[] numbers)
+    {
+        return numbers.Select((v, i)=>(v+i+1)%10).ToArray(); // replace with your code ..
+    }
+
+    public static void Main(){
+        Console.WriteLine(string.Join(", ", Incrementer(new int[]{1, 2, 3})));
+    }
+}
