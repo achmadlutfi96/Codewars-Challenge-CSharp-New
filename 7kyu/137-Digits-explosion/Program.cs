f5539488b53498e8b6d3dcd020d4e8302f0b8f2a
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-17 13:41:09 +0700

    Create 7kyu\137-Digits-explosion

---
+++ 7kyu/137-Digits-explosion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Digits
+{
+  public static string Explode(string s)
+  {
+    // Insert your solution here
+    return string.Concat(s.Select(x=>string.Concat(Enumerable.Repeat(x, (int)char.GetNumericValue(x)))));
+
+    // Alternative 1
+    // return string.Join("", s.Select(c => new String(c, int.Parse(c.ToString()))));
+    // Alternative 2
+    // return string.Concat(s.Select(n => new string(n, n - 48)));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Explode("312"));
+  }
+}
