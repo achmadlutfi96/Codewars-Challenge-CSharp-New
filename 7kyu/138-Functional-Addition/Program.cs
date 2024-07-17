1f1a261eb25d4b5b289b9d54431dd2a8cd8ccb94
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-17 13:56:43 +0700

    Create 7kyu\138-Functional-Addition

---
+++ 7kyu/138-Functional-Addition/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Kata
+{
+  public static Func<double, double> Add(double n)
+  {
+    return (m)=>n+m;    
+  }
+
+  public static void Main(){
+    Console.WriteLine(Add(1)(3));
+  }
+}
