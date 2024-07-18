831c451ce5c4190f4ca8397adfb08d22d6e4cf53
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-18 22:25:47 +0700

    Create 7kyu\142-Folding-your-way-to-the-moon

---
+++ 7kyu/142-Folding-your-way-to-the-moon/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Kata
+{
+  public static int? FoldTo(double distance)
+  {
+    if(distance < 0) return null;
+    int fold = 0;
+    double thickness = 0.0001;
+    while (thickness < distance)
+    {
+        thickness *= 2;
+        fold++;     
+    }
+    return fold;
+
+    // Alternative
+    // return distance < 0 ? null : (int?) (distance < 0.0001 ? 0 : Math.Ceiling(Math.Log(distance * 10000, 2)));
+  }
+
+  public static void Main(){
+    Console.WriteLine(FoldTo(384000000));
+  }
+}
