2615eda48031221ba4adc138078230aaeef30db2
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-18 22:01:32 +0700

    Create 7kyu\141-Char-Code-Calculation

---
+++ 7kyu/141-Char-Code-Calculation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static Int32 Calc(String s)
+  {
+    string total1 = string.Concat(s.Select(x=>((int)x).ToString()));
+    string total2 = total1.Replace("7", "1");
+    return total1.Sum(a=>(int)char.GetNumericValue(a))-total2.Sum(b=>(int)char.GetNumericValue(b));
+
+    // Alternative
+    // return string.Concat(s.Select(x => (int) x)).Count(x => x == '7') * 6;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Calc("ABC"));
+    Console.WriteLine(Calc("sUuPmNnnJOOCAGOuyzmcHQGJhXHYgZLY"));
+  }
+}
