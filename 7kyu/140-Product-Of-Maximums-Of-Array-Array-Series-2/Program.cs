d5c318bbbffb1fea583df264d3b7f957a46ce737
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-18 21:30:45 +0700

    Create 7kyu\140-Product-Of-Maximums-Of-Array-Array-Series-2

---
+++ 7kyu/140-Product-Of-Maximums-Of-Array-Array-Series-2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+public class Kata {
+  public static int MaxProduct(int[] arr, int size) {
+    // your code here
+    return arr.OrderBy(x => x).TakeLast(size).Aggregate((a,b)=>a*b);
+  }
+
+  public static void Main(){
+    Console.WriteLine(MaxProduct(new int[] { 4, 3, 5 }, 2));
+  }
+}
