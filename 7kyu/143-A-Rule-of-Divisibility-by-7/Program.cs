d7be8422774d231b2dee3775432f0d12566e5d13
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-19 21:36:01 +0700

    Create 7kyu\143-A-Rule-of-Divisibility-by-7

---
+++ 7kyu/143-A-Rule-of-Divisibility-by-7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class DivSeven
+{
+    public static long[] Seven(long m)
+    {
+        long n = 0;
+        // your code
+        while (m > 99)
+        {
+            string s = m.ToString();
+            m = (long)int.Parse(s.Substring(0, s.Length - 1)) - (2 * int.Parse(s.Substring(s.Length - 1, 1)));
+            n++;
+        }
+        return new[] { m, n };
+
+        // Alternative
+        // int step = 0;
+
+        // while (m > 99)
+        // {
+        //     m = m / 10 - m % 10 * 2;
+        //     step++;
+        // }
+        // return new long[] { m, step };
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", Seven(1603)));
+    }
+}
