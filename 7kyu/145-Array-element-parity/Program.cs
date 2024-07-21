7dc6258fd87f71d2fb8d792a5f9baf44830711ed
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-21 13:19:50 +0700

    Create 7kyu\145-Array-element-parity

---
+++ 7kyu/145-Array-element-parity/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static int Solve(List<int> arr)
+  {
+    // throw new NotImplementedException();
+    return arr.Where(x=>!arr.Contains(-x)).First();
+
+    // Alternative 1
+    // return arr.Distinct().Sum();
+
+    // Alternative 2
+    // return arr.First(x => !arr.Contains(-x));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Solve(new List<int> {-3,1,2,3,-1,-4,-2}));
+  }
+}
