991d828b16f5792b364057ebfb2ddddfbfa94477
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-22 11:39:04 +0700

    Create 7kyu\146-Indexed-capitalization\146-Indexed-capitalization.csproj

---
+++ 7kyu/146-Indexed-capitalization/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static string Capitalize(string s, List<int> idxs)
+  {
+    // throw new NotImplementedException();
+    return string.Concat(s.Select((v,i)=>idxs.Contains(i) ? char.ToUpper(v) : v));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Capitalize("abcdef", new List<int> {1, 2, 5}));
+  }
+}
