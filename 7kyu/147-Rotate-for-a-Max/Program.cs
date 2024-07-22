3427e7403a07ba65a760a39cdfb2c783b555583c
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-22 13:34:44 +0700

    Create 7kyu\147-Rotate-for-a-Max

---
+++ 7kyu/147-Rotate-for-a-Max/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class MaxRotate
+{
+
+    public static long MaxRot(long n)
+    {
+        // your code
+        string num = n.ToString();
+
+        for (int i = 0; i < num.Length - 1; i++) {
+            num = num.Substring(0, i) + 
+                    num.Substring(i + 1) + 
+                    num.ElementAt(i);
+            
+            if (long.Parse(num) > n) {
+                n = long.Parse(num);
+            }
+        }
+        return n;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(MaxRot(38458215));
+    }
+}
