db452e2abeecef6b52cb3503f719e17b7fe8baac
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-22 13:42:12 +0700

    Create 7kyu\148-FIXME-Get-Full-Name

---
+++ 7kyu/148-FIXME-Get-Full-Name/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Dinglemouse
+{
+  private string firstName;
+  private string lastName;
+  public string FullName
+  {
+    get
+    {
+      return (firstName+" "+lastName).Trim();
+    }
+  }
+  
+  public Dinglemouse(string firstName, string lastName)
+  {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
