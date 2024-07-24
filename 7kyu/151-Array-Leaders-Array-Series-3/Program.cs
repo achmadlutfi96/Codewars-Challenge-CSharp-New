84d7b91c1ac9eec7ae0b4d2500662a975905aea1
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-24 09:54:38 +0700

    Create 7kyu\151-Array-Leaders-Array-Series-3

---
+++ 7kyu/151-Array-Leaders-Array-Series-3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int[] ArrayLeaders(int[] numbers)
+  {
+    // throw new NotImplementedException();
+    return numbers.Where((x,i)=>x > numbers.Skip(i+1).Sum()).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(',',ArrayLeaders(new int[] { 16, 17, 4, 3, 5, 2 })));
+  }
+}
