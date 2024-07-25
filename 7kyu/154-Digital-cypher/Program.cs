86d8762c3b5c4e0c2a51351040e1276e745ced3f
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-25 10:23:27 +0700

    Create 7kyu\154-Digital-cypher

---
+++ 7kyu/154-Digital-cypher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System;
+
+public class Kata
+{
+    public static int[] Encode(string str, int n)
+    {
+        int[] strArr= str.Select(x=>x-96).ToArray();
+        int[] nArr= n.ToString().Select(x=>(int)char.GetNumericValue(x)).ToArray();
+        return strArr.Select((x,i)=>x+nArr[i%nArr.Length]).ToArray();
+
+        // Alternative
+        // return str.Select((c, i) => c - 96 + ($"{n}"[i % $"{n}".Length] - '0')).ToArray();
+    }
+
+    public static void Main(){
+        Console.WriteLine(string.Join(",", Encode("masterpiece", 1939)));
+    }
+}
