6510eb1efe565d4b0460d686d4710a7d7c63a654
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-25 10:00:06 +0700

    Create 7kyu\153-All-Inclusive

---
+++ 7kyu/153-All-Inclusive/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Rotations
+{
+  public static Boolean ContainAllRots(string strng, List<string> arr) 
+  {
+      // your code
+      return Enumerable.Repeat(strng , strng.Length).Select((x,i)=>x.Substring(i,strng.Length-i)+x.Substring(0,i)).Select(arr.Contains).All(x=>x == true);
+
+      // Alternative
+    // return !s.Where((c, i) => !arr.Contains((s + s).Substring(i, s.Length))).Any();
+  }
+
+  public static void Main(){
+    Console.WriteLine(ContainAllRots("bsjq",new List<string>(){ "bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs" }));
+  }
+}
