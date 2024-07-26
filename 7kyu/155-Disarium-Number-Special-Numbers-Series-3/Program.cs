c24fdaae28658879b294fcbe700854b5e15ec452
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-26 13:08:35 +0700

    Create 7kyu\155-Disarium-Number-Special-Numbers-Series-3

---
+++ 7kyu/155-Disarium-Number-Special-Numbers-Series-3/Program.cs
@@ -0,0 +1,16 @@
+﻿class Kata
+{
+    public static string DisariumNumber(int number)
+    {
+        //Do Some Magic
+        char[] n = number.ToString().ToCharArray();
+        return Enumerable.Range(1, n.Length).Select((x,i)=>Math.Pow(char.GetNumericValue(n[i]), x)).Sum() == number ? "Disarium !!" : "Not !!";
+
+        // Alternative
+        // return number.ToString().Select((x,i) => Math.Pow(char.GetNumericValue(x), ++i)).Sum() == number ? "Disarium !!" : "Not !!";
+    }
+
+    public static void Main(){
+        Console.WriteLine(DisariumNumber(89));
+    }
+}
