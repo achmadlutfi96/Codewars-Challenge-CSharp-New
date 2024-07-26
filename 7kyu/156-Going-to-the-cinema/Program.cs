92bad6a3bdcfb14fd4621a8be3fda473d5ac6e44
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-26 15:12:45 +0700

    Create 7kyu\156-Going-to-the-cinema

---
+++ 7kyu/156-Going-to-the-cinema/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class MovieC
+{
+
+    public static int Movie(int card, int ticket, double perc)
+    {
+        double systemA = 0;
+        double systemB = card;
+        double prev = ticket;
+        int times = 0;
+        while (systemA <= Math.Ceiling(systemB))
+        {
+            systemA += ticket;
+            prev *= perc;
+            systemB += prev;
+            times += 1;
+        }
+        return times;
+
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Movie(500, 15, 0.9));
+        Console.WriteLine(Movie(100, 10, 0.95));
+    }
+}
