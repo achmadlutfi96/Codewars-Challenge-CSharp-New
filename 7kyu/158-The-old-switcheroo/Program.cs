6069a70a69df25a8e3ca8439f3964d445be40aa5
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-27 14:30:23 +0700

    Create 7kyu\158-The-old-switcheroo

---
+++ 7kyu/158-The-old-switcheroo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string Vowel2Index(string str)
+  {
+    //code me
+    return string.Concat(str.Select((v,i)=>"aiueo".Contains(v)? (i+1).ToString()  : v.ToString()));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Vowel2Index("this is my string"));
+  }
+}
