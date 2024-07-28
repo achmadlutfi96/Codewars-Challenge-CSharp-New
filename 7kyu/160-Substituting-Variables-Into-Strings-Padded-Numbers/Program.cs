f2777e499a4c582e4e7f7a5acfd029dc57070b5b
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-28 12:32:19 +0700

    Create 7kyu\160-Substituting-Variables-Into-Strings-Pad

---
+++ 7kyu/160-Substituting-Variables-Into-Strings-Padded-Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿public class PaddedNumbers
+{
+    public static string Solution(int value)
+    {
+        return $"Value is {value.ToString().PadLeft(5, '0')}";
+
+        // Alternative
+        // return $"Value is {value:D5}";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Solution(5));
+    }
+}
