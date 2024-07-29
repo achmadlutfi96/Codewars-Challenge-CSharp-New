d0d8c16aa1ea8afb90cd1498865c2822269005b0
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-29 09:26:41 +0700

    Create 7kyu\161-Longest-vowel-chain

---
+++ 7kyu/161-Longest-vowel-chain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+  public static int Solve(string str)
+  {
+    // throw new NotImplementedException();
+    return Regex.Split(str, @"[^aiueo]").Select(x=>x.Length).Max();
+
+    // Alternative
+    // return Regex.Split(str, "[^aeiou]+").Max(e => e.Length);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Solve("suoidea"));
+  }
+}
