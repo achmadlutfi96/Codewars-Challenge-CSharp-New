1d75af05638df2065189feacba93515dba0f9112
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-30 11:11:06 +0700

    Create 7kyu\164-Word-values

---
+++ 7kyu/164-Word-values/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static int[] WordValue(string[] a)
+  {
+    return a.Select((v,i)=>v.Select(x=>x!=' '?(int)x-96 : 0).Sum()*(i+1)).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", WordValue(new string[] {"codewars", "abc", "xyz"})));
+  }
+}
