c4e674dc0a8e9e3e5e3534a960c6e743f4ef3a6f
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-30 11:02:16 +0700

    Create 7kyu\163-Numbers-to-Letters

---
+++ 7kyu/163-Numbers-to-Letters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static string Switcher(string[] x)
+    {
+        // throw new NotImplementedException();
+        string y = "zyxwvutsrqponmlkjihgfedcba!? ";
+        return string.Concat(x.Select(x=>y[int.Parse(x)-1]));
+    }
+
+    public static void Main(){
+        Console.WriteLine(Switcher(new string[] { "24", "12", "23", "22", "4", "26", "9", "8" }));
+    }
+}
