5d4c1d11e9326bcfefaaf01edcc3005f9e88484a
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-31 10:05:40 +0700

    Create 7kyu\165-Return-the-closest-number-multiple-of-10

---
+++ 7kyu/165-Return-the-closest-number-multiple-of-10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Kata
+{
+    public static int ClosestMultiple10(int num)
+    {
+        return (int) Math.Round((double)num/10, MidpointRounding.AwayFromZero)*10;
+    }
+
+    public static void Main(){
+        Console.WriteLine(ClosestMultiple10(25));
+    }
+}
