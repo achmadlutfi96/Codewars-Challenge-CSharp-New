509872f8c93f00f3a142b33785a20366016c16e4
Achmad Lutfi <alhacker1996@gmail.com>
2024-07-31 10:28:16 +0700

    Create 7kyu\166-Product-Array-Array-Series-5

---
+++ 7kyu/166-Product-Array-Array-Series-5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+class Kata
+{
+    public static int[] ProductArray(int[] array)
+    {
+        //Do Some Magic
+        return array.Select((x, i) => array.Where((a, b) => b != i).Aggregate((x, y) => x * y)).ToArray();
+
+        // Alternative 1
+        // return array.Select(i => array.Aggregate((x, y) => x * y) / i).ToArray();
+        // Alternative 2
+        // var product = array.Aggregate((x,y) => x*y);
+        // return array.Select(x => product / x).ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", ProductArray(new int[] { 1, 5, 2 })));
+    }
+}
