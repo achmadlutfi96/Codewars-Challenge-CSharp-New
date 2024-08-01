a8b9c275cf19079c5c3ac5796726b5a6a24da8db
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-01 11:22:51 +0700

    Create 7kyu\168-Cat-and-Mouse-Easy-Version

---
+++ 7kyu/168-Cat-and-Mouse-Easy-Version/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+  public static string CatMouse(string x)
+  {
+    // Your code here...
+    return x.Where(x=>x == '.').Count() <= 3 ? "Caught!" : "Escaped!";
+
+    // Alternative
+    // return x.Length > 5 ? "Escaped!" : "Caught!";
+    // return Regex.IsMatch(x, "C.{0,3}m") ? "Caught!" : "Escaped!";
+  }
+
+  public static void Main(){
+    Console.WriteLine(CatMouse("C..m"));
+  }
+}
