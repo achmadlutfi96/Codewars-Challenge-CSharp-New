ba4cf0e26450c9ea4b2d2ea68a3ca0e41e3c425d
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-02 09:50:38 +0700

    Create 7kyu\169-Complete-The-Pattern-1

---
+++ 7kyu/169-Complete-The-Pattern-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static string Pattern(int n)
+  {
+    // Happy Coding ^_^
+    if(n<1) return "";
+    return String.Join("\n", Enumerable.Range(1, n).Select((v,i)=>String.Concat(Enumerable.Repeat(v.ToString(), i+1))));
+
+    // Alternative 
+    // return string.Join("\n",
+    //   Enumerable
+    //     .Range(1, Math.Max(0, n))
+    //     .Select(i => i.ToString().Repeat(i)));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Pattern(11));
+  }
+}
