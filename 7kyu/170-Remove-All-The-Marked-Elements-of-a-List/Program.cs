d7fb6e93a7bc2b66d64c5257ef61e23059a02ec7
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-02 10:01:58 +0700

    Create 7kyu\170-Remove-All-The-Marked-Elements-of-a-List

---
+++ 7kyu/170-Remove-All-The-Marked-Elements-of-a-List/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+  public static int[] Remove(int[] integerList, int[] valuesList)
+  {
+    // your code here
+    return integerList.Where(x=>!valuesList.Contains(x)).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", Remove([1, 1, 2, 3, 1, 2, 3, 4], [1,3])));
+  }
+}
