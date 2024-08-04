da5e15d65db6bd655e16778859ccd9a43e9bed43
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-04 11:10:48 +0700

    Create 7kyu\174-Special-Number-Special-Numbers-Series-5

---
+++ 7kyu/174-Special-Number-Special-Numbers-Series-5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Kata
+{
+    public static string SpecialNumber(int number)
+    {
+        //Do Some Magic
+        return number.ToString().Select(x=>char.GetNumericValue(x) > 5 ? false : true).All(x=>x==true) ? "Special!!" : "NOT!!";
+
+        // Alternative
+        // return Regex.IsMatch(number.ToString(), "[^0-5]") ? "NOT!!" : "Special!!";
+    }
+
+    public static void Main(){
+        Console.WriteLine(SpecialNumber(00011));
+    }
+}
