0664e56645072a22470800f3c89520677ad0d137
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-04 11:02:17 +0700

    Create 7kyu\173-Return-the-first-M-multiples-of-N

---
+++ 7kyu/173-Return-the-first-M-multiples-of-N/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static double[] Multiples(int m, double n)
+  {
+    // code here
+    return Enumerable.Range(1, m).Select(i => i * n).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", Multiples(3, 5)));
+  }
+}
