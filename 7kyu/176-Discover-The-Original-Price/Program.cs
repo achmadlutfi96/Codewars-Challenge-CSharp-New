f9d834e7b52955b48936c2d55cb961c80fe305fd
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-05 11:35:40 +0700

    Create 7kyu\176-Discover-The-Original-Price

---
+++ 7kyu/176-Discover-The-Original-Price/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+    public static decimal DiscoverOriginalPrice(decimal discountedPrice, decimal salePercentage)
+    {
+        //TODO: Some code here!
+        return Math.Round(100M*discountedPrice/(100M-salePercentage), 2);
+    }
+
+    public static void Main(){
+        Console.WriteLine(DiscoverOriginalPrice(75M,25M));
+    }
+}
