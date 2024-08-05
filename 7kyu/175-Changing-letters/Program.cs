83455385caface96568b9c32875acdc63af8fd4b
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-05 11:12:49 +0700

    Create 7kyu\175-Changing-letters

---
+++ 7kyu/175-Changing-letters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+  public static string Swap(string s)
+  {
+    // throw new NotImplementedException();
+    return string.Concat(s.Select(x=>"aiueo".Contains(x) ? char.ToUpper(x) : x));
+
+    // Alternative
+    // return Regex.Replace(s, "[aeiou]", m => m.Value.ToUpper());
+  }
+
+  public static void Main(){
+    Console.WriteLine(Swap("HelloWorld!"));
+  }
+}
