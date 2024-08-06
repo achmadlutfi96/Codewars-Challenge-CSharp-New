0379942534b4391e2a9f55b04e0d985c15737da6
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-06 11:04:52 +0700

    Create 7kyu\178-Find-Count-of-Most-Frequent-Item-in-an-Array

---
+++ 7kyu/178-Find-Count-of-Most-Frequent-Item-in-an-Array/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+    public static int MostFrequentItemCount(int[] collection)
+    {
+        // Do your magic. :)
+        return collection.Length > 0 ?collection.Distinct().Select(x=>collection.Count(y=>x==y)).Max() : 0;
+
+        // Alternative
+        // return collection.Any() ? collection.GroupBy(i => i).Max(g => g.Count()) : 0;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(MostFrequentItemCount(new int[] {3, -1, -1}));
+    }
+}
