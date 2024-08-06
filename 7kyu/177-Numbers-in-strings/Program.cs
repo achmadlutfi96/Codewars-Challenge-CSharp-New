1c7942bb85004fcbf3845140710a9a168ac287b7
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-06 10:52:17 +0700

    Create 7kyu\177-Numbers-in-strings

---
+++ 7kyu/177-Numbers-in-strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static int Solve(string s)
+    {
+        // throw new NotImplementedException();
+        return Regex.Split(s, "[a-zA-Z]").Where(x=>x.Length>0).Select(int.Parse).Max(x=>x);
+
+        // Alternative
+        // return Regex.Matches(s, @"\d+").Max(x => int.Parse(x.Value));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Solve("gh12cdy695m1"));
+        Console.WriteLine(Solve("2ti9iei7qhr5"));
+    }
+}
