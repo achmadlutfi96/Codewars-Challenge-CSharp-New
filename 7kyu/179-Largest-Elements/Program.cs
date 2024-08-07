bcbd5e22033b68ee14894e6b2648a26457f1c5ee
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-07 11:14:24 +0700

    Create 7kyu\179-Largest-Elements

---
+++ 7kyu/179-Largest-Elements/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+  public static List<int> Largest(int n, List<int> xs)
+  {
+    // Find the n highest elements in a list
+    return xs.OrderBy(x=>x).TakeLast(n).ToList();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", Largest(3, new List<int> {5, 1, 5, 2, 3, 1, 2, 3, 5})));
+  }
+}
