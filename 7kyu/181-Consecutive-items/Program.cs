057bab39af210be920144fa4ee11875f344e92ce
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-08 11:51:34 +0700

    Create 7kyu\181-Consecutive-items

---
+++ 7kyu/181-Consecutive-items/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Kata
+    {
+        public static bool Consecutive(int[] arr, int a, int b)
+        {
+            int indexA = Array.IndexOf(arr, a);
+            int indexB = Array.IndexOf(arr, b);
+            return indexA > 0 ? indexA-1 == indexB || indexA+1 == indexB : indexA+1 == indexB;
+
+            // Alternative
+            // return Math.Abs(Array.IndexOf(arr,a)- Array.IndexOf(arr, b)) == 1;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Consecutive(new int[] { 1, 3, 5, 7 }, 3, 1));
+            Console.WriteLine(Consecutive(new int[]{1, 6, 9, -3, 4, -78, 0}, -3, 4));
+        }
+    }
+}
