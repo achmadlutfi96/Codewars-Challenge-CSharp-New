d4591474d1a797b480ee2435cb51424aa04e4000
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-08 11:57:12 +0700

    Create 7kyu\182-Digitize

---
+++ 7kyu/182-Digitize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static int[] digitize(int n)
+  {
+    return n.ToString().Select(x=>(int)char.GetNumericValue(x)).ToArray();
+
+    // Alternative
+    // return $"{n}".Select(c => c - 48).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", digitize(10)));
+  }
+}
