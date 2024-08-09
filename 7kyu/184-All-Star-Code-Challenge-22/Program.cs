34485be63a5ddeea57f9e2de333b021298d70a3f
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-09 12:36:35 +0700

    Create 7kyu\184-All-Star-Code-Challenge-22

---
+++ 7kyu/184-All-Star-Code-Challenge-22/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Kata 
+{
+    public static string ToTime(int seconds) 
+    {
+        return $"{seconds/3600} hour(s) and {seconds%3600/60} minute(s)";
+    }
+
+    public static void Main(){
+        Console.WriteLine(ToTime(323500));
+    }
+}
