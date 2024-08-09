4f82d5abbd72db89d21b54bcc118cacf5fafe79d
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-09 12:29:23 +0700

    Create 7kyu\183-Return-String-of-First-Characters

---
+++ 7kyu/183-Return-String-of-First-Characters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public partial class Kata
+{
+  public static string MakeString(string s)
+  {
+    return string.Concat(s.Split(" ").Select(x=>x.First()));  
+
+    // Alternative
+    // return string.Concat(s.Split().Select(e => e[0]));   
+  }
+
+  public static void Main(){
+    Console.WriteLine(MakeString("coding is awesome"));
+  }
+}
