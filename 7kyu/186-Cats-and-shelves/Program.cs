5637b67e4229f641ddae0eda159dc1b73f318894
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-10 12:30:18 +0700

    Create 7kyu\186-Cats-and-shelves

---
+++ 7kyu/186-Cats-and-shelves/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Kata 
+{
+    public static int Cats(int start, int finish) 
+    {
+      //Mew
+        int diff = finish - start;
+        return diff/3 + diff % 3;
+    }
+
+    public static void Main(){
+        Console.WriteLine(Cats(1,5));
+    }
+}
