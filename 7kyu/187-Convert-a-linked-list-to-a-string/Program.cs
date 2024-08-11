7793bb129193f59fed020d294d4fa074eba2ed4f
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-11 10:25:50 +0700

    Create 7kyu\187-Convert-a-linked-list-to-a-string

---
+++ 7kyu/187-Convert-a-linked-list-to-a-string/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Kata
+{
+  public static string Stringify(Node list)
+  {
+    // Write your code here
+    string str = "";
+    Node temp = list;
+    while(temp != null){
+        str += $"{temp.Data} -> ";
+        temp = temp.Next;
+    }
+
+    return str + "null";
+
+    // Alternative 
+    // return list == null ? "null" : list.Data + " -> " + Stringify(list.Next);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Stringify(new Node(1, new Node(2, new Node(3)))));
+  }
+}
+
+public class Node {
+  public int Data { get; private set; }
+  public Node Next { get; private set; }
+
+  public Node(int data, Node next = null) {
+    Data = data;
+    Next = next;
+  }
+}
