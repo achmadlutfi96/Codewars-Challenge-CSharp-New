f10c2d6c676ec94ea350cea3d908b8ff05ee9d05
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-11 10:47:13 +0700

    Create 7kyu\188-Unique-string-characters

---
+++ 7kyu/188-Unique-string-characters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+  public static string Solve(string a, string b)
+  {
+    return string.Concat(a.Concat(b).Where(x=>!a.Intersect(b).Contains(x)));
+
+    // Alternative 1
+    // return Regex.Replace(a, $"[{b}]", "") + Replace(b, $"[{a}]", "");
+  }
+
+  public static void Main(){
+    Console.WriteLine(Solve("xyab", "xzca"));
+    Console.WriteLine(Solve("xyabb", "xzca"));
+  }
+}
