9b516aa550818fc86c2d2f0c86ef8981a88c534e
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-12 10:54:22 +0700

    Create 7kyu\189-Valid-Spacing

---
+++ 7kyu/189-Valid-Spacing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Kata 
+{
+  public static bool ValidSpacing(string s)
+  {
+    //your code
+    return !s.StartsWith(' ') && !s.EndsWith(' ') && !Regex.IsMatch(s, @"  ");
+
+    // Alternative 1
+    // return s.Trim() == s && !s.Contains("  ");
+    // Alternative 2
+    // return Regex.IsMatch(s,@"^$|^(\w+\s)*\w+$");
+  }
+
+  public static void Main(){
+    Console.WriteLine(ValidSpacing("Hello wo rld"));
+  }
+}
