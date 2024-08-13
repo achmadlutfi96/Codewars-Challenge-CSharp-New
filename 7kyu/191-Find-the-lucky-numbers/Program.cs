cc0746d7e51dd75af0697ed10e17e8774ad477ee
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-13 13:23:17 +0700

    Create 7kyu\191-Find-the-lucky-numbers

---
+++ 7kyu/191-Find-the-lucky-numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Kata
+{
+  public static int[] FilterLucky(int[] x)
+  {
+    return x.Where(x=>x.ToString().Contains('7')).ToArray();
+
+    // Alternative 
+    // return Array.FindAll(x, m => $"{m}".Contains('7'));
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",",FilterLucky(new int[] {1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17})));
+  }
+}
