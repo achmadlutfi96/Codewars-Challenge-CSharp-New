f6d56a9590c9d989d95f63bf02e527113acf856b
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-14 12:00:24 +0700

    Create 7kyu\194-Head-Tail-Init-and-Last

---
+++ 7kyu/194-Head-Tail-Init-and-Last/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class ArrayMethods
+{
+  // TODO: Implement the four functions provided.
+
+  public static TSource Head<TSource>(this List<TSource> list)
+  {
+     return list.First();
+  }
+  
+  public static List<TSource> Tail<TSource>(this List<TSource> list)
+  {
+    return list.Skip(1).ToList();
+  }
+
+  public static List<TSource> Init<TSource>(this List<TSource> list)
+  {
+    return list.SkipLast(1).ToList();
+  }
+  
+  public static TSource Last_<TSource>(this List<TSource> list)
+  {
+    return list.Last();
+  }
+}
