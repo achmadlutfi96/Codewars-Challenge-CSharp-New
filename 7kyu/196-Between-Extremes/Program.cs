66ed5633771e3a89fffe3a12f16ece096daf7aed
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-15 10:55:07 +0700

    Create 7kyu\196-Between-Extremes

---
+++ 7kyu/196-Between-Extremes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static int BetweenExtremes(int[] numbers)
+  {
+    // your code goes here
+    return numbers.Max() - numbers.Min();
+  }
+
+  public static void Main(){
+    Console.WriteLine(BetweenExtremes(new int[]{21, 34, 54, 43, 26, 12}));
+  }
+}
