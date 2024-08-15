979cc4c1f964e1dbf6b52f387d9a367ffc773515
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-15 11:16:56 +0700

    Create 7kyu\198-Inspiring-Strings

---
+++ 7kyu/198-Inspiring-Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+  public static string LongestWord(string stringOfWords)
+  {
+    //Give me back the longest word!
+    return stringOfWords.Split(" ").OrderBy(x=>x.Length).Last();
+  }
+
+  public static void Main(){
+    Console.WriteLine(LongestWord("one two three"));
+  }
+}
