e278245df14f5857a4c6669ba2a0fba8d8e80200
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-16 11:13:36 +0700

    Create 7kyu\200-How-many-arguments

---
+++ 7kyu/200-How-many-arguments/Program.cs
@@ -0,0 +1,10 @@
+﻿public static class Kata {
+    // Create a function called CountArgs that returns the number of arguments provided (as an int)
+    public static int CountArgs(params object[] args){
+        return args.Length;
+    }
+
+    public static void Main(){
+        Console.WriteLine(CountArgs('a', 865, "asfhgajsf", new object[] { "dawdjio", null, new List<object>() }));
+    }
+}
