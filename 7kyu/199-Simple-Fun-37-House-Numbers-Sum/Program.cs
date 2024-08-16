7f9cfeca27cd9093dce08f5b698d69ec61faa0e3
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-16 11:04:59 +0700

    Create 7kyu\199-Simple-Fun-37-House-Numbers-Sum

---
+++ 7kyu/199-Simple-Fun-37-House-Numbers-Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public static int HouseNumbersSum(int[] inputArray)
+        {
+            //coding and coding..
+            int res = 0;
+            foreach (int i in inputArray)
+            {
+                if (i == 0)
+                {
+                    break;
+                }
+                res += i;
+            }
+            return res;
+
+            // Alternative
+            // return inputArray.TakeWhile(x => x != 0).Sum();
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(HouseNumbersSum(new int[] { 5, 1, 2, 3, 0, 1, 5, 0, 2 }));
+            Console.WriteLine(HouseNumbersSum(new int[] { 4, 1, 2, 3, 0, 10, 2 }));
+        }
+    }
+}
