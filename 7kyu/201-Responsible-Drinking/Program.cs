efc332bfc3e8f94077acc2cfcd8eb8acbdeaf735
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-17 13:06:18 +0700

    Create 7kyu\201-Responsible-Drinking

---
+++ 7kyu/201-Responsible-Drinking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+
+public class Drinkin
+{
+    public static string hydrate(string drinkString)
+    {
+        // Insert party here
+        int totalWater = Regex.Replace(drinkString, @"[A-Za-z]", ",").Replace(",", "").Split(" ").Where(x => !string.IsNullOrEmpty(x)).Sum(int.Parse);
+        return string.Format("{0} {1} of water", totalWater, totalWater == 1 ? "glass" : "glasses");
+
+        // Alternative
+        //  var s = Regex.Matches(drinkString, "\\d").Sum(m => int.Parse(m.Value));
+        // return $"{s} glass{(s > 1 ? "es" : "")} of water";
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(hydrate("1 beer"));
+        Console.WriteLine(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"));
+    }
+}
