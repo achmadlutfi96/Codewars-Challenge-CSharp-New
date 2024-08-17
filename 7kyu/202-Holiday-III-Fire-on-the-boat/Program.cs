912bb548d0c8656dda5b35d7e9a18cdb27b32c0c
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-17 13:11:09 +0700

    Create 7kyu\202-Holiday-III-Fire-on-the-boat

---
+++ 7kyu/202-Holiday-III-Fire-on-the-boat/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static string FireFight(string s)
+  {
+    return s.Replace("Fire", "~~");
+  }
+
+  public static void Main(){
+    Console.WriteLine(FireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"));
+  }
+}
