411dab7e8932fa1c08881c27cb78d50ecd190f0e
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-18 12:01:45 +0700

    Create 7kyu\203-Return-the-Missing-Element

---
+++ 7kyu/203-Return-the-Missing-Element/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int GetMissingElement(int[] superImportantArray)
+  {
+    //TODO
+    return Enumerable.Range(0, 10).Except(superImportantArray).First();
+
+    // Alternative 1
+    // return (int)Enumerable.Range(0,9).Except(superImportantArray).FirstOrDefault();
+
+    // Alternative 2
+    // return 45 - superImportantArray.Sum();
+  }
+
+  public static void Main(){
+    Console.WriteLine(GetMissingElement(new int[9] {0,5,1,3,2,9,7,6,4}));
+  }
+}
