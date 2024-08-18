cc5bc74eb10440f358405f0605815016d078f2c3
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-18 12:12:31 +0700

    Create 7kyu\204-Two-numbers-are-positive

---
+++ 7kyu/204-Two-numbers-are-positive/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Solution {
+    public static class Kata {
+        public static bool TwoArePositive(int a, int b, int c) {
+            // Happy Coding
+            return new int[]{a,b,c}.Where(x=>x>0).Count() == 2;
+        }
+
+        public static void Main(){
+            Console.WriteLine(TwoArePositive(2, 4, -3));
+            Console.WriteLine(TwoArePositive(-14, -3, -4));
+        }
+    }
+}
