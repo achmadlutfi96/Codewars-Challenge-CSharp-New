653cc1ad1b03bafb58e32810b60c2f8830f398c2
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-20 10:11:01 +0700

    Create 7kyu\207-Count-all-the-sheep-on-farm

---
+++ 7kyu/207-Count-all-the-sheep-on-farm-in-the-heights-of-New-Zealand/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+namespace CodeWars
+{
+    public class Kata
+    {
+        public static int lostSheep(int[] friday, int[] saturday, int total)
+        {
+            // Code here
+            return total - (friday.Sum() + saturday.Sum());
+
+            // Alternative 
+            // return total - friday.Concat(saturday).Sum();
+        }
+
+        public static void Main(){
+            Console.WriteLine(lostSheep(new int[] { 1, 2 }, new int[] { 3, 4 }, 15));
+        }
+    }
+}
