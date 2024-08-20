c7603cc51a89f43fd724cfc81671399af4f17828
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-20 10:28:54 +0700

    Create 7kyu\208-Band-name-generator

---
+++ 7kyu/208-Band-name-generator/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Kata
+{
+  public static string BandNameGenerator(string str)
+  {
+    // Happy coding
+    if(str.First() == str.Last()){
+        return string.Concat(char.ToUpper(str[0]),str.Substring(1),str.Substring(1));
+    }else{
+        return $"The {char.ToUpper(str[0])}{str.Substring(1)}";
+    }
+
+    // Alternative 1
+    // return (str[0] != str[^1] ? $"The {char.ToUpper(str[0])}" : char.ToUpper(str[0]) + str[1..]) + str[1..];
+
+    // Alternative 2
+    // return str.First()==str[str.Length-1] ? str.First().ToString().ToUpper()+str.Substring(1)+str.Substring(1) : "The"+" "+str.First().ToString().ToUpper()+str.Substring(1);
+  }
+
+  public static void Main(){
+    Console.WriteLine(BandNameGenerator("knife"));
+    Console.WriteLine(BandNameGenerator("tart"));
+  }
+}
