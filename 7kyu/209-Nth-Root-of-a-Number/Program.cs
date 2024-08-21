edb1c5e3c7b3a1d35e971121252d4afbc4620938
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-21 14:53:09 +0700

    Create 7kyu\209-Nth-Root-of-a-Number

---
+++ 7kyu/209-Nth-Root-of-a-Number/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NthRoot
+{
+  public class Kata
+  {
+    public static double FindRoot(double x, int n)
+    {
+       return Math.Pow(x, (1.0/n));
+    }
+
+    public static void Main(){
+        Console.WriteLine(FindRoot(4, 2));
+        Console.WriteLine(FindRoot(8,3));
+        Console.WriteLine(FindRoot(256, 4));
+        Console.WriteLine(FindRoot(9, 2));
+        Console.WriteLine(FindRoot(6.25, 2));
+    }
+  }
+}
