a4a350b9830a4e60cdb50437ad16f02a627b095e
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-22 12:34:54 +0700

    Create 7kyu\211-Basic-Math-Add-or-Subtract

---
+++ 7kyu/211-Basic-Math-Add-or-Subtract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+public static class Kata
+{
+  public static string Calculate(string str)
+  {
+    //your code
+    return str.Replace("plus", " ").Replace("minus"," -").Split(" ").Select(int.Parse).Sum().ToString();
+  }
+
+  public static void Main(){
+    Console.WriteLine(Calculate("1plus2plus3plus4"));
+  }
+}
