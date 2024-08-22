78c95ee560ca6e788c44dc2d24b3edb6ef134199
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-22 14:32:24 +0700

    Create 7kyu\212-Compare-Strings-by-Sum-of-Chars

---
+++ 7kyu/212-Compare-Strings-by-Sum-of-Chars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static bool Compare(string s1 = "", string s2 = "")
+  { 
+
+    int GetSum(string s) => s.All(char.IsLetter) ? s.Sum(c => (int) char.ToUpper(c)) : 0;
+    return GetSum(s1 ?? "") == GetSum(s2 ?? "");
+  }
+
+  public static void Main(){
+    Console.WriteLine(Compare("AD", "BC"));
+    Console.WriteLine(Compare("AD", "DD"));
+    Console.WriteLine(Compare("##", "1176"));
+    Console.WriteLine(Compare(null, null));
+  }
+}
