3b07fc23455a7dd398e6fcd8da4374f767823e20
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-23 10:50:35 +0700

    Create 7kyu\213-Reverse-the-bits-in-an-integer

---
+++ 7kyu/213-Reverse-the-bits-in-an-integer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Kata
+{
+    public static long ReverseBits(long n)
+    {
+        return Convert.ToInt64(string.Concat(Convert.ToString(n, 2).Reverse()),2);
+
+        // Alternative
+        // long m = 0;
+        // while (n > 0)
+        // {
+        //     m <<= 1;
+        //     if (n % 2 == 1) m ^= 1;
+        //     n >>= 1;
+        // }
+        // return m;
+    }
+
+    public static void Main(){
+        Console.WriteLine(ReverseBits(417) == 267);
+    }
+}
