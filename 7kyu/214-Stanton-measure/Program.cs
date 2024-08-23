a5c23747e1226b617ad1542c5bcc2d2e0695dcae
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-23 11:02:06 +0700

    Create 7kyu\214-Stanton-measure

---
+++ 7kyu/214-Stanton-measure/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+  /// <summary>
+  /// Returns the Stanton Measure of the array</summary>
+  /// <param name="arr">Int32[] to be evaluated.</param>
+  public static int StantonMeasure(int[] arr)
+  {
+    return arr.Where(y=> y == arr.Where(x=>x==1).Count()).Count();
+
+    // Alternative
+    // int n = arr.Count(x => x == 1);
+    // return arr.Count(x => x == n);
+  }
+
+  public static void Main(){
+    Console.WriteLine(StantonMeasure(new int[] {1, 4, 3, 2, 1, 2, 3, 2}));
+  }
+}
