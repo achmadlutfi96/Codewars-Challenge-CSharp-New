7a5e8cb3d1e2c5b05ecccd6800478754364f28cd
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-24 12:36:34 +0700

    Create 7kyu\215-Letterbox-Paint-Squad

---
+++ 7kyu/215-Letterbox-Paint-Squad/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+    public static IEnumerable<int> PaintLetterBoxes(int start, int end)
+    {
+        // Your code here
+        List<int> rangeNumbers = Enumerable.Range(start, end - start + 1).ToList();
+        List<int> letterBoxes = new List<int>();
+        foreach (var number in rangeNumbers)
+        {
+            string numberString = number.ToString();
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                letterBoxes.Add((int)char.GetNumericValue(numberString[i]));
+            }
+        }
+        return Enumerable.Range(0, 10).Select(x => letterBoxes.Where(y => x == y).Count());
+
+        // Alternative
+        // return ("0123456789" + string.Concat(Enumerable.Range(start, end - start + 1)))
+        // .GroupBy(c => c)
+        // .Select(x => x.Count() - 1);
+    }
+
+    public static void Main()
+    {
+        foreach (var item in PaintLetterBoxes(125, 132))
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
