c4e9a3878571044376a4367847f0196d22c1bd41
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-24 12:49:15 +0700

    Create 7kyu\216-Nth-power-rules-them-all

---
+++ 7kyu/216-Nth-power-rules-them-all/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static int ModifiedSum(int[] a, int n)
+  {
+    // Write your code here
+    return (int) (a.Select(x=>Math.Pow(x,n)).Sum() - a.Sum());
+
+    // Alternative
+    // return a.Sum(x => (int) Math.Pow(x, n) - x);
+  }
+
+  public static void Main(){
+    Console.WriteLine(ModifiedSum(new int[] {1, 2, 3}, 3));
+  }
+}
