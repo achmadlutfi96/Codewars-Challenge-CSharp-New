66b8b628668a04e9ebf621ee4cfde2d97c6f4243
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-25 11:45:10 +0700

    Create 7kyu\217-Average-Scores

---
+++ 7kyu/217-Average-Scores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static int Average(int[] scores)
+  {
+    return (int) Math.Round(scores.Average(),MidpointRounding.AwayFromZero);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Average(new int[] { 49,3,5,300,7 }));
+  }
+}
