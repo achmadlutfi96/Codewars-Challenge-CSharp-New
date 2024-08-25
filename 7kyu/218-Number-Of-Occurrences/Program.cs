b219a2aab540a24896ac6f2258dd87330fb3ce4a
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-25 11:59:08 +0700

    Create 7kyu\218-Number-Of-Occurrences

---
+++ 7kyu/218-Number-Of-Occurrences/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class OccurrencesKata
+{
+    public static int NumberOfOccurrences(int x, int[] xs)
+    {
+        return xs.Where(y=>y==x).Count();
+
+        // Alternative
+        // return xs.Count(Item => Item == x);
+    }
+
+    public static void Main(){
+        Console.WriteLine(NumberOfOccurrences(4, new int[] { 4, 0, 4 }));
+    }
+}
