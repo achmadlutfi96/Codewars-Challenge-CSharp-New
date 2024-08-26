54f0ccfeeeca2517613ef9b58604b9300354e9fd
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-26 11:49:43 +0700

    Create 7kyu\220-By-3-or-not-by-3-That-is-the-question

---
+++ 7kyu/220-By-3-or-not-by-3-That-is-the-question/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Kata
+{
+    public static bool DivisibleByThree(string n)
+    {
+        //     throw new NotImplementedException();
+
+        return (n.Sum(x=>x) - (n.Sum(x=>x)/3*3)) == 0; 
+
+        // Alternative
+        // return BigInteger.Parse(n) % 3 == 0;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(DivisibleByThree("1891009"));
+        Console.WriteLine(DivisibleByThree("000009"));
+    }
+}
