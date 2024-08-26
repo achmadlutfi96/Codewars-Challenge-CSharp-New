e77e9bd2cd59adafcbc78d0975cc41025ac7dcd2
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-26 11:14:08 +0700

    Create 7kyu\219-Length-and-two-values

---
+++ 7kyu/219-Length-and-two-values/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Kata
+{
+    public static object[] Alternate(int n, object firstValue, object secondValue)
+    {
+        object[] result = new object[n];
+        for (int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                result[i] = firstValue;
+            }else{
+                result[i] = secondValue;
+            }
+        }
+        return result;
+
+        // Alternative
+        // return Enumerable.Range(0, n).Select(x => x % 2 == 1 ? secondValue : firstValue).ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", Alternate(5, true, false)));
+    }
+}
