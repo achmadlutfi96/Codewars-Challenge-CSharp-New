8ebd5f35eb3edb76f99399d74776777f8b8ccf10
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-27 13:10:07 +0700

    Create 7kyu\222-Say-hello

---
+++ 7kyu/222-Say-hello/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Greetings
+{
+    public static string greet(string name)
+    {
+        //Here add your greet code
+        if (string.IsNullOrEmpty(name)){
+            return null;
+        }
+        return $"hello {name}!";
+
+        // Alternative
+        // return string.IsNullOrEmpty(name) ? null : $"hello {name}!";
+    }
+
+    public static void Main(){
+        Console.WriteLine(greet(null));
+    }
+}
