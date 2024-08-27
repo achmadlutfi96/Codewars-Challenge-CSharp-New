54ee6ba4076e268a7d376cbecb6355c9d2eb5f19
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-27 12:13:30 +0700

    Create 7kyu\221-makeBackronym

---
+++ 7kyu/221-makeBackronym/Program.cs
@@ -0,0 +1,11 @@
+﻿public partial class Kata
+{
+  public static string MakeBackronym(string s)
+  {
+    return string.Join(" ",s.Select(x=>dict[char.ToUpper(x)])); // "awesome"
+  }
+
+  public static void Main(){
+    Console.WriteLine(MakeBackronym("interesting"));
+  }
+}
