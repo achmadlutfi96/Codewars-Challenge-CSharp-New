24e944c1ea404936a74a21103b34103174fe7139
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-28 12:33:29 +0700

    Create 7kyu\223-Simple-Fun-2-Circle-of-Numbers

---
+++ 7kyu/223-Simple-Fun-2-Circle-of-Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public static int CircleOfNumbers(int n, int FirstNumber){
+          //coding and coding..
+          
+          return n/2 > FirstNumber ? n/2+FirstNumber : FirstNumber-n/2; 
+        }
+
+        public static void Main(){
+            Console.WriteLine(CircleOfNumbers(10,2));
+            Console.WriteLine(CircleOfNumbers(10,7));
+            Console.WriteLine(CircleOfNumbers(4,1));
+            Console.WriteLine(CircleOfNumbers(6,3));
+        }
+    }
+}
