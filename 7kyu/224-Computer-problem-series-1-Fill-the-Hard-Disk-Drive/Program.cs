f87c1a5099f4968cda1c15a1bb428962ed7dd4b3
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-28 13:03:50 +0700

    Create 7kyu\224-Computer-problem-series-1-Fill-the-Hard

---
+++ 7kyu/224-Computer-problem-series-1-Fill-the-Hard-Disk-Drive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+public class Kata
+{
+  public static int Save(int[] sizes, int hd)
+  {
+    //Write your code here
+    Stack<int> results = new Stack<int>(sizes);
+    while(results.Sum() > hd){
+        results.Pop();
+    }
+    return results.Count();
+
+    // Alternative
+    // return sizes.Where((x, i) => sizes.Take(i + 1).Sum() <= hd).Count();
+  }
+
+  public static void Main(){
+    Console.WriteLine(Save(new int[]{4,4,4,3,3}, 12));
+  }
+}
