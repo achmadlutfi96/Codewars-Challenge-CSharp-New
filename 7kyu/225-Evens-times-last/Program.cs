7ca72970d12ff1ed42c4e7bc59ba257826485d2d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-29 13:22:54 +0700

    Create 7kyu\225-Evens-times-last

---
+++ 7kyu/225-Evens-times-last/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace EvenTimesLast
+{
+	public static class Solver
+	{
+		public static int EvenTimesLast(int[] arr)
+		{
+            if(arr.Length == 0){
+                return 0;
+            }
+			return arr.Where((v,i)=> i%2 == 0).Sum() * arr.Last();
+		}
+
+        public static void Main(){
+            Console.WriteLine(EvenTimesLast(new int[] {2, 3, 4, 5}));
+        }
+	}
+}
