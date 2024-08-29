fec9bbe74ddd7152655b381a9b74d42454fb1f8a
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-29 13:33:15 +0700

    Create 7kyu\226-Building-Strings-From-a-Hash

---
+++ 7kyu/226-Building-Strings-From-a-Hash/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+    public static string StringifyDict<TKey, TValue>(Dictionary<TKey, TValue> hash)
+    {
+        // throw new NotImplementedException();
+        return string.Join(",", hash.Select(x=>$"{x.Key} = {x.Value}"));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(StringifyDict(new Dictionary<char, int> {{'a', 1}, {'b', 2}}));
+    }
+}
