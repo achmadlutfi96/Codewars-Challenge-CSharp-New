3baadb9b9db585ce68f109d3ea91522895a8336b
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-30 14:06:29 +0700

    Create 7kyu\227-Interview-Question-easy

---
+++ 7kyu/227-Interview-Question-easy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string GetStrings(string city)
+    {
+        return string.Join(",", city.ToLower().Distinct().Where(x=>x!=' ').Select(x=>$"{x}:{string.Concat(Enumerable.Repeat('*',city.ToLower().Where(y=>y==x).Count()))}"));
+
+        // Alternative
+        // return string.Join(",", city.Where(char.IsLetter).GroupBy(char.ToLower).Select(g => $"{g.Key}:{new string('*', g.Count())}"));
+    }
+
+    public static void Main(){
+        Console.WriteLine(GetStrings("Las Vegas"));
+    }
+}
