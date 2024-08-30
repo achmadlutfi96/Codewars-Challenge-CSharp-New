bf15a70cf58623ca88bc4f4d3621e96f964a940f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-30 18:10:28 +0700

    Create 7kyu\228-Covfefe

---
+++ 7kyu/228-Covfefe/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace azarias{
+public static class Solution
+{
+  public static string Covfefe(string tweet)
+  {
+    // covfefe
+    return tweet.Contains("coverage") ? tweet.Replace("coverage", "covfefe") : $"{tweet} covfefe";
+  }
+
+  public static void Main(){
+    Console.WriteLine(Covfefe("nothing"));
+    Console.WriteLine(Covfefe("coverage coverage"));
+  }
+}
+}
