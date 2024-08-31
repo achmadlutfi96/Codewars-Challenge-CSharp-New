61185b949346de5de59f892ff33e6a9ff8cdd88f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-31 12:03:27 +0700

    Create 7kyu\230-Initialize-my-name

---
+++ 7kyu/230-Initialize-my-name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string InitializeNames(string name)
+    {
+        if(name.Split(" ").Count() < 3){
+            return name;
+        }
+        return string.Join(" ", name.Split(" ").First(), string.Join(" ",name.Split(" ").Skip(1).SkipLast(1).Select(x=>$"{x[0]}.")), name.Split(" ").Last());
+
+        // Alternative
+        // return name.Split().Skip(1).SkipLast(1).Aggregate(name, (s, n) => s.Replace(n, n[0] + "."));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(InitializeNames("Alice Betty Catherine Davis"));
+    }
+}
