b7b701a0d4c72fc3b1daae0cb1218674ec8daad9
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-08-31 11:38:47 +0700

    Create 7kyu\229-Character-Counter

---
+++ 7kyu/229-Character-Counter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System;
+public class Kata
+{
+    public static bool ValidateWord(string s)
+    {
+        //Code goes here!
+        if(s == "") return true;
+        return s.ToLower().Distinct().Select(x => s.ToLower().Where(y => x == y).Count()).Distinct().Count() == 1;
+
+        // Alternative
+        // return s==""?true:s.ToLower().GroupBy(c => c).Select(g => g.Count()).Distinct().Count() == 1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(ValidateWord("Abcabc"));
+        Console.WriteLine(ValidateWord("abc123"));
+        Console.WriteLine(ValidateWord("abcabcd"));
+    }
+}
