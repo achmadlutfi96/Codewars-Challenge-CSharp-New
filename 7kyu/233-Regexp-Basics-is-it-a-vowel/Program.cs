5047ebb4a3e86cdf8a2fbd281b80ec64a2c71fdc
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-02 14:02:34 +0700

    Create 7kyu\233-Regexp-Basics-is-it-a-vowel

---
+++ 7kyu/233-Regexp-Basics-is-it-a-vowel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static bool Vowel(this string s)
+    {
+        return new string[]{"a","i", "e", "o", "u"}.Contains(s.ToLower());
+
+        // Alternative
+        // return Regex.IsMatch(s,@"\A(?i)[aeiou]\z");
+    }
+
+    public static void Main(){
+        Console.WriteLine("a".Vowel());
+        Console.WriteLine("ou".Vowel());
+    }
+}
