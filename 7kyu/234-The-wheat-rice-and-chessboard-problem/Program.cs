dbbcb032e3ecb8e5275129190f128fc1725c62b2
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-02 14:52:35 +0700

    Create 7kyu\234-The-wheat-rice-and-chessboard-problem

---
+++ 7kyu/234-The-wheat-rice-and-chessboard-problem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Kata
+{
+    public static int SquaresNeeded(long grains)
+    {
+        // int res = 0;
+        // while(res*2 <= grains){
+        //     res++;
+        // }
+        // return res;
+
+        int res = 0;
+        while(grains > 0){
+            grains/=2;
+            res++;
+        }
+
+        return res;
+
+        // Alternative
+        // return grains > 0 ? 1 + SquaresNeeded(grains/2) : 0;
+    }
+
+    public static void Main(){
+        Console.WriteLine(SquaresNeeded(4));
+    }
+}
