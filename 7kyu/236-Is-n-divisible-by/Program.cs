6052190ac63169099acb2b4c0a07773c6ccab5e2
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-03 13:55:23 +0700

    Create 7kyu\236-Is-n-divisible-by

---
+++ 7kyu/236-Is-n-divisible-by/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Codewars {
+  public static class KataChallenge {
+    public static bool IsDivisible(params int[] numbers)
+    {
+       //Start coding!!
+       return numbers.All(x=>numbers[0]%x == 0);
+    }
+
+    public static void Main(){
+        Console.WriteLine(IsDivisible(12, 3, 4));
+    }
+  }
+}
