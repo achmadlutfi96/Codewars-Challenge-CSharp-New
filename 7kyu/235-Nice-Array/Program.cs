44518c84a37719c9f8e2c776504e86ed52166beb
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-03 12:40:19 +0700

    Create 7kyu\235-Nice-Array

---
+++ 7kyu/235-Nice-Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static bool IsNice(int[] arr)
+  {
+    // throw new NotImplementedException();
+    if(arr.Length == 0){
+        return false;
+    }
+    return arr.Where(x=>arr.Contains(x-1) || arr.Contains(x+1)).Count() == arr.Length;
+
+    // Alternative
+    // return arr.Any() && arr.All(x => arr.Contains(x - 1) || arr.Contains(x + 1));
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsNice(new int[] {2,10,9,3}));
+    Console.WriteLine(IsNice(new int[] {3,4,5,7}));
+  }
+}
