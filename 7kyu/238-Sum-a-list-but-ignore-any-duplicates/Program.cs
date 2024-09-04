d91c858ebeccf7dc27a643ca959bc4e29fb99975
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-04 13:07:07 +0700

    Create 7kyu\238-Sum-a-list-but-ignore-any-duplicates

---
+++ 7kyu/238-Sum-a-list-but-ignore-any-duplicates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int SumNoDuplicates(int[] arr)
+  {
+    //write your solution here
+    return arr.Distinct().Where(x=>arr.Where(y=>x==y).Count() == 1).Sum();
+
+    // Alternative
+    // return arr.Sum(x => arr.Count(i => i == x) == 1 ? x : 0);
+  }
+
+  public static void Main(){
+    Console.WriteLine(SumNoDuplicates(new int[]{1,1,2,3}));
+  }
+}
