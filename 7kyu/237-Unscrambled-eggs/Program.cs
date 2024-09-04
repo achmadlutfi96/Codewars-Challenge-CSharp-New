8d567c408bab9d48c1cf39ac1f728c7f65201998
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-04 12:59:34 +0700

    Create 7kyu\237-Unscrambled-eggs

---
+++ 7kyu/237-Unscrambled-eggs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+  public static string UnscrambleEggs(string word)
+  {
+    //coding is feggunegg
+    return word.Replace("egg", "");
+  }
+  
+  public static void Main(){
+    Console.WriteLine(UnscrambleEggs("ceggodegge heggeregge"));
+  }
+}
