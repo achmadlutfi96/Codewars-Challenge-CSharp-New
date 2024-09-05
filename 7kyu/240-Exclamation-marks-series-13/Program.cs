12f35c2a2bd5030fc834278ecd0ff613a1d9e0e7
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-05 15:50:18 +0700

    Create 7kyu\240-Exclamation-marks-series-13

---
+++ 7kyu/240-Exclamation-marks-series-13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int Product(string str)
+  {
+    return str.Where(x=>x.Equals('!')).Count() * str.Where(y=>y.Equals('?')).Count();
+
+    // Alternative
+    // return str.Count(c => c == '!') * str.Count(c => c == '?')
+  }
+
+  public static void Main(){
+    Console.WriteLine(Product("!!???????!!!!!"));
+  }
+}
