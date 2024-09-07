7bbfce03328c7d88d8001e060327a022627a08eb
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-07 16:40:29 +0700

    Create 7kyu\241-How-many-consecutive-numbers-are-needed

---
+++ 7kyu/241-How-many-consecutive-numbers-are-needed/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System;
+public class Kata
+{
+  public static int Consecutive(int[] arr)
+  {
+    return arr.Length < 3 ? 0 : arr.Max()-(arr.Min()+1)-(arr.Length-2);
+
+    // Alternative
+    // return arr.Any() ? arr.Max() - arr.Min() - arr.Length + 1 : 0;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Consecutive(new int[] { 4, 8, 6 }));
+    Console.WriteLine(Consecutive(new int[] { -1, -5 }));
+  }
+}
