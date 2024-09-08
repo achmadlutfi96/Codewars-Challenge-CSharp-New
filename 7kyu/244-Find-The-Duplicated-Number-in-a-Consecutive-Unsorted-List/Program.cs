fd9cc3abce42bfa1a2f77309a8725528d19fb94f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-08 21:38:23 +0700

    Create 7kyu\244-Find-The-Duplicated-Number-in

---
+++ 7kyu/244-Find-The-Duplicated-Number-in-a-Consecutive-Unsorted-List/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int FindDup(int[] arr)
+  {
+    return arr.Distinct().Where(x=>arr.Where(y=>x==y).Count() > 1).First();
+  }
+
+  public static void Main(){
+    Console.WriteLine(FindDup(new int[] {1, 1, 2, 3}));
+  }
+}
