79b316ca5898dd98909e7e25656e19c642566f8d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-08 21:20:14 +0700

    Create 7kyu\243-Find-all-occurrences-of-an-element-in-an-array

---
+++ 7kyu/243-Find-all-occurrences-of-an-element-in-an-array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int[] FindAll(int[] array, int n)
+  {
+    // your code here
+    return array.Select((v, i)=> v == n ? i : -1).Where(x=> x >= 0).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",",FindAll(new int[] {6, 9, 3, 4, 3, 82, 11}, 3)));
+  }
+}
