841c0871614e636ac9371193c4858b51eed92b14
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-08 21:05:43 +0700

    Create 7kyu\242-See-You-Next-Happy-Year

---
+++ 7kyu/242-See-You-Next-Happy-Year/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Kata
+{
+    public static short NextHappyYear(short year)
+    {
+        //Happy Coding!
+        short a, b, c, d;
+        do
+        {
+            year = (short)(year + 1);
+            a = (short) (year / 1000);
+            b = (short)((year - a * 1000) / 100);
+            c = (short)((year - a * 1000 - b * 100) / 10);
+            d = (short)(year % 10);
+        }
+        while (!(a != b && b != c && c != d && a != c && a != d && b != d));
+        return year;
+
+        // Alternative
+        // return (++year).ToString().Distinct().Count() == 4 ? year : nextHappyYear(year);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(NextHappyYear(1001));
+    }
+}
