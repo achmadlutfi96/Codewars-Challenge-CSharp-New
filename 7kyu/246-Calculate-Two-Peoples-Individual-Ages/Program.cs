2360f5c873e674ef7997865ee07bd1965d1bec6b
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-09 14:01:07 +0700

    Create 7kyu\246-Calculate-Two-Peoples-Individual-Ages

---
+++ 7kyu/246-Calculate-Two-Peoples-Individual-Ages/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Kata
+{
+  public static double[] GetAges(int sum, int difference)
+  {
+    if(sum < 0 || difference < 0){
+       return null; 
+    }
+    double x = (double)sum / 2;
+    double y = (double)difference / 2;
+
+    double ageA = x + y;
+    double ageB = x - y;
+
+    if(ageA < 0 || ageB < 0){
+        return null;
+    }
+    return new double[] { ageA, ageB };
+
+    // Alternative
+    // return sum < 0 || difference < 0 || sum < difference
+        // ? null
+        // : new[] {(sum + difference) / 2d, (sum - difference) / 2d};
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",",GetAges(24, 4)));
+    Console.WriteLine(string.Join(",",GetAges(63, 14)));
+  }
+}
