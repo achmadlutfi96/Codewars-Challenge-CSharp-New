f0066141939679c571f2c4b0319a5f9ab3053295
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-09 13:23:07 +0700

    Create 7kyu\245-Spoonerize-Me

---
+++ 7kyu/245-Spoonerize-Me/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+public static class ReadySet
+{
+  public static string Spoonerize(string str)
+  {
+    string[] letters = str.Split(" ");
+    string firstLetter = letters.First();
+    string lastLetter = letters.Last();
+    string tempFirstLetter = "";
+    string tempLastLetter = "";
+
+    for (int i = 0; i < firstLetter.Length; i++)
+    {
+        if(("aiueo".Contains(char.ToLower(firstLetter[i])) && i != 0) || i == 1){
+            firstLetter = firstLetter.Substring(i);
+            break;
+        }
+        tempLastLetter  += firstLetter[i]; 
+    }
+
+    for (int i = 0; i < lastLetter.Length; i++)
+    {
+        if(("aiueo".Contains(char.ToLower(lastLetter[i]))&& i != 0) || i == 1){
+            lastLetter = lastLetter.Substring(i);
+            break;
+        }
+        tempFirstLetter  += lastLetter[i]; 
+    }
+
+    firstLetter = tempFirstLetter + firstLetter;
+    lastLetter = tempLastLetter + lastLetter;
+    if( letters.Length > 3){
+        return $"{firstLetter} {string.Join(' ', letters.Skip(1).SkipLast(1))} {lastLetter}";
+    }else if (letters.Length == 3){
+        return $"{firstLetter} {letters[1]} {lastLetter}";
+    }else{
+        return $"{firstLetter} {lastLetter}";
+    }
+
+    // Alternative 1
+    // string[] words = str.Split(' ');
+    // return words[1][0] + words[0][1..] + ' ' + words[0][0] + words[1][1..];
+
+    // Alternative 2
+    // char[] chars = str.ToCharArray();
+    // chars[0] = str[str.IndexOf(' ') + 1];
+    // chars[str.IndexOf(' ') + 1] = str[0];
+    // return new string(chars);
+  }
+
+  public static void Main(){
+    Console.WriteLine(Spoonerize("flat battery"));
+    Console.WriteLine(Spoonerize("pack of lies"));
+  }
+}
