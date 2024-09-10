2f516f21358371efe0bde78b3e43728c47942fbd
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-10 12:19:35 +0700

    Create 7kyu\247-Sort-arrays-1

---
+++ 7kyu/247-Sort-arrays-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string[] SortMe(string[] names)
+  {
+    return names.OrderBy(x=>x).ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", SortMe(new [] {"one", "two", "three"})));
+  }
+}
