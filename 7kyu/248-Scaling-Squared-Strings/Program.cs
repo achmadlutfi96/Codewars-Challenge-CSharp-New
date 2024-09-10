7ff19ee3bfd3a61b2bc53fa9b2fcab732675a2f5
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-10 12:49:53 +0700

    Create 7kyu\248-Scaling-Squared-Strings

---
+++ 7kyu/248-Scaling-Squared-Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+public class Scaling
+{
+    public static string Scale(string strng, int k, int n) 
+    {
+        // your code
+        if(strng.Length == 0) { return string.Empty; }
+        var newString = strng.Split("\n").Select(x => string.Join("\n",Enumerable.Repeat(string.Concat(x.Select(y=>string.Concat(Enumerable.Repeat(y, k)))), n)));        
+        return string.Join("\n", newString);
+
+        // Alternative
+        // return string.Join('\n', s.Split('\n').Select(x => x.Any()
+        // ? string.Join('\n', Enumerable.Repeat(string.Concat(x.Select(c => new string(c, k))), n))
+        // : ""));
+    }
+
+    public static void Main(){
+        Console.WriteLine(Scale("abcd\nefgh\nijkl\nmnop", 2, 3));
+    }
+}
