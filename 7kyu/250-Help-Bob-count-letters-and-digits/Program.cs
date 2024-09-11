d076b6b863a2da75de6efa978182f01d249c279d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-11 13:11:10 +0700

    Create 7kyu\250-Help-Bob-count-letters-and-digits

---
+++ 7kyu/250-Help-Bob-count-letters-and-digits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int CountLettersAndDigits(string input)
+    {
+        //TODO:  
+        // return input.Where(char.IsAsciiLetterOrDigit).Count();
+
+        // Alternative
+        return input.Where(char.IsLetterOrDigit).Count();
+    }
+
+    public static void Main(){
+        Console.WriteLine(CountLettersAndDigits("n!!ice!!123"));
+    }
+}
