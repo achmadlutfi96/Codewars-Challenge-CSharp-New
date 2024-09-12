3450df1b725fcc2e949eb083528a037ec155f315
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-12 12:03:27 +0700

    Create 7kyu\252-Count-consonants

---
+++ 7kyu/252-Count-consonants/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int ConsonantCount(string str)
+  {
+    // ...
+    return str.Where(x=>char.IsLetter(x) && !"aiueo".Contains(char.ToLower(x))).Count();
+
+    // Alternative
+    // return Regex.Matches(str.ToLower(), "[b-df-hj-np-tv-z]").Count;
+  }
+
+  public static void Main(){
+    Console.WriteLine(ConsonantCount("h^$&^#$&^elLo world"));
+  }
+}
