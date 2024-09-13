1a3a851a515e76977751ea712e9298c3e7b9641f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-13 13:51:55 +0700

    Create 7kyu\254-last-digits-of-a-number

---
+++ 7kyu/254-last-digits-of-a-number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int[] LastDigit(long n, int d)
+    {
+        return n.ToString().TakeLast(d).Select(x=>(int)char.GetNumericValue(x)).ToArray();
+
+        // Alternative
+        // return $"{n}".TakeLast(d).Select(c => c - '0').ToArray();
+    }
+
+    public static void Main(){
+        Console.WriteLine(string.Join(",",LastDigit(34625647867585, 10)));
+    }
+}
