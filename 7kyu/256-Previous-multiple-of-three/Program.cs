fb354aefe592a5158b7d291574fbf26e0793f0ff
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-14 13:04:03 +0700

    Create 7kyu\256-Previous-multiple-of-three

---
+++ 7kyu/256-Previous-multiple-of-three/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Kata
+{
+  public static int? PreviousMultipleOfThree(int n)
+  {
+    while(n%3 != 0){
+        n = n/10;
+    }
+    return n == 0 ? null : n; // your code ..
+
+    // Alternative
+    // return n == 0 ? null : n % 3 == 0 ? n : PreviousMultipleOfThree(n / 10);
+  }
+
+  public static void Main(){
+    Console.WriteLine(PreviousMultipleOfThree(952406));
+    Console.WriteLine(PreviousMultipleOfThree(25));
+  }
+}
