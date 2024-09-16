e9bd674c37302a990d3100144d3010b6f0d9124b
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-16 13:48:51 +0700

    Create 7kyu\259-Numbers-with-this-digit-inside

---
+++ 7kyu/259-Numbers-with-this-digit-inside/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+public class Kata
+{
+  public static long[] NumbersWithDigitInside(long x, long d)
+  {
+    List<long> numbers = new();
+    for (int i = 1; i <= x; i++)
+    {
+        numbers.Add(i);
+    }
+    long[] n = numbers.Where(a=>a.ToString().Contains(d.ToString())).ToArray();
+    return n.Length == 0 ? new long[]{0,0,0} : new long[] { n.Count(), n.Sum(), n.Aggregate((value, next)=> value * next)};
+
+    // Alternative
+    // var n = Enumerable.Range(1, (int) x).Where(i => $"{i}".Contains($"{d}")).ToList();
+    // return new[] {n.Count, n.Sum(), n.Any() ? n.Aggregate(1L, (a, b) => a * b) : 0};
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", NumbersWithDigitInside(11, 1)));
+    Console.WriteLine(string.Join(",", NumbersWithDigitInside(20, 0)));
+    Console.WriteLine(string.Join(",", NumbersWithDigitInside(5, 6)));
+  }
+}
