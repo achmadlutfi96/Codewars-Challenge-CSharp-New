a98b8c26f522cfd1ad2b82576d02bf85b29bfc6d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-17 10:27:01 +0700

    Create 7kyu\261-Javascript-filter-1

---
+++ 7kyu/261-Javascript-filter-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string[][] search_names(string[][] logins)
+    {
+        // your code
+        return logins.Where(x=> x[0].EndsWith("_")).ToArray();
+    }
+
+    public static void Main(){
+        string[][] a = { new[] { "foo", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
+        Console.WriteLine(search_names(a));
+    }
+}
