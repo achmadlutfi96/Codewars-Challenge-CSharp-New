b9b24e714e1ed1076cfad883093e2edbe520c29d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-17 10:35:28 +0700

    Create 7kyu\262-The-Office-III-Broken-Photocopier

---
+++ 7kyu/262-The-Office-III-Broken-Photocopier/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static string Broken(string x)
+  {
+    return string.Concat(x.Select(b=> b == '0' ? '1' : '0'));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Broken("100010"));
+  }
+}
