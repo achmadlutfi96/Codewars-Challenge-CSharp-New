fdc6bbe4ad1419534562d42109cc9acd19372471
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-18 12:58:08 +0700

    Create 7kyu\264-Extra-Perfect-Numbers-Special-Numbers

---
+++ 7kyu/264-Extra-Perfect-Numbers-Special-Numbers-Series-7/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+  public static int[] ExtraPerfect(int n)
+  {
+    //your code here
+    return Enumerable.Range(1, n).Where(x=>Convert.ToString(x, 2).StartsWith("1") && Convert.ToString(x, 2).EndsWith("1")).ToArray();
+
+    // Alternative
+    //  return Enumerable.Range(1, n).Where(x => x % 2 != 0);
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", ExtraPerfect(28)));
+  }
+}
