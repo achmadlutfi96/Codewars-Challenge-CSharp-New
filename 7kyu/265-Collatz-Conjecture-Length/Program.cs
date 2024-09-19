3dbe8e813e04195eeda61813801481e5ab853e1e
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-19 10:57:18 +0700

    Create 7kyu\265-Collatz-Conjecture-Length

---
+++ 7kyu/265-Collatz-Conjecture-Length/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Kata
+{
+  public static int Collatz(int n)
+  {
+    // your code here
+    int result = 1;
+    do{
+        n = n%2 != 0 ? n*3+1 : n/2;
+        result++; 
+    }while(!(n==1));
+    return result;
+  }
+
+  public static void Main(){
+    Console.WriteLine(Collatz(20));
+  }
+}
