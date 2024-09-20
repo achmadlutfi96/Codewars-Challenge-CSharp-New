0191fea8daf73dfda5c6662bbb4749640f150212
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-20 11:13:38 +0700

    Create 7kyu\268-Complete-The-Pattern-2

---
+++ 7kyu/268-Complete-The-Pattern-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string Pattern(int n)
+    {
+        return n < 1 ? "" :string.Join("\n", Enumerable.Range(1, n).Select(x=> String.Concat(Enumerable.Range(x, n-x+1).Reverse())));
+
+        // Alternative
+        // return string.Join("\n", Range(0, Math.Max(0, n)).Select(x => string.Concat(Range(0, n - x).Select(i => n - i))));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Pattern(5));
+    }
+}
