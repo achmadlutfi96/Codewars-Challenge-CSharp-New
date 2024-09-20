bd6a26f58614fbcccecaa05cf7cf21239bc44339
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-20 10:54:15 +0700

    Create 7kyu\267-Unique-Sum

---
+++ 7kyu/267-Unique-Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+  public static int? UniqueSum(List<int> lst)
+  {
+    // throw new NotImplementedException();
+    return lst.Capacity == 0 ? null : lst.Distinct().Sum();
+  }
+
+  public static void Main(){
+    Console.WriteLine(UniqueSum(new List<int>() {1, 3, 8, 1, 8}));
+  }
+}
