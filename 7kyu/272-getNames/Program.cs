cd9fa9ef71a880fb9d0e2cc5b96f07224522103b
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-22 10:37:13 +0700

    Create 7kyu\272-getNames

---
+++ 7kyu/272-getNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string[] GetNames(Person[] data)
+  {
+    return data.Select(x=>x.Name).ToArray();
+  }
+
+}
+
+public class Person
+{
+  public int Age;
+  public string Name;
+
+  public Person(string name = "John", int age = 21)
+  {
+    Age = age;
+    Name = name;
+  }
+}
