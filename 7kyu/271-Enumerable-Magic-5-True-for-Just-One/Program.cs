db81fa21600f7afa6a76cae33fed67ff48c6e27c
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-22 10:18:34 +0700

    Create 7kyu\271-Enumerable-Magic-5-True-for-Just-One

---
+++ 7kyu/271-Enumerable-Magic-5-True-for-Just-One/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Kata
+{
+    public static bool One(int[] arr, Func<int, bool> fun)
+    {
+        return arr.Select(fun).Where(x=>x==true).Count() == 1;
+
+        // Alternative
+        // return arr.Count(fun) == 1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(One(new int[] { 1,2,3,4,5 }, v => v < 2));
+    }
+}
