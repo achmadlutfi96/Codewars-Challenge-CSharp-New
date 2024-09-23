8deda638455b2bf454bb3085eb5de1efe8ce2834
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-23 13:18:37 +0700

    Create 7kyu\274-What-dominates-your-array

---
+++ 7kyu/274-What-dominates-your-array/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+  public static int WhoDominates(int[] array)
+  {
+    return array.DistinctBy(x=>x).Select(x=> new int[]{x, array.Where(y=>x==y).Count()}).Where(x=> x[1] > array.Length/2).DefaultIfEmpty(new int[]{-1,0}).First()[0];
+
+    // Alternative
+    // return array.GroupBy(i => i).FirstOrDefault(g => g.Count() > array.Length / 2)?.Key ?? -1;
+  }
+
+  public static void Main(){
+    Console.WriteLine(WhoDominates(new int[] { 3,4,3,2,3,1,3,3 }));
+    Console.WriteLine(WhoDominates(new int[] { 1,1,1,2,2,2 }));
+  }
+}
