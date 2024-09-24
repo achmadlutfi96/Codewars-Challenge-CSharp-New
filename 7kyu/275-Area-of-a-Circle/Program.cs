7c31e60fe92f5dbb862ca346606fe1f004f8eefa
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-24 09:42:21 +0700

    Create 7kyu\275-Area-of-a-Circle

---
+++ 7kyu/275-Area-of-a-Circle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public static class Kata
+{
+  public static double CalculateAreaOfCircle(double radius)
+  {
+    if(radius <= 0) throw new ArgumentException();
+    return Math.Round(Math.PI * Math.Pow(radius, 2),3);
+  }
+
+  public static void Main(){
+    Console.WriteLine(CalculateAreaOfCircle(43.2673));
+  }
+}
