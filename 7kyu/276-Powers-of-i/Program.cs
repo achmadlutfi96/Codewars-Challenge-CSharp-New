a4a9896996a7b62527d150150b8aae56fc77a52d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-24 09:57:45 +0700

    Create 7kyu\276-Powers-of-i

---
+++ 7kyu/276-Powers-of-i/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Kata {
+
+  public static string Pofi(int n) {
+    return (n%4) switch 
+    {
+        1 => "i",
+        2 => "-1",
+        3 => "-i",
+        _ => "1" 
+    };
+
+    // Alternative
+    // return new[] { "1", "i", "-1", "-i" }[n % 4];
+  }
+
+  public static void Main(){
+    Console.WriteLine(Pofi(0));
+    Console.WriteLine(Pofi(1));
+    Console.WriteLine(Pofi(2));
+    Console.WriteLine(Pofi(3));
+    Console.WriteLine(Pofi(4));
+  }
+
+}
