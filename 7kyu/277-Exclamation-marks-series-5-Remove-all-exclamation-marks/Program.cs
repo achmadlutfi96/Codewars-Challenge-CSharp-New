87e020b9c22a7da41c4d5e8713c789eedee4d77d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-25 09:56:32 +0700

    Create 7kyu\277-Exclamation-marks-series-5-Remove

---
+++ 7kyu/277-Exclamation-marks-series-5-Remove-all-exclamation-marks/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+    public static string Remove(string s)
+    {
+        //coding and coding....
+        return string.Join(" ",s.Split(" ").Select(x=>x.TrimEnd('!')));
+
+        // Alternative
+        // return Regex.Replace(s, "\\b!*", "");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Remove("!!!Hi !!hi!!! !hi"));
+    }
+}
