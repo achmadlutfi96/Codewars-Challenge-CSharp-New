4879999e7fc03e82624a7430792d35557f1fe505
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-25 11:22:05 +0700

    Create 7kyu\278-Determine-if-the-poker-hand-is-flush

---
+++ 7kyu/278-Determine-if-the-poker-hand-is-flush/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static bool CheckIfFlush(string[] cards)
+  {
+    //Write your code here
+    
+     return cards.All(x=>x.EndsWith("S")) || cards.All(x=>x.EndsWith("D")) || cards.All(x=>x.EndsWith("C")) || cards.All(x=>x.EndsWith("H"));
+
+    //  Alternative
+    // return cards.All(x => x.Last() == cards.First().Last())
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(CheckIfFlush(["10D", "QD", "7D", "KD", "5D"]));
+  }
+}
