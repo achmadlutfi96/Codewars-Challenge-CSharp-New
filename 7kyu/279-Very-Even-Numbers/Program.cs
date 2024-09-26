eb5c14ce8333d0b2bb4e8e0702b92393f6dcb920
Achmad Lutfi <alhacker1996@gmail.com>
2024-09-26 20:23:28 +0700

    Create 7kyu\279-Very-Even-Numbers

---
+++ 7kyu/279-Very-Even-Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+    public static bool IsVeryEvenNumber(int number)
+    {
+        while(number.ToString().Length > 1){
+            number = (int) number.ToString().Sum(x=>char.GetNumericValue(x));
+        }
+        return number%2==0;
+
+        // Alternative
+        // return (--n % 9 + 1) % 2 == 0;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(IsVeryEvenNumber(88));
+    }
+}
