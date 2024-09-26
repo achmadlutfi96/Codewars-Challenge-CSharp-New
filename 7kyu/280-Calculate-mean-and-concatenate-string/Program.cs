0c79de418a571063065aa7bb6e5d4be7b7e61c7c
Achmad Lutfi <alhacker1996@gmail.com>
2024-09-26 20:36:45 +0700

    Create 7kyu\280-Calculate-mean-and-concatenate-string

---
+++ 7kyu/280-Calculate-mean-and-concatenate-string/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static object[] Mean(char[] lst)
+  {
+    return new object[]{lst.Where(x=>char.IsNumber(x)).Average(x=>char.GetNumericValue(x)), string.Concat(lst.Where(x=>char.IsLetter(x)))};
+
+    // Alternative
+    // return new object[] {lst.Where(char.IsNumber).Average(c => c - 48), lst.Where(char.IsLetter)};
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(",", Mean(new [] { 'u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0' })));
+  }
+}
