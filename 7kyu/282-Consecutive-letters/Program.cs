f7507d0663b85d58ea4d5cf832f6369be67b2e4c
Achmad Lutfi <alhacker1996@gmail.com>
2024-09-28 15:04:41 +0700

    Create 7kyu\282-Consecutive-letters

---
+++ 7kyu/282-Consecutive-letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution
+{
+    public static bool solve(string s)
+    {
+        //..
+        return s.Distinct().Count() == s.Length ? s.Max() - s.Min() == s.Length - 1 : false;
+
+        // Alternative 1
+        // return Enumerable.Range(s.Min(), s.Distinct().Count()).Sum() == s.Sum(c => c);
+
+        // Alternative 2
+        // return "abcdefghijklmnopqrstuvwxyz".Contains(String.Concat(s.OrderBy(c => c)));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(solve("abc"));
+        Console.WriteLine(solve("abd"));
+        Console.WriteLine(solve("dabc"));
+        Console.WriteLine(solve("abbc"));
+        Console.WriteLine(solve("adda"));
+    }
+}
