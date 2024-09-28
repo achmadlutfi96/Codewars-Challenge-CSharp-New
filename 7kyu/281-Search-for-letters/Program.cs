1bb8ab7e1560e070c7462958d0e8a8428fe62689
Achmad Lutfi <alhacker1996@gmail.com>
2024-09-28 14:42:25 +0700

    Create 7kyu\281-Search-for-letters

---
+++ 7kyu/281-Search-for-letters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static string Change(string input)
+    {
+        // ...
+        return String.Concat("abcdefghijklmnopqrstuvwxyz".Select(x=> input.ToLower().Contains(x) ? '1': '0'));
+
+        // Alternative
+        // return string.Concat(Enumerable.Range('a', 26).Select(i => input.ToLower().Contains((char) i) ? 1 : 0));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Change("a **&  bZ"));
+    }
+}
