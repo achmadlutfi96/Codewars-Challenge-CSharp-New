90700b5e895e41c6a7324919501b94d68753a4e2
Achmad Lutfi <alhacker1996@gmail.com>
2024-09-29 17:13:29 +0700

    Create 7kyu\284-Help-Suzuki-rake-his-garden

---
+++ 7kyu/284-Help-Suzuki-rake-his-garden/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string RakeGarden(string garden)
+  {
+    return string.Join(" ", garden.Split(" ").Select(x=>x.Equals("rock") || x.Equals("gravel") ? x : "gravel"));
+
+    // Alternative 
+    // return Regex.Replace(garden, @"\b(?!(rock|gravel)\b)\w+", "gravel");
+  }
+
+  public static void Main(){
+    Console.WriteLine(RakeGarden("slug spider rock gravel gravel gravel gravel gravel gravel gravel"));
+  }
+}
