1ed6a3c301ef8035250dab1d0032b0ba0e90644d
Achmad Lutfi <alhacker1996@gmail.com>
2024-09-30 16:06:22 +0700

    Create 7kyu\285-Exclamation-marks-series-7-Remove-words

---
+++ 7kyu/285-Exclamation-marks-series-7-Remove-words/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string Remove(string s)
+  {
+    return string.Join(" ",s.Split(" ").Where(x=>!(x.Where(y=>y == '!').Count() == 1)));
+
+    // Alternative
+    // return string.Join(" ", s.Split(' ').Where(x => x.Count(c => c == '!') != 1));
+  }
+
+  public static void Main(){
+    Console.WriteLine(Remove("Hi! Hi!! Hi!"));
+  }
+}
