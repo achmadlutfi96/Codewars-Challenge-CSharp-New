6d0345b18da68883c9c15fe5d69f5d040eeb1a80
Achmad Lutfi <alhacker1996@gmail.com>
2024-09-30 16:15:08 +0700

    Create 7kyu\286-Partial-Word-Searching

---
+++ 7kyu/286-Partial-Word-Searching/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static string[] WordSearch(string query, string[] seq)
+  {
+    return seq.Where(x=>x.ToLower().Contains(query.ToLower())).DefaultIfEmpty("Empty").ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(String.Join(",", WordSearch("ab", new string[] {"za", "ab", "abc", "zab", "zbc"})));
+  }
+}
