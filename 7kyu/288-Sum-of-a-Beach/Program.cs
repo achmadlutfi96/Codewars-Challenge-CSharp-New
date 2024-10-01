26295d9851bba7807de642a41835349d600abcb9
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-01 18:09:45 +0700

    Create 7kyu\288-Sum-of-a-Beach

---
+++ 7kyu/288-Sum-of-a-Beach/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static int SumOfABeach(string s)
+    {
+        // your code
+        return Regex.Matches(s.ToLower(), @"(sand)|(water)|(sun)|(fish)").Count();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(SumOfABeach("sunsunsunsun"));
+    }
+}
