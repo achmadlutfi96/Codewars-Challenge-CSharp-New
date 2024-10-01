ba22175982b6e66d9f208228500b162d77048326
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-01 17:59:12 +0700

    Create 7kyu\287-Multiply-Word-in-String

---
+++ 7kyu/287-Multiply-Word-in-String/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+    public static string ModifyMultiply(string str,int loc,int num)
+    {
+        return string.Join("-", Enumerable.Repeat(str.Split(" ").ElementAt(loc), num));
+    }
+
+    public static void Main(){
+        Console.WriteLine(ModifyMultiply("Talk is cheap Show me the code",2,4));
+    }
+}
