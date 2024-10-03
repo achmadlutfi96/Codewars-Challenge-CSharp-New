b0006ba53ceb61df6868a46eaa54c0619bb1958a
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-03 11:56:45 +0700

    Create 7kyu\289-Valid-Parentheses

---
+++ 7kyu/289-Valid-Parentheses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static bool ValidParentheses(string str)
+    {
+        while(str.Contains("()")){
+            str = str.Replace("()","");
+        }
+        return str.Length == 0;
+    }
+
+    public static void Main(){
+        Console.WriteLine(ValidParentheses("())(()"));
+    }
+
+}
