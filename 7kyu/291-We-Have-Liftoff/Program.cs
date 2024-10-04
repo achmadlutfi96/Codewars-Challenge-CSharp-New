5e0523189f14fd2eea706db32270524e3a5d69ac
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-04 11:32:32 +0700

    Create 7kyu\291-We-Have-Liftof

---
+++ 7kyu/291-We-Have-Liftoff/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+    public static string Liftoff(List<int> instructions)
+    {
+        // throw new NotImplementedException();
+        return string.Join(" ", instructions.OrderByDescending(x=>x)) + " liftoff!";  
+    }
+
+    public static void Main(){
+        Console.WriteLine(Liftoff(new List<int> {2, 8, 10, 9, 1, 3, 4, 7, 6, 5}));
+    }
+}
