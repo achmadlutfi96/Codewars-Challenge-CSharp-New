ee1e9f4d1a2836c92763c3c4ee5baab8e93541cd
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-04 12:25:03 +0700

    Create 7kyu\292-How-Green-Is-My-Valley

---
+++ 7kyu/292-How-Green-Is-My-Valley/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+public class Valley
+{
+
+    public static int[] MakeValley(int[] arr)
+    {
+        // your code
+        Array.Sort(arr);
+        Array.Reverse(arr);
+        Queue<int> arr1 = new();
+        Stack<int> arr2 = new();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                arr1.Enqueue(arr[i]);
+            }
+            else
+            {
+                arr2.Push(arr[i]);
+            }
+        }
+        return arr1.Concat(arr2).ToArray();
+
+        // Alternative
+        // var leftList = new List<int>();
+        // var rightList = new List<int>();
+        // var inputList = arr.ToList();
+        // inputList.Sort();
+        // var counter = inputList.Count - 1;
+        // while (counter >= 0)
+        // {
+        // 	leftList.Add(inputList[counter]);
+        // 	counter--;
+        // 	if (counter >= 0)
+        // 	{
+        // 		rightList.Add(inputList[counter]);
+        // 	}
+        // 	counter--;
+        // }
+        // rightList.Reverse();
+        // leftList.AddRange(rightList);
+        // return leftList.ToArray();
+
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", MakeValley(new int[] { 17, 17, 15, 14, 8, 7, 7, 5, 4, 4, 1 })));
+    }
+}
