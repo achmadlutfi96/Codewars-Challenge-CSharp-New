01969f197d8d7a8bd3a5f4e071de79d00a85fd21
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-06 11:32:58 +0700

    Create 7kyu\296-Herons-formula

---
+++ 7kyu/296-Herons-formula/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static double Heron(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            return Math.Round(Math.Sqrt(s * (s-a) * (s-b) * (s-c)), 2);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Heron(6, 8, 10));
+        }
+    }
+}
