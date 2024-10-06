e506b2e4725daa00161b50b4a1151f3d371afff1
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-06 11:23:52 +0700

    Create 7kyu\295-Fibonacci

---
+++ 7kyu/295-Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Fibonacci
+{
+    public static int Fib(int n)
+    {
+        // your code here!
+        if(n == 1){
+            return 0;
+        }
+        if(n == 2){
+            return 1;
+        }
+
+        int x = 0;
+        int y = 1;
+        int sum = 0;
+        for(int i = 2; i <= n; i++){
+            sum = x + y;
+            x = y;
+            y = sum;
+        }
+
+        return sum;
+
+        // Alternative
+        // if (n==1||n==2)
+            // {
+            //     return 1;
+            // }
+            // return Fib(n - 1) + Fib(n - 2);
+    }
+
+    public static void Main(){
+        Console.WriteLine(Fib(10));
+    }
+}
