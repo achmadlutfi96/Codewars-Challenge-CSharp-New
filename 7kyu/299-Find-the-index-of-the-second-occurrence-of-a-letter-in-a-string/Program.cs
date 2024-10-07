3f198e41fd0314e7fa0f2c78a5294be0b1be86cb
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-07 11:15:34 +0700

    Create 7kyu\299-Find-the-index-of-the-second

---
+++ 7kyu/299-Find-the-index-of-the-second-occurrence-of-a-letter-in-a-string/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Kata
+{
+    public static int SecondSymbol(string str, char symbol)
+    {
+        if (!str.Contains(symbol))
+        {
+            return -1;
+        }
+        int[] res = str.Select((v, i) => v.Equals(symbol) ? i : -1).Where(x => x != -1).ToArray();
+        return res.Length > 1 ? res[1] : -1;
+
+        // Alternative 1
+        // int occur = 0;
+        // for (int i = 0; i < str.Length; i++) {
+        //   if (str[i] == symbol) occur++;
+        //   if (occur == 2) return i;
+        // }
+        // return -1;
+
+        // Alternative 2
+        // return str.IndexOf(symbol, str.IndexOf(symbol) + 1);
+
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(SecondSymbol("Hello world!!", 'l'));
+        Console.WriteLine(SecondSymbol("", 'q'));
+        Console.WriteLine(SecondSymbol(" ", 'q'));
+    }
+}
