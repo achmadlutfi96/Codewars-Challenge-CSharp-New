414cd32cc2075dca4a13ccc57091121a1da7edc3
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-07 11:25:32 +0700

    Create 7kyu\300-sPoNgEbOb-MeMe

---
+++ 7kyu/300-sPoNgEbOb-MeMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string SpongeMeme(string sentence)
+  {
+    return string.Concat(sentence.Select((x,i)=>i%2==0 ? char.ToUpper(x):char.ToLower(x)));
+  }
+
+  public static void Main(){
+    Console.WriteLine(SpongeMeme("stop Making spongebob Memes!"));
+  }
+}
