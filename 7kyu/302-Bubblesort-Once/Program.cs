12360ff3ee3f4a1c2108712c7612fd56cf4e62c4
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-08 11:23:28 +0700

    Create 7kyu\302-Bubblesort-Once

---
+++ 7kyu/302-Bubblesort-Once/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Kata
+{
+    public static int[] BubbleSortOnce(int[] input)
+    {
+        // Code the Bubblesort Algorithm here :D
+        int[] res = input.ToArray();
+        for (int i = 0; i < res.Length-1; i++)
+        {
+            if (res[i] > res[i+1]){
+                int temp = res[i];
+                res[i] = res[i+1];
+                res[i+1] = temp;
+            }
+        }
+        return res;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", BubbleSortOnce(new int[] { 9, 7, 5, 3, 1, 2, 4, 6, 8 })));
+    }
+}
