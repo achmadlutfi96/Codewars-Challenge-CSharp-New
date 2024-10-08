57e38e3213d5ada155fa908b42178cc9d945eb4e
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-08 10:25:41 +0700

    Create 7kyu\301-Odder-Than-the-Rest

---
+++ 7kyu/301-Odder-Than-the-Rest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+  public static int OddOne(List<int> list)
+  {
+    // Code here
+    return list.All(x=>Math.Abs(x)%2 == 0) ? -1 : list.IndexOf(list.Where(x=>Math.Abs(x)%2 == 1).First());
+
+    // Alternative
+    // return list.FindIndex(i => i % 2 != 0);
+  }
+
+  public static void Main(){
+    Console.WriteLine(OddOne(new List<int> {4, -8, 98, -12, -7, 90, 100}));
+    Console.WriteLine(OddOne(new List<int> {2, 4, 6, 8}));
+  }
+}
