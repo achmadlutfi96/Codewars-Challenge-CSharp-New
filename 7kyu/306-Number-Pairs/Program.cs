9ac6da5240e50feee22199640f5fa2edf9aad83a
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-09 12:20:11 +0700

    Create 7kyu\306-Number-Pairs

---
+++ 7kyu/306-Number-Pairs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] getLargerNumbers(int[] a, int[] b)
+    {
+        return a.Select((v,i)=> v > b[i] ? v : b[i]).ToArray();
+
+        // Alternative
+        // return a.Zip(b, Max).ToArray();
+    }
+
+    public static void Main()
+    {
+        int[] arr1 = new int[] { 13, 64, 5, 7, 88 };
+        int[] arr2 = new int[] { 23, 4, 53, 17, 80 };
+        Console.WriteLine(string.Join(",", getLargerNumbers(arr1, arr2)));
+    }
+}
