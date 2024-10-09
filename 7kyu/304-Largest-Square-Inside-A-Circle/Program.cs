70c1510dc9a878ac26cfa8dd7547a2bc6e8d130f
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-09 11:31:41 +0700

    Create 7kyu\304-Largest-Square-Inside-A-Circle

---
+++ 7kyu/304-Largest-Square-Inside-A-Circle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Kata
+{
+    using System;
+    public class Circle
+    {
+        public static double AreaLargestSquare(int r)
+        {
+            return r * (r * 2);
+
+            //   Alternative
+            // return 2*r*r;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(AreaLargestSquare(15));
+        }
+    }
+}
