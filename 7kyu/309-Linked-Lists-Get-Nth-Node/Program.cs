a3b15661adbe14179eb3097ae4fd3b826e4e1981
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-10 12:39:18 +0700

    Create 7kyu\309-Linked-Lists-Get-Nth-Nod

---
+++ 7kyu/309-Linked-Lists-Get-Nth-Node/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public partial class Node
+{
+  public int Data;
+  public Node Next;
+  
+  public Node(int data)
+  {
+    this.Data = data;
+    this.Next = null;
+  }
+  
+  public static Node GetNth(Node node, int index)
+  {
+    // throw new NotImplementedException();
+    return node == null ? throw new ArgumentException() : index == 0 ? node : GetNth(node.Next, index - 1);
+  }
+}
