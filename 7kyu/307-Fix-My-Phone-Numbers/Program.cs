6bc9e21f98418598f1ec305f0a6a16bbc60628e3
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-10 11:34:39 +0700

    Create 7kyu\307-Fix-My-Phone-Numbers

---
+++ 7kyu/307-Fix-My-Phone-Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System;
+public class Kata
+{
+  public static String IsItANum(string str) {
+    // your code here;
+    string res = string.Concat(str.Where(x=>char.IsDigit(x)));
+    return res.StartsWith("0") && res.Length == 11 ? res : "Not a phone number";
+
+    // Alternative
+    // var phone = Regex.Replace(str, @"\D", "");
+    // return Regex.IsMatch(phone, @"^0\d{10}$") ? phone : "Not a phone number";
+  }
+
+  public static void Main(){
+    Console.WriteLine(IsItANum("S:)0207ERGQREG88349F82!efRF)"));
+    Console.WriteLine(IsItANum("sjfniebienvr12312312312ehfWh"));
+  }
+}
