ee56627ab6902ecaf045248a2d90c8ffb318e466
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-10 11:58:29 +0700

    Create 7kyu\308-Linked-Lists-Push-BuildOneTwoThree

---
+++ 7kyu/308-Linked-Lists-Push-BuildOneTwoThree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Node
+{
+    public int Data;
+    public Node Next;
+
+    public Node(int data)
+    {
+        this.Data = data;
+        this.Next = null;
+    }
+
+    public static Node Push(Node head, int data)
+    {
+        // throw new NotImplementedException();
+        Node res = new Node(data);
+        res.Next = head;
+        return res;
+
+    }
+
+    public static Node BuildOneTwoThree()
+    {
+        // throw new NotImplementedException();
+        Node chained = null;
+        chained = Node.Push(chained, 3);
+        chained = Node.Push(chained, 2);
+        chained = Node.Push(chained, 1);
+        return chained;
+    }
+}
