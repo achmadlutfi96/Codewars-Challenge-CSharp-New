4f50e77e60a76fa1a7b34e33a2d23a715d7ee916
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-11 13:56:28 +0700

    Create 7kyu\310-Powers-of-3

---
+++ 7kyu/310-Powers-of-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Kata
+{
+  public static int LargestPower(int n)
+  {
+    // Code goes here.
+    // Watch out for overflows!
+    int k = -1;
+    while (Math.Pow(3, k+1) < n){
+      k++;
+    }
+    return k;
+
+    // Alternative
+    // return (int)Math.Ceiling(Math.Log(n, 3)) - 1;
+  }
+
+  public static void Main(){
+    Console.WriteLine(LargestPower(3));
+    Console.WriteLine(LargestPower(4));
+  }
+}
