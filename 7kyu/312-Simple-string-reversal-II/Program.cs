49eb624ff2e979b7d4c3acb06b39737c839c4acb
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-11 14:39:43 +0700

    Create 7kyu\312-Simple-string-reversal-II

---
+++ 7kyu/312-Simple-string-reversal-II/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public static String solve(String s, int a, int b)
+    {
+        //..
+        b = s.Length > b ? b + 1 : s.Length;
+        return s[..a] + string.Concat(s[a..b].Reverse()) + s[b..];
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(solve("codingIsFun", 2, 100));
+        Console.WriteLine(solve("codewars", 1, 5));
+        Console.WriteLine(solve("FunctionalProgramming", 2, 15));
+    }
+}
