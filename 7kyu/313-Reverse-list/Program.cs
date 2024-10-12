3c7e3c0d1c73f59a48422cc1e4c1719465a288e9
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-12 11:42:32 +0700

    Create 7kyu\313-Reverse-list

---
+++ 7kyu/313-Reverse-list/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static int[] reverseList(int[] list)
+        {
+            Array.Reverse(list);
+            return list;
+        }
+
+        public static void Main(){
+            Console.WriteLine(string.Join(",", reverseList(new int[]{1,2,3})));
+        }
+    }
+}
