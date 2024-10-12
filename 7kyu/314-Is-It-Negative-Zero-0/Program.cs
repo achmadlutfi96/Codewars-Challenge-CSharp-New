01e0e8356c6e683411737f44311a611bb347716b
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-12 11:55:33 +0700

    Create 7kyu\314-Is-It-Negative-Zero-0

---
+++ 7kyu/314-Is-It-Negative-Zero-0/Program.cs
@@ -0,0 +1,15 @@
+﻿public static class Kata {
+    public static bool IsNegativeZero(double n) {
+        return n.ToString().Equals("-0");
+
+        // Alternative 1
+        // return 1 / n == 1 / -0.0;
+        // Alternative 2
+        // return double.IsNegative(n) && n == 0.0;
+    }
+
+    public static void Main(){
+        Console.WriteLine(IsNegativeZero(-0.0));
+        Console.WriteLine(IsNegativeZero(0.0));
+    }
+}
