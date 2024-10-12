5d226ce6cb501f4624955eb7665b8eec18bae1ce
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-12 12:24:18 +0700

    Create 7kyu\315-Exclamation-marks-series-3-Remo

---
+++ 7kyu/315-Exclamation-marks-series-3-Remove-all-exclamation-marks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+public class Kata
+{
+  public static string Remove(string s)
+  {
+    //coding and coding....
+    return Regex.Replace(s, "(?!!*$)!", "");
+  }
+
+  public static void Main(){
+    Console.WriteLine(Remove("Hi! Hi!"));
+  }
+}
