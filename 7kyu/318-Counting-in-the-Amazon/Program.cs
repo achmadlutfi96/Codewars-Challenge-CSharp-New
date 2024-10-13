123e0bdbd9f2b5008f2930c71e844bc6efc76648
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-13 21:14:11 +0700

    Create 7kyu\318-Counting-in-the-Amazon

---
+++ 7kyu/318-Counting-in-the-Amazon/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Kata
+{
+    public static string CountArara(int n)
+    {
+        //your code here
+        if (n == 1) return "anane";
+        return string.Join(" ", Enumerable.Range(0, n/2).Select(x=>"adak")) + (n%2==1? " anane" : "");
+
+        // Alternative
+        // return string.Concat(Enumerable.Repeat("adak ", (n - 1) / 2)) + (n % 2 == 1 ? "anane" : "adak");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(CountArara(1));
+    }
+}
