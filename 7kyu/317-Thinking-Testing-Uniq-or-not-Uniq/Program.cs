da5d5b404f174c3b94ba85ada015f00812a15c39
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-13 20:51:39 +0700

    Create 7kyu\317-Thinking-Testing-Uniq-or-not-Uniq

---
+++ 7kyu/317-Thinking-Testing-Uniq-or-not-Uniq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public static int[] Testit(int[] a, int[] b){
+          return a.Distinct().Concat(b.Distinct()).OrderBy(x=>x).ToArray();
+        }
+
+        public static void Main(){
+            Console.WriteLine(string.Join(",", Testit(new int[]{1,2},new int[]{1,2})));
+        }
+    }
+}
