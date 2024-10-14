4a727347ce8f8a65a87e3021c4166c2ff2e7a2a3
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-14 16:45:56 +0700

    Create 7kyu\320-Simple-equation-reversal

---
+++ 7kyu/320-Simple-equation-reversal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{    
+    public static string solve(string eq){
+        //..
+        string[] v = eq.Replace("*", " ").Replace("+", " ").Replace("-", " ").Replace("/", " ").Split(" ").Reverse().ToArray();
+        char[] o = eq.Where(x=>"*+-/".Contains(x)).Reverse().ToArray();
+        string res = ""; 
+        for (int i = 0; i < v.Length; i++)
+        {
+            res+=v[i];
+            if(i != v.Length-1){
+                res+=o[i];
+            }
+        }
+        return res;
+
+        // Alternative 1
+        // return string.Concat(
+        // eq.Split("+-*/".ToArray()).Reverse()
+        // .Zip(
+        //     eq.Where(c => "+-*/".Contains(c)).Select(c => $"{c}").Reverse().Append("")
+        //     , (m, o) => m + o));
+
+        // Alternative 2
+        // return string.Join("", Regex.Split(eq, @"(\*)|(\+)|(-)|(/)").Reverse());
+
+        // Alternative 3
+        // return string.Concat(Regex.Matches(eq, @"\w+|.").Reverse());
+    }
+
+    public static void Main(){
+        Console.WriteLine(solve("a+b-c/d*30"));
+        Console.WriteLine(solve("100*b/y"));
+    }
+}
