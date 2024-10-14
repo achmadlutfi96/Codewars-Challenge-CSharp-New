cbe75efd356142ad012e14cc649c87d55f3e1484
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-14 17:10:49 +0700

    Create 7kyu\321-Kooka-Counter

---
+++ 7kyu/321-Kooka-Counter/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Dinglemouse
+{
+    public static int KookaCounter(string laughing)
+    {
+        // Your code here!
+        int male = laughing.Split("ha").Where(x=>x!="").Count();
+        int female = laughing.Split("Ha").Where(x=>x!="").Count();
+        return male+female;
+
+        // Alternative
+        // return Regex.Matches(laughing, "(Ha)+|(ha)+").Count;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(KookaCounter(string.Empty));
+        Console.WriteLine(KookaCounter("hahahahaha"));
+        Console.WriteLine(KookaCounter("hahahahahaHaHaHa"));
+        Console.WriteLine(KookaCounter("HaHaHahahaHaHa"));
+    }
+}
