05c528fed72bf10359fc0a567057a6e967617260
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-15 14:44:57 +0700

    Create 7kyu\324-Simple-Fun-10-Range-Bit-Counting

---
+++ 7kyu/324-Simple-Fun-10-Range-Bit-Counting/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int RangeBitCount(int a, int b)
+        {
+            //coding and coding..
+            return Enumerable.Range(a, b - a + 1).Select(x => Convert.ToString(x, 2).Where(y => y == '1').Count()).Sum();
+
+            // Alternative 1
+            //   return Enumerable.Range(a,b-a+1).Select(x=>x.ToString("b").Where(y=>y=='1').Count()).Sum();
+
+            // Alternative 2
+            // return string.Concat(Enumerable.Range(a, b - a + 1).Select(x => Convert.ToString(x, 2))).Count(x=> x == '1');
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(RangeBitCount(2, 7));
+            Console.WriteLine(RangeBitCount(0, 1));
+            Console.WriteLine(RangeBitCount(4, 4));
+        }
+    }
+}
