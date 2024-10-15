9d5afed23fe9fe2aedf4db91ccabdf710af8676d
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-15 13:16:51 +0700

    Create 7kyu\322-Ghostbusters-whitespace-removal

---
+++ 7kyu/322-Ghostbusters-whitespace-removal/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Kata
+{
+    public static string GhostBusters(string building)
+    {
+        //Code goes here!
+        return building.Contains(' ') ? building.Replace(" ","") : "You just wanted my autograph didn't you?";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GhostBusters("O  f fi ce"));
+    }
+}
