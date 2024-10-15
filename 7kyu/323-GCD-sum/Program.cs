6c919c2320525ce9228b505f30fe4c352b2e1aed
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-15 14:14:59 +0700

    Create 7kyu\323-GCD-sum

---
+++ 7kyu/323-GCD-sum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class GCDsum
+{
+    public static (int,int) solve (int s, int g){         
+         //..
+         return s%g == 0 ? (g, s-g): (-1,-1);
+    }
+
+    public static void Main(){
+        Console.WriteLine(solve(10,2));
+    }        
+}
