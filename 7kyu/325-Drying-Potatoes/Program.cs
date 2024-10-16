eebb414b20f7900ff8da46d139146cfb9e00cf58
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-16 12:19:03 +0700

    Create 7kyu\325-Drying-Potatoes

---
+++ 7kyu/325-Drying-Potatoes/Program.cs
@@ -0,0 +1,23 @@
+﻿public class DryPotatoes
+{
+
+    public static int Potatoes(int p0, int w0, int p1)
+    {
+        // your code
+        decimal a =(decimal) p0/100;
+        decimal b =(decimal) a*w0;
+        decimal c = w0-b;
+        decimal d = (decimal)(100-p1)/100;
+        int res = (int) (c/d);
+        return res;
+
+        // Alternative
+        // return w0 * (100 - p0) / (100 - p1);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Potatoes(99, 100, 98));
+        Console.WriteLine(Potatoes(82, 127, 80));
+    }
+}
