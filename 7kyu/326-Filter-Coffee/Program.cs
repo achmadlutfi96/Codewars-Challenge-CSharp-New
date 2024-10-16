954ce05d7223a5af657cc312eec60041be4914e6
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-16 13:41:57 +0700

    Create 7kyu\326-Filter-Coffee

---
+++ 7kyu/326-Filter-Coffee/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Kata
+{
+    public static string Search(int budget, int[] prices)
+    {
+        // return array pf prices that are within budget
+        return string.Join(",",prices.OrderBy(x=>x).Where(x=>budget>=x));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Search(14, new int[]{7,3,23,9,14,20,7}));
+    }
+}
