a2f74f361e99f74016536a0a745282f27216ade0
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-16 13:53:12 +0700

    Create 7kyu\327-Histogram-H1

---
+++ 7kyu/327-Histogram-H1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+public class Dinglemouse
+{
+    public static string Histogram(int[] results)
+    {
+        return string.Join("\n",results.Select((v,i)=>v==0 ? $"{i+1}|" :$"{i+1}|{string.Concat(Enumerable.Repeat('#',v))} {v}").Reverse())+"\n";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Histogram(new int[]{ 7,3,10,1,0,5 }));
+    }
+}
