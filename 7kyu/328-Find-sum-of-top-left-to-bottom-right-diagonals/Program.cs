b5468a067bd2757b2b4d0172c5192a6875900647
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-17 14:48:29 +0700

    Create 7kyu\328-Find-sum-of-top-left-to-bottom-right-diagon

---
+++ 7kyu/328-Find-sum-of-top-left-to-bottom-right-diagonals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int DiagonalSum(int[,] matrix)
+    {
+        //TODO
+        int res = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (i == j)
+                {
+                    res += matrix[i, j];
+                }
+            }
+        }
+        return res;
+
+        // Alternative 1
+        // return Enumerable.Range(0, matrix.GetLength(0)).Sum(i => matrix[i, i]);
+
+        // Alternative 2
+        // int result = 0;
+        // for (int i = 0; i < matrix.GetLength(matrix.Rank - 1); i++)
+        // {
+        //     result += matrix[i, i];
+        // }
+        // return result;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(DiagonalSum(new int[,] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } }));
+    }
+}
