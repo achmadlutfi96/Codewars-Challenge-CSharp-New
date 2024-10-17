27a8cb745a716228447ef08ddb665c3fda8605d5
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-17 15:57:20 +0700

    Create 7kyu\330-Sort-the-Vowels

---
+++ 7kyu/330-Sort-the-Vowels/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string SortVowels(string s)
+    {
+        if (s == null)
+        {
+            return "";
+        }
+        return string.Join("\n", s.Select(x => "aiueo".Contains(char.ToLower(x)) ? $"|{x}" : $"{x}|"));
+
+        // Alternative 
+        // return string.IsNullOrEmpty(s)
+        // ? ""
+        // : string.Join("\n", s.Select(c => "aeiouAEIOU".Contains(c) ? $"|{c}" : $"{c}|"));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(SortVowels("Codewars"));
+        Console.WriteLine(SortVowels("Rnd Te5T"));
+    }
+}
