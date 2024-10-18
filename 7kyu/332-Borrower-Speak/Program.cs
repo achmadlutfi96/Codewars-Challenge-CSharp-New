eff760498bb891917f77572d9c005865f216d67b
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-18 10:57:04 +0700

    Create 7kyu\332-Borrower-Speak

---
+++ 7kyu/332-Borrower-Speak/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static string Borrow(string s)
+    {
+        return string.Concat(s.Where(char.IsLetter).Select(char.ToLower));
+
+        // Alternative
+        // return string.Concat(s.ToLower().Where(c => c >= 'a' && c <= 'z'));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Borrow("WhAt! FiCK! DaMn CAke?"));
+    }
+}
