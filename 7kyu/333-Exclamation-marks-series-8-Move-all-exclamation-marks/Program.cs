a837c6d7b6bd16382267168e0f54b90aadcf9f1d
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-18 11:05:53 +0700

    Create 7kyu\333-Exclamation-marks-series-8-Move-all

---
+++ 7kyu/333-Exclamation-marks-series-8-Move-all-exclamation-marks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Remove(string s)
+    {
+        return string.Concat(s.Replace("!",""), string.Concat(s.Where(x=>x=='!')));
+        // Alternative
+        // return string.Concat(s.OrderBy(c => c == '!');
+    }
+
+    public static void Main(){
+        Console.WriteLine(Remove("Hi! Hi!! Hi!"));
+    }
+}
