87f8b33f7489789206f971ab2de74fc353194d76
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-19 13:20:21 +0700

    Create 7kyu\334-Monotone-trave

---
+++ 7kyu/334-Monotone-travel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static bool IsMonotone(int[] arr)
+    {
+        // TODO
+        return arr.OrderBy(x=>x).SequenceEqual(arr);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(IsMonotone(new int[] {1, 2, 3, 3, 4, 5}));
+        Console.WriteLine(IsMonotone(Enumerable.Range(1, 5).Reverse().ToArray()));
+        Console.WriteLine(IsMonotone(new int[] {}));
+    }
+}
