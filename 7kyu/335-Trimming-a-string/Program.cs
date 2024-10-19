e4dce51077a01622eb79134f5867490913628fc1
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-19 13:51:41 +0700

    Create 7kyu\335-Trimming-a-string

---
+++ 7kyu/335-Trimming-a-string/Program.cs
@@ -0,0 +1,24 @@
+﻿public class TrimStringKata
+{
+    public static string TrimString(string phrase, int len)
+    {
+        if (phrase.Length > len)
+        {
+            if (len <= 3)
+            {
+                return phrase.Substring(0, len) + "...";
+            }
+            return phrase.Substring(0, len - 3) + "...";
+        }
+        return phrase;
+
+        // Alternative
+        // return phrase.Length > len ? phrase[..(len > 3 ? len - 3 : len)] + "..." : phrase;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(TrimString("Creating kata is fun", 14));
+        Console.WriteLine(TrimString("Hey", 3));
+    }
+}
