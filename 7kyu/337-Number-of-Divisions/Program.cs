b428a3ba679dc38df26f63558587c127807f0b14
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-20 10:07:47 +0700

    Create 7kyu\337-Number-of-Divisions

---
+++ 7kyu/337-Number-of-Divisions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Kata
+{
+    public static int Divisions(int n, int divisor)
+    {
+        int t = 0;
+        while(n/divisor > 0){
+            n = n/divisor;
+            t++;
+        }
+        return t;
+
+        // Alternative
+        // return (int)Math.Log(n, divisor);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Divisions(100,2));
+    }
+}
