7733f2e38158ef6346988efa859822edc88ac823
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-20 12:19:04 +0700

    Create7kyu\339-Sort-an-array-by-value-and-index

---
+++ 7kyu/339-Sort-an-array-by-value-and-index/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] SortByValueAndIndex(int[] array)
+    {
+        return array.Select((v,i)=>new int[]{v , v*(i+1)}).OrderBy(x=>x[1]).Select(x=>x[0]).ToArray();
+
+        // Alternative
+        // return array.OrderBy(n => n * i++).ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",",SortByValueAndIndex(new int[] { 23, 2, 3, 4, 5 })));
+    }
+}
