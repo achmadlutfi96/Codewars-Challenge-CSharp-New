17e34e8f5df1f3bcfd1cbc5170c618ee23d619d4
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-21 12:12:42 +0700

    Create 7kyu\342-Simple-Fun-3-Late-Ride

---
+++ 7kyu/342-Simple-Fun-3-Late-Ride/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public static int LateRide(int n)
+        {
+            //coding and coding..
+            return (n / 60).ToString().Sum(x => (int)char.GetNumericValue(x)) + (n % 60).ToString().Sum(x => (int)char.GetNumericValue(x));
+
+            // Alternative
+            // return $"{n / 60}{n % 60}".Sum(c => c - '0');
+
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(LateRide(808));
+        }
+    }
+}
