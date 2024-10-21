10e6b1a0161d8bc8c9cb3d65e876277e05d6bbf2
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-21 09:56:18 +0700

    Create 7kyu\341-Least-Larger

---
+++ 7kyu/341-Least-Larger/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Solution
+{
+    public class Kata
+    {
+        public static int LeastLarger(int[] a, int i)
+        {
+            int n = a.Where(x=>x>a[i]).OrderBy(x=>x).FirstOrDefault();
+            return a.Where(x=>x>a[i]).Count() > 0 ? Array.IndexOf(a, n) : -1;
+
+            // Alternative
+            // return Array.IndexOf(a, a.Min(x => x > a[i] ? x : null));
+        }
+
+        public static void Main(){
+            Console.WriteLine(LeastLarger(new[] { 4, 1, 3, 5, 6 }, 0));
+            Console.WriteLine(LeastLarger(new[] { 4, 1, 3, 5, 6 }, 4));
+        }
+    }
+}
