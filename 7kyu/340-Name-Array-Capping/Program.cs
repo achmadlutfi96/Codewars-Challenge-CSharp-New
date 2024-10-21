356f18d06137dbd6a0a14b6d06cf60d6cc9a86a7
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-21 09:06:12 +0700

    Create 7kyu\340-Name-Array-Capping

---
+++ 7kyu/340-Name-Array-Capping/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string[] CapMe(string[] strings)
+    {
+       return strings.Select(s=>char.ToUpper(s[0])+s.Substring(1).ToLower()).ToArray(); 
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", CapMe(new string[] {"KARLY", "DANIEL", "KELSEY"})));
+    }
+}
