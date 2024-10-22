30d504698e8abd3ceb0f463d1c1bab05ee08a701
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-22 16:03:13 +0700

    Create 7kyu\343-Hungarian-Vowel-Harmony-easy

---
+++ 7kyu/343-Hungarian-Vowel-Harmony-easy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+public static class Kata
+{
+    public static string Dative(string word)
+    {
+        char[] a = new char[]{'e', 'é', 'i', 'í', 'ö', 'ő', 'ü', 'ű'};
+        char[] b = new char[]{'a', 'á', 'o', 'ó', 'u', 'ú'};
+        return a.Contains(word.Where(x=> a.Contains(x) || b.Contains(x)).Last()) ? $"{word}nek" : $"{word}nak";
+
+        // Alternative
+        // return word + ("aáoóuú".Contains(word.Last(c => "eéiíöőüűaáoóuú".Contains(c))) ? "nak" : "nek");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Dative("virág"));
+    }
+}
