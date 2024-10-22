f3ca337129cd63a9756ba61f08463fcd01d7f602
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-22 16:55:01 +0700

    Create 7kyu\345-Making-Copies

---
+++ 7kyu/345-Making-Copies/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ListExtensions
+{
+    public static List<T> Copy<T>(this List<T> lst)
+    {
+        return lst.Take(lst.Count).ToList();
+
+        // Alternative
+        // return new List<T>(lst);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", Copy<int>(new List<int> {1, 2, 3, 4})));
+    }
+}
