6f155ad64ab93f08dea311db9a8391b85badb979
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-23 11:09:03 +0700

    Create 7kyu\346-MOD-256-without-the-MOD-operator

---
+++ 7kyu/346-MOD-256-without-the-MOD-operator/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+    public static int Mod256WithoutMod(int number)
+    {
+        return number - (number/256*256);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Mod256WithoutMod(254));
+    }
+}
