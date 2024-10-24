780121b99e016d9eb96346927123eccf82111626
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-24 11:21:00 +0700

    Create 7kyu\349-First-Class-Function-Factory

---
+++ 7kyu/349-First-Class-Function-Factory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class FunctionFactory
+{
+    public static Func<int[], int[]> factory(int x)
+    {
+        // Good Luck!
+        return y => y.Select(z=>z*x).ToArray();
+    }
+
+    public static void Main()
+    {
+        Func<int[], int[]> threes = FunctionFactory.factory(3);
+        int[] myArr = new int[] { 1, 2, 3 };
+        Console.WriteLine(string.Join(",", threes(myArr)));
+    }
+}
