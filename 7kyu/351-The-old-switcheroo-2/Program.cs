d05764306d9a3ffe7b01898a67d97aaa14ffb075
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-24 11:47:25 +0700

    Create 7kyu\351-The-old-switcheroo-2

---
+++ 7kyu/351-The-old-switcheroo-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string Encode(string str)
+    {
+        return string.Concat(str.Select(x=>char.IsLetter(x) ? (char.ToLower(x)-96).ToString() : x.ToString()));
+
+        // Alternative
+        // return string.Concat(str.Select(c => char.IsLetter(c) ? $"{c % 32}" : $"{c}"));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Concat(Encode("this is a long string!! Please [encode] @C0RrEctly")));
+    }
+}
