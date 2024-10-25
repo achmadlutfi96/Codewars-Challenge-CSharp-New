fbc45a53c41223f1612be3f1bafd0c9647ba72f0
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-25 11:51:51 +0700

    Create 7kyu\354-Ce-s-r-d-Strings

---
+++ 7kyu/354-Ce-s-r-d-Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Kata
+{
+    public static string Uncensor(string infected, string discovered)
+    {
+        string res = "";
+        int i = 0;
+        foreach (char item in infected)
+        {
+            if (item == '*'){
+                res += discovered[i];
+                i++;
+            }else{
+                res += item;
+            }
+            
+        }
+        return res;
+
+        // Alternative
+        // return string.Concat(infected.Select(c => c == '*' ? discovered[i++] : c));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae"));
+    }
+}
