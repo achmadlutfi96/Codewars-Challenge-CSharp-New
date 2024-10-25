107166a562793c546f6664ff1b1f79773945df53
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-25 11:32:13 +0700

    Create 7kyu\352-Circle-area-inside-square

---
+++ 7kyu/352-Circle-area-inside-square/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Kata
+{
+    using System;
+    public class Convert
+    {
+        public static double SquareAreaToCircle(double size)
+        {
+            return Math.Round(size * Math.PI /4.0,8);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(SquareAreaToCircle(9));
+            Console.WriteLine(SquareAreaToCircle(20));
+        }
+    }
+}
