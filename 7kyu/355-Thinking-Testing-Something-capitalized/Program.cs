e7898b9a1da5c2c3a97c50433600e626431166ca
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-26 12:10:33 +0700

    Create7kyu\355-Thinking-Testing-Something-capitalized

---
+++ 7kyu/355-Thinking-Testing-Something-capitalized/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Kata
+    {
+        public static string Testit(string s)
+        {
+            return s.Length == 0 ? "" : string.Join(" ", s.Split(" ").Select(x => x.Substring(0, x.Length - 1) + x.Last().ToString().ToUpper()));
+
+            //   Alternative 
+            // return Regex.Replace(s, ".\\b", Word => Word.ToString().ToUpper());
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Testit("ab ab"));
+        }
+    }
+}
