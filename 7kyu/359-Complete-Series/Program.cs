21b54e442615bdddeccc6a4ccd2221e89f0508ef
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-27 12:26:53 +0700

    Create 7kyu\359-Complete-Series

---
+++ 7kyu/359-Complete-Series/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SeriesKata
+{
+    using System;
+    using System.Linq;
+
+    public class Series
+    {
+
+        public static int[] CompleteSeries(int[] arr)
+        {
+            return arr.Distinct().Count() == arr.Length ? Enumerable.Range(0, arr.Max()+1).ToArray() : new int[] {0};
+
+            // Alternative
+            // return Enumerable.Range(0, arr.Distinct().SequenceEqual(arr) ? arr.Max() + 1 : 1).ToArray();
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(string.Join(", ",CompleteSeries([2,1])));
+        }
+    }
+}
