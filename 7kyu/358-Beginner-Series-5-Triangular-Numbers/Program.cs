3bfb8acbaffd2a2b14399365b4bd9dd9f91a8779
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-27 10:55:08 +0700

    Create 7kyu\358-Beginner-Series-5-Triangular-Numbers

---
+++ 7kyu/358-Beginner-Series-5-Triangular-Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class Triangular
+{
+    public static bool isTriangular(int T)
+    {
+        // Good Luck ;)
+        if (T < 0 || T > 2147483646)
+        {
+            return false;
+        }
+
+        // A Triangular number must be sum of first n
+        // natural numbers
+        long sum = 0;
+        for (int n = 1; sum <= T; n++)
+        {
+            sum = sum + n;
+            if (sum == T)
+                return true;
+        }
+
+        return false;
+
+        // Alternative 1
+        // return Math.Sqrt(8L * T + 1) % 1 == 0;
+
+        // Alternative 2
+        // int i = 1;
+        // while (T > 0) T -= i++;
+        // return (T == 0);
+
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(isTriangular(6));
+        Console.WriteLine(isTriangular(7));
+        Console.WriteLine(isTriangular(27));
+        Console.WriteLine(isTriangular(21));
+        Console.WriteLine(isTriangular(2147483646));
+    }
+}
