f34507ef2e394f0cc82c94549c990daf1b01487d
Achmad Lutfi <alhacker1996@gmail.com>
2024-10-28 12:45:28 +0700

    Create 7kyu\362-Whats-my-golf-score

---
+++ 7kyu/362-Whats-my-golf-score/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GolfScoreCalculator(string par, string score)
+    {
+        // throw new NotImplementedException();
+
+        return(int) Enumerable.Range(0,par.Length).Select(i=>char.GetNumericValue(score[i])-char.GetNumericValue(par[i])).Sum();
+
+        // Alternative
+        // return s.Zip(p, (x, y) => x - y).Sum();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GolfScoreCalculator("453454444344544443", "354445334534445348"));
+    }
+}
