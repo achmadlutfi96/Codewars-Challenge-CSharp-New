507d24a6c219e9640222a6905e909902ce4d10f6
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-10-29 20:37:48 +0700

    Create 7kyu\364-Possibilities-Array

---
+++ 7kyu/364-Possibilities-Array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static bool IsAllPossibilities(int[] arr)
+    {
+        Array.Sort(arr);
+        return Enumerable.Range(0, arr.Length).Select((v,i)=>v == arr[i]).All(x=>x==true);
+
+        // Alternative
+        // return arr.Any() && Enumerable.Range(0, arr.Length).All(arr.Contains);
+    }
+
+    public static void Main(){
+        Console.WriteLine(IsAllPossibilities(new int[] {0, 1, 2, 3}));
+        Console.WriteLine(IsAllPossibilities(new int[] {1, 2, 3, 4}));
+    }
+}
