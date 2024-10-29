31c382b31ef2431732917a4b5cf4a0e48bba205a
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-10-29 21:08:22 +0700

    Create 7kyu\365-Happy-Birthday-Darling

---
+++ 7kyu/365-Happy-Birthday-Darling/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Solution
+{
+    using System;
+
+    public class Kata
+    {
+        public static string WomensAge(int n)
+        {
+            //Happy coding..
+            int age = n%2 == 0 ? 20 : 21;
+            return $"{n}? That's just {age}, in base {n/2}!";
+
+            // Alternative
+            // return $"{n}? That's just {20+n%2}, in base {n/2}!";
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(WomensAge(83));
+        }
+    }
+}
