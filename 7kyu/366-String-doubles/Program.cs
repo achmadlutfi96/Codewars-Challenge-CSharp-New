a8cb673551bb780432f006cce155834bbf501000
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-10-29 21:39:07 +0700

    Create 7kyu\366-String-doubles

---
+++ 7kyu/366-String-doubles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Doubles(string str)
+    {
+
+        for (int i = 0; i < str.Length - 1; i++)
+        {
+            if (str[i] == str[i + 1])
+            {
+                str = str.Remove(i, 2);
+                i = -1;
+            }
+        }
+        return str;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Doubles("rrrmooomqqqqj"));
+    }
+}
