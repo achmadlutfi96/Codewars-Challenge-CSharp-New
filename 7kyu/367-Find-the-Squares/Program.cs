e0ac0ef8f44ae7872fe641caa0d3a96287556618
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-10-30 23:17:08 +0700

    Create 7kyu\367-Find-the-Squares

---
+++ 7kyu/367-Find-the-Squares/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Solution
+{
+    public static string FindSquares(int n)
+    {
+        return $"{Math.Pow(Math.Ceiling((double)n/2),2)}-{Math.Pow(Math.Floor((double)n/2),2)}";
+
+        // Alternative
+        // return $"{((n + 1) / 2) * ((n + 1) / 2)}-{(n / 2) * (n / 2)}";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(FindSquares(9));
+        // Console.WriteLine(FindSquares(99991));
+    }
+}
