9ab77d9204510fa9a6ab28acfc73f50ed0e00124
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-10-30 23:42:24 +0700

    Create 7kyu\369-Pull-your-words-together-man

---
+++ 7kyu/369-Pull-your-words-together-man/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Sentencify(string[] words)
+    {
+        // throw new NotImplementedException();
+        return char.ToUpper(words[0][0])+string.Join(" ", words).Substring(1)+".";
+
+        // Alternative
+        // return $"{char.ToUpper(words[0][0])}{string.Join(" ", words)[1..]}.";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Sentencify(new string[] {"i'm","afraid","I","can't","let","you","do","that"}));
+    }
+}
