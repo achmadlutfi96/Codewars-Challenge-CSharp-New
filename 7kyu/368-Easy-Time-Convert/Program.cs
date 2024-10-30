9b15bd19e83a9939aa1ea90de28ed6f58d916efe
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-10-30 23:32:31 +0700

    Create 7kyu\368-Easy-Time-Convert

---
+++ 7kyu/368-Easy-Time-Convert/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Kata
+{
+  public static string TimeConvert(int num)
+  {
+    // throw new NotImplementedException();
+    return num > 0 ? $"{num/60:d2}:{num%60:d2}" : "00:00";
+  }
+
+  public static void Main(){
+    Console.WriteLine(TimeConvert(75));
+    Console.WriteLine(TimeConvert(32));
+    Console.WriteLine(TimeConvert(565757));
+  }
+}
