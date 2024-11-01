88506eaa7e96fa5d71575c700cb32b2448baa300
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-01 10:53:03 +0700

    Create 7kyu\375-Find-twins

---
+++ 7kyu/375-Find-twins/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static object? Elimination(int[] arr)
+    {
+        return arr.Distinct().Count() == arr.Length ? "null" : arr.Distinct().Where(x=>arr.Count(y=>x==y) == 2).First();
+
+        // Alternative 
+        // return arr.GroupBy(i => i).FirstOrDefault(g => g.Count() == 2)?.Key;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Elimination(new int[] {2, 2, 34, 1, 22}));
+        Console.WriteLine(Elimination(new int[] {2, 5, 34, 1, 22}));
+    }
+}
