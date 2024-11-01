61dc1c30089f9df52cef959bbe2da6f39d0f4b0c
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-01 09:28:53 +0700

    Create 7kyu\374-Complete-The-Pattern-4

---
+++ 7kyu/374-Complete-The-Pattern-4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Pattern4
+{
+    public static string Pattern(int n)
+    {
+        return n > 0 ? string.Join("\n", Enumerable.Range(1,n).Select((v,i)=>string.Concat(Enumerable.Range(v,n-i)))) : "";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Pattern(5));
+    }
+}
