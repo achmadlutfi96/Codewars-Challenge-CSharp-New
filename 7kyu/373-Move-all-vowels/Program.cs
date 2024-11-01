6ee5a58af607a2a7724e126feda411e24a4143f1
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-01 09:16:52 +0700

    Create 7kyu\373-Move-all-vowels

---
+++ 7kyu/373-Move-all-vowels/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string MoveVowel(string input)
+    {
+        // throw new NotImplementedError();
+        return string.Concat(input.Where(x=>!"aeiou".Contains(x)))+string.Concat(input.Where(x=>"aeiou".Contains(x)));
+
+        // Alternative 
+        // return string.Concat(input.OrderBy(x => "aeiou".Contains(x)));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(MoveVowel("maker"));
+    }
+}
