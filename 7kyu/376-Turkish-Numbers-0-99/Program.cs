d7948622ecd1d72c5c13ec936be4298083763cf3
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-02 11:10:35 +0700

    Create 7kyu\376-Turkish-Numbers-0-99

---
+++ 7kyu/376-Turkish-Numbers-0-99/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+    public static string GetTurkishNumber(int num)
+    {
+        // TODO
+        string[] units = ["sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"];
+        string[] dozens = ["", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"];
+        return num < 10 ? units[num] : num%10 == 0 ? dozens[num/10] : $"{dozens[num/10]} {units[num%10]}";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GetTurkishNumber(3));
+        Console.WriteLine(GetTurkishNumber(13));
+    }
+}
