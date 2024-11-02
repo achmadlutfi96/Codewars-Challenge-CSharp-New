da3872faaddd85314da8378e6493abbb51f1b0e5
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-02 12:19:28 +0700

    Create 7kyu\378-Simple-fibonacci-strings

---
+++ 7kyu/378-Simple-fibonacci-strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public static String solve(int n)
+    {
+        //..
+        string res = "";
+        string t1 = "0";
+        string t2 = "1";
+        int i = -1;
+        while (i < n)
+        {
+            res += t1;
+            t1 = t2;
+            t2 = res;
+            i++;
+        }
+        return res;
+
+        // Alternative
+        // if (n == 0)
+        // {
+        //     return "0";
+        // }
+        // if (n == 1)
+        // {
+        //     return "01";
+        // }
+        // return solve(n - 1) + solve(n - 2);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(solve(3));
+        Console.WriteLine(solve(5));
+    }
+}
