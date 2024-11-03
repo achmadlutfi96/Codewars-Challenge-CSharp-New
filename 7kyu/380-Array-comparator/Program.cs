e4567e4347d933ad95a808be2db1089c723ad45f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-03 13:00:22 +0700

    Create 7kyu\380-Array-comparator

---
+++ 7kyu/380-Array-comparator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+public class Kata
+{
+    public static int MatchArrays(int[] v, int[] r)
+    {
+        return v.Intersect(r).Count();
+
+        // Alternative
+        // return r.Count(v.Contains);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(MatchArrays(new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 3, 4, 5, 6 }));
+    }
+}
