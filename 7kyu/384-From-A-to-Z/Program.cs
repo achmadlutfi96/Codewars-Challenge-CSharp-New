13d66a8205d6d702b8cf2a6af0c156936a2a4235
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-04 12:58:43 +0700

    Create 7kyu\384-From-A-to-Z

---
+++ 7kyu/384-From-A-to-Z/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string GimmeTheLetters(string sp)
+    {
+        return string.Concat(Enumerable.Range(sp.First(), sp.Last()-sp.First()+1).Select(x=>Convert.ToChar(x)));
+
+        // Alternative 
+        // return string.Concat(Enumerable.Range(sp[0], sp[2] - sp[0] + 1).Select(i => (char)i));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GimmeTheLetters("F-O"));
+    }
+}
