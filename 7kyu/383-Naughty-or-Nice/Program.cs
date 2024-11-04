68188a9052781d1e589084e416a2515f46fc6efa
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-04 12:43:15 +0700

    Create 7kyu\383-Naughty-or-Nic

---
+++ 7kyu/383-Naughty-or-Nice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class NaughtyOrNice
+{
+    public static IEnumerable<string> GetNiceNames(IEnumerable<Person> people)
+    {
+        return people.Where(x=>x.WasNice == true).Select(x=>x.Name);
+    }
+
+    public static IEnumerable<string> GetNaughtyNames(IEnumerable<Person> people)
+    {
+        return people.Where(x=>x.WasNice == false).Select(x=>x.Name);
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public bool WasNice { get; set; }
+}
