8ddde80a4181f46567778d5b6b2d442dc675b709
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-04 12:03:08 +0700

    Create 7kyu\382-Genetic-Algorithm-Series-1-Generat

---
+++ 7kyu/382-Genetic-Algorithm-Series-1-Generate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static string Generate(int length)
+    {
+        Random rn = new Random();
+        return string.Concat(Enumerable.Range(0, length).Select(x=>rn.Next(0,2).ToString()));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Generate(16));
+    }
+}
