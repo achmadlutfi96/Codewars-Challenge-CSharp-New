ac0489f2c0b8aa92a285de849e24d1851ba9798c
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-06 12:19:00 +0700

    Create 7kyu\389-Number-climber

---
+++ 7kyu/389-Number-climber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+    public static int[] Climb(int n)
+    {
+        Stack<int> res = new Stack<int>();
+        while(n > 0){
+            res.Push(n);
+            n /= 2;
+        }
+        return res.ToArray();
+
+        // Alternative
+        // return new int[(int) Math.Log(n, 2)].Select(_ => n /= 2).Prepend(n).Reverse().ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", Climb(10)));
+    }
+}
