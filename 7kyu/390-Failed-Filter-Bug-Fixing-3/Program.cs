967d65520f334d241383546a5274a2d680e5bb1b
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-06 15:02:32 +0700

    Create 7kyu\390-Failed-Filter-Bug-Fixing-3

---
+++ 7kyu/390-Failed-Filter-Bug-Fixing-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string FilterNumbers(string str)
+    {
+        return string.Concat(str.ToCharArray().Where(c => !char.IsDigit(c)));
+
+        // Alternative
+        // return new string(str.Where(c => !Char.IsDigit(c)).ToArray());
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(FilterNumbers("aa1bb2cc3dd"));
+    }
+}
