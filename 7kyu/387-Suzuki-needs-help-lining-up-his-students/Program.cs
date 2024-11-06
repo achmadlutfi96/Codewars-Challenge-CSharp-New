db043b23bf3d2a8fb06b8554f8d1d47b229d03bb
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-06 11:06:29 +0700

    Create 7kyu\388-Ninja-vs-Samurai-Strike

---
+++ 7kyu/387-Suzuki-needs-help-lining-up-his-students/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static String[] LineupStudents(String a)
+    {
+        //code here
+        return a.Split(" ").OrderBy(x=>x).OrderBy(x=>x.Length).Reverse().ToArray();
+
+        // Alternative
+        // return a.Split().OrderBy(s => -s.Length).ThenByDescending(s => s).ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ", LineupStudents("Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi")));
+    }
+}
