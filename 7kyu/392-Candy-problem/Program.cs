0d80c3f2be352560b9f767db719c985bcb804e06
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-07 10:11:01 +0700

    Create 7kyu\392-Candy-problem

---
+++ 7kyu/392-Candy-problem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+public class CandyProblem
+{
+    public static int GetMissingCandies(int[] candies)
+    {
+        //TODO: add your code here
+        if (candies.Length < 2)
+        {
+            return -1;
+        }
+        return candies.Select(x => candies.Max() - x).Sum();
+
+        // Alternative
+        // if (candies.Length <= 1)
+        // {
+        //     return -1;
+        // }
+
+        // return candies.Max() * candies.Length - candies.Sum();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GetMissingCandies(new int[] { 5, 8, 6, 4 }));
+    }
+}
