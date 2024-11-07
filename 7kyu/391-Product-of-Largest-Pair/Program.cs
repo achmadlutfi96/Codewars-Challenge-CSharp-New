839dafe520804867e769e37bd094a0efb8a24a1c
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-07 10:03:31 +0700

    Create 7kyu\391-Product-of-Largest-Pair

---
+++ 7kyu/391-Product-of-Largest-Pair/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+public class Kata
+{
+    public static int MaxProduct(int[] array)
+    {
+        Array.Sort(array);
+        return array[array.Length - 1] * array[array.Length-2];
+
+        // Alternative
+        // return array.OrderBy(x => -x).Take(2).Aggregate((a, b) => a * b);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(MaxProduct(new int[] { 56, 335, 195, 443, 6, 494, 252 }));
+    }
+}
