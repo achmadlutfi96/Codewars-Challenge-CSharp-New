b7a7a600afbc2c836334bb63ccc51501095ab212
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-08 12:12:43 +0700

    Create 7kyu\394-Invisible-cubes

---
+++ 7kyu/394-Invisible-cubes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Kata
+{
+  public static int NotVisibleCubes(int n)
+  {
+    // your code!
+    return n > 2 ? (int)Math.Pow(n-2, 3) : 0;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(NotVisibleCubes(3));
+  }
+}
