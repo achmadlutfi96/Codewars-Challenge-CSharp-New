93224e8e850d6fa0eaa96a98ab14696318b22311
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-08 13:17:21 +0700

    Create 7kyu\396-Next-Palindromic-Number

---
+++ 7kyu/396-Next-Palindromic-Number/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Kata
+{
+    public static int NextPal(int val)
+    {
+        do
+        {
+            val++;
+        }
+        while (!(Convert.ToInt32(string.Concat(val.ToString().Reverse())) == val));
+
+        return val;
+
+        // Alternative 1
+        // return Enumerable.Range(val + 1, val).First(x => $"{x}" == string.Concat($"{x}".Reverse()));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(NextPal(11));
+        Console.WriteLine(NextPal(188));
+        Console.WriteLine(NextPal(191));
+        Console.WriteLine(NextPal(2541));
+    }
+}
