41144e0286a73109a4388c0410c4cb7004803183
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-08 12:20:50 +0700

    Create 7kyu\395-Simple-Fun-20-First-Reverse-Try

---
+++ 7kyu/395-Simple-Fun-20-First-Reverse-Try/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public static int[] FirstReverseTry(int[] arr)
+        {
+            //coding and coding..
+            if (arr.Length < 2)
+            {
+                return arr;
+            }
+            int temp = arr[0];
+            arr[0] = arr[arr.Length - 1];
+            arr[arr.Length - 1] = temp;
+            return arr;
+
+            //   Alternative
+            // return arr.Length > 1 ? arr[1..^1].Prepend(arr[^1]).Append(arr[0]).ToArray() : arr;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(string.Join(", ", FirstReverseTry(new int[] { 23, 54, 12, 3, 4, 56, 23, 12, 5, 324 })));
+        }
+    }
+}
