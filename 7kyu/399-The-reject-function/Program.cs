78c5171720a860c4eb1776de3859dc9b5216d38c
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-09 19:50:36 +0700

    Create 7kyu\399-The-reject-function

---
+++ 7kyu/399-The-reject-function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] Reject(int[] array, Func<int, bool> predicate)
+    {
+        List<int> res = new List<int>();
+        foreach (int item in array)
+        {
+            if(!predicate(item)){
+                res.Add(item);
+            }
+        }
+        return res.ToArray();
+
+        // Alternative 1
+        // return array.Where(x=>!predicate(x)).ToArray();
+
+        // Alternative 2
+        // return Array.FindAll(array, i => !predicate(i));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ", Reject(new int[] {1, 2, 3, 4, 5, 6}, (n) => n % 2 == 0)));
+    }
+}
