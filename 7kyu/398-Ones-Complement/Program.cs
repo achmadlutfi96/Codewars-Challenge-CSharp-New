3c69bf578e8f25b101b5ec27427ada1ea286450e
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-09 19:41:28 +0700

    Create 7kyu\398-Ones-Complement

---
+++ 7kyu/398-Ones-Complement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string OnesComplement(string n)
+    {
+        // throw new NotImplementedException();
+        return string.Concat(n.Select(x=> x == '1' ? '0' : '1'));
+
+        // Alternative
+        // return string.Concat(n.Select(c => (char) (c ^ 1)));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(OnesComplement("1101"));
+    }
+}
