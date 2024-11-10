24cfcca82071362062c87e938bcf2bc0525f72ca
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-10 12:33:21 +0700

    Create 7kyu\402-Simple-Fun-144-Distinct-Digit-Year

---
+++ 7kyu/402-Simple-Fun-144-Distinct-Digit-Year/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int DistinctDigitYear(int year)
+        {
+            //coding and coding..
+            do
+            {
+                year++;
+            } while (!(year.ToString().Distinct().Count() == year.ToString().Length));
+            return year;
+
+            //   Alternative
+            // return (++year).ToString().Distinct().Count() == 4 ? year : DistinctDigitYear(year);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(DistinctDigitYear(1987));
+        }
+    }
+}
