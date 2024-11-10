0bb40e573652b9abd0ccfc886c335cb6a953ee85
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-10 11:32:05 +0700

    Create 7kyu\400-Pairs-of-integers-from-m-to-n

---
+++ 7kyu/400-Pairs-of-integers-from-m-to-n/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static IEnumerable<(int, int)> GeneratePairs(int m, int n) 
+    {
+        // user code ..
+        List<(int, int)> pairs = new();
+        for (int i = m; i <= n; i++)
+        {
+            for (int j = i; j <= n; j++)
+            {
+                pairs.Add((i, j));
+            }
+        }
+        return pairs;
+
+        // Alternative
+        // return Enumerable.Range(m, n - m + 1).SelectMany(x => Enumerable.Range(x, n - x + 1).Select(y => (x, y)));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ", GeneratePairs(2,4)));
+    }  
+}
