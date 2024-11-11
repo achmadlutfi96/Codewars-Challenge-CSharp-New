62f293d5e4411abfeae354ce557ada0e9019080e
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-11 20:29:52 +0700

    Create 7kyu\405-Most-valuable-character

---
+++ 7kyu/405-Most-valuable-character/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class MostValuable
+{
+    public static char solve(string s){  
+        //..
+        var firstIndex = new Dictionary<char, int>(); 
+        var lastIndex = new Dictionary<char, int>(); 
+        for (int i = 0; i < s.Length; i++) 
+        { 
+          if (!firstIndex.ContainsKey(s[i])) 
+          { 
+            firstIndex[s[i]] = i; 
+          } 
+          lastIndex[s[i]] = i; 
+        } 
+        char maxVal = '\0'; 
+        int maxDiff = -1; 
+        foreach (var kvp in firstIndex) 
+        { 
+          char c = kvp.Key; 
+          int diff = lastIndex[c] - kvp.Value; 
+          if (diff > maxDiff || (diff == maxDiff && c < maxVal)) 
+          { 
+            maxDiff = diff; 
+            maxVal = c; 
+          } 
+        } return maxVal;
+
+        // Alternative
+        // return s.Distinct().OrderByDescending(x => s.LastIndexOf(x) - s.IndexOf(x)).ThenBy(x=>x).First();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(solve("aabccc"));
+    }
+}
