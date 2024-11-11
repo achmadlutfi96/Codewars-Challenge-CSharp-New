d9a1ff84dbe7faaa86b22917b6c740dd29530657
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-11 19:03:54 +0700

    Create 7kyu\403-Easy-mathematical-callback

---
+++ 7kyu/403-Easy-mathematical-callback/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Kata
+{
+    public static int[] ProcessArray(int[] arr, Func<int, int> callback)
+    {
+        // TODO
+        int[] res = new int[arr.Length];
+        for(int i = 0; i < arr.Length; i++){
+            res[i] = callback(arr[i]);
+        }
+        return res;
+
+        // Alternative 
+        // return arr.Select(callback).ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ",ProcessArray(new int[] {4, 8, 2, 7, 5}, (Func<int, int>)((v) => v * 2))));
+    }
+}
