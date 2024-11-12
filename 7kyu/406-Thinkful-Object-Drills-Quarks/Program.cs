f443d1f95392cb8f7a04efe1383531848fa0baa8
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-12 19:45:27 +0700

    Create 7kyu\406-Thinkful-Object-Drills-Quarks

---
+++ 7kyu/406-Thinkful-Object-Drills-Quarks/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+
+public class Quark {
+  // Your code here.
+  public string Color { get; set; }
+  public string Flavor { get; set; }
+  public double BaryonNumber { get; set; } = 1/3;
+
+  public Quark(string color, string flavor) {
+    this.Color = color;
+    this.Flavor = flavor;
+  }
+
+  public void Interact(Quark quark){
+    string tempColor = Color;
+    string tempFlavor = Flavor;
+    this.Color = quark.Color;
+    this.Flavor = quark.Flavor;
+    quark.Color = tempColor;
+    quark.Flavor = tempFlavor;
+  }
+}
+
+// Alternative
+// public class Quark
+// {
+//   public string Color;
+//   public string Flavor;
+
+//   public Quark(string color, string flavor)
+//   {
+//       Color = color;
+//       Flavor = flavor;
+//   }
+
+//   public double BaryonNumber => 1 / 3d;
+//   public void Interact(Quark q) => (Color, q.Color) = (q.Color, Color);
+// }
