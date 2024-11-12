b51ba1ceed1893cae9cc72779facb779b0946936
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-12 20:33:55 +0700

    Create 7kyu\408-Triangle-area

---
+++ 7kyu/408-Triangle-area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution 
+{
+    public static class Kata 
+    {
+        public static double TArea(string s) 
+        {
+            return Math.Pow(s.Split("\n").Length-1, 2) / 2;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(TArea(". \n. .\n. . .\n. . . ."));
+            Console.WriteLine(TArea(". \n. ."));
+        }
+    }
+}
