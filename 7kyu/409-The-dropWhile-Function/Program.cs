dc754111df9aa83152eb23a107a840680514da4f
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-13 07:55:12 +0700

    Create 7kyu\409-The-dropWhile-Function

---
+++ 7kyu/409-The-dropWhile-Function/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] DropWhile(int[] arr, Func<int, bool> pred)
+    {
+        return arr.SkipWhile(pred).ToArray();
+
+        // Alternative
+        // int i;
+        // for (i = 0; i < arr.Length; i++)
+        //     if (!pred(arr[i])) break;
+        // return arr[i..];
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ", DropWhile(new int[] { 2, 6, 4, 10, 1, 5, 4, 3 }, (value) => value % 2 == 0)));
+    }
+}
