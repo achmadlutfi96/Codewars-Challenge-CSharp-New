1c4f2fa88ce76a9f2862b772bbfc9714f9bc3af4
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-13 08:05:06 +0700

    Create 7kyu\410-isEven-Bitwise-Series

---
+++ 7kyu/410-isEven-Bitwise-Series/Program.cs
@@ -0,0 +1,11 @@
+﻿public static class Kata
+{
+    public static bool IsEven(int n)
+    {
+        return n == Math.Round((double)n/2)*2;
+
+        // Alternative
+        // return (n & 1) == 0;
+
+    }
+}
