0a5f2387de98f7ac27dacedbb0d760ef132f62fa
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-13 08:26:57 +0700

    Create 7kyu\411-Converting-integer-to-currency-format

---
+++ 7kyu/411-Converting-integer-to-currency-format/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+public class Kata
+{
+  public static string ToCurrency(int price)
+  {
+    // ...
+    return price.ToString("N0", CultureInfo.InvariantCulture);
+
+    // Alternative
+    // return price.ToString("N0");
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(ToCurrency(5678545));
+  }
+}
