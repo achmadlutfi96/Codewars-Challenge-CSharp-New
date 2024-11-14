37e216425728ca93e1f2d60a3f78e268ede26c27
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-14 07:41:26 +0700

    Create 7kyu\413-Email-Address-Obfuscator

---
+++ 7kyu/413-Email-Address-Obfuscator/Program.cs
@@ -0,0 +1,14 @@
+﻿public class EmailObfuscator 
+{
+    
+    public static string Obfuscate(string email) 
+    {
+
+        return email.Replace("@", " [at] ").Replace(".", " [dot] ");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Obfuscate("test@123.com"));
+    }
+}
