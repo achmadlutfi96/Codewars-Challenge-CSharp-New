4fd32645d47ed58177790362c8231775c4e7be9b
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-14 08:00:50 +0700

    Create 7kyu\414-Building-Spheres

---
+++ 7kyu/414-Building-Spheres/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class Sphere
+{
+    // Good Luck!
+    private int radius;
+    private int mass;
+    public Sphere(int r, int m)
+    {
+        this.radius = r;
+        this.mass = m;
+    }
+
+    public int GetRadius()
+    {
+        return radius;
+    }
+
+    public int GetMass()
+    {
+        return mass;
+    }
+
+    public double GetVolume()
+    {
+        return Math.Round( Math.PI * GetRadius() * GetRadius() * GetRadius() * 4/3, 5);
+    }
+
+    public double GetSurfaceArea()
+    {
+        return Math.Round(4 * Math.PI * Math.Pow(GetRadius(), 2), 5);
+    }
+
+    public double GetDensity()
+    {
+        return Math.Round(GetMass()/GetVolume(), 5);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Sphere sphere= new Sphere(2,50);
+        Console.WriteLine(sphere.GetRadius());
+        Console.WriteLine(sphere.GetMass());
+        Console.WriteLine(sphere.GetVolume());
+    }
+}
