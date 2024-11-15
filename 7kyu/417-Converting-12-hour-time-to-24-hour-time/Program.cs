1f5f3ca2d168284c538b442e2437ba1802273108
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-15 10:09:13 +0700

    Create 7kyu\417-Converting-12-hour-time-to-24-hour-time

---
+++ 7kyu/417-Converting-12-hour-time-to-24-hour-time/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Kata
+{
+    public class TimeConvert
+    {
+        public static string Convert12hTo24h(int hours, int minutes, string period)
+        {
+            if (period == "am" && hours == 12)
+            {
+                return $"00{minutes:D2}";
+            }
+            if (period == "pm" && hours == 12)
+            {
+                return $"12{minutes:D2}";
+            }
+            return Convert.ToString(period == "pm" ? hours + 12 : hours).PadLeft(2, '0') + $"{minutes}".PadLeft(2, '0');
+
+            // Alternative 1
+            // return TimeOnly.Parse($"{hours}:{minutes}{period}").ToString("HHmm");
+
+            // Alternative 2
+            // return $"{hours % 12 + (period == "pm" ? 12 : 0) :D2}{minutes:D2}";
+
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Convert12hTo24h(2, 15, "pm"));
+        }
+    }
+}
