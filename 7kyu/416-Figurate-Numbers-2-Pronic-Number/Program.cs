6778a62e6dfb0d0adbfcedae1305b5737538f4e2
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-15 09:37:24 +0700

    Create 7kyu\416-Figurate-Numbers-2-Pronic-Number

---
+++ 7kyu/416-Figurate-Numbers-2-Pronic-Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Kata
+{
+  public static bool IsPronic(int n)
+  {
+    // Happy Coding ^_^
+    int i = (int)Math.Sqrt(n);
+    return i*(i+1) == n;
+
+    // Alternative
+    // return Math.Sqrt(1 + 4 * n) % 1 == 0;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(IsPronic(2));
+    Console.WriteLine(IsPronic(3));
+  }
+}
