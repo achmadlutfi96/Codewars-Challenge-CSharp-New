60a31b5fd1324dc0ebd2a97ef485bab69a950679
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-16 13:50:54 +0700

    Create 7kyu\420-Finding-Remainder-Without-Using-%-Operator

---
+++ 7kyu/420-Finding-Remainder-Without-Using-%-Operator/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Kata
+{
+  public static int Remainder(int D, int d)
+  {  
+    // Your Code Goes Here
+    return D-(D/d*d); 
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Remainder(34,7));
+  }
+}
