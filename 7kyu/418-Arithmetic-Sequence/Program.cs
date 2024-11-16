9bcee3df558b66935cbc6b1e7fed0c6efc48e746
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-16 10:28:48 +0700

    Create 7kyu\418-Arithmetic-Sequence

---
+++ 7kyu/418-Arithmetic-Sequence/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Kata
+{
+  public static int Nthterm(int first, int n, int c)
+  {
+    // Write your code here
+    return n*c+first;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Nthterm(-50, 10, 20));
+  }
+}
