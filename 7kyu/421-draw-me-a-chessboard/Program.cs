8a2759fb6b4d199daebc49db52d7b4dfe9e2ce72
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-17 15:09:46 +0700

    Create 7kyu\421-draw-me-a-chessboard

---
+++ 7kyu/421-draw-me-a-chessboard/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static char[][] ChessBoard(int row, int columns)
+  {
+    return Enumerable.Range(0,row).Select((x,i)=>Enumerable.Range(0,columns).Select((y,j)=>i%2 == j%2 ? 'O' : 'X').ToArray()).ToArray();
+
+    // Alternative 
+    // return Enumerable.Range(0,h).Select(i=>Enumerable.Range(0,w).Select(j=>"OX"[(i+j)%2]).ToArray()).ToArray();
+  }
+
+  public static void Main()
+  {
+    foreach (char[] item in ChessBoard(6,4))
+    {
+        Console.WriteLine(string.Join(", ", item));
+    }
+    
+  }
+}
