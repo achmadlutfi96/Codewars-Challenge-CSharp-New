eb4253509fbcbb9094056f3eb6307e8ec9456f5c
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-17 16:18:54 +0700

    Create 7kyu\422-Birthday-I-Cake

---
+++ 7kyu/422-Birthday-I-Cake/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Birthday
+{
+  public static string Cake(int x, string y)
+  {
+    // Console.WriteLine(y.Select((v,i)=> i%2 == 0 ? v : v-96).Sum() * 0.7);
+    // Console.WriteLine("abc".Select((v,i)=>i%2 == 0 ? v : v-96).Sum());
+    return y.Select((v,i)=> i%2 == 0 ? v : v-96).Sum() >= x * 0.7 ? "Fire!" : "That was close!" ;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Cake(900, "abcdef"));
+    Console.WriteLine(Cake(56, "ifkhchlhfd"));
+    Console.WriteLine(Cake(256, "aaaaaddddr"));
+    Console.WriteLine(Cake(501, "ipmnwnpen"));
+    Console.WriteLine(Cake(339, "aoq"));
+  }
+}
