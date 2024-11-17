a18f44de2f68967daa0d625f37f570725e75b29c
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-17 16:25:28 +0700

    Create 7kyu\423-Elapsed-Seconds

---
+++ 7kyu/423-Elapsed-Seconds/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Kata
+{
+  public static int ElapsedSeconds(DateTime startDate, DateTime endDate)
+  {
+    return (int) endDate.Subtract(startDate).TotalSeconds;
+
+    // Alternative
+    // return (int)(endDate - startDate).TotalSeconds;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(ElapsedSeconds(new DateTime(2013, 1, 1, 0, 0, 1), new DateTime(2013, 1, 1, 0, 0, 2)));
+  }
+}
