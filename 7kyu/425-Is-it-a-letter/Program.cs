ab5760901cdbeec9120dae06bc472d56568dc3c2
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-18 09:53:59 +0700

    Create 7kyu\425-Is-it-a-letter

---
+++ 7kyu/425-Is-it-a-letter/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+    public static bool IsItLetter(char c)
+    {
+        return char.IsLetter(c);
+
+        // Alternative
+        // return ('A'<=c&&c<='Z')||('a'<=c&&c<='z');
+    }  
+
+    public static void Main()
+    {
+        Console.WriteLine(IsItLetter('a'));
+    }
+}
