99225775d76b27925814ecbb43a67f75e265f622
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-18 09:50:56 +0700

    Create 7kyu\424-Count-the-Ones

---
+++ 7kyu/424-Count-the-Ones/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Kata
+{
+  public static int HammingWeight(int x)
+  {
+    // throw new NotImplementedException(); // replace with your code
+    return Convert.ToString(x,2).Replace("0", "").Length;
+
+    // Alternative
+    // return x == 0 ? 0 : 1 + HammingWeight(x & (x - 1));
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(HammingWeight(21));
+  }
+}
