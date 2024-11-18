8d935181f4b4a2da1caaf135ae459bd176ae29c7
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-18 10:07:59 +0700

    Create 7kyu\426-Float-Precision

---
+++ 7kyu/426-Float-Precision/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+  public static double Round(double n)
+  {
+    return Math.Round(n,2);
+
+    // Alternative 1
+    // double res = ((int)((n + 0.005)* 100));
+    //   return res / 100;
+
+    
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Round(5.678));
+  }
+}
