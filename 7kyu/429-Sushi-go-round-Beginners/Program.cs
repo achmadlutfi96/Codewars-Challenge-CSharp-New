f3e255721da63a8b9f6b575418c90932634c0c7e
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-19 11:18:59 +0700

    Create 7kyu\429-Sushi-go-round-Beginners

---
+++ 7kyu/429-Sushi-go-round-Beginners/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static int TotalBill(string str)
+  {
+    //Write your function here...
+    int count = str.Count(x=>x=='r');
+    return (count-(count/5))*2; 
+
+    // Alternative
+    // int plates = str.Replace(" ", "").Length;
+    // return (plates - plates / 5) * 2;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(TotalBill("rrrrrrrrrrrrrrrrrr   rr r"));
+  }
+}
