02e26751ec152f80d6f29e0e9493e60716326c89
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-19 10:57:51 +0700

    Create 7kyu\428-Count-salutes

---
+++ 7kyu/428-Count-salutes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int CountSalutes(string hallway)
+    {
+        int sum = 0;
+        for (int i = 0; i < hallway.Length - 1; i++)
+        {
+            for (int j = i+1; j < hallway.Length; j++)
+            {
+                if ((hallway[i] == '>') && (hallway[j] == '<'))
+                {
+                    sum+=2;
+                }
+            }
+        }
+        return sum;
+
+        // Alternative 
+        // return hallway.Select((p, i) => p=='>' ? hallway.Skip(i).Count(p => p=='<') : 0).Sum() * 2;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(CountSalutes("<---->---<---<-->"));
+    }
+}
