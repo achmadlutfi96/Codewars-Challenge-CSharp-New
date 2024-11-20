bb0cfaa4429765f71b376378dda5fa0fae0d5c57
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-20 15:52:01 +0700

    Create 7kyu\431-noobCode-03-CHECK-THESE-LETTERS

---
+++ 7kyu/431-noobCode-03-CHECK-THESE-LETTERS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static bool LetterCheck(string[] arr)
+    {
+        return arr[1].ToLower().Distinct().Select(x=> arr[0].ToLower().Contains(x)).All(x=>x==true);
+
+        // Alternative 1
+        // return arr[1].ToUpper().All(arr[0].ToUpper().Contains);
+
+        // Alternative 2
+        // return !arr[1].ToUpper().Except(arr[0].ToUpper()).Any();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(LetterCheck(new string[] { "trances", "nectar" }));
+        Console.WriteLine(LetterCheck(new string[] { "assert", "staring" }));
+    }
+}
