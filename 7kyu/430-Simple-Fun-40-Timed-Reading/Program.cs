628415548194fa91717db1256d585a7cfdaec9bc
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-20 15:34:22 +0700

    Create 7kyu\430-Simple-Fun-40-Timed-Reading

---
+++ 7kyu/430-Simple-Fun-40-Timed-Reading/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public static int TimedReading(int MaxLength, string text)
+        {
+            //coding and coding..
+            text = string.Concat(text.Where(x => char.IsLetter(x) || x == ' '));
+            return text.Equals(" ") || text.Equals("") ? 0 : text.Split(" ").Count(x => x.Length <= MaxLength);
+
+            //   Alternative 1
+            // string[] separators = {" ",",",".","!","\'","?","\""};
+            //   return text.Split(separators,StringSplitOptions.RemoveEmptyEntries)
+            //         .ToArray()
+            //         .Where(x=>x.Length<=maxLength)
+            //         .Count();
+
+            // Alternative 2
+            // return Regex.Replace(text, @"[;,\.\?\!']","").Split(' ').Count(w => w.Any() && w.Length <= MaxLength);  
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(TimedReading(4, "The Fox asked the stork, 'How is the soup?'"));
+            Console.WriteLine(TimedReading(1, "..."));
+            // Console.WriteLine(TimedReading(1,"Oh!"));
+        }
+    }
+}
