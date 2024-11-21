6e2fcc426e9eccdebece4ed942511111a923b656
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-21 15:25:30 +0700

    Create 7kyu\433-Spot-the-Differences

---
+++ 7kyu/433-Spot-the-Differences/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+  public static List<int> Spot(string s1, string s2)
+  {
+    // Code here
+    return Enumerable.Range(0, s1.Length).Where(x=>s1[x]!=s2[x]).ToList();
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ", Spot("abcdefg", "abcqetg")));
+  }
+}
