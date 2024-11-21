ff69c2528a93f20b72c2015f4f0753533ee5f9ae
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-21 15:34:04 +0700

    Create 7kyu\434-String-reverse-slicing-101

---
+++ 7kyu/434-String-reverse-slicing-101/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string[] ReverseSlice(string str)
+  {
+    return Enumerable.Range(1, str.Length).Select(v=>string.Concat(str.Substring(0,v).Reverse())).Reverse().ToArray();
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ", ReverseSlice("abcde")));
+  }
+}
