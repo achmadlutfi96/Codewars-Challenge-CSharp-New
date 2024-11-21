32430eaa3f54a74e7023b201b5113962216ce78c
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-21 15:45:06 +0700

    Create 7kyu\435-Regex-Failure-Bug-Fixing-2

---
+++ 7kyu/435-Regex-Failure-Bug-Fixing-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+  public static string filterWords(string phrase)
+  {
+      string pattern = @"bad|mean|ugly|horrible|hideous\b";
+      string replacement = "awesome";
+      Regex rgx = new Regex(pattern,  RegexOptions.IgnoreCase);
+      return rgx.Replace(phrase, replacement);
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(filterWords("You're Meanish!! timmy!"));
+  }
+}
