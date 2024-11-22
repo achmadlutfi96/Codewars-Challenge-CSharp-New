8f534a3c19fa71a7f695ac2183973e732d9001fd
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-22 15:01:55 +0700

    Create 7kyu\438-Find-Fibonacci-last-digit

---
+++ 7kyu/438-Find-Fibonacci-last-digit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+public class Kata
+{
+    public static int GetLastDigit(int index)
+    {
+        // Your code here
+        int a = 0, b = 1;
+
+        for (int i = 0; i < index; ++i)
+        {
+            b = a + b;
+            a = b - a;
+            b %= 10;
+        }
+
+        return a;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GetLastDigit(15));
+        Console.WriteLine(GetLastDigit(300));
+    }
+}
