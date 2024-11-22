dba5240e8bdd1bdb7ac6b65a11dc42cd082da318
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-22 12:09:30 +0700

    Create 7kyu\436-Complete-The-Pattern-6-Odd-Ladder

---
+++ 7kyu/436-Complete-The-Pattern-6-Odd-Ladder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string OddLadder(int n)
+  {
+    if(n<1)
+    {
+        return "";
+    }
+    return string.Join("\n",Enumerable.Range(1, n).Where(x=>x%2 == 1).Select(x=>string.Concat(Enumerable.Repeat(x,x))));
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(OddLadder(6));
+  }
+}
