621d0378b722aa84a27fc79b51210491c1989f45
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-22 12:51:08 +0700

    Create 7kyu\437-Baby-shark-lyrics-generator

---
+++ 7kyu/437-Baby-shark-lyrics-generator/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Linq;
+public static class Kata {
+  public static string BabySharkLyrics()=>string.Concat(new[]{"Baby","Mommy","Daddy","Grandma","Grandpa"}.Select(s=>s+" shark").Append("Let's go hunt").Select(x=>string.Join(", doo doo doo doo doo doo\n",Enumerable.Repeat(x,4))+"!\n"))+"Run away,…\n";
+}
