82ede62e53c9865544cd0cd8f56dc1589e9ce98e
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-23 13:45:43 +0700

    Create 7kyu\441-SillyCASE

---
+++ 7kyu/441-SillyCASE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string SillyCase(string str)
+    {
+        // throw new NotImplementedException();
+        return string.Concat(str.Select((v, i) => i + 1 <= Math.Ceiling((double)str.Length / 2) ? char.ToLower(v) : char.ToUpper(v)));
+
+        // Alternative
+        // int half = (str.Length + 1) / 2;
+        // return str[..half].ToLower() + str[half..].ToUpper();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(SillyCase("codewars"));
+        Console.WriteLine(SillyCase("brian"));
+    }
+}
