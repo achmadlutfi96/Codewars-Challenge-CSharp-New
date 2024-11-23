1645ab58f66eef732c8050933c3c8fe4fb1d88dd
Achmad Lutfi <alhacker1996@gmail.com>
2024-11-23 09:24:16 +0700

    Create 7kyu\439-Easy-Line

---
+++ 7kyu/439-Easy-Line/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+public class Easyline
+{
+    public static BigInteger EasyLine(int n)
+    {
+        // your code
+        BigInteger sum = 1;
+        for (var i = 1; i <= n; i++)
+        {
+            sum = sum * (n + i) / i;
+        }
+
+        return sum;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(EasyLine(4));
+    }
+}
