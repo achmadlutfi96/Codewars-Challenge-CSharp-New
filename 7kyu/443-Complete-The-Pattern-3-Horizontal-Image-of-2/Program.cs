bdd7b7c16146eba8032c477b238f592896204a26
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-24 14:53:57 +0700

    Create 7kyu\443-Complete-The-Pattern-3-Horizontal-Image-of-2

---
+++ 7kyu/443-Complete-The-Pattern-3-Horizontal-Image-of-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public class HorizontalImageOfPart2
+{
+  public static string Pattern(int n)
+  {
+    return n > 0 ? string.Join("\n", Enumerable.Range(1,n).Select(x=> string.Concat(Enumerable.Range(x,n-x+1).Reverse())).Reverse()) : "";
+
+    // Alternative
+    // string s = "";
+    // return n<1?"": string.Join("\n",Enumerable.Range(1,n).Select(t=>s+=n--));
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Pattern(6));
+  }
+}
