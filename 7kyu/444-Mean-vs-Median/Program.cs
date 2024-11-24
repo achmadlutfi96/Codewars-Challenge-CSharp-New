513f03117c3837300199481533149bfa32d0549c
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-24 15:12:59 +0700

    Create 7kyu\444-Mean-vs-Median

---
+++ 7kyu/444-Mean-vs-Median/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+class Solution
+{
+  public static string MeanVsMedian(int[] numbers)
+  {
+    Array.Sort(numbers);
+    int mean = (int)numbers.Average();
+    int median = numbers[numbers.Length/2];
+    return mean == median ? "same" : mean > median ? "mean" : "median";
+
+    // Alternative
+    // return numbers.Average().CompareTo(numbers.OrderBy(x => x).ToArray()[numbers.Length / 2]) switch
+    // {
+    //     1 => "mean",
+    //     -1 => "median",
+    //     _ => "same"
+    // };
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(MeanVsMedian(new int[] {1, 1, 1}));
+    Console.WriteLine(MeanVsMedian(new int[] {1, 2, 37}));
+    Console.WriteLine(MeanVsMedian(new int[] {7, 14, -70}));
+  }
+}
