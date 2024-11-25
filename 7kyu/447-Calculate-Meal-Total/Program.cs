39198aee870c4cbbe0b7e5398c31ed0c68b4bbe1
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-25 13:12:18 +0700

    Create 7kyu\447-Calculate-Meal-Total

---
+++ 7kyu/447-Calculate-Meal-Total/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Kata
+{
+  public static double CalculateTotal(double subtotal, int tax, int tip)
+  {
+    // TODO: your solution here
+    return Math.Round(subtotal + (subtotal*tax/100) + (subtotal*tip/100) ,2);
+  }
+
+  public static void Main(){
+    Console.WriteLine(CalculateTotal(5.00, 5, 10));
+  }
+}
