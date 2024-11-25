264f6c4806fe2eedbf430b573b51a645b2a7ab19
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-25 12:28:59 +0700

    Create 7kyu\446-Invalid-Input-Error-Handling-1

---
+++ 7kyu/446-Invalid-Input-Error-Handling-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static Counter GetCount(object word)
+    {
+        if (!word.GetType().Equals(typeof(string)))
+        {
+            return new Counter(0, 0);
+        }
+        int vowels = word.ToString().ToLower().Where("aiueo".Contains).Count();
+        int consonants = word.ToString().Where(char.IsLetter).Count() - vowels;
+        return new Counter(vowels, consonants);
+
+        // Alternative
+        // var s = (word as string ?? "").ToLower().Where(char.IsLetter).ToArray();
+        // return new Counter(s.Count("aeiou".Contains), s.Count(c => !"aeiou".Contains(c)));
+    }
+}
+
+public class Counter
+{
+    public int Vowels { get; }
+    public int Consonants { get; }
+
+    public Counter(int vowels, int consonants)
+    {
+        this.Vowels = vowels;
+        this.Consonants = consonants;
+    }
+}
