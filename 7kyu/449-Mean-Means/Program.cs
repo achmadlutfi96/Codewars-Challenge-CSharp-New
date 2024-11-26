b8fa8403b037d5da74afda7eb572acd5dcbd39c1
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-26 15:47:36 +0700

    Create 7kyu\449-Mean-Means

---
+++ 7kyu/449-Mean-Means/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static double Geo_Mean(int[] nums, double arith_mean)
+    {
+        double numLast = arith_mean * (nums.Length+1) - nums.Sum();
+        return Math.Pow(nums.Aggregate(1,(a,b)=> a*b) * numLast, (double)1/(nums.Length+1));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Geo_Mean(new int[] {1,2}, 3));
+        Console.WriteLine(Geo_Mean(new int[] { 4,6,7,2 }, 5));
+    }
+}
