72c8bab64c1b4da40091c255f83708e53ce88fe0
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-27 21:53:58 +0700

    Create 7kyu\451-Which-string-is-worth-more

---
+++ 7kyu/451-Which-string-is-worth-more/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string HighestValue(string a, string b)
+    {
+        return a.Sum(x=>x) >= b.Sum(x=>x) ? a : b;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(HighestValue("AaBbCcXxYyZz0189", "KkLlMmNnOoPp4567"));
+    }
+}
