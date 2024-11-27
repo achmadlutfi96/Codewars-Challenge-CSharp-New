bd22d6420d8a3dc401687698f6e158c390753de3
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-27 22:01:32 +0700

    Create 7kyu\452-Lowercase-strings-in-array

---
+++ 7kyu/452-Lowercase-strings-in-array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System;
+public class Kata
+{
+  public static object[] ArrayLowerCase(object[] arr)
+  {
+    return arr.Select(x=>x.GetType() == typeof(string) ? x.ToString().ToLower() : x).ToArray();
+
+    // Alternative
+    // return arr.Select(o => (o as string)?.ToLower() ?? o).ToArray();
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ",ArrayLowerCase(new object[] { "Red", "Green" })));
+  }
+}
