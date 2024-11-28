282da84ee2815fd22834c547125a4f94ba933ef9
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-28 09:40:05 +0700

    Create 7kyu\456-Negation-of-a-Value

---
+++ 7kyu/456-Negation-of-a-Value/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Kata
+{
+  public static bool NegationValue(string str, bool value)
+  {
+    // your code here
+    return str.Length%2 == 0 ? value : !value;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(NegationValue("!!!", false));
+  }
+}
