38ceef345b2d3a70ef3e50d7abece86aa69ed393
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-28 09:32:53 +0700

    Create 7kyu\455-Simple-Fun-13-Magical-Well

---
+++ 7kyu/455-Simple-Fun-13-Magical-Well/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int MagicalWell(int a, int b, int n)
+        {
+            //coding and coding..
+
+            return Enumerable.Range(0, n).Select(x => (a + x) * (b + x)).Sum();
+
+            //    Alternative
+            // return n == 0 ? 0 : a * b + MagicalWell(a + 1, b + 1, n - 1);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(MagicalWell(6, 5, 3));
+        }
+    }
+}
