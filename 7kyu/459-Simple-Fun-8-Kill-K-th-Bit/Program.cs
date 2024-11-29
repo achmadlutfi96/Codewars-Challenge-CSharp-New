b4a1b757c0ce27e9795109c8d792deab3a6598c1
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-29 17:59:22 +0700

    Create 7kyu\459-Simple-Fun-8-Kill-K-th-Bit

---
+++ 7kyu/459-Simple-Fun-8-Kill-K-th-Bit/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int KillKthBit(int n, int k)
+        {
+            //coding and coding..
+
+            return Convert.ToInt32(string.Concat(Convert.ToString(n, 2).Reverse().Select((v, i) => i + 1 == k ? '0' : v).Reverse()), 2);
+
+            // Alternative 1
+            // return n & ~(1<<k-1);
+
+            // Alternative 2
+            // return n&~(1<<--k);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(KillKthBit(37, 3) == 33);
+            Console.WriteLine(KillKthBit(37, 4) == 37);
+        }
+    }
+}
