36165689d5960e927bc4488b2cf1bc8a3866f1c8
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-29 17:39:53 +0700

    Create 7kyu\458-Comfortable-words

---
+++ 7kyu/458-Comfortable-words/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Kata
+{
+  public static bool ComfortableWord(string word)
+  {
+    string right = "yuiophjklnm";
+    string left = "qwertasdfgzxcvb";
+    
+    for (int i = 1; i < word.Length; i++)
+    {
+        if(right.Contains(word[i-1]) == right.Contains(word[i]) || left.Contains(word[i-1]) == left.Contains(word[i])){
+            return false;
+        }
+    }
+    return true; // todo
+
+    // Alternative
+    // var s = string.Concat(word.Select(c => "yuiophjklnm".Contains(c) ? "R" : "L"));
+    // return !(s.Contains("LL") || s.Contains("RR"));
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(ComfortableWord("their"));
+    Console.WriteLine(ComfortableWord("leisure"));
+  }
+}
