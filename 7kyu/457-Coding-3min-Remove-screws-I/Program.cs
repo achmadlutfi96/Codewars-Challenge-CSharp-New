f1935285c278046d7d2643edec7346c3f841a840
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-29 16:20:05 +0700

    Create 7kyu\457-Coding-3min-Remove-screws-I

---
+++ 7kyu/457-Coding-3min-Remove-screws-I/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int Sc(string screws)
+    {
+        int res = 0;
+        for (int i = 0; i < screws.Length; i++)
+        {
+            res += 1;
+            if (i + 1 != screws.Length)
+            {
+                if (screws[i] != screws[i + 1])
+                {
+                    res += 5;
+                }
+                res += 1;
+            }
+        }
+        return res;
+
+        // Alternative 1
+        // return screws.Select((c, i) => i == 0 ? 1 : c == screws[i - 1] ? 2 : 7).Sum();
+
+        // Alternative 2
+        // int t = screws.Length * 2 - 1;
+        //   for(int i = 1; i < screws.Length; i++){
+        //     if(screws[i] != screws[i - 1]) t += 5;
+        //   }
+        //   return t;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Sc("-+-+-+"));
+    }
+}
