22e86912b017ea006cf9e05f4dada5247184a4d3
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-30 12:14:46 +0700

    Create 7kyu\462-The-Hidden-Word

---
+++ 7kyu/462-The-Hidden-Word/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace CodeWars
+{
+    public class Kata
+    {
+        public static string hidden(int num)
+        {
+            // Code here
+            char[] w= new char[]{'o','b','l','i','e','t','a','d','n','m'};
+            return string.Concat(num.ToString().Select(x=>w[(int)char.GetNumericValue(x)]));
+
+            // Alternative 1
+            // string key = "oblietadnm";
+            // return new string(num.ToString().Select(x => key[int.Parse(x.ToString())]).ToArray());
+
+            // Alternative 2
+            // return string.Concat($"{num}".Select(c => "oblietadnm"[c - '0']));
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(hidden(7415));
+        }
+    }
+}
