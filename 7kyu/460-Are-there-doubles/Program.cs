f7af849a733f751106d0ef9b52f35ea8da77e5dc
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-30 10:45:48 +0700

    Create 7kyu\460-Are-there-doubles

---
+++ 7kyu/460-Are-there-doubles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static bool Double_check(string strng)
+    {
+        // Your Code
+        return strng.Distinct().Any(x=>strng.ToLower().Contains($"{x}{x}"));
+
+        // Alternative
+        // return Regex.IsMatch(s,@"(?i)(.)\1");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Double_check("a 11 c d"));
+        Console.WriteLine(Double_check("2020"));
+        Console.WriteLine(Double_check("a b  c"));
+
+    }
+}
