a1b4cea78850f1117167973a2c33c19ffdc19a15
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-11-30 11:58:49 +0700

    Create 7kyu\461-Alan-Partridge-III-London

---
+++ 7kyu/461-Alan-Partridge-III-London/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static string Alan(string[] x)
+    {
+        string[] str = new string[] {"Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway"};
+        return str.All(y=> x.Contains(y)) ? "Smell my cheese you mother!": "No, seriously, run. You will miss it.";
+
+        // Alternative
+        // return Array.TrueForAll(new string[] {"Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway"}, v => x.Any(i => i == v)) ? "Smell my cheese you mother!" : "No, seriously, run. You will miss it.";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Alan(new string[] {"Norwich", "Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway", "London"}));
+        Console.WriteLine(Alan(new string[] {"London", "Norwich"}));
+    }
+}
