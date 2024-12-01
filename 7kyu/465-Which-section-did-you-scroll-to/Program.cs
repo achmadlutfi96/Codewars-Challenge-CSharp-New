d8cabf7068604be5c44ae09f947c31960571977c
Achmad Lutfi <alhacker1996@gmail.com>
2024-12-01 17:06:43 +0700

    Create 7kyu\465-Which-section-did-you-scroll-to

---
+++ 7kyu/465-Which-section-did-you-scroll-to/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Kata
+{
+    public static int GetSectionId(int n, int[] a)
+    {
+        // your code here
+        int res = -1;
+        int temp = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            temp += a[i];
+            if(temp > n){
+                res = i;
+                break;
+            }
+        }
+        return res;
+
+        // Alternative 1
+        // return a.Sum() > n ? a.TakeWhile(s => (n -= s) >= 0).Count() : -1;
+
+        // Alternative 2
+        // return a.Sum() > n ? a.Count(x => (n -= x) >= 0) : -1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GetSectionId(300, new int[] { 300, 200, 400, 600, 100 }));
+    }
+}
