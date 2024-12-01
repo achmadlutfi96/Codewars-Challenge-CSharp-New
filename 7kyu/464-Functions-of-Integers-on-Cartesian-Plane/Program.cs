f63123fcbe72ab136ba541063935d8b9146221d8
Achmad Lutfi <alhacker1996@gmail.com>
2024-12-01 15:41:39 +0700

    Create 7kyu\464-Functions-of-Integers-on-Cartesian-Plan

---
+++ 7kyu/464-Functions-of-Integers-on-Cartesian-Plane/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Funcij {
+
+	public static ulong sumin(ulong n)
+	{
+		  // your code
+          return n * (n + 1) * (2 * n + 1) / 6;
+	}
+	public static ulong sumax(ulong n)
+	{
+		  // your 
+          return n * n * n - n * (n - 1) * (2 * n - 1) / 6;
+	}
+	public static ulong sumsum(ulong n)
+	{
+		  // your code
+          return sumin(n) + sumax(n);
+	}
+}
