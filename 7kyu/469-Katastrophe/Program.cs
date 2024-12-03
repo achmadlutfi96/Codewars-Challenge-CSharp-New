d1770967bcf6588b648d230a4e726f28dec36aa1
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-03 14:13:40 +0700

    Create 7kyu\469-Katastrophe

---
+++ 7kyu/469-Katastrophe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static string StrongEnough(int[][] earthquake, int age)
+  {
+    // your code here
+    double strong  = 1000;
+    for (int i = 0; i < age; i++)
+    {
+        strong -= strong*1d/100d;
+    }
+
+    int totalEarthQuake = earthquake.Select(x=>x.Sum()).Aggregate((a,b)=>a*b);
+
+    return strong > totalEarthQuake ? "Safe!" : "Needs Reinforcement!";
+
+    // Alternative 
+    // return earthquake.Aggregate(1,(s,n)=> s * n.Sum(e=>e))< 1000* Math.Pow(0.99,age) ?  "Safe!" : "Needs Reinforcement!";
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(StrongEnough(new int[][] {new int[] {5, 8, 7}, new int[] {3, 3, 1,}, new int[] {4, 1, 2}}, 2));
+    Console.WriteLine(StrongEnough(new int[][] {new int[] {5, 8, 7}, new int[] {3, 3, 1,}, new int[] {4, 1, 2}}, 3));
+  }
+
+}
