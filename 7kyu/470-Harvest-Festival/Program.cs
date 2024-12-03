d02dd9b525c9ade9e90f28aaf4d80c38304c9b85
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-03 14:44:39 +0700

    Create 7kyu\470-Harvest-Festival

---
+++ 7kyu/470-Harvest-Festival/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+static class Kata
+{
+    public static string Plant(char seed, int water, int fert, int temp)
+    {
+        return temp >= 20 && temp <= 30 ? string.Concat(Enumerable.Repeat(string.Concat(Enumerable.Repeat('-',water))+string.Concat(Enumerable.Repeat(seed, fert)),water)) : string.Concat(Enumerable.Repeat('-',water*water))+seed;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Plant('@', 4, 3, 23));
+    }
+}
