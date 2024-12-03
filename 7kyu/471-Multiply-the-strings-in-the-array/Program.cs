94ce049db3d9e3b6222a9d4cde3c941402782f72
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-03 14:49:31 +0700

    Create 7kyu\471-Multiply-the-strings-in-the-array

---
+++ 7kyu/471-Multiply-the-strings-in-the-array/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class Kata
+{
+  public static string ArrMultiply(string[] arr)
+  {
+    return (Convert.ToInt32(arr[0]) * Convert.ToInt32(arr[1])).ToString();
+  }
+}
