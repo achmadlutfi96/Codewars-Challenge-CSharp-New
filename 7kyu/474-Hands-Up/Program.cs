7e1327e145c3c8eeb0910b543f5e8392f5a7d512
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-04 18:16:09 +0700

    Create 7kyu\473-Consecutive-Digits-To-Form-Sum

---
+++ 7kyu/474-Hands-Up/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Hands
+{
+  public static int[] GetPositions(int s)
+  {
+    // your code
+		return new int[]{s%3, s/3%3, s/9%3};
+
+        // Alternative
+        // return new int[3] { s%3, s%9/3, s%27/9};
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ",GetPositions( 5 )));
+  }  
+}
