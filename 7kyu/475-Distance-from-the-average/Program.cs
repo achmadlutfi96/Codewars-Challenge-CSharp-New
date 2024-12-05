d8f404d5ed26bb38ed3d474af1cf49011895f1b5
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-05 15:11:08 +0700

    Create7kyu\475-Distance-from-the-average

---
+++ 7kyu/475-Distance-from-the-average/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static double[] DistancesFromAverage(int[] input)
+    {
+        //your code here
+        double average = input.Average();
+        return input.Select(x=>Math.Round(average-x,2)).ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ",DistancesFromAverage(new[] { 55, 95, 62, 36, 48 })));
+    }
+}
