7bc3cb56d8747f6eccbeb09fd195754d7dafe94d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-05 15:51:12 +0700

    Create 7kyu\476-Consecutive-Vowels-in-a-String

---
+++ 7kyu/476-Consecutive-Vowels-in-a-String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+public class Kata {
+  public static int GetTheVowels(string word) {
+    char[] sv = word.Where("aeiou".Contains).ToArray();
+    var res = new List<char>();
+    for (int i = 0; i < sv.Length; i++)
+    {
+        if(sv[i]=='a' && (res.Count == 0 || res.LastOrDefault() == 'u')){
+            res.Add(sv[i]);
+        }
+        if(sv[i] == 'e' && res.LastOrDefault() == 'a'){
+            res.Add(sv[i]);
+        }
+        if(sv[i] == 'i' && res.LastOrDefault() == 'e'){
+            res.Add(sv[i]);
+        }
+        if(sv[i] == 'o' && res.LastOrDefault() == 'i'){
+            res.Add(sv[i]);
+        }
+        if(sv[i] == 'u' && res.LastOrDefault() == 'o'){
+            res.Add(sv[i]);
+        }
+    }
+    return res.Count();
+
+    // Alternative
+    // return word.Aggregate(0, (i, c) => i + (c == "aeiou"[i % 5] ? 1 : 0));
+  }
+  public static void Main()
+  {
+    Console.WriteLine(GetTheVowels("sudnfhrakekdhhfkakjdjdhvneidkvnudntomcnnamjemdmfudkfhjamvcjedkfdnridnmnbvfhbdjdidncbvchencchdjdodncvchfndnrnencncnffduncbhjdfja"));
+  }
+}
