efd3d1f68a63fc440bd68831bea9e762c00a9bb8
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-06 12:35:41 +0700

    Create 7kyu\478-Interlocking-Binary-Pairs

---
+++ 7kyu/478-Interlocking-Binary-Pairs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+public static class Kata
+{
+    public static bool Interlockable(ulong a, ulong b)
+    {
+        //  <----  hajime!
+        string bin1 = Convert.ToString((long)a, 2);
+        string bin2 = Convert.ToString((long)b, 2);
+        if(bin1.Length > bin2.Length){
+            bin2 = bin2.PadLeft(bin1.Length,'0');
+        }else{
+            bin1 = bin1.PadLeft(bin2.Length,'0');
+        }
+        return bin1.Select((v,i)=>v == '1' && bin2[i] == '1' ).All(x => x == false);
+
+        // Alternative
+        // return (a & b) == 0;
+        // https://en.wikipedia.org/wiki/Bitwise_operation
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Interlockable(9UL, 4UL));
+        Console.WriteLine(Interlockable(5UL, 6UL));
+    }
+}
