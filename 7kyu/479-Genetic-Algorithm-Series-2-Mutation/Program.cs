6c0d632daab2665ee8030ea6d51e848b505c084d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-06 13:46:13 +0700

    Create 7kyu\479-Genetic-Algorithm-Series-2-Mutation

---
+++ 7kyu/479-Genetic-Algorithm-Series-2-Mutation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static string Mutate(string chromosome, double probability)
+    {
+        Random random = new Random();
+        return String.Join("", chromosome.Select(c => random.NextDouble() <= probability ? (c == '0' ? "1" : "0") : c.ToString()));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Mutate("1111", 1));
+    }
+}
