40d22abe231d103516550430b20645cc360ddaf0
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-07 14:52:34 +0700

    Create return string.Concat(functionName.Split("_").Select((v,i)=>i == 0 ? v : char.ToUpper(v[0])+v.Substring(1)));

---
+++ 7kyu/482-Zebulans-Nightmare/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static string ZebulansNightmare(string functionName)
+  {
+    return string.Concat(functionName.Split("_").Select((v,i)=>i == 0 ? v : char.ToUpper(v[0])+v.Substring(1)));
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(ZebulansNightmare("mark_as_issue"));
+  }
+}
