ef0a31db091ca742161570cd8d08df21eaad7a82
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-07 15:22:55 +0700

    Create 7kyu\483-Mobile-Display-Keystrokes

---
+++ 7kyu/483-Mobile-Display-Keystrokes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int MobileKeyboard(string str)
+    {
+        string[] mKeyboard = new string[] { "1", "2abc", "3def", "4ghi", "5jkl", "6mno", "7pqrs", "8tuv", "9wxyz","*","0","#"};
+        return str.Select(x=>mKeyboard.Where(y=>y.Contains(x)).First().IndexOf(x)+1).Sum();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(MobileKeyboard("thisisasms"));
+    }
+}
