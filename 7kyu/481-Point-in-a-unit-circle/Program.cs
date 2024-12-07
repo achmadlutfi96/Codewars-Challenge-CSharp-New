9de04995057ce0c0ca0d72f828b3e06ad78fc301
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-07 14:40:24 +0700

    Create 7kyu\481-Point-in-a-unit-circle

---
+++ 7kyu/481-Point-in-a-unit-circle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Kata
+{
+  public static bool PointInCircle(double x, double y)
+  {
+    return x * x + y * y < 1;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(PointInCircle(0.5,0.5));
+    Console.WriteLine(PointInCircle(2,0));
+  }
+}
