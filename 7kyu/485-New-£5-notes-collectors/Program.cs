3e3d9bee4b9744ab8e0a8d60af1228adaa023765
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-08 13:25:20 +0700

    Create 7kyu\485-New-£5-notes-collectors

---
+++ 7kyu/485-New-£5-notes-collectors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static int GetNewNotes(int salary, int[] bills)
+  {
+    if(salary < bills.Sum()) return 0;
+    return (salary-bills.Sum())/5;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(GetNewNotes(2000, new int [] { 500, 160, 400 }));
+  }
+}
