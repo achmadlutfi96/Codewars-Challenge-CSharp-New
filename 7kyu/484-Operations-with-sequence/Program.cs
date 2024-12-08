141e3d75e55ab0628a31e5073dd824fad1c0ae63
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-08 13:17:45 +0700

    Create 7kyu\484-Operations-with-sequence

---
+++ 7kyu/484-Operations-with-sequence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int Calc(int[] array)
+    {
+      return (int) array.Select(v=>v > 0 ? Math.Pow(v,2) : v).Select((v,i)=>(i+1)%3 == 0 ? v*3 : v).Select((v,i)=>(i+1)%5 == 0 ? v*-1 : v).Sum();
+
+    //   Alternative 
+    // return array.Select((x,i) => 
+    //     (x > 0 ? x * x : x)
+    //     * (i % 3 == 2 ? 3 : 1)
+    //     * (i % 5 == 4 ? -1 : 1))
+    //   .Sum();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Calc(new[]{ 0, 2, 1, -6, -3, 3 }));
+    }
+}
