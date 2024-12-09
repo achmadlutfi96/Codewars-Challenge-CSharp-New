9387f6f84f5111a8405720f67178c95f552975c3
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-09 13:32:23 +0700

    Create 7kyu\487-Doubleton-number

---
+++ 7kyu/487-Doubleton-number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+public static class Kata
+{
+    public static int Doubleton(int num)
+    {
+        // Insert code here ..
+        do
+        {
+            num++;
+        } while (num.ToString().Distinct().Count() != 2);
+        return num;
+
+        // Alternative 
+        // while ((++num).ToString().Distinct().Count() != 2);
+        // return num;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Doubleton(1234));
+    }
+}
