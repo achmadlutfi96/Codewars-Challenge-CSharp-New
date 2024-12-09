bcc152faa619e60374d42feaa8b69ec96eb0c4ad
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-09 13:36:57 +0700

    Create 7kyu\488-Only-one

---
+++ 7kyu/488-Only-one/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static bool OnlyOne(params bool[] flags)
+  {
+    return flags.Count(x=>x) == 1;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(OnlyOne(true, false));
+  }
+}
