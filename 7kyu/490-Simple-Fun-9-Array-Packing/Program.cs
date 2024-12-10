1480153d6a09f2f2ec67d8499b09c00f0d68cf2e
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-10 13:53:24 +0700

    Create 7kyu\490-Simple-Fun-9-Array-Packing

---
+++ 7kyu/490-Simple-Fun-9-Array-Packing/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int ArrayPacking(int[] a)
+        {
+            //coding and coding..
+            return Convert.ToInt32(string.Concat(a.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')).Reverse()), 2);
+
+            //   Alternative 1
+            // return arr.Reverse().Aggregate(0, (a, v) => (a << 8) + v);
+
+            // Alternative 2
+            // return a.Select((x, i) => x << i * 8).Sum();
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(ArrayPacking(new int[] { 24, 85, 0 }));
+        }
+    }
+}
