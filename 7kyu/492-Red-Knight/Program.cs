7330e81c04dc5624d9478b1c5f8f88250764f91d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-10 14:35:54 +0700

    Create 7kyu\492-Red-Knight

---
+++ 7kyu/492-Red-Knight/Program.cs
@@ -0,0 +1,9 @@
+﻿static class CodeWars
+{
+    public static (string color, long position) RedKnight(int knight, long pawn)
+    {
+      // Your solution goes here
+      return ((pawn-knight)%2==1 ? "Black":"White", pawn*2);
+    }
+
+}
