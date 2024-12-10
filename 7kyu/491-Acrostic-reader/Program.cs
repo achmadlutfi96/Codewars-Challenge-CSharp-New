853641b3a14a18c78655b4db571c3cb2aa829cfa
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-10 14:27:14 +0700

    Create 7kyu\491-Acrostic-reader

---
+++ 7kyu/491-Acrostic-reader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string ReadOut(string[] acrostic)
+  {
+    // throw new NotImplementedException();
+    return string.Concat(acrostic.Select(x=>x[0]));
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(ReadOut(new string[] {"Jolly", "Amazing", "Courteous", "Keen"}));
+  }
+}
