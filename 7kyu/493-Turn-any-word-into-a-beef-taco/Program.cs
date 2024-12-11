db659ea3c0ef956169be24b9ca92cbea04bce203
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-11 17:22:25 +0700

    Create 7kyu\493-Turn-any-word-into-a-beef-taco

---
+++ 7kyu/493-Turn-any-word-into-a-beef-taco/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+public class Kata
+{
+    public static string[] Tacofy(string word)
+    {
+        Dictionary<string, string> words = new() {
+            {"aeiou", "beef"},
+            {"t", "tomato"},
+            {"l", "lettuce"},
+            {"c", "cheese"},
+            {"g", "guacamole"},
+            {"s", "salsa"}
+        };
+        List<string> res = new() { "shell" };
+
+        return res.Concat(word.ToLower().Select(x => words.Where(y => y.Key.Contains(x)).FirstOrDefault().Value).Where(x => x != null).Append("shell")).ToArray();
+
+        // Alternative
+        // return new List<string> {"shell"}.Concat(word.ToLower().Where(c => "aeioutlcgs".Contains(c))
+        //   .Select(c => c switch
+        //   {
+        //       't' => "tomato",
+        //       'c' => "cheese",
+        //       'g' => "guacamole",
+        //       'l' => "lettuce",
+        //       's' => "salsa",
+        //       _ => "beef"
+        //   })).Append("shell").ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ", Tacofy("ydjkpwqrzto")));
+    }
+}
