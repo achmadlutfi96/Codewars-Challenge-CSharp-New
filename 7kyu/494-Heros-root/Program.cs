2fcd13af8a3d3aa200d9ab8b3f10c8d3f65d94b6
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-11 18:26:08 +0700

    Create 7kyu\494-Heros-root

---
+++ 7kyu/494-Heros-root/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class IntSqRoot 
+{
+	public static long IntRac(long n, long guess)  
+	{
+		// your code
+        long x = guess;
+        long res = 1;
+        do{
+            x = (x + n / x) / 2;
+            res++;
+        }while(x != (long) Math.Sqrt(n));
+        return res;
+	}
+
+    public static void Main()
+    {
+        Console.WriteLine(IntRac(25,1));
+        Console.WriteLine(IntRac(125348, 300));
+    }
+}
