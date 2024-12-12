0bb56e6bafa48ecfb9724632531b5b09ca435b5e
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-12 15:32:33 +0700

    Create 7kyu\498-International-Morse-Code-Encryption

---
+++ 7kyu/498-International-Morse-Code-Encryption/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+// Dictionary CHAR_TO_MORSE in the class Preloaded is already defined to convert characters into their Morse Code equivilant.
+public class MorseEncrypt
+{
+  public static string ToMorse(string englishStr)
+  {
+    Console.Write(Preloaded.CHAR_TO_MORSE);
+    return string.Join(" ",englishStr.Select(x=> x==' ' ? " " : Preloaded.CHAR_TO_MORSE[x]));
+  }
+}
