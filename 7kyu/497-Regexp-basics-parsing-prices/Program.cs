79384009d01cd8ed7cc45d69e005c9475577ac55
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-12 15:22:12 +0700

    Create 7kyu\497-Regexp-basics-parsing-prices

---
+++ 7kyu/497-Regexp-basics-parsing-prices/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static int? ToCents(this string price)
+    {
+        var s = Regex.Replace(Regex.Match(price, @"^\$\d{1,}\.\d{2}\z").Value, @"(\.|\$)", "");
+        return s.Any() ? int.Parse(s) : (int?) null;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(ToCents("$12345678.90"));
+    }
+}
