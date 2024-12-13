53e714eb344061a1549f58029c640d0711ca0d4e
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-13 16:44:31 +0700

    Create 7kyu\499-Suitcase-packing

---
+++ 7kyu/499-Suitcase-packing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public static class MrSquare {
+    public static bool FitSquares(int a, int b, int m, int n) {
+        // You may code here
+        return (a+b) <= Math.Max(m,n)  && Math.Max(a,b) <= Math.Min(m,n);
+
+        // Alternative
+        // returm Math.Max(m,n) >= a+b && Math.Max(a,b) <= Math.Min(m,n);
+    }
+
+    public static void Main(){
+        Console.WriteLine(FitSquares(1, 2, 3, 2));
+        Console.WriteLine(FitSquares(1, 2, 2, 1));
+    }
+}
