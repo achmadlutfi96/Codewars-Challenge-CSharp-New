70153dc776ed7b57cc2f5da439cc29790d61e9b9
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-13 17:27:25 +0700

    Create 7kyu\500-Collision-Detection

---
+++ 7kyu/500-Collision-Detection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Kata 
+{
+  public static bool Collision(double x1, double y1, double r1, double x2, double y2, double r2) 
+  {
+    // throw new NotImplementedException();
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)) <= r1+r2; 
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Collision(1, 1, 1, 1.1, 1.1, 0.1));
+    Console.WriteLine(Collision(0, 1.7, 0.36, 2.1, 2, 0.8));
+  }
+}
