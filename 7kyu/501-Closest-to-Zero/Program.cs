1b6afad6514a45c1fd25e43916f57903bad0eb95
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-13 17:50:00 +0700

    Create 7kyu\501-Closest-to-Zero

---
+++ 7kyu/501-Closest-to-Zero/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static int? Closest(int[] arr)
+  {
+    // return null if it is not possible to return 1 unique closest value
+    int min = arr.Min(Math.Abs);
+    int[] res = arr.Where(x=>Math.Abs(x) == min).ToArray();
+    return res.Any(x=>x < 0) && res.Any(x=> x > 0) ? null : res.First();
+
+    // Alternative 
+    // var min = arr.Distinct().Where(x => Math.Abs(x) == arr.Min(Math.Abs));
+    // return min.Count() == 1 ? min.First() : (int?) null;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Closest(new int[] {10, 3, 9, 1}));
+  }
+}
