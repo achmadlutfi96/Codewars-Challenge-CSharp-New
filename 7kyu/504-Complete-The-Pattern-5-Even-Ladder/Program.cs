8fef370071886cd0691ac78796213fc3525932a5
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-14 16:04:26 +0700

    Create 7kyu\504-Complete-The-Pattern-5-Even-Ladder

---
+++ 7kyu/504-Complete-The-Pattern-5-Even-Ladder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+public class EvenLadder
+{
+  public static string Pattern(int n)
+  {
+    return n > 0 ? string.Join("\n",Enumerable.Range(1,n).Where(x=>x%2==0).Select(x=>string.Concat(Enumerable.Repeat($"{x}",x)))) : "";
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Pattern(5));
+  }
+}
