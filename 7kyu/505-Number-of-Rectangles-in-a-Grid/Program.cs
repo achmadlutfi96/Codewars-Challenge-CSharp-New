b8ca0419b4a6a20fe5f87050bb82f11d21ebe103
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-15 15:24:41 +0700

    Create 7kyu\505-Number-of-Rectangles-in-a-Grid

---
+++ 7kyu/505-Number-of-Rectangles-in-a-Grid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+public class Grid {
+  public static int NumberOfRectangles(int m, int n) {
+    // Your code here!
+    return Enumerable.Range(1,m).Select(x=>Enumerable.Range(1,n).Select(y=>x*y).Sum()).Sum();
+
+    // Alternative
+    // return (m*(m+1)*n*(n+1))/4;
+
+    // Alternative 2
+    // return m * n * ++m * ++n / 4;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(NumberOfRectangles(3,2));
+  }
+}
