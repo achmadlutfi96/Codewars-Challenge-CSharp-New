19658588f0464b0612174935d53b68e4e630dac6
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-15 17:19:35 +0700

    Create 7kyu\507-Reverse-Factorials

---
+++ 7kyu/507-Reverse-Factorials/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class Kata
+{  
+  public static string ReverseFactorial(long num)
+  {
+    // throw new NotImplementedException();
+    long res = 1;
+    int i = 0;
+    do{
+        i++;
+        res *= i;
+    }while(res < num);
+    return res == num ? $"{i}!" : "None";
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(ReverseFactorial(3628800));
+    Console.WriteLine(ReverseFactorial(150));
+  }
+}
