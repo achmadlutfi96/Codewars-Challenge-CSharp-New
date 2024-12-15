78815050ce9eae498b9b47799d49e0394c28d782
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-15 16:46:44 +0700

    Create 7kyu\506-Concatenated-Sum

---
+++ 7kyu/506-Concatenated-Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  /// <summary>
+  /// Returns a Boolean representing if a number with its digits concatenated an arbitrary number of times and summed is the same as the original number.</summary>
+  /// <param name="number">int to be tested.</param>
+  /// <param name="n">int which represents the number of times to concatenate each digit</param>
+  public static bool CheckConcatenatedSum(int number, int n)
+  {
+    // return number == Math.Abs(number).ToString().Select(x=>int.IsNegative(number) ? "-"+string.Concat(Enumerable.Repeat($"{x}",n)) : string.Concat(Enumerable.Repeat($"{x}",n))).Sum(long.Parse);
+
+    return Math.Abs(number) == Math.Abs(number).ToString().Select(x=>string.Concat(Enumerable.Repeat($"{x}",n))).Sum(long.Parse);
+
+    // Alternative 
+    // return $"{Math.Abs(number)}".Sum(c => Math.Sign(number) * long.Parse(new string(c, n))) == number;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(CheckConcatenatedSum(2997,3));
+    Console.WriteLine(CheckConcatenatedSum(-2997,3));
+  }
+}
