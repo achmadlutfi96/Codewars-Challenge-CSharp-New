3c5196d83070e9d92da37a3823bae5c74d5428f8
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-16 09:18:42 +0700

    Create 7kyu\508-Battle-of-the-characters

---
+++ 7kyu/508-Battle-of-the-characters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static string Battle(string x, string y)
+  {
+    // Lets the battle begin!
+    float a = x.Sum(v=>char.IsUpper(v) ? (float)v-64 : (float)(v-96)/2);
+    float b = y.Sum(v=>char.IsUpper(v) ? (float)v-64 : (float)(v-96)/2);
+    return a == b ? "Tie!" : a > b ? x : y;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Battle("One", "Two"));
+    Console.WriteLine(Battle("Foo", "BAR"));
+  }
+}
