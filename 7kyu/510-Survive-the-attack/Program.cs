bf6456cce54e043184124530b73ad33711090809
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-17 08:46:25 +0700

    Create 7kyu\510-Survive-the-attack

---
+++ 7kyu/510-Survive-the-attack/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static bool HasSurvived(int[] attackers, int[] defenders)
+    {
+        // throw new NotImplementedException();
+        if (attackers.Length == 0)
+        {
+            return true;
+        }
+        if (defenders.Length == 0)
+        {
+            return false;
+        }
+        int[] att = attackers.Where((v, i) => i < defenders.Length ? v > defenders[i] : true).ToArray();
+        int[] def = defenders.Where((v, i) => i < attackers.Length ? v > attackers[i] : true).ToArray();
+        if (att.Length == def.Length)
+        {
+            return attackers.Sum() <= defenders.Sum();
+        }
+        return att.Length < def.Length;
+
+        // Alternative
+        // //get difference between defender and attacker
+        // int left = D.Length - A.Length;
+
+        // //loop the min value between D and A
+        // //accumulate sum
+        // //Size: 0	If value is equal to zero
+        // //1	If value is greater than zero
+        // //-1	If value is lesser than zero
+        // for (int i = 0; i < Math.Min(D.Length, A.Length); i++)
+        //   left += Math.Sign(D[i] - A[i]);
+
+        // //if left greater or equal to 0 AND defender is more than attacker
+        // return left > 0 || left == 0 && D.Sum() >= A.Sum();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(HasSurvived(new[] { 1, 3, 5, 7 }, new[] { 2, 4, 6, 8 }));
+        Console.WriteLine(HasSurvived(new[] { 2, 9, 9, 7 }, new[] { 1, 1, 3, 8 }));
+    }
+}
---
+++ 7kyu/511-Excel-sheet-column-numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class ExcelToNumber {
+    public static long TitleToNumber(string title) {
+        // your code
+        return title.Reverse().Select((v,i)=> (v-64)*(long)Math.Pow(26, i)).Sum();
+        
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(TitleToNumber("A"));
+        Console.WriteLine(TitleToNumber("AA"));
+        Console.WriteLine(TitleToNumber("AZ"));
+        Console.WriteLine(TitleToNumber("BA"));
+    }
+}
