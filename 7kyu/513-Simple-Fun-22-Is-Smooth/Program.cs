de2a612869f91b6f687790c4f46216b9a516b7c6
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-17 09:22:01 +0700

    Create 7kyu\513-Simple-Fun-22-Is-Smooth

---
+++ 7kyu/513-Simple-Fun-22-Is-Smooth/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public static bool IsSmooth(int[] arr){
+          //coding and coding..
+          int a = arr[0];
+          int b = arr[arr.Length-1];
+          int c = arr.Length % 2 == 0 ? arr[(arr.Length/2)-1]+arr[arr.Length/2] : arr[arr.Length/2]; 
+          return a == b && b == c;
+
+        //   Alternative
+        // return arr[0] == arr[^1] && arr[((arr.Length - 1) / 2)..][..(arr.Length % 2 == 0 ? 2 : 1)].Sum() == arr[0];
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(IsSmooth(new int[]{7, 2, 2, 5, 10, 7}));
+        }
+    }
+}
