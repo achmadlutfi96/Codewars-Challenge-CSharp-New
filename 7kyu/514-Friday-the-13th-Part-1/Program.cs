35f5b1724a56114bf4689eaf7d8c2c1cc7ea3f3f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-18 08:07:54 +0700

    Create 7kyu\514-Friday-the-13th-Part-1

---
+++ 7kyu/514-Friday-the-13th-Part-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+  public static string[] KillCount(Dictionary<string, int> counselors, int jason)
+  {
+    // insert your code here
+    return counselors.Where(x=>x.Value < jason).Select(x=>x.Key).ToArray();
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ",KillCount(new Dictionary<string, int> 
+      {
+        {"Mike", 7},
+        {"Alysa", 3}
+      }, 7)));
+  }
+}
