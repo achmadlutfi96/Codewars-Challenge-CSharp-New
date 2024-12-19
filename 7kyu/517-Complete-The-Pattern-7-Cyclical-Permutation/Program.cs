bd39c8828490ef57d3b8cadc2e77c67e4faf8a62
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-19 18:12:59 +0700

    Create 7kyu\517-Complete-The-Pattern-7-Cyclical-Permutation

---
+++ 7kyu/517-Complete-The-Pattern-7-Cyclical-Permutation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class CyclicalPermutation
+{
+  public static string Pattern(int n)
+  {
+    if(n < 1){
+        return "";
+    }
+    return string.Join("\n",Enumerable.Range(0,n).Select(x=>string.Concat(Enumerable.Range(x+1, n-x))+string.Concat(Enumerable.Range(1, x))));
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Pattern(4));
+  }
+}
