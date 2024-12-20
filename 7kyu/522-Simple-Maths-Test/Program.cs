fccd7d41dc9d545f6afdb53ec4bc9fa4eef8e95d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-20 17:56:36 +0700

    Create 7kyu\522-Simple-Maths-Test

---
+++ 7kyu/522-Simple-Maths-Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+  public static bool[] NumberProperty(int n)
+  {
+    bool prime = n > 0 ? Enumerable.Range(1,n).Where(v=>n%v == 0).Count() < 3 : false;
+    bool even = n%2 == 0;
+    bool m10 = n%10 == 0;
+    return new bool[] {prime, even, m10};
+  }
+  
+  public static void Main(){
+    Console.WriteLine(string.Join(", ", NumberProperty(-10)));
+    Console.WriteLine(string.Join(", ", NumberProperty(2)));
+    Console.WriteLine(string.Join(", ", NumberProperty(120)));
+    Console.WriteLine(string.Join(", ", NumberProperty(125)));
+  }
+}
