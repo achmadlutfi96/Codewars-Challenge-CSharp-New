39685ad2703ed9aa2d6d5639dbdd478f77eb5629
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-20 17:33:14 +0700

    Create 7kyu\521-Deutschstunde

---
+++ 7kyu/521-Deutschstunde/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public class Deutschstunde
+{
+  public static string DerDieDas(string wort)
+  {
+    //Du kannst es schaffen!
+    int vowels = wort.ToLower().Where("aeiouäöü".Contains).Count();
+    return vowels < 2  ? $"das {wort}" : (vowels == 2 || vowels == 3) ? $"die {wort}" : $"der {wort}";
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(DerDieDas("OOOpa"));
+    Console.WriteLine(DerDieDas("Suppenhuhn"));
+    Console.WriteLine(DerDieDas("Fisch"));
+  }
+}
