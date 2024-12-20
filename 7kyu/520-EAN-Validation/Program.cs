33fb52bfd45e5c4e3dc56d233bbaba68a197f447
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-20 16:57:58 +0700

    Create 7kyu\520-EAN-Validation

---
+++ 7kyu/520-EAN-Validation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class EANValidator{
+  public static bool Validate(string eanCode){
+    //Your code here
+    // Console.WriteLine((10-(eanCode.SkipLast(1).Select((v,i)=> i%2 == 0 ? char.GetNumericValue(v)*1 : char.GetNumericValue(v)*3).Sum()%10)).ToString().Last());
+    // Console.WriteLine(eanCode.Last().ToString());
+    return (10-(eanCode.SkipLast(1).Select((v,i)=> i%2 == 0 ? char.GetNumericValue(v)*1 : char.GetNumericValue(v)*3).Sum()%10)).ToString().Last() == eanCode.Last();
+
+    // Alternative
+    // return eanCode
+        // .Select(c => int.Parse($"{c}"))
+        // .Select((x, i) => i % 2 == 0 ? x : x * 3)
+        // .Sum() % 10 == 0;
+  }
+
+  public static void Main()
+  {
+    // Console.WriteLine(Validate("4003301018398"));
+    Console.WriteLine(Validate("9783815820865"));
+    Console.WriteLine(Validate("9783815820864"));
+    Console.WriteLine(Validate("9783827317100"));
+  }
+}
