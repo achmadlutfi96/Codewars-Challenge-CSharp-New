ae54ca45db26993d9062d54d3feddefa95c3508a
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-21 15:22:34 +0700

    Create 7kyu\524-Temperature-analysis-I

---
+++ 7kyu/524-Temperature-analysis-I/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System;
+
+public class Kata
+{
+  public static int? LowestTemperature(string t)
+  {
+    return t == "" ? null : t.Split(" ").Min(x=>Convert.ToInt32(x));
+
+    // Alternative
+    // return t.Any() ? t.Split().Min(int.Parse) : (int?) null;
+  }
+}
