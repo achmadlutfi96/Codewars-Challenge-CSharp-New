1a4068dea7fc4efd32e84d70c5216c173bfbd066
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-21 15:36:58 +0700

    Create 7kyu\525-Simple-Fun-384-Is-Turings-Equation

---
+++ 7kyu/525-Simple-Fun-384-Is-Turings-Equation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static bool IsTuringEquation(string str)
+  {
+    // throw new NotImplementedException();
+   ] + v[1] == v[2]; int[] v = str.Split('+','=').Select(x=>Convert.ToInt32(string.Concat(x.Reverse()))).ToArray();
+    return v[0
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(IsTuringEquation("73+42=16"));
+    Console.WriteLine(IsTuringEquation("5+8=13"));
+  }
+}
