c0b07caba8f8580aaedb03fe4ecd962b80467ec0
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-21 15:18:06 +0700

    Create 7kyu\523-Password-System

---
+++ 7kyu/523-Password-System/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+public class Crisis
+{
+    public static string HelpZoom(int[] key)
+    {
+        return key.Reverse().Select((v,i)=> v == key[i]).All(x=>x==true) ? "Yes" : "No";
+
+        // Alternative
+        // return key.Reverse().SequenceEqual(key) ? "Yes" : "No";
+    }
+}
