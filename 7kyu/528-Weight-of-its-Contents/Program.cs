7b0ab06a200d23b9d799d5bd139197690725bb80
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-22 17:30:07 +0700

    Create 7kyu\528-Weight-of-its-Contents

---
+++ 7kyu/528-Weight-of-its-Contents/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Solution
+{
+  public static class SolutionClass
+  {
+    public static int ContentWeight(int bottleWeight, string scaleString)
+    {
+      // Code goes here ...
+      string[] scales = scaleString.Split(" ");
+      int scale = Convert.ToInt32(scales[0]);
+      string compare = scales[2];
+
+      float bottleOnlyWeight = bottleWeight / (scale+1);
+      int res = 0;
+      if(compare == "larger"){
+        res = (int) bottleOnlyWeight * scale;
+      }else{
+        res = (int) bottleOnlyWeight*1;
+      }
+      return res;
+
+    //   Alternative
+    // var scale = int.Parse(string.Concat(scaleString.Where(char.IsDigit)));
+    // return scaleString.Contains("larger") ? bottleWeight * scale / (scale + 1) : bottleWeight / (scale + 1);
+    }
+  }
+}
