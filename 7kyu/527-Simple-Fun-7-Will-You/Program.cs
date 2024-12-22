19330d5f86dbbcf721adb3040700300c7c459be1
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-22 16:44:24 +0700

    Create 7kyu\527-Simple-Fun-7-Will-You

---
+++ 7kyu/527-Simple-Fun-7-Will-You/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public bool WillYou(bool young, bool beautiful, bool loved){
+          //coding and coding..
+          
+          return !((young && beautiful) == loved);
+
+          // Alternative
+          // return loved != (young && beautiful);
+        }
+    }
+}
