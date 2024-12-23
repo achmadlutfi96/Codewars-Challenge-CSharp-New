4a34d4886b66c3fa64288095ade28bf2fe81e410
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-23 16:17:53 +0700

    Create 7kyu\531-Progressive-Spiral-Number-Position

---
+++ 7kyu/531-Progressive-Spiral-Number-Position/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Kata
+{
+    public static long Layers(long n)
+    {
+        long l = 1;
+        long m = 1;
+        while((long)Math.Pow(m,2) < n){
+            m+=2;
+            l++;
+        }
+        return l;
+
+        // Alternative
+        // return (long)Math.Ceiling((Math.Sqrt(n) + 1) / 2);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Layers(1));
+        Console.WriteLine(Layers(5));
+        Console.WriteLine(Layers(25));
+        Console.WriteLine(Layers(30));
+        Console.WriteLine(Layers(50));
+    }
+}
