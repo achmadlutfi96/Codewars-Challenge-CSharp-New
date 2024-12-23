e836124269bd3d5376216c76c17fa0fb6f9eaedc
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-23 15:50:22 +0700

    Create 7kyu\530-Sorted-Union

---
+++ 7kyu/530-Sorted-Union/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public static object[] UniteUnique(object[][] array)
+    {
+        return array.Aggregate((x,y)=> x.Concat(y).ToArray()).Distinct().ToArray();
+
+        // Alternative
+        // return array.SelectMany(x=>x).Distinct().ToArray();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ",UniteUnique(new [] { new object[] { 1, 3, 2 }, new object[] { 5, 2, 1, 4 }, new object[] { 2, 1 } })));
+    }
+}
