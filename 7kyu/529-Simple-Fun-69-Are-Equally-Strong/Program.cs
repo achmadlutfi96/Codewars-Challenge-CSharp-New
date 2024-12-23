3b55cf71135d0909f3f2864c43c399fffa186d55
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-23 15:38:35 +0700

    Create 7kyu\529-Simple-Fun-69-Are-Equally-Strong

---
+++ 7kyu/529-Simple-Fun-69-Are-Equally-Strong/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public bool AreEquallyStrong(int YourLeft, int YourRight, int FriendsLeft, int FriendsRight){
+          //coding and coding..
+          if(!((YourLeft == FriendsLeft || YourLeft == FriendsRight) && (YourRight == FriendsLeft || YourRight == FriendsRight))){
+            return false;
+          }
+          return (YourLeft+YourRight) == (FriendsLeft+FriendsRight);
+
+        //   Alternative
+        // int[]yourPower = {yourLeft, yourRight};
+        //   int[]friendsPower = {friendsLeft, friendsRight};
+        //   return yourPower.Max()==friendsPower.Max() && yourPower.Min()==friendsPower.Min();
+        }
+    }
+}
