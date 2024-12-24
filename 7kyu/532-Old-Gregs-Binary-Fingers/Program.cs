e77d1ed7bd4577adb28e1e3b4f20171d55c3b8ba
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-24 11:58:44 +0700

    Create 7kyu\532-Old-Gregs-Binary-Fingers

---
+++ 7kyu/532-Old-Gregs-Binary-Fingers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+public class BinaryFingers
+{
+  public static string[] GetFingers(string binary)
+  {
+    string[] fingers = new string[]{"Pinkie", "Ring", "Middle", "Index", "Thumb"};
+    return binary.PadLeft(5,'0').Select((v,i)=>v=='1'?fingers[i] : null).Where(x=>x!=null).ToArray();
+
+    // Alternative
+    // return new[] { "Pinkie", "Ring", "Middle", "Index", "Thumb" }.Skip(5 - binary.Length)
+        // .Where((_, i) => binary[i] == '1')
+        // .ToArray();
+  }
+
+  public static void Main(){
+    Console.WriteLine(string.Join(", ", GetFingers("101")));
+    Console.WriteLine(string.Join(", ", GetFingers("11111")));
+  }
+}
