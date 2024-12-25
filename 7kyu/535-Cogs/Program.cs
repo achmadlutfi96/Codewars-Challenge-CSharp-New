9d9619ed60d4bc8e1417ab55a64b87135516afc9
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-25 12:38:21 +0700

    Create 7kyu\535-Cogs

---
+++ 7kyu/535-Cogs/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Cogs {
+
+  public static double cogRpm(int[] cogs) {
+    // Your code here!
+    return (cogs.Length % 2 == 0 ? -1d : 1d) * cogs[0] / cogs[cogs.Length-1];
+  }
+    public static void Main()
+    {
+        Console.WriteLine(cogRpm(new[] {100, 75}));
+    }
+}
