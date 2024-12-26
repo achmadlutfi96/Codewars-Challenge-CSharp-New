9d3b2ce4b53cd422835b9fd889bc6cc6ad4d2d1c
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-26 09:53:02 +0700

    Create 7kyu\538-Binary-scORe

---
+++ 7kyu/538-Binary-scORe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+public class BinaryScore
+{
+    public static BigInteger Score(BigInteger n)
+    {
+        // Good Luck!
+        BigInteger score = 0;
+        while(n > 0){
+            score |= n;
+            n >>= 1;
+        }
+        return score;
+
+        // Alternative 1
+        // return BigInteger.Pow(2, (int) Math.Ceiling(BigInteger.Log(n + 1, 2))) - 1;
+
+        
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Score(BigInteger.Parse("49")));
+    }
+}
