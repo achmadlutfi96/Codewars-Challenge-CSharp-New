7c4f551e3075f8309d0cccc4c2fc2d178808a1dc
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-26 13:36:02 +0700

    Create 7kyu\540-Every-nth-array-element

---
+++ 7kyu/540-Every-nth-array-element/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+  public static IEnumerable<T> Every<T>(this IEnumerable<T> arr, int interval, int start)
+  {
+    // throw new NotImplementedException();
+    return arr.Skip(start).Where((v,i)=> i%interval == 0);
+  }
+
+  public static IEnumerable<T> Every<T>(this IEnumerable<T> arr, int interval)
+  {
+    // throw new NotImplementedException();
+    return arr.Where((v,i)=> i%interval == 0);
+  }
+
+  public static IEnumerable<T> Every<T>(this IEnumerable<T> arr)
+  {
+    // throw new NotImplementedException();
+    return arr;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ", new int []{0,1,2,3,4}.Every()));
+    Console.WriteLine(string.Join(", ", new int []{0,1,2,3,4}.Every(1)));
+    Console.WriteLine(string.Join(", ", new int []{0,1,2,3,4}.Every(2)));
+  }
+}
