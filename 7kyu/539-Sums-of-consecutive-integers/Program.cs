ff8b8b6bf0e463f7b9dc5d66f400e8768a7fa170
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-26 10:49:23 +0700

    Create 7kyu\539-Sums-of-consecutive-integers

---
+++ 7kyu/539-Sums-of-consecutive-integers/Program.cs
@@ -0,0 +1,17 @@
+﻿public class SumOfConsecutiveIntegers
+{
+    public static int Position(int x, int y, int n)
+    {
+        // TODO: return consecutive integer at requested position
+        int a = (y - (x * (x - 1)) / 2) / x; 
+        return a + n;
+
+        // Alternative
+        // return (2*y-x*x -x)/(2*x) + n+1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Position(4,14,3));
+    }
+}
