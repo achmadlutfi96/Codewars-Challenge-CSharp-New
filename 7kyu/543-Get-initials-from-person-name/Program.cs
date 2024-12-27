a37183fb5a70d7ae8f638cac65a0f5ca0df1f157
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-27 15:29:54 +0700

    Create 7kyu\543-Get-initials-from-person-name

---
+++ 7kyu/543-Get-initials-from-person-name/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace Solution
+{
+  public static class Program
+  {
+    public static string toInitials(string name)
+    {
+      return string.Join(" ",name.Split(" ").Select(x=>x[0]+"."));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(toInitials("Tom Araya"));
+    }
+  }
+}
