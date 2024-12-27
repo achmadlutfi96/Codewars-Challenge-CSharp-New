5739d9ef35649d000844d04ae6bf14245a272a61
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-27 15:25:14 +0700

    Create 7kyu\542-Stacked-Balls-2D

---
+++ 7kyu/542-Stacked-Balls-2D/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Dinglemouse
+{
+    public static double StackHeight2d(int layers)
+    {
+        // Your code here!
+        if (layers == 0) return 0; 
+        return 1 * (1 + (layers - 1) * Math.Sqrt(3) / 2);
+
+        // Alternative
+        // return layers > 0 ? Math.Sqrt(3) / 2 * (layers - 1) + 1 : 0;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(StackHeight2d(2));
+    }
+}
