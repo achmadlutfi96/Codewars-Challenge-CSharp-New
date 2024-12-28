460175260870a90685737df3505580179328e43d
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-28 13:37:12 +0700

    Create 7kyu\546-Put-a-Letter-in-a-Column

---
+++ 7kyu/546-Put-a-Letter-in-a-Column/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static string BuildRowText(int index, char character)
+  {
+    // Do your magic :)
+    return "|"+string.Join("|",Enumerable.Repeat(' ',9).Select((v,i)=>i==index ? character : v))+"|";
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(BuildRowText(2,'A'));
+  }
+}
