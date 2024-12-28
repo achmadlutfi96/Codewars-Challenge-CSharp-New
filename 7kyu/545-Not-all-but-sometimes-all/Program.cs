23d774e0741283ca5eae558fed63e3dccc7da452
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-28 13:05:25 +0700

    Create 7kyu\545-Not-all-but-sometimes-all

---
+++ 7kyu/545-Not-all-but-sometimes-all/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+  public static string Remove(string str, Dictionary<char,int> what)
+  {
+    string res = "";
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (what.ContainsKey(str[i]) && what[str[i]] > 0)
+        {
+            what[str[i]]--;            
+        }else{
+            res += $"{str[i]}";
+        }
+    }
+    return res;
+
+    // Alternative
+    // return new string(str.Where(c => !what.ContainsKey(c) || what[c]-- < 1).ToArray());
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Remove("this is a string", new Dictionary<char,int> { {'t', 1 }, {'i', 2 }}));
+  }
+}
