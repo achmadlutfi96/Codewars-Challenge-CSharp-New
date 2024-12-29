91e9afbbffc6fbceccf426acb7d996ff4b2a78ce
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-29 12:56:13 +0700

    Create 7kyu\549-Find-factors-of-a-number

---
+++ 7kyu/549-Find-factors-of-a-number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int[] Factors(int num)
+  {
+    // return array of factors
+    return num > 0 ? Enumerable.Range(1,num).Where(x=>num%x == 0).Reverse().ToArray() : new int[0];
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ", Factors(54)));
+  }
+}
