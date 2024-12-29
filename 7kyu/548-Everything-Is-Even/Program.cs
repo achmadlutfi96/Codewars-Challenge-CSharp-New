9903ec8d64b428665d6d38426a71587ff5a26b72
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-29 12:42:56 +0700

    Create 7kyu\548-Everything-Is-Even

---
+++ 7kyu/548-Everything-Is-Even/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Solution {
+  public class Kata
+  {
+    public static int EnsureEven(double number) 
+    {
+      // Code on squire...
+      return (int)(number >= 0 ? 
+        (Math.Floor(number)%2 == 0 ? Math.Floor(number) : Math.Floor(number)+(Math.Floor(number)%2)) : (Math.Floor(Math.Abs(number))%2 == 0 ? Math.Floor(Math.Abs(number)) : Math.Floor(Math.Abs(number))+(Math.Floor(Math.Abs(number))%2))*-1);
+
+        // Alternative
+        // return (int)Math.Round(number / 2, MidpointRounding.AwayFromZero) * 2;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(EnsureEven(1));
+        Console.WriteLine(EnsureEven(0.5));
+        Console.WriteLine(EnsureEven(-1));
+        Console.WriteLine(EnsureEven(-0.5));
+        Console.WriteLine(EnsureEven(1.356));
+    }
+  }
+}
