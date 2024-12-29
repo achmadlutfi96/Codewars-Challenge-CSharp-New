d448dc23c5496d21621a1ee4186b3ac5f7163b76
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-29 12:21:18 +0700

    Create 7kyu\547-Sum-of-powers-of-2

---
+++ 7kyu/547-Sum-of-powers-of-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Kata {
+  public static int[] Powers(int n) {
+    // your code here
+    return Convert.ToString(n, 2).Reverse().Select((v,i)=>v == '1' ? (int)Math.Pow(2,i) : 0).Where(x=>x>0).ToArray();
+
+    // Alternative
+    // return Enumerable.Range(0,31).Select(x => 1 << x).Where(b => (b & n) == b).ToArray();
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(",", Powers(6)));
+  }
+}
