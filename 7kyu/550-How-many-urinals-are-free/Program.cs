0894beae38e933d9d4d1f3ceb50d192f85b27caf
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-30 11:20:06 +0700

    Create 7kyu\550-How-many-urinals-are-free

---
+++ 7kyu/550-How-many-urinals-are-free/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Solution
+{
+    public static class FreeUrinals
+    {
+        public static int GetFreeUrinals(string urinals)
+        {
+            //Have fun :)
+            if (urinals.Contains("11"))
+            {
+                return -1;
+            }
+
+            int freeUrinals = 0;
+            char[] urinalArray = urinals.ToCharArray();
+            for (int i = 0; i < urinalArray.Length; i++)
+            {
+                if (urinalArray[i] == '0' &&
+                    (i == 0 || urinalArray[i - 1] != '1') &&
+                    (i == urinalArray.Length - 1 || urinalArray[i + 1] != '1'))
+                {
+                    urinalArray[i] = '1';
+                    freeUrinals++;
+                }
+            }
+            return freeUrinals;
+
+            // Alternative
+            // return urinals.Contains("11") ? -1 : Regex.Matches(urinals, @"(^|0)0(?!1)").Count;
+        }
+
+    }
+}
