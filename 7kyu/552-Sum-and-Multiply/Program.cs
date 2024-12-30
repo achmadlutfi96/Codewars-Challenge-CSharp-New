43eb445c87129d0472ff55c452c7cb1fdd72384e
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-30 12:25:19 +0700

    Create 7kyu\552-Sum-and-Multiply

---
+++ 7kyu/552-Sum-and-Multiply/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Kata
+{
+  public static int[] SumAndMultiply(int sum, int multiply)
+  {
+    // write your code here
+    int[] res = new int[2];
+    for (int i = 0; i <= sum; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+            if((i+j == sum)&&(i*j == multiply))
+            {
+                res[0] = i;
+                res[1] = j;
+                return res;
+            }
+        }
+    }
+    return null;
+
+    // Alternative
+    // for (var i = 0; i <= sum / 2; ++i)
+    // {
+    //   if ((sum - i) * i == multiply) return new[] {i, sum - i};
+    // }
+    // return null;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(string.Join(", ", SumAndMultiply(12,32)));
+  }
+}
