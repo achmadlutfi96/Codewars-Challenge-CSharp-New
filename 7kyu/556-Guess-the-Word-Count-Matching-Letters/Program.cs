4046277bede5d3302834b6981efbc836044b1e6c
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-01 01:03:17 +0700

    Create 7kyu\556-Guess-the-Word-Count-Matching-Letters

---
+++ 7kyu/556-Guess-the-Word-Count-Matching-Letters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata
+{
+  public class WordGuesser
+  {
+    public int CountCorrectCharacters(string correctWord, string guess)
+    {
+        if(correctWord.Length != guess.Length) throw new InvalidOperationException();
+        return correctWord.Where((v,i)=>v == guess[i]).Count();
+    }
+  }
+}
