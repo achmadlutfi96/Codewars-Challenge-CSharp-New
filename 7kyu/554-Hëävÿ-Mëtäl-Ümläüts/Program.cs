2143420a91f3f7b092b93e59939e8adf37c8b414
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-31 12:43:29 +0700

    Create 7kyu\554-Hëävÿ-Mëtäl-Ümläüts

---
+++ 7kyu/554-Hëävÿ-Mëtäl-Ümläüts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Kata
+{
+  public static string HeavyMetalUmlauts(string boringText)
+  {
+    return boringText.Replace('A','Ä')
+                    .Replace('O' , 'Ö')
+                    .Replace('a' , 'ä')
+                    .Replace('o' , 'ö')
+                    .Replace('E' , 'Ë')
+                    .Replace('U' , 'Ü')
+                    .Replace('e' , 'ë')
+                    .Replace('u' , 'ü')
+                    .Replace('I' , 'Ï')
+                    .Replace('Y' , 'Ÿ')
+                    .Replace('i' , 'ï')
+                    .Replace('y' , 'ÿ');
+
+    // Alternative
+    // string strfrom ="AEIOUYaeiouy";
+    // string strto =  "ÄËÏÖÜŸäëïöüÿ";
+    // for (int i=0; i < strfrom.Length;i++)
+    //   boringText = boringText.Replace(strfrom[i],strto[i]);
+    // return boringText;
+  }  
+}
