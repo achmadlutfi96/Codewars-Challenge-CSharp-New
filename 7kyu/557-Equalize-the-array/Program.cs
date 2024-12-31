d39bea754702e82a2ebdb887a033aa9aa1e8984f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-01 01:11:07 +0700

    Create 7kyu\557-Equalize-the-array

---
+++ 7kyu/557-Equalize-the-array/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+public class Kata
+{
+  public static string[] Equalize(int[] array)
+  {
+    return array.Select(x=>x>=array[0] ? $"+{x-array[0]}" : $"-{array[0]-x}").ToArray();
+
+    // Alternative
+    // return array.Select(i => $"{i - array[0]:+0;-#}").ToArray();
+  }
+}
