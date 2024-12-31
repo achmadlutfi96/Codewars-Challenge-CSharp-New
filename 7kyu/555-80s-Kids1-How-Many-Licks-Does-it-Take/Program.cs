2b630eb391021a8cd6ea19f90829b36d9154a4d0
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-12-31 13:35:06 +0700

    Create 7kyu\555-80s-Kids1-How-Many-Licks-Does-it-Take

---
+++ 7kyu/555-80s-Kids1-How-Many-Licks-Does-it-Take/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public string TotalLicks(Dictionary<string, int> env)
+    {
+        int normalLicks = 252;
+        int totalLicks = normalLicks + env.Sum(x=>x.Value);
+        return env.Where(x=>x.Value > 0).Count() > 0 ? 
+          $"It took {totalLicks} licks to get to the tootsie roll center of a tootsie pop. The toughest challenge was {env.OrderByDescending(x=>x.Value).First().Key}." : 
+          $"It took {totalLicks} licks to get to the tootsie roll center of a tootsie pop.";
+    }
+}
