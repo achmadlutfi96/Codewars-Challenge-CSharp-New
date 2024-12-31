f85a0bbf545b5773f18a666f9a8c26056c1e85ad
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-01 01:20:23 +0700

    Create 7kyu\558-TIY-FizzBuzz

---
+++ 7kyu/558-TIY-FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Solution {
+  public static class TIY {
+   public static string FizzBuzz(string sentence)
+    {
+        return string.Concat(sentence.Select(x=>char.IsUpper(x) ? ("AEIUO".Contains(x) ? "Iron Yard" : "Iron") : ("aeiuo".Contains(x) ? "Yard" : x.ToString())));
+    }
+  }
+}
+
