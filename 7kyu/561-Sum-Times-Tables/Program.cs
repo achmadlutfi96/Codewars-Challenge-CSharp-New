a5fdc6dd2489d1ad3abc9342f5d724fff99ec2ed
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-02 17:42:50 +0700

    Create 7kyu\561-Sum-Times-Tables

---
+++ 7kyu/561-Sum-Times-Tables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static long SumTimesTable(List<int> tables, long min, long max)
+  {
+    long sums = 0;
+    for(long i = min; i <= max; i++){
+      sums += i; 
+    }
+    return tables.Sum()*sums;
+
+    // Alternative
+    // return tables.Sum(x => (long)x) * ((min + max) * (max - min + 1) / 2);
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(SumTimesTable(new List<int> {2, 3}, 1, 3));
+  }
+}
