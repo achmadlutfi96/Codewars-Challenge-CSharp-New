14da97ccd192a45ce90afbe707904ce1644d2175
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-03 11:49:34 +0700

    Create 7kyu\563-Bobs-Short-Forms

---
+++ 7kyu/563-Bobs-Short-Forms/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+
+    public class Kata
+    {
+        public string ShortForm(string str)
+        {
+            // do something
+            return string.Concat(str.Where((v, i) => !"aeiou".Contains(char.ToLower(v)) || i == 0 || i == str.Length - 1));
+
+            //   Alternative
+            // return Regex.Replace(str,@"(?!^)[aeiouAEIOU](?!$)","");
+        }
+    }
+}
