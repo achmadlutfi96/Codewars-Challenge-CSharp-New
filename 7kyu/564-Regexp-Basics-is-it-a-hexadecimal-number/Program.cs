a4f550711ad610936d351b0ddfe780d6018ec715
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-03 12:15:02 +0700

    Create 7kyu\564-Regexp-Basics-is-it-a-hexadecimal-number

---
+++ 7kyu/564-Regexp-Basics-is-it-a-hexadecimal-number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static bool HexNumber(this string s)
+    {
+        if("0\n" == s) return false;
+        return Regex.IsMatch(s , @"^(0[xX])?[0-9a-fA-F]+$");
+
+        // Alternative
+        // return Regex.IsMatch(s, @"^(0x)?[A-Fa-f\d]+\z");
+    }
+}
