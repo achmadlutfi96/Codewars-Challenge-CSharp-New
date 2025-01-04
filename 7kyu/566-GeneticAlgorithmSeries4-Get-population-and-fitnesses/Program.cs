56f3c7656678652bdcc053566ec5eb9d75348d78
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-04 10:15:10 +0700

    Create 7kyu\566-GeneticAlgorithmSeries4-Get-populatio

---
+++ 7kyu/566-GeneticAlgorithmSeries4-Get-population-and-fitnesses/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Kata
+{
+    public IEnumerable<ChromosomeWrap> MapPopulationFit(IEnumerable<string> population, Func<string, double> fitness)
+    {
+        var res = new List<ChromosomeWrap>();
+        foreach(var c in population)
+        {
+          double f = fitness(c);
+          res.Add(new ChromosomeWrap
+          {
+            Chromosome = c,
+            Fitness = f
+          });
+        }
+        return res;
+    }
+}
+
+public class ChromosomeWrap
+{
+    public string Chromosome { get; set; }
+    public double Fitness { get; set; }
+}
