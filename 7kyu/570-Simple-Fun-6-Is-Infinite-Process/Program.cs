1a150156269aea9bd7c6a987d0b13710553db0bf
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-05 14:56:28 +0700

    Create 7kyu\570-Simple-Fun-6-Is-Infinite-Process

---
+++ 7kyu/570-Simple-Fun-6-Is-Infinite-Process/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public bool IsInfiniteProcess(int a, int b)
+        {
+            //coding and coding..
+
+            while (a != b)
+            {
+                a++;
+                b--;
+
+                if(a == 0 || b == 0 || a == 100 || b == 100) return true;
+            }
+            return false;
+
+            // Alternative
+            // return a>b || a<b && (b-a) % 2 == 1;
+        }
+    }
+}
