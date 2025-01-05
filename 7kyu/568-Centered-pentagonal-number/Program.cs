769e06370d2938b8da0106fc246657428bc23cdc
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-05 14:06:25 +0700

    Create 7kyu\568-Centered-pentagonal-number

---
+++ 7kyu/568-Centered-pentagonal-number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Kata
+{
+  public static long Pentagonal(long n)
+  {
+    if(n<1){
+        return -1;
+    }
+    return 5 * (n * (n-1)/2) +1;
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(Pentagonal(1));
+    Console.WriteLine(Pentagonal(2));
+    Console.WriteLine(Pentagonal(3));
+    Console.WriteLine(Pentagonal(4));
+    Console.WriteLine(Pentagonal(8));
+  }
+}
