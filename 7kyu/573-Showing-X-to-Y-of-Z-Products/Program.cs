edc82450b5c90b7fce19ce621f29101f8d36459b
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-06 19:45:14 +0700

    Create 7kyu\573-Showing-X-to-Y-of-Z-Products

---
+++ 7kyu/573-Showing-X-to-Y-of-Z-Products/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Kata
+{
+    public static string PaginationText(int pageNumber, int pageSize, int totalProducts)
+    {
+        int startProduct = (pageNumber - 1) * pageSize + 1;
+        int endProduct = Math.Min(pageNumber * pageSize, totalProducts); 
+        return $"Showing {startProduct} to {endProduct} of {totalProducts} Products.";
+
+    }
+}
