65437e4c1e7a9bfb3f418b6a4efd33f4e6f07e06
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-06 16:01:51 +0700

    Create 7kyu\571-Monkeys-MATH-01-How-many-ZEROs

---
+++ 7kyu/571-Monkeys-MATH-01-How-many-ZEROs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Kata
+{
+    public static int CountZero(string s)
+    {
+        int cnt = 0;
+        s = s.Replace("()", "0");
+        foreach (char ch in s)
+        {
+            if ("abdegopq069DOPQR".Contains(ch.ToString())) cnt++;
+            if ("%&B8".Contains(ch.ToString())) cnt += 2;
+        }
+        return cnt;
+    }
+}
