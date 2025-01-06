694233d5a88ee49b7ff3007aa0d98b6c5ce1ea20
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-06 19:15:21 +0700

    Create 7kyu\572-Remove-the-noise-from-the-string

---
+++ 7kyu/572-Remove-the-noise-from-the-string/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+public class Kata
+{
+  public static string removeNoise(string equation)
+  {
+    //Code goes here
+    return string.Concat(equation.Where(x=>char.IsAsciiLetterOrDigit(x) || " ?.".Contains(x)));
+
+    // Alternative
+    // return Regex.Replace(equation,@"[^a-zA-Z0-9\t\. ?!]", "");
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine(removeNoise("h%e&·%$·llo w&%or&$l·$%d.?"));
+  }
+}
