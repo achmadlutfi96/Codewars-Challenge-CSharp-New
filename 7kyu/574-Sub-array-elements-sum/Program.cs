e1532e016c8f4b4ceb0edb226b9c54be4c897ca4
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-07 16:19:55 +0700

    Create 7kyu\574-Sub-array-elements-sum

---
+++ 7kyu/574-Sub-array-elements-sum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Kata
+{
+    public static int ElementsSum(int[][] arr, int d = 0)
+    {
+        return arr.Select((v,i)=> v.Length > arr.Length-i-1 ? v[arr.Length-i-1]: d).Sum();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(ElementsSum(new[] { new[] { 3, 2, 1, 0 }, new[] { 4, 6, 5, 3, 2 }, new[] { 9, 8, 7, 4 } }));
+    }
+}
