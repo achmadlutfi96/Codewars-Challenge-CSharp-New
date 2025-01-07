0675006071e92aa630ebc2a2f626bc1945edcda8
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-07 17:57:32 +0700

    Create 7kyu\576-Simple-Fun-11-Swap-Adjacent-Bits

---
+++ 7kyu/576-Simple-Fun-11-Swap-Adjacent-Bits/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace myjinxin
+{
+    using System;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int SwapAdjacentBits(int n)
+        {
+            //coding and coding..
+            string binary = Convert.ToString(n, 2).PadLeft(32, '0');
+            return Convert.ToInt32(string.Concat(Enumerable.Range(0, 16).Select(x => binary.Substring(x * 2, 2)).Select(x => $"{x[1]}{x[0]}")), 2);
+
+            //   Alternative
+            // return unchecked((n & (int)0x55555555) << 1 | (n & (int)0xaaaaaaaa) >> 1);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(SwapAdjacentBits(74));
+        }
+    }
+}
