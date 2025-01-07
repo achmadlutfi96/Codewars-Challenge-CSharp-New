42664490901c9ebb323d6ab8fc23a129c78efb61
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-07 17:26:34 +0700

    Create 7kyu\575-Send-in-the-Clones

---
+++ 7kyu/575-Send-in-the-Clones/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Solution 
+{
+  public static class Clones
+  {
+    public static int[] Clonewars(int kataPerDay)
+    {
+        //Code it!
+        int numberOfClones = 1; 
+        int totalKatasSolved = 0; 
+        while (kataPerDay > 0) 
+        { 
+            totalKatasSolved += kataPerDay * numberOfClones;
+            kataPerDay--;
+            if(kataPerDay > 0){
+              numberOfClones*=2;
+            }
+        } 
+        return new int[] { numberOfClones, totalKatasSolved };
+
+        // Alternative
+        // return new[] {Math.Max((int) Math.Pow(2, kataPerDay - 1), 1), (int) Math.Pow(2, kataPerDay + 1) - 2 - kataPerDay};
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ",Clonewars(5)));
+    }
+  }
+}
