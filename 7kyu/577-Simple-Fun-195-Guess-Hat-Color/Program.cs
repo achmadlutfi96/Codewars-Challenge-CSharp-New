1e5a88efc34ea2da029a9da3b3e4c114265f7224
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-08 10:41:07 +0700

    Create 7kyu\577-Simple-Fun-195-Guess-Hat-Color

---
+++ 7kyu/577-Simple-Fun-195-Guess-Hat-Color/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public int GuessHatColor(string a, string b, string c, string d){
+          //coding and coding..
+          return b == c && a == d ? 1 : 2;
+          
+        }
+    }
+}
