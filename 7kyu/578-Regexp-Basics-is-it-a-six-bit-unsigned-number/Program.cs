e8b32225336990bbc140bff27c80fddfb3a96741
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-08 11:11:40 +0700

    Create 7kyu\578-Regexp-Basics-is-it-a-six-bit-unsigned-number

---
+++ 7kyu/578-Regexp-Basics-is-it-a-six-bit-unsigned-number/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+static class SimpleExtensions {
+  public static bool SixBitNumber(this string str) {
+    // Your code here
+    if (str.Contains("\n")) { return false; }
+    return Regex.IsMatch(str, @"^(0|[1-5]?\d|6[0-3])$");
+  }
+}
