4af82a710c6ee43c864f865ccbcc8f6efc81d19f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-08 11:29:28 +0700

    Create 7kyu\579-How-many-e-mails-we-sent-today

---
+++ 7kyu/579-How-many-e-mails-we-sent-today/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CountEmails
+{
+  using System;
+  public static class Program
+  {
+    public static string CountEmails(int sent, int limit = 1000)
+    {
+      // your code here, please check your arguments too!
+      return sent == 0 ? "No e-mails sent" : sent >= limit ? "Daily limit is reached" : $"{Math.Floor(((double)sent/limit)*100)}%";
+    }
+  }
+}
