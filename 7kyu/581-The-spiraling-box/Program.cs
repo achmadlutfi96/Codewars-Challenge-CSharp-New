1120f1c276be4ed7640cf915592f00534534133a
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-09 15:35:04 +0700

    Create 7kyu\581-The-spiraling-box

---
+++ 7kyu/581-The-spiraling-box/Program.cs
@@ -0,0 +1,31 @@
+﻿public class SpiralingBox
+{
+    public static int[,] CreateBox(int m, int n)
+    {
+        // Put your code here.
+        int[,] array = new int[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                array[i,j] = 1+Math.Min(Math.Min(i, n - i - 1), Math.Min(j, m - j - 1));
+            }
+        }
+        return array;
+    }
+
+    public static void Main()
+    {
+        int[,] res = CreateBox(4, 2);
+        for (int i = 0; i < res.GetLength(0); i++)
+        {
+            for (int j = 0; j < res.GetLength(1); j++)
+            {
+                Console.Write(res[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
