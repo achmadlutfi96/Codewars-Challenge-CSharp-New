404902521500cc898441de7a2186704d536df74c
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-10 16:39:59 +0700

    Create 7kyu\585-Wealth-equality-finally

---
+++ 7kyu/585-Wealth-equality-finally/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+public class Kata {
+  public static void RedistributeWealth(double[] wealth) {
+    // Start! Remember to mutate the input
+    double avg = wealth.Average();
+    for (int i = 0; i < wealth.Length; i++)
+    {
+        wealth[i] = avg;
+    }
+
+    // Alternative
+    // var avg = wealth.Average();
+    // Array.Fill(wealth, avg);
+  }
+}
