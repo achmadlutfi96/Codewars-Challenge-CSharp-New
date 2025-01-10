06042eda679f0367271f9472afe3145f1df5e30b
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-10 16:23:16 +0700

    Create 7kyu\584-All-or-Nothing

---
+++ 7kyu/584-All-or-Nothing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+public class Kata
+{
+    public static bool PossiblyPerfect(string[] key, string[] ans)
+    {
+        return key.Select((v, i) => v == ans[i] || v == "_" ? true : false).All(x => x == true) ||
+                key.Select((v, i) => v != ans[i] || v == "_" ? false : true).All(x => x == false);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(PossiblyPerfect("A_C_B".ToCharArray().Select(x => $"{x}").ToArray(), "ADCEB".ToCharArray().Select(x => $"{x}").ToArray()));
+    }
+}
