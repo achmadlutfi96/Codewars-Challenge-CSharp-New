30a26c6840cd67c0a998596a1ea3fd6199148fab
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-11 10:32:32 +0700

    Create 7kyu\587-Describe-a-list

---
+++ 7kyu/587-Describe-a-list/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+public class Describe
+{
+    public static string describeList(List<int> list)
+    {
+        if (list.Count == 0)
+        {
+            return "empty";
+        }
+        if (list.Count == 1)
+        {
+            return "singleton";
+        }
+        return "longer";
+
+        //    Alternative
+        // return list.Count switch {0 => "empty", 1 => "singleton", _ => "longer"};
+    }
+}
