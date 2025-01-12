df2eee537acebe324dbc32c12146b2b0895742a5
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-12 16:47:09 +0700

    Create 7kyu\590-Describe-the-shape

--- 7kyu/590-Describe-the-shape/Program.cs
+++ 7kyu/590-Describe-the-shape/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class Kata
+{
+  public static string DescribeTheShape(int angles)
+  {
+    if (angles <= 2) 
+    { 
+      return "this will be a line segment or a dot"; 
+    } 
+    
+    int sides = angles; // Since the number of angles in a regular polygon is equal to the number of sides. 
+    int interiorAngle = (sides - 2) * 180 / sides; // Formula for the measure of each interior angle in a regular polygon. 
+    
+    return $"This shape has {sides} sides and each angle measures {Math.Floor((double)interiorAngle)}";
+
+    // Alternative
+    // angles < 3
+      // ? "this will be a line segment or a dot"
+      // : $"This shape has {angles} sides and each angle measures {Math.Floor(180 - 360d / angles)}";
+  }
+}
