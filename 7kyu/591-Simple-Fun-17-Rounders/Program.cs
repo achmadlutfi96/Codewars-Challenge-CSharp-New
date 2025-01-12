14ff88ae1c602b8865a33fba96419783f1d71f15
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-12 17:18:54 +0700

    Create 7kyu\591-Simple-Fun-17-Rounders

---
+++ 7kyu/591-Simple-Fun-17-Rounders/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public static int Rounders(int Value)
+        {
+            //coding and coding..
+            int res = Value;
+            for (int i = 1; i < Value.ToString().Length; i++)
+            {
+                res = (int)(Math.Round(res/Math.Pow(10,i), MidpointRounding.AwayFromZero) * Math.Pow(10,i));
+            }
+            return res;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Rounders(15));
+            Console.WriteLine(Rounders(1234));
+            Console.WriteLine(Rounders(1445));
+        }
+    }
+}
