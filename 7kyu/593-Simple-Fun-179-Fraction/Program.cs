4b0924009dd4d96ebd2df56639343b2f60170321
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-13 16:15:03 +0700

    Create 7kyu\593-Simple-Fun-179-Fraction

---
+++ 7kyu/593-Simple-Fun-179-Fraction/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public int Fraction(int a, int b)
+        {
+            //coding and coding..
+
+            int gcd = GCD(a, b);
+            int reducedNumerator = a / gcd;
+            int reducedDenominator = b / gcd;
+            return reducedNumerator + reducedDenominator;
+
+            // Alternative
+            // int gcd = (int)BigInteger.GreatestCommonDivisor(new BigInteger(a), new BigInteger(b));
+            //   return a / gcd + b / gcd;
+        }
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
