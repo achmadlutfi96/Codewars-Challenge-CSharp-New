d98723bc44791b8061f98a1c223e2ebdd03c5d09
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-13 17:26:17 +0700

    Create 7kyu\594-Simple-string-division

---
+++ 7kyu/594-Simple-string-division/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution
+{
+    public static long solve(String st, int k)
+    {
+        //..
+        long res = 0L;
+        int n = st.Length - k;
+        for (int i = 0; i < st.Length-n+1; ++i){
+            string a = st.Substring(i,n);
+            res = Math.Max(res,Convert.ToInt64(a));
+        }
+        return res;
+
+        // Alternative
+        // return Enumerable.Range(0, k + 1).Max(i => long.Parse(string.Concat(st.Skip(i).Take(st.Length - k))));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(solve("123", 1));
+        Console.WriteLine(solve("1234", 1));
+    }
+}
