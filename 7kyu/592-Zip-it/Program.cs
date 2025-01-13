b86216ec6492ad51d1c599e1735bd8395b1bf8ac
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-13 14:40:43 +0700

    Create 7kyu\592-Zip-it

---
+++ 7kyu/592-Zip-it/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static object[] ZipIt(this object[] a, object[] b, Func<object, object, object> fn)
+    {
+        object[] res = new object[a.Length < b.Length ? a.Length : b.Length];
+        return res.Select((v,i)=> fn(a[i], b[i])).ToArray();
+
+        // Alternative
+        // return a.Zip(b, fn).ToArray();
+    }
+
+    public static void Main()
+    {
+        var a = new object[] { 1, 2, 3, 4, 5 };
+        var b = new object[] { 'a','b' };
+        Console.WriteLine(string.Join(", ", a.ZipIt(b, (c, d) => c + "" + d)));
+    }
+}
