33036b8fc3ada6f2fef594f3254a37837787ad97
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-14 10:58:04 +0700

    Create 7kyu\595-Merge-overlapping-strings

---
+++ 7kyu/595-Merge-overlapping-strings/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Solution;
+
+public class Kata
+{
+    public static string MergeStrings(string first, string second)
+    {
+        int maxOverlap = 0;
+        int overlapIdx = 0;
+        // Find the longest overlap 
+        for (int i = 1; i <= Math.Min(first.Length, second.Length); i++)
+        {
+            if (first.Substring(first.Length - i) == second.Substring(0, i))
+            {
+                maxOverlap = i;
+                overlapIdx = i;
+            }
+        } // Merge the strings with the maximum overlap 
+        return first + second.Substring(overlapIdx);
+
+        // Alternative
+        // int overlapLength = 0;
+        // int minLength = Math.Min(first.Length, second.Length);
+
+        // for (int i = 1; i <= minLength; i++)
+        // {
+        //     if (first.EndsWith(second.Substring(0, i)))
+        //     {
+        //         overlapLength = i;
+        //     }
+        // }
+
+        // return first + second.Substring(overlapLength);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(MergeStrings("abc", "ab"));
+        Console.WriteLine(MergeStrings("abcde", "cdefgh"));
+    }
+}
