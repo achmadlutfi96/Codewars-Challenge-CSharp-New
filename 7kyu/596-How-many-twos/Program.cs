d6c5c1329c1b61d9456b9e45a79f1100c6ddff2b
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-14 11:33:38 +0700

    Create 7kyu\596-How-many-twos

---
+++ 7kyu/596-How-many-twos/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+  public static int TwoCount(int n)
+  {
+    // Your code 
+    int count = 0; 
+    while (n % 2 == 0) { 
+        count++; 
+        n /= 2; 
+    } 
+    return count;
+
+    // Alternative
+    // return (int) Math.Log(n & -n, 2);
+  }
+}
