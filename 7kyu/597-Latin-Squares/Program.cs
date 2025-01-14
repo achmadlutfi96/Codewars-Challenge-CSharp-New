9fdb29dd71b74424a30f60c8d390a3e76a0f70c6
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2025-01-14 11:43:29 +0700

    Create 7kyu\597-Latin-Squares

---
+++ 7kyu/597-Latin-Squares/Program.cs
@@ -0,0 +1,31 @@
+﻿public class LatinSquare
+{
+    public static int[][] MakeLatinSquare(int n)
+    {
+        // Make all rows and columns 1, 2, ..., n
+        int[][] latinSquare = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            latinSquare[i] = new int[n];
+            for (int j = 0; j < n; j++)
+            {
+                latinSquare[i][j] = j + 1;
+            }
+        }
+
+        // Make changes
+        // Modify the Latin square if needed
+        for (int i = 0; i < n; i++) { 
+          latinSquare[i] = new int[n]; 
+          for (int j = 0; j < n; j++) { 
+            latinSquare[i][j] = (i + j) % n + 1; 
+          } 
+        }
+
+        return latinSquare;
+
+        // Alternative
+        // var arr = Enumerable.Range(1, n).ToArray();
+    // return arr.Select(i => arr.Select(j => (i + j) % n + 1).ToArray()).ToArray();
+    }
+}
